e3cc90dd0aff09ccb9b3e6075452b9ec8b175cfb
apostolis.bekiaris@gmail.com <apostolis.bekiaris@gmail.com@1b56877a-5f70-11de-84d6-ef71daef37f8>
2009-10-13 15:26:41 +0000

    ViewVariants module name change

--- eXpand/eXpand.ExpressApp.Modules/ViewVariants.Win/Properties/AssemblyInfo.cs
+++ eXpand/eXpand.ExpressApp.Modules/ViewVariants.Win/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eXpand.ExpressApp.ViewVariantsModule.Win")]
+[assembly: AssemblyTitle("eXpand.ExpressApp.ViewVariants.Win")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("-")]
-[assembly: AssemblyProduct("eXpand.ExpressApp.ViewVariantsModule.Win")]
+[assembly: AssemblyProduct("eXpand.ExpressApp.ViewVariants.Win")]
 [assembly: AssemblyCopyright("Copyright © - 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
