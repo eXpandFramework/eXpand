652db28a5f8ea2fb6fc90b56d979b14bb90e8724
dimajanzen <dima.janzen@gmail.com>
2009-12-03 00:57:50 +0800

    fixed issue 2 when query for user module, user klass key property is read properly

--- eXpand/eXpand.ExpressApp.Modules/ModelDifference/DataStore/Queries/QueryUserModelDifferenceObject.cs
+++ eXpand/eXpand.ExpressApp.Modules/ModelDifference/DataStore/Queries/QueryUserModelDifferenceObject.cs
@@ -2,31 +2,37 @@ using System.Linq;
 using DevExpress.Data.Filtering;
 using DevExpress.ExpressApp;
 using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
 using eXpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 
-namespace eXpand.ExpressApp.ModelDifference.DataStore.Queries{
-    public class QueryUserModelDifferenceObject:QueryDifferenceObject<UserModelDifferenceObject>{
-        public QueryUserModelDifferenceObject(Session session) : base(session){
+namespace eXpand.ExpressApp.ModelDifference.DataStore.Queries
+{
+    public class QueryUserModelDifferenceObject : QueryDifferenceObject<UserModelDifferenceObject>
+    {
+        public QueryUserModelDifferenceObject(Session session)
+            : base(session)
+        {
+        }
+
+        private static ContainsOperator UsersContainsOperator
+        {
+            get
+            {
+                XPMemberInfo mi = ((XPBaseObject)SecuritySystem.CurrentUser).ClassInfo.KeyProperty;
+                return new ContainsOperator("Users", new BinaryOperator(mi.Name, mi.GetValue(SecuritySystem.CurrentUser)));
+            }
         }
 
         public override IQueryable<UserModelDifferenceObject> GetActiveModelDifferences(string applicationName)
         {
-            var containsOperator = new ContainsOperator("Users",
-                                                        new BinaryOperator("Oid", ((XPBaseObject)SecuritySystem.CurrentUser)
-                                                                                     .ClassInfo.KeyProperty.GetValue(
-                                                                                     SecuritySystem.CurrentUser)));
-            IQueryable<UserModelDifferenceObject> queryable = base.GetActiveModelDifferences(applicationName).OfType<UserModelDifferenceObject>();
-            return queryable.ToList().Where(o => o.Fit(containsOperator.ToString())).AsQueryable();
+            var queryable = base.GetActiveModelDifferences(applicationName).OfType<UserModelDifferenceObject>();
+            return queryable.ToList().Where(o => o.Fit(UsersContainsOperator)).AsQueryable();
         }
 
         public override UserModelDifferenceObject GetActiveModelDifference(string applicationName)
         {
-            var containsOperator = new ContainsOperator("Users",
-                                                        new BinaryOperator("Oid", ((XPBaseObject)SecuritySystem.CurrentUser)
-                                                                                     .ClassInfo.KeyProperty.GetValue(
-                                                                                     SecuritySystem.CurrentUser)));
-            var queryable = GetActiveModelDifferences(applicationName).OfType<UserModelDifferenceObject>();
-            return queryable.ToList().Where(o => o.Fit(containsOperator.ToString())).FirstOrDefault();
+            var queryable = this.GetActiveModelDifferences(applicationName).OfType<UserModelDifferenceObject>();
+            return queryable.ToList().Where(o => o.Fit(UsersContainsOperator)).FirstOrDefault();
         }
     }
 }
