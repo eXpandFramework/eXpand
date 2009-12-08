21ed20dcbd3a6f7a8811193dbcb1436bd4ca15ab
apobekiaris <apostolis.bekiaris@gmail.com>
2009-12-08 14:46:28 +0200

    left over

---
+++ eXpand/eXpand.Persistent/eXpand.Persistent.BaseImpl/PersistentMetaData/PersistentAttributeInfos/PersistentValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using DevExpress.Xpo;
+using eXpand.Persistent.Base.PersistentMetaData.PersistentAttributeInfos;
+
+namespace eXpand.Persistent.BaseImpl.PersistentMetaData.PersistentAttributeInfos
+{
+    public class PersistentValueConverter : PersistentAttributeInfo
+    {
+        public PersistentValueConverter(Session session) : base(session) {
+        }
+        private Type _converterType;
+        public Type ConverterType
+        {
+            get
+            {
+                return _converterType;
+            }
+            set
+            {
+                SetPropertyValue("ConverterType", ref _converterType, value);
+            }
+        }
+        public override AttributeInfo Create() {
+            ConstructorInfo constructorInfo = typeof(ValueConverterAttribute).GetConstructor(new[] { typeof(Type) });
+            return new AttributeInfo(constructorInfo,ConverterType);
+        }
+    }
+}
