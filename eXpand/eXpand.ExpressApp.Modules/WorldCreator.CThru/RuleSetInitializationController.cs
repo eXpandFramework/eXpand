1a421978a59bb7a84a6b2186e87a6c238b765414
apobekiaris <apostolis.bekiaris@gmail.com>
2010-01-13 01:18:48 +0200

    some speed tests

--- eXpand/eXpand.ExpressApp.Modules/WorldCreator.CThru/RuleSetInitializationController.cs
+++ eXpand/eXpand.ExpressApp.Modules/WorldCreator.CThru/RuleSetInitializationController.cs
@@ -11,22 +11,22 @@ namespace eXpand.ExpressApp.WorldCreator.CThru
             InitializeComponent();
             RegisterActions(components);
         }
-//        protected override void OnActivated()
-//        {
-//            if (Application != null) {
-//                var module = (ValidationModule) Application.Modules.FindModule(typeof (ValidationModule));
-//                if (module != null) {
-//                    CThruEngine.AddAspect(new ExistentMembersEnableValidationAspect());
-//                    CThruEngine.StartListening();
-//                    module.RuleSetInitialized += (sender, args) => CThruEngine.StopListeningAndReset();
-//                    module.InitializeRuleSet();
-//                }
-//                else {
-//                    Tracing.Tracer.LogWarning("Cannot find Validation module in Module list: {0}",
-//                                              typeof (ValidationModule).AssemblyQualifiedName);
-//                }
-//            }
-//        }
+        protected override void OnActivated()
+        {
+            if (Application != null) {
+                var module = (ValidationModule) Application.Modules.FindModule(typeof (ValidationModule));
+                if (module != null) {
+                    CThruEngine.AddAspect(new ExistentMembersEnableValidationAspect());
+                    CThruEngine.StartListening();
+                    module.RuleSetInitialized += (sender, args) => CThruEngine.StopListeningAndReset();
+                    module.InitializeRuleSet();
+                }
+                else {
+                    Tracing.Tracer.LogWarning("Cannot find Validation module in Module list: {0}",
+                                              typeof (ValidationModule).AssemblyQualifiedName);
+                }
+            }
+        }
         
     }
 }
--- eXpand/eXpand.ExpressApp.Modules/WorldCreator.CThru/WorldCreatorCThruModule.cs
+++ eXpand/eXpand.ExpressApp.Modules/WorldCreator.CThru/WorldCreatorCThruModule.cs
@@ -6,15 +6,15 @@ namespace eXpand.ExpressApp.WorldCreator.CThru
 {
     public sealed class WorldCreatorCThruModule : ModuleBase
     {
-        public override void Setup(ApplicationModulesManager moduleManager)
-        {
-            base.Setup(moduleManager);
-            var validationModule =                
-                (ValidationModule) moduleManager.Modules.FindModule(typeof (ValidationModule));
-            CThruEngine.AddAspect(new ExistentMembersEnableValidationAspect());
-            CThruEngine.StartListening();
-            validationModule.RuleSetInitialized += (sender, args) => CThruEngine.StopListeningAndReset();            
-        }
+//        public override void Setup(ApplicationModulesManager moduleManager)
+//        {
+//            base.Setup(moduleManager);
+//            var validationModule =                
+//                (ValidationModule) moduleManager.Modules.FindModule(typeof (ValidationModule));
+//            CThruEngine.AddAspect(new ExistentMembersEnableValidationAspect());
+//
+//            validationModule.RuleSetInitialized += (sender, args) => CThruEngine.StopListeningAndReset();            
+//        }
     }
 
 }
