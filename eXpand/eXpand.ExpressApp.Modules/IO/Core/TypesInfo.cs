56a9b770ddb1a4ecaacbd22e00e88af81a8d9ea7
apobekiaris <apostolis.bekiaris@gmail.com>
2010-01-21 17:54:55 +0200

    fixed "No Class that implemenets xxxx found at AdditionalBusinessClasses list. eXpand load exception by adding a more descriptive message

--- eXpand/eXpand.ExpressApp.Modules/IO/Core/TypesInfo.cs
+++ eXpand/eXpand.ExpressApp.Modules/IO/Core/TypesInfo.cs
@@ -28,17 +28,18 @@ namespace eXpand.ExpressApp.IO.Core {
 
         public void AddTypes(IEnumerable<Type> types) {
             SerializationConfigurationType = GetInfoType(types, typeof (ISerializationConfiguration));
-            ClassInfoGraphNodeType = GetInfoType(types, typeof(IClassInfoGraphNode));
-            XmlFileChooserType = GetInfoType(types, typeof(IXmlFileChooser));
+            ClassInfoGraphNodeType = GetInfoType(types, typeof (IClassInfoGraphNode));
+            XmlFileChooserType = GetInfoType(types, typeof (IXmlFileChooser));
         }
 
         Type GetInfoType(IEnumerable<Type> types, Type type1) {
             Type infoType =
-                types.Where(type => type1.IsAssignableFrom(type)).GroupBy(type => type).Select(grouping => grouping.Key)
+                types.Where(type1.IsAssignableFrom).GroupBy(type => type).Select(grouping => grouping.Key)
                     .FirstOrDefault();
             if (infoType == null)
                 throw new NoNullAllowedException("No Class that implemenets " + type1.AssemblyQualifiedName +
-                                                 " found at AdditionalBusinessClasses list");
+                                                 " found at AdditionalBusinessClasses list. " + typeof (IOModule).Name +
+                                                 " should be the last module added to Application.Modules. Please check InitializeComponent method of your XafApplication descenant");
             return infoType;
         }
     }
--- eXpand/eXpand.ExpressApp.Modules/WorldCreator/Core/TypesInfo.cs
+++ eXpand/eXpand.ExpressApp.Modules/WorldCreator/Core/TypesInfo.cs
@@ -56,7 +56,9 @@ namespace eXpand.ExpressApp.WorldCreator.Core {
         private Type GetInfoType(IEnumerable<Type> types, Type type1) {
             var infoType = types.Where(type1.IsAssignableFrom).GroupBy(type => type).Select(grouping => grouping.Key).FirstOrDefault();
             if (infoType== null)
-                throw new NoNullAllowedException("No Class that implemenets " + type1.AssemblyQualifiedName + " found at AdditionalBusinessClasses list");
+                throw new NoNullAllowedException("No Class that implemenets " + type1.AssemblyQualifiedName +
+                                                 " found at AdditionalBusinessClasses list. " + typeof(WorldCreatorModule).Name +
+                                                 " should be the last module added to Application.Modules. Please check InitializeComponent method of your XafApplication descenant");
             return infoType;
         }
 
