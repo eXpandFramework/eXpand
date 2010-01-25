a91daa02e073b6d9831b92e335977407642a082a
apobekiaris <apostolis.bekiaris@gmail.com>
2010-01-25 17:15:45 +0200

    add missing modules to templates

--- buildall32bit.cmd
+++ buildall32bit.cmd
@@ -48,4 +48,15 @@ call buildproject.cmd eXpand.Persistent.BaseImpl ".\eXpand\eXpand.Persistent\eXp
 
 %sn% -q -T eXpand.Dll\eXpand.Utils.dll > PublicKeyToken.txt
 
+rem Install VS Template
+set templates="%ProgramFiles%\Microsoft Visual Studio 9.0\Common7\IDE\ProjectTemplates\CSharp\eXpand\"
+set devenv="%ProgramFiles%\Microsoft Visual Studio 9.0\Common7\IDE\"
+
+echo Installing and refreshing visual studio templates
+xcopy "eXpand.DesignExperience\vs_templates\*.*" %templates% /Y /R /I
+
+cd\
+cd %devenv%
+devenv.exe /InstallVSTemplates
+
 pause
