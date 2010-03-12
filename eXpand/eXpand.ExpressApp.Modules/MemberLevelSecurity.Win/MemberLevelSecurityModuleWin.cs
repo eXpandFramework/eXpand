cbc3f4ec506c3d763ffdf52d9a4e5d19c6af4dfb
apobekiaris <apostolis.bekiaris@gmail.com>
2010-03-12 16:32:11 +0200

    added MemberLevelSecurity.Web module to control currentcomparer

--- eXpand/eXpand.ExpressApp.Modules/MemberLevelSecurity.Win/MemberLevelSecurityModuleWin.cs
+++ eXpand/eXpand.ExpressApp.Modules/MemberLevelSecurity.Win/MemberLevelSecurityModuleWin.cs
@@ -1,16 +1,17 @@
 using System.ComponentModel;
-using DevExpress.ExpressApp;
 using DevExpress.Utils;
 
 namespace eXpand.ExpressApp.MemberLevelSecurity.Win
 {
     [Description("Allows user to protect a member for a specific record"), ToolboxTabName("eXpressApp"),
      EditorBrowsable(EditorBrowsableState.Always), Browsable(true), ToolboxItem(true)]
-    public sealed partial class MemberLevelSecurityModuleWin : ModuleBase
+    public sealed partial class MemberLevelSecurityModuleWin : MemberLevelSecurityModuleBase
     {
         public MemberLevelSecurityModuleWin()
         {
             InitializeComponent();
         }
+
+        public override bool? ComparerIsSet { get; set; }
     }
 }
