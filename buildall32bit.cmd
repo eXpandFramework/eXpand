ba1211fee0078a08f7657ac5ca304535646b4ee5
unknown <Administrator@.(none)>
2010-03-31 16:39:56 +0300

    Bug When installing eXpand from different drive than SystemDrive

--- buildall32bit.cmd
+++ buildall32bit.cmd
@@ -25,6 +25,7 @@ set devenv="%ProgramFiles%\Microsoft Visual Studio 9.0\Common7\IDE\"
 echo Installing and refreshing visual studio templates
 xcopy "eXpand.DesignExperience\vs_templates\*.*" %templates% /Y /R /I
 
+%SystemDrive%
 cd\
 cd %devenv%
 devenv.exe /InstallVSTemplates
