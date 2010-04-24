1749487ce8f8cd5df39fab2f9b340ded3d58f81a
apobekiaris <apostolis.bekiaris@gmail.com>
2010-04-24 15:10:42 +0300

    allow instanciation of non persistent objects from newobject action + allow instanciate different object type than current for a view

---
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/SystemModule/WinNewObjectViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using eXpand.ExpressApp.Attributes;
+using eXpand.ExpressApp.Core.DictionaryHelpers;
+
+namespace eXpand.ExpressApp.Win.SystemModule {
+    public class WinNewObjectViewController : DevExpress.ExpressApp.Win.SystemModule.WinNewObjectViewController {
+        public const string ClassTypeToInstantiate = "ClassTypeToInstantiate";
+        protected override void UpdateActionState() {
+            base.UpdateActionState();
+            foreach (ITypeInfo typeInfo in currentViewTypes) {
+                var b = typeInfo.FindAttribute<CanInstantiate>() != null;
+                if (b) {
+                    String diagnosticInfo;
+                    if (!CanCreateAndEdit(typeInfo.Type, out diagnosticInfo) ) {
+                        if (NewObjectAction.Items.Count > 0)
+                            NewObjectAction.Items[0].BeginGroup = true;
+                        DictionaryNode findChildNode = Application.Info.FindChildNode("CreatableItems").GetChildNode(
+                            "Item", "ClassName", typeInfo.FullName);
+                        if (findChildNode != null)
+                            NewObjectAction.Items.Insert(0, CreateItem(typeInfo.Type, findChildNode));
+                    }
+                }
+            }
+            var type = ReflectionHelper.FindType(View.Info.GetAttributeValue(ClassTypeToInstantiate));
+            if (type != null)
+            {
+                DictionaryNode findChildNode = Application.Info.FindChildNode("CreatableItems").GetChildNode(
+                            "Item", "ClassName", type.FullName);
+                if (findChildNode != null)
+                {
+                    NewObjectAction.Items.RemoveAt(0);
+                    NewObjectAction.Items.Insert(0, CreateItem(type, findChildNode));
+                }
+            }
+        }
+        public override Schema GetSchema()
+        {
+            var schema = base.GetSchema();
+            var schemaBuilder = new SchemaBuilder();
+            schema.CombineWith(new Schema(schemaBuilder.Inject(@"<Attribute Name=""" + ClassTypeToInstantiate + @""" RefNodeName=""/Application/BOModel/Class""/>", ModelElement.ListView)));
+            schema.CombineWith(new Schema(schemaBuilder.Inject(@"<Attribute Name=""" + ClassTypeToInstantiate + @""" RefNodeName=""/Application/BOModel/Class""/>", ModelElement.DetailView)));
+            return schema;
+        }   
+
+    }
+}
