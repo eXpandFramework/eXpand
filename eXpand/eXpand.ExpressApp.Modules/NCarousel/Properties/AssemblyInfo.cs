4c51a5b9273258dacbb8bc023c14bb1602b70a7c
apobekiaris <apostolis.bekiaris@gmail.com>
2010-05-13 11:02:08 +0300

    NCarousel module first commit

---
+++ eXpand/eXpand.ExpressApp.Modules/NCarousel/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("eXpand.NCarousel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("eXpand.NCarousel")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("785f34bb-5f46-4eec-ab2c-8f3008819420")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.09314.08")]
+[assembly: AssemblyFileVersion("0.0.09314.08")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.jquery-1.4.2.min.js", "text/javascript")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.jquery.jcarousel.min.js", "text/javascript")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.NCarousel.js", "text/javascript")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.Skins.skin.css", "text/css", PerformSubstitution = true)]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.Skins.next-horizontal.png", "image/png")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.Skins.next-vertical.png", "image/png")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.Skins.prev-horizontal.png", "image/png")]
+[assembly: System.Web.UI.WebResource("eXpand.NCarousel.Resources.Skins.prev-vertical.png", "image/png")]
---
+++ eXpand/eXpand.Persistent/eXpand.Persistent.Base/NCarousel/INCarouselItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace eXpand.Persistent.Base.NCarousel {
+    public interface INCarouselItem
+    {
+        string ID { get; }
+        Image Image { get; }
+    }
+}
