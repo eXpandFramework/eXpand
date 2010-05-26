586bea243faa99b512a50f144e7034f80e39d54a
noxe <ma_praxmarer@hotmail.com>
2010-05-26 09:58:48 +0200

    set SearchMemberMode to Include as default

--- eXpand/eXpand.ExpressApp/eXpand.ExpressApp/SystemModule/SearchFromViewController.cs
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp/SystemModule/SearchFromViewController.cs
@@ -1,11 +1,13 @@
 ﻿using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Filtering;
 using DevExpress.ExpressApp.Model;
+using System.ComponentModel;
 
 namespace eXpand.ExpressApp.SystemModule {
 
     public interface IModelViewPropertySearchMode : IModelNode
     {
+        [DefaultValue(SearchMemberMode.Include)]
         SearchMemberMode SearchMemberMode { get; set; }
     }
 
