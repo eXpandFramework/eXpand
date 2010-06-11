c900f0d474b1a6461e8eb9e2d888f5d54c0d6a50
apobekiaris <apostolis.bekiaris@gmail.com>
2010-06-11 15:32:19 +0300

    Localized Apply Filter

--- eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/SystemModule/FilterControlListViewController.cs
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/SystemModule/FilterControlListViewController.cs
@@ -8,6 +8,7 @@ using DevExpress.Persistent.Base;
 using DevExpress.XtraEditors;
 using DevExpress.XtraGrid;
 using Forms = System.Windows.Forms;
+using DevExpress.ExpressApp.Utils;
 
 namespace eXpand.ExpressApp.Win.SystemModule
 {
@@ -73,7 +74,7 @@ namespace eXpand.ExpressApp.Win.SystemModule
                 setCriteriaFromView(filterControl);
             }
 
-            var accept = new SimpleButton {Text = "Accept filter"};
+            var accept = new SimpleButton { Text = CaptionHelper.GetLocalizedText("eXpand", "AcceptFilter") };
             accept.Click += ((o, args) => filterControl.ApplyFilter());
             accept.Dock = Forms.DockStyle.Bottom;
             filterControl.Controls.Add(accept);
