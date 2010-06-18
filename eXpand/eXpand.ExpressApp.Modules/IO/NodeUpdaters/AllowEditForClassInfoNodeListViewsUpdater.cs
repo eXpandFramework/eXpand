57fe0cf70b50293c470bf8493203a0544d55ae87
apobekiaris <apostolis.bekiaris@gmail.com>
2010-06-18 16:56:16 +0300

    remove updatemodel code and add IO NodeUpdaters instead

---
+++ eXpand/eXpand.ExpressApp.Modules/IO/NodeUpdaters/AllowEditForClassInfoNodeListViewsUpdater.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using eXpand.ExpressApp.IO.Core;
+
+namespace eXpand.ExpressApp.IO.NodeUpdaters {
+    public class AllowEditForClassInfoNodeListViewsUpdater : ModelNodesGeneratorUpdater<ModelListViewNodesGenerator> {
+        public override void UpdateNode(ModelNode node) {
+            var listView = ((IModelListView) node);
+            if (listView.ModelClass.TypeInfo.Type == TypesInfo.Instance.ClassInfoGraphNodeType)
+                listView.AllowEdit = true;
+        }
+    }
+}
