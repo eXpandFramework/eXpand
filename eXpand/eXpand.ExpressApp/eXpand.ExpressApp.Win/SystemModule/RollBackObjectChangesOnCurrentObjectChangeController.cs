b5a86a83383b101592eb7f7edd5a77a55809422c
apobekiaris <apostolis.bekiaris@gmail.com>
2010-06-30 11:28:24 +0300

    missing files

---
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/SystemModule/RollBackObjectChangesOnCurrentObjectChangeController.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Win.SystemModule;
+
+namespace eXpand.ExpressApp.Win.SystemModule
+{
+    public interface IModelClassRollBackObjectChangesOnCurrentObjectChange : IModelNode
+    {
+        [Category("eXpand")]
+        [DefaultValue(true)]
+        [Description("If set to false when the view queries if it can change current object all changes will remain in transaction, thus allowing to save them at a later time")]
+        bool RollBackObjectChangesOnCurrentObjectChange { get; set; }
+    }
+    public interface IModelViewRollBackChangesBeforeViewChanges : IModelNode
+    {
+        [Category("eXpand")]
+        [DefaultValue(true)]
+        [Description("If set to false when the view queries if it can change current object all changes will remain in transaction, thus allowing to save them at a later time")]
+        [ModelValueCalculator("((IModelClassRollBackObjectChangesOnCurrentObjectChange)ModelClass)", "RollBackObjectChangesOnCurrentObjectChange")]
+        bool RollBackObjectChangesOnCurrentObjectChange { get; set; }
+    }
+
+    public class RollBackObjectChangesOnCurrentObjectChangeController : WinDetailViewController, IModelExtender
+    {
+        protected override void OnViewQueryCanChangeCurrentObject(CancelEventArgs e)
+        {
+            if (((IModelViewRollBackChangesBeforeViewChanges)View.Model)
+                .RollBackObjectChangesOnCurrentObjectChange)
+                base.OnViewQueryCanChangeCurrentObject(e);
+        }
+
+        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders)
+        {
+            extenders.Add<IModelClass, IModelClassRollBackObjectChangesOnCurrentObjectChange>();
+            extenders.Add<IModelView, IModelViewRollBackChangesBeforeViewChanges>();
+        }
+    }
+}
