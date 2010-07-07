929f0d8a68b705d94cf4c60b098a61073f3bff1d
apobekiaris <apostolis.bekiaris@gmail.com>
2010-07-07 21:34:27 +0300

    replace BeForeGet implementation

--- eXpand/eXpand.ExpressApp.Modules/Logic/DomainLogic/ModelGroupContextsDefaultContextDomainLogic.cs
+++ eXpand/eXpand.ExpressApp.Modules/Logic/DomainLogic/ModelGroupContextsDefaultContextDomainLogic.cs
@@ -6,19 +6,8 @@ using eXpand.ExpressApp.Logic.NodeUpdaters;
 namespace eXpand.ExpressApp.Logic.DomainLogic {
     [DomainLogic(typeof (IModelGroupContexts))]
     public class ModelGroupContextsDefaultContextDomainLogic {
-        bool gettingValue;
-
-        public void BeforeGet(object logicRule, string propertyName) {
-            if (propertyName == "DefaultContext") {
-                if (!gettingValue) {
-                    gettingValue = true;
-                    var rule = ((IModelGroupContexts) logicRule);
-                    if (rule.DefaultContext == null)
-                        rule.DefaultContext =
-                            rule.Where(context => context.Id == LogicDefaultGroupContextNodeUpdater.Default).Single();
-                    gettingValue = false;
-                }
-            }
+        public static IModelExecutionContexts Get_DefaultContext(IModelGroupContexts modelGroupContexts){
+            return modelGroupContexts.Where(context => context.Id == LogicDefaultGroupContextNodeUpdater.Default).Single();
         }
     }
 }
