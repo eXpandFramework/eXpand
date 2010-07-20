1879bb9fc910156415621d0d04eb5b59bd01b44d
apobekiaris <apostolis.bekiaris@gmail.com>
2010-07-20 04:15:53 +0300

    AVC-->add some dummy interfaces to avoid compilation errors

---
+++ eXpand/eXpand.ExpressApp.Modules/AdditionalViewControlsProvider/Model/IModelLogicAdditionalViewControls.cs
@@ -0,0 +1,7 @@
+﻿using eXpand.ExpressApp.Logic.Model;
+
+namespace eXpand.ExpressApp.AdditionalViewControlsProvider.Model {
+    public interface IModelLogicAdditionalViewControls:IModelLogic {
+        
+    }
+}
