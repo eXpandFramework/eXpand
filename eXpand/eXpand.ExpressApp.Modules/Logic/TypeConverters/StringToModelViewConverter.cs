b86f0bba3ea760c951ec0769c26287e8d427fe83
apobekiaris <apostolis.bekiaris@gmail.com>
2010-07-20 04:24:45 +0300

    Logic-->friendly exception

--- eXpand/eXpand.ExpressApp.Modules/Logic/TypeConverters/StringToModelViewConverter.cs
+++ eXpand/eXpand.ExpressApp.Modules/Logic/TypeConverters/StringToModelViewConverter.cs
@@ -13,9 +13,11 @@ namespace eXpand.ExpressApp.Logic.TypeConverters {
             _modelApplication = modelApplication;
         }
 
-        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
-        {
-            return _modelApplication.Views.Where(view => view.Id == value.ToString()).Single();
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType) {
+            IModelView modelView = _modelApplication.Views.Where(view => view.Id == value.ToString()).SingleOrDefault();
+            if (modelView== null)
+                throw new NullReferenceException(value.ToString());
+            return modelView;
         }
     }
 }
