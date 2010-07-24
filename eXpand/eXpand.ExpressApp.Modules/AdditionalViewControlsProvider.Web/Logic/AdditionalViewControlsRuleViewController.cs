bcc4e15ed7deb2231aa6146829da3ecf22cf2697
apobekiaris <apostolis.bekiaris@gmail.com>
2010-07-24 13:26:35 +0300

    avc--so it can compile i will fix web modules at a later time

--- eXpand/eXpand.ExpressApp.Modules/AdditionalViewControlsProvider.Web/Logic/AdditionalViewControlsRuleViewController.cs
+++ eXpand/eXpand.ExpressApp.Modules/AdditionalViewControlsProvider.Web/Logic/AdditionalViewControlsRuleViewController.cs
@@ -1,22 +1,25 @@
-﻿using System.Web.UI;
+﻿using System;
+using System.Web.UI;
 using eXpand.ExpressApp.AdditionalViewControlsProvider.Logic;
 using eXpand.ExpressApp.Logic;
 
 namespace eXpand.ExpressApp.AdditionalViewControlsProvider.Web.Logic {
     public class AdditionalViewControlsRuleViewController : AdditionalViewControlsProvider.Logic.AdditionalViewControlsRuleViewController{
 
-        protected override void InitializeControl(object viewSiteControl, LogicRuleInfo<IAdditionalViewControlsRule> logicRuleInfo, AdditionalViewControlsProviderCalculator additionalViewControlsProviderCalculator, ExecutionContext executionContext)
+        protected override void InitializeControl(object control, LogicRuleInfo<IAdditionalViewControlsRule> logicRuleInfo, AdditionalViewControlsProviderCalculator additionalViewControlsProviderCalculator, ExecutionContext executionContext)
         {
-            ControlCollection collection = ((Control)viewSiteControl).Controls;
-            ((Control)control).Visible = true;
-            switch (additionalViewControlsRule.Rule.Position) {
-                case Position.Top:
-                    collection.AddAt(0, ((Control)control));
-                    break;
-                default:
-                    collection.Add(((Control)control));
-                    break;
-            }
+            throw new NotImplementedException();
+
+//            ControlCollection collection = ((Control)viewSiteControl).Controls;
+//            ((Control)control).Visible = true;
+//            switch (additionalViewControlsRule.Rule.Position) {
+//                case Position.Top:
+//                    collection.AddAt(0, ((Control)control));
+//                    break;
+//                default:
+//                    collection.Add(((Control)control));
+//                    break;
+//            }
         }
     }
 }
