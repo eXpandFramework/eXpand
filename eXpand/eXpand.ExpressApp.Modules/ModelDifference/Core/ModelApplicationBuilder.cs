d136090956d10482b611342da00ca7a2a6dfe9d9
apobekiaris <apostolis.bekiaris@gmail.com>
2010-08-12 04:06:15 +0300

    when loading models types should be loaded in a different typesinfo system

--- eXpand/eXpand.ExpressApp.Modules/ModelDifference/Core/ModelApplicationBuilder.cs
+++ eXpand/eXpand.ExpressApp.Modules/ModelDifference/Core/ModelApplicationBuilder.cs
@@ -3,31 +3,73 @@ using System.Configuration;
 using System.IO;
 using System.Web.Configuration;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Core;
+using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.Persistent.Base;
 
 namespace eXpand.ExpressApp.ModelDifference.Core {
+    public class XpoTypeInfoSource : DevExpress.ExpressApp.DC.Xpo.XpoTypeInfoSource
+    {
+        public XpoTypeInfoSource(TypesInfo typesInfo) : base(typesInfo) {
+        }
+
+        public new Type GetFirstRegisteredTypeForEntity(Type from)
+        {
+            return base.GetFirstRegisteredTypeForEntity(from);
+        }
+    }
     public class ModelApplicationBuilder {
         readonly string _executableName;
 
         public ModelApplicationBuilder(string executableName) {
             _executableName = executableName;
         }
+        public ApplicationModulesManager CreateApplicationModelManager(XafApplication application, string configFileName, string assembliesPath, ITypesInfo typesInfo)
+        {
+            if (!string.IsNullOrEmpty(configFileName)) {
+                bool isWebApplicationModel =
+                    string.Compare(Path.GetFileNameWithoutExtension(configFileName), "web", true) == 0;
+                if (string.IsNullOrEmpty(assembliesPath)) {
+                    assembliesPath = Path.GetDirectoryName(configFileName);
+                    if (isWebApplicationModel) {
+                        assembliesPath = Path.Combine(assembliesPath, "Bin");
+                    }
+                }
+            }
+            ReflectionHelper.AddResolvePath(assembliesPath);
+            try {
+                var result = new ExpressApp.Core.ApplicationModulesManager(new ControllersManager(), assembliesPath,typesInfo);
+                if (application != null) {
+                    foreach (DevExpress.ExpressApp.ModuleBase module in application.Modules) {
+                        result.AddModule(module);
+                    }
+                    result.Security = application.Security;
+                }
+                return result;
+            }
+            finally {
+                ReflectionHelper.RemoveResolvePath(assembliesPath);
+            }
+        }
 
         public ModelApplicationBase GetMasterModel()
         {
+            var typesInfo = new TypesInfo();
+            typesInfo.AddSource(new ReflectionTypeInfoSource());
+            var xpoSource = new XpoTypeInfoSource(typesInfo);
+            typesInfo.AddSource(xpoSource);
+            typesInfo.AddSource(new DynamicTypeInfoSource());
+//            typesInfo.SetRedirectStrategy((@from, info) => xpoSource.GetFirstRegisteredTypeForEntity(from) ?? from);   
+            var application = GetApplication(_executableName, typesInfo);
 
-            var application = GetApplication(_executableName);
-
-            var modulesManager = new DesignerModelFactory().CreateApplicationModelManager(
-                application,
-                string.Empty,
-                AppDomain.CurrentDomain.SetupInformation.ApplicationBase);
+            var modulesManager = CreateApplicationModelManager(application,string.Empty,
+                AppDomain.CurrentDomain.SetupInformation.ApplicationBase,typesInfo);
 
             ReadModulesFromConfig(modulesManager, application);
 
-            modulesManager.Load();
+            modulesManager.Load(typesInfo);
 
             var modelsManager = new ApplicationModelsManager(
                 modulesManager.Modules,
@@ -38,19 +80,22 @@ namespace eXpand.ExpressApp.ModelDifference.Core {
             ModuleBase.ModelApplicationCreator = null;
             var modelApplication = modelsManager.CreateModelApplication();
             ModuleBase.ModelApplicationCreator=modelApplicationCreator;
+            application.Dispose();
             return (ModelApplicationBase) modelApplication;
         }
 
-        private XafApplication GetApplication(string executableName)
+        private XafApplication GetApplication(string executableName, TypesInfo typesInfo)
         {
             string assemblyPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
             try
             {
                 ReflectionHelper.AddResolvePath(assemblyPath);
                 var assembly = ReflectionHelper.GetAssembly(Path.GetFileNameWithoutExtension(executableName), assemblyPath);
-                var assemblyInfo = XafTypesInfo.Instance.FindAssemblyInfo(assembly);
-                XafTypesInfo.Instance.LoadTypes(assembly);
-                return Enumerator.GetFirst(ReflectionHelper.FindTypeDescendants(assemblyInfo, XafTypesInfo.Instance.FindTypeInfo(typeof(XafApplication)), false)).CreateInstance(new object[0]) as XafApplication;
+                var assemblyInfo = typesInfo.FindAssemblyInfo(assembly);
+                ((ITypesInfo) typesInfo).LoadTypes(assembly);
+                var findTypeInfo = typesInfo.FindTypeInfo(typeof(XafApplication));
+                var findTypeDescendants = ReflectionHelper.FindTypeDescendants(assemblyInfo, findTypeInfo, false);
+                return Enumerator.GetFirst(findTypeDescendants).CreateInstance(new object[0]) as XafApplication;
             }
             finally
             {
---
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp/Core/ApplicationModulesManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Core;
+using DevExpress.ExpressApp.DC;
+
+namespace eXpand.ExpressApp.Core
+{
+    public class ApplicationModulesManager : DevExpress.ExpressApp.ApplicationModulesManager
+    {
+        readonly ITypesInfo _typesInfo;
+
+        public ApplicationModulesManager() {
+        }
+
+        public ApplicationModulesManager(ControllersManager controllersManager, string assembliesPath,ITypesInfo typesInfo) : base(controllersManager, assembliesPath) {
+            _typesInfo = typesInfo;
+        }
+
+        protected override void LoadTypesInfo(IList<DevExpress.ExpressApp.ModuleBase> modules) {
+            foreach (DevExpress.ExpressApp.ModuleBase module in modules){
+                allDomainComponents.AddRange(module.BusinessClasses);
+                allDomainComponents.AddRange(module.BusinessClassAssemblies.GetBusinessClasses());
+            }
+            if (Security != null){
+                allDomainComponents.AddRange(Security.GetBusinessClasses());
+            }
+            foreach (Type type in allDomainComponents){
+                _typesInfo.RegisterEntity(type);
+            }
+
+        }
+    }
+}
