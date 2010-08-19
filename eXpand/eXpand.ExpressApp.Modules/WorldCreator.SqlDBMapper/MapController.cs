e8de66e831d8d71522f0e0fdcd00751e6de8913c
apobekiaris <apostolis.bekiaris@gmail.com>
2010-08-19 22:44:07 +0300

    improve SQLMapper speed

--- eXpand/eXpand.ExpressApp.Modules/WorldCreator.SqlDBMapper/MapController.cs
+++ eXpand/eXpand.ExpressApp.Modules/WorldCreator.SqlDBMapper/MapController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.SqlClient;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Actions;
+using DevExpress.Xpo;
 using eXpand.ExpressApp.WorldCreator.Controllers;
 using eXpand.Persistent.Base.PersistentMetaData;
 using Microsoft.SqlServer.Management.Common;
@@ -18,22 +19,37 @@ namespace eXpand.ExpressApp.WorldCreator.SqlDBMapper
         {
             base.OnActivated();
             var assemblyToolsController = Frame.GetController<AssemblyToolsController>();
-            assemblyToolsController.SingleChoiceAction.Items.Add(new ChoiceActionItem("Map Database", "MapSqlDB"));
-            assemblyToolsController.SingleChoiceAction.Execute +=AssemblyToolsControllerOnToolExecuted;
+            if (assemblyToolsController.SingleChoiceAction.Items.Find("MapSqlDB")== null) {
+                assemblyToolsController.SingleChoiceAction.Items.Add(new ChoiceActionItem("Map Database", "MapSqlDB"));
+                assemblyToolsController.SingleChoiceAction.Execute +=AssemblyToolsControllerOnToolExecuted;
+            }
         }
 
         void AssemblyToolsControllerOnToolExecuted(object sender, SingleChoiceActionExecuteEventArgs singleChoiceActionExecuteEventArgs) {
             if ((string) singleChoiceActionExecuteEventArgs.SelectedChoiceActionItem.Data=="MapSqlDB") {
                 var persistentAssemblyInfo = (IPersistentAssemblyInfo)singleChoiceActionExecuteEventArgs.CurrentObject;
-                const string ConnectionString = @"Integrated Security=SSPI;Pooling=false;Data Source=.\SQLExpress;Initial Catalog=mce;";
-                var cn = new SqlConnection(ConnectionString);
-                var server = new Server(new ServerConnection(cn));
-                Database _database = server.Databases[cn.Database];
-                _database.Refresh();
-                var dbMapper = new DbMapper(ObjectSpace, persistentAssemblyInfo);
-                dbMapper.Map(_database);
+                var objectSpace = new ObjectSpace(new UnitOfWork(ObjectSpace.Session.DataLayer),XafTypesInfo.Instance);
+                IPersistentAssemblyInfo assemblyInfo = GetMappedAssemblyInfo(objectSpace, persistentAssemblyInfo);
+                Show(singleChoiceActionExecuteEventArgs, objectSpace, assemblyInfo);
             }
         }
 
+        void Show(SingleChoiceActionExecuteEventArgs singleChoiceActionExecuteEventArgs, ObjectSpace objectSpace, IPersistentAssemblyInfo assemblyInfo) {
+            ShowViewParameters showViewParameters = singleChoiceActionExecuteEventArgs.ShowViewParameters;
+            showViewParameters.TargetWindow=TargetWindow.Current;
+            showViewParameters.CreatedView = Application.CreateDetailView(objectSpace, assemblyInfo, View.IsRoot);
+        }
+
+        IPersistentAssemblyInfo GetMappedAssemblyInfo(ObjectSpace objectSpace, IPersistentAssemblyInfo persistentAssemblyInfo) {
+            const string ConnectionString = @"Integrated Security=SSPI;Pooling=false;Data Source=.\SQLExpress;Initial Catalog=mce;";
+            var cn = new SqlConnection(ConnectionString);
+            var server = new Server(new ServerConnection(cn));
+            Database _database = server.Databases[cn.Database];
+            _database.Refresh();
+            IPersistentAssemblyInfo assemblyInfo = objectSpace.GetObject(persistentAssemblyInfo);
+            var dbMapper = new DbMapper(objectSpace, assemblyInfo);
+            dbMapper.Map(_database);
+            return assemblyInfo;
+        }
     }
 }
