bd3c1693a70a7664f17c0f9f86e1912b9b144f99
apobekiaris <apostolis.bekiaris@gmail.com>
2010-08-19 03:52:58 +0300

    set the controltypes of header and messages using the collectedrules event approach

--- FeatureCenter/FeatureCenter.Module.Win/WinModule.cs
+++ FeatureCenter/FeatureCenter.Module.Win/WinModule.cs
@@ -1,10 +1,13 @@
+using System;
 using System.ComponentModel;
 using System.Linq;
 using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Win.SystemModule;
+using eXpand.ExpressApp.AdditionalViewControlsProvider;
+using eXpand.ExpressApp.AdditionalViewControlsProvider.Logic;
 using eXpand.ExpressApp.FilterDataStore.Core;
 using eXpand.ExpressApp.FilterDataStore.Win.Providers;
+using eXpand.ExpressApp.Logic;
 using eXpand.ExpressApp.ModelDifference;
 using CreateCustomModelDifferenceStoreEventArgs = eXpand.ExpressApp.ModelDifference.CreateCustomModelDifferenceStoreEventArgs;
 
@@ -24,19 +27,27 @@ namespace FeatureCenter.Module.Win
                     mbase => typeof(ModelDifferenceBaseModule).IsAssignableFrom(mbase.GetType())).SingleOrDefault();
             if (modelDifferenceBaseModule != null)
                 modelDifferenceBaseModule.CreateCustomModelDifferenceStore += ModelDifferenceBaseModuleOnCreateCustomModelDifferenceStore;
+            var additionalViewControlsModule = (AdditionalViewControlsModule)Application.Modules.FindModule(typeof(AdditionalViewControlsModule));
+            additionalViewControlsModule.RulesCollected += AdditionalViewControlsModuleOnRulesCollected;
+        }
+
+        void AdditionalViewControlsModuleOnRulesCollected(object sender, EventArgs e) {
+            foreach (var typeInfo in XafTypesInfo.Instance.PersistentTypes){
+                var additionalViewControlsRules = LogicRuleManager<IAdditionalViewControlsRule>.Instance[typeInfo];
+                foreach (var additionalViewControlsRule in additionalViewControlsRules){
+                    if (additionalViewControlsRule.Id.StartsWith(Module.Captions.Header))
+                        additionalViewControlsRule.ControlType = typeof(ApplicationCaption);
+                    else if (additionalViewControlsRule.Id.StartsWith(Module.Captions.ViewMessage))
+                        additionalViewControlsRule.ControlType = typeof(FooterMessagePanel);
+                }    
+            }
         }
 
         void ModelDifferenceBaseModuleOnCreateCustomModelDifferenceStore(object sender, CreateCustomModelDifferenceStoreEventArgs e) {
             SkinFilterProvider skinFilterProvider = FilterProviderManager.Providers.OfType<SkinFilterProvider>().FirstOrDefault();
             if (skinFilterProvider != null)
                 skinFilterProvider.FilterValue = ((IModelApplicationOptionsSkin)Application.Model.Options).Skin;
-            return;
         }
 
-        public override void AddGeneratorUpdaters(ModelNodesGeneratorUpdaters updaters)
-        {
-            base.AddGeneratorUpdaters(updaters);
-            updaters.Add(new AdditionalViewControlsNodeUpdater());
-        }
     }
 }
