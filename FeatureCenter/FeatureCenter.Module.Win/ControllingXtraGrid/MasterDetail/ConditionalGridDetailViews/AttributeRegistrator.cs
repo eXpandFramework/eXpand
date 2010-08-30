30518a4a072bea6c79e5d22ed75a9dcf8d03a709
apobekiaris <apostolis.bekiaris@gmail.com>
2010-08-30 06:21:43 +0300

    move conditionalgriddetailviews to another namespace to avoid naming conflicts with conditionaldetailviews

--- FeatureCenter/FeatureCenter.Module.Win/ControllingXtraGrid/MasterDetail/ConditionalGridDetailViews/AttributeRegistrator.cs
+++ FeatureCenter/FeatureCenter.Module.Win/ControllingXtraGrid/MasterDetail/ConditionalGridDetailViews/AttributeRegistrator.cs
@@ -6,7 +6,7 @@ using eXpand.ExpressApp.AdditionalViewControlsProvider.Logic;
 using eXpand.ExpressApp.Attributes;
 using eXpand.ExpressApp.MasterDetail.Logic;
 
-namespace FeatureCenter.Module.Win.ControllingXtraGrid.MasterDetail.ConditionalDetailViews
+namespace FeatureCenter.Module.Win.ControllingXtraGrid.MasterDetail.ConditionalGridDetailViews
 {
     public class AttributeRegistrator : Module.AttributeRegistrator
     {
