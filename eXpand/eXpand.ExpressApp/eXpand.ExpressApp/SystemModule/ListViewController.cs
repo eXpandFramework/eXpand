1a2e8050f98f21c800186ef0a1f6b41329431404
unknown <martin@.prakom.local>
2010-09-02 15:17:16 +0200

    ListViewController.cs -> did not worked with ViewVariants - handling ViewChanging events now

--- eXpand/eXpand.ExpressApp/eXpand.ExpressApp/SystemModule/ListViewController.cs
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp/SystemModule/ListViewController.cs
@@ -1,12 +1,33 @@
-﻿using DevExpress.ExpressApp;
+﻿using System;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 
-namespace eXpand.ExpressApp.SystemModule {
-    public abstract class ListViewController<TListEditor>:ViewController<ListView> where TListEditor:ListEditor{
-        protected override void OnActivated()
+namespace eXpand.ExpressApp.SystemModule
+{
+    public abstract class ListViewController<TListEditor> : ViewController<ListView> where TListEditor : ListEditor
+    {
+        protected override void OnViewChanging(View view)
         {
-            base.OnActivated();
-            Active["Editor is not " + typeof (TListEditor).Name] =typeof (TListEditor).IsAssignableFrom(View.Model.EditorType);
+            base.OnViewChanging(view);
+            if (View is ListView)
+            {
+                ((ListView)View).EditorChanged -= listView_EditorChanged;
+            }
+            if (view is ListView)
+            {
+                ((ListView)view).EditorChanged += listView_EditorChanged;
+                UpdateActiveState((view as ListView).Model.EditorType);
+            }
+        }
+
+        private void listView_EditorChanged(object sender, System.EventArgs e)
+        {
+            UpdateActiveState(((ListView)sender).Editor.GetType());
+        }
+
+        private void UpdateActiveState(Type editorType)
+        {
+            Active["Editor is not " + typeof(TListEditor).Name] = typeof(TListEditor).IsAssignableFrom(editorType);
         }
     }
 }
