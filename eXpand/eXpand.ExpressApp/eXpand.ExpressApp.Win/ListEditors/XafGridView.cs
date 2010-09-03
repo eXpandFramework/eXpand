024365b6a3f25fe33e65ace0cef052e5bdec5b08
noxe <mp@prakom.net>
2010-09-03 13:49:22 +0200

    Fixed issue with MasterDetail - Columns had assigned a wrong GridView object

--- eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/ListEditors/XafGridView.cs
+++ eXpand/eXpand.ExpressApp/eXpand.ExpressApp.Win/ListEditors/XafGridView.cs
@@ -3,21 +3,26 @@ using System.ComponentModel;
 using System.Linq;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.XtraGrid.Columns;
 using DevExpress.XtraGrid.Views.Base;
 
-namespace eXpand.ExpressApp.Win.ListEditors {
+namespace eXpand.ExpressApp.Win.ListEditors
+{
     public class XafGridView : DevExpress.ExpressApp.Win.Editors.XafGridView
     {
         readonly DevExpress.ExpressApp.Win.Editors.GridListEditor _gridListEditor;
         private static readonly object instanceCreated = new object();
-        
 
-        public XafGridView() {
+
+        public XafGridView()
+        {
         }
-        public XafGridView(DevExpress.ExpressApp.Win.Editors.GridListEditor gridListEditor) {
+        public XafGridView(DevExpress.ExpressApp.Win.Editors.GridListEditor gridListEditor)
+        {
             _gridListEditor = gridListEditor;
         }
-        
+
         protected virtual void OnInstanceCreated(GridViewInstanceCreatedArgs e)
         {
             var handler = (EventHandler<GridViewInstanceCreatedArgs>)Events[instanceCreated];
@@ -36,7 +41,7 @@ namespace eXpand.ExpressApp.Win.ListEditors {
 
         public Frame MasterFrame { get; set; }
 
-        
+
 
 
         protected override BaseView CreateInstance()
@@ -48,29 +53,37 @@ namespace eXpand.ExpressApp.Win.ListEditors {
         }
         public override void Assign(BaseView v, bool copyEvents)
         {
-            var xafGridView = ((XafGridView) v);
+            var xafGridView = ((XafGridView)v);
             Window = xafGridView.Window;
             MasterFrame = xafGridView.MasterFrame;
             Events.AddHandler(instanceCreated, xafGridView.Events[instanceCreated]);
             base.Assign(v, copyEvents);
         }
-        protected override void AssignColumns(ColumnView cv, bool synchronize) {
-            if (_gridListEditor== null) {
+        protected override void AssignColumns(ColumnView cv, bool synchronize)
+        {
+            if (_gridListEditor == null)
+            {
                 base.AssignColumns(cv, synchronize);
                 return;
             }
-            if (synchronize) {
+            if (synchronize)
+            {
                 base.AssignColumns(cv, true);
             }
-            else {
+            else
+            {
                 Columns.Clear();
-                var columnsListEditorModelSynchronizer = new ColumnsListEditorModelSynchronizer(_gridListEditor,_gridListEditor.Model);
+                var method = typeof(XafGridColumn).GetMethod("Assign", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic, null, new Type[] { typeof(GridColumn) }, null);
+                var columnsListEditorModelSynchronizer = new ColumnsListEditorModelSynchronizer(_gridListEditor, _gridListEditor.Model);
                 columnsListEditorModelSynchronizer.ApplyModel();
                 var gridColumns = _gridListEditor.GridView.Columns.OfType<DevExpress.ExpressApp.Win.Editors.XafGridColumn>();
-                foreach (var column in gridColumns) {
-                    Columns.Add(column);
+                foreach (var column in gridColumns)
+                {
+                    var col = new XafGridColumn(column.TypeInfo, _gridListEditor);
+                    col.ApplyModel(column.Model);
+                    this.Columns.Add(col);
+                    method.Invoke(col, new object[] { column });
                 }
-                
             }
         }
     }
