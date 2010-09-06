333bc7875fdc262ef7a8bbc751ed53cc1627a38d
apobekiaris <apostolis.bekiaris@gmail.com>
2010-09-06 07:17:14 +0300

    minor changes

--- FeatureCenter/FeatureCenter.Module/Miscellaneous/LookUpListSearch/AttributeRegistrator.cs
+++ FeatureCenter/FeatureCenter.Module/Miscellaneous/LookUpListSearch/AttributeRegistrator.cs
@@ -9,16 +9,18 @@ namespace FeatureCenter.Module.Miscellaneous.LookUpListSearch
 {
     public class AttributeRegistrator:Xpand.ExpressApp.Core.AttributeRegistrator
     {
+        private const string LookUpListSearch_LookupListView = "LookUpListSearch_LookupListView";
+        private const string LookUpListSearch_DetailView = "LookUpListSearch_DetailView";
         public override IEnumerable<Attribute> GetAttributes(ITypeInfo typesInfo) {
             if (typesInfo.Type == typeof(Customer)) {
-               yield return new CloneViewAttribute(CloneViewType.ListView, "LookUpListViewSearch_LookupListView");
+               yield return new CloneViewAttribute(CloneViewType.ListView, LookUpListSearch_LookupListView);
             }
             if (typesInfo.Type==typeof(Order)) {
-                yield return new AdditionalViewControlsRuleAttribute(Captions.ViewMessage + " " + Captions.HeaderLookUpListSearch, "1=1", "1=1", Captions.ViewMessageLookUpListSearch, Position.Bottom){ViewType = ViewType.DetailView, View = "LookUpListViewSearch_DetailView"};
-                yield return new AdditionalViewControlsRuleAttribute(Captions.Header + " " + Captions.HeaderLookUpListSearch, "1=1", "1=1", Captions.HeaderLookUpListSearch, Position.Top) { ViewType = ViewType.DetailView, View = "LookUpListViewSearch_DetailView" };
-                yield return new XpandNavigationItemAttribute(Captions.Miscellaneous + "Lookup ListView Search", "LookUpListViewSearch_DetailView");
-                yield return new CloneViewAttribute(CloneViewType.DetailView, "LookUpListViewSearch_DetailView");
-                yield return new DisplayFeatureModelAttribute("LookUpListViewSearch_DetailView");
+                yield return new AdditionalViewControlsRuleAttribute(Captions.ViewMessage + " " + Captions.HeaderLookUpListSearch, "1=1", "1=1", Captions.ViewMessageLookUpListSearch, Position.Bottom){ViewType = ViewType.DetailView, View = LookUpListSearch_DetailView};
+                yield return new AdditionalViewControlsRuleAttribute(Captions.Header + " " + Captions.HeaderLookUpListSearch, "1=1", "1=1", Captions.HeaderLookUpListSearch, Position.Top) { ViewType = ViewType.DetailView, View = LookUpListSearch_DetailView };
+                yield return new XpandNavigationItemAttribute(Captions.Miscellaneous + "Lookup List Search", LookUpListSearch_DetailView);
+                yield return new CloneViewAttribute(CloneViewType.DetailView, LookUpListSearch_DetailView);
+                yield return new DisplayFeatureModelAttribute(LookUpListSearch_DetailView);
             }
         }
     }
