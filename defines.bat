9b242e49cc924bdcdd8e5d6f27b699eb92ab8f1f
apobekiaris <apostolis.bekiaris@gmail.com>
2010-09-12 15:23:02 +0300

    target now is Vs2010

--- defines.bat
+++ defines.bat
@@ -5,8 +5,8 @@ set configuration=Debug
 
 set GACPATH="%WinDir%\assembly\GAC_MSIL\"
 
-rem VS2008 paths
-set vsver=vs2008
-set sn="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin\sn.exe" 
-set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin\gacutil.exe"
+rem VS2010 paths
+set vsver=vs2010
+set sn="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" 
+set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\gacutil.exe"
 set msbuild="%WinDir%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
