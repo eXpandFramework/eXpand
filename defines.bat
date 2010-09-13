ce56c343536bf9287672fa085bf95dda15fb654b
apobekiaris <apostolis.bekiaris@gmail.com>
2010-09-13 21:06:58 +0300

    support old sdk path

--- defines.bat
+++ defines.bat
@@ -1,5 +1,5 @@
-@echo off
 
+@echo off
 set exver=v0.1
 set configuration=Debug
 
@@ -9,4 +9,8 @@ rem VS2010 paths
 set vsver=vs2010
 set sn="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe" 
 set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\gacutil.exe"
+set sdkDir=%ProgramFiles%\Microsoft SDKs\Windows\v7.0A
+IF NOT EXIST sdkDir set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin\gacutil.exe"
+
+
 set msbuild="%WinDir%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
