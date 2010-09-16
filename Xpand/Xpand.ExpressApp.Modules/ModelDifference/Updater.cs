bab0fbf47bac7e7195165341aac94505bec9ea00
noxe <mp@prakom.net>
2010-09-16 12:56:23 +0200

    Added Updater to convert existing Model to new Aspect structure

---
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/Updater.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Xpo;
+using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
+using Xpand.Utils.GeneralDataStructures;
+
+namespace Xpand.ExpressApp.ModelDifference
+{
+    public class Updater : ModuleUpdater
+    {
+        public Updater(Session session, Version currentDBVersion) : base(session, currentDBVersion) { }
+        public override void UpdateDatabaseBeforeUpdateSchema()
+        {
+            base.UpdateDatabaseBeforeUpdateSchema();
+            if (this.CurrentDBVersion >= new Version(0, 0, 0, 0) && this.CurrentDBVersion <= new Version(10, 1, 6))
+            {
+                var differenceObjects = new Dictionary<object, string>();
+                using (var reader = this.ExecuteReader("select [Oid], [Model] from [ModelDifferenceObject] where [Model] is not null", false))
+                {
+                    while (reader.Read())
+                    {
+                        differenceObjects.Add(reader[0], reader[1] as string);
+                    }
+                }
+
+                using (var uow = new UnitOfWork(Session.DataLayer))
+                {
+                    foreach (var differenceObject in differenceObjects)
+                    {
+                        var modelDifferenceObject = uow.GetObjectByKey<ModelDifferenceObject>(differenceObject.Key);
+                        var serializableDictionary = new SerializableDictionary<string, string>();
+                        var xmlReader = XmlReader.Create(new StringReader(differenceObject.Value), new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Auto });
+                        serializableDictionary.ReadXml(xmlReader);
+                        var aspects = serializableDictionary["aspects"].Split(',').ToList();
+                        var defaultAspect = serializableDictionary["DefaultAspect"];
+                        defaultAspect = GetDefaultAspectFromVersion9(serializableDictionary, aspects, defaultAspect);
+
+                        if (!string.IsNullOrEmpty(defaultAspect))
+                            modelDifferenceObject.AspectObjects.Add(new AspectObject(uow) { Name = CaptionHelper.DefaultLanguage, Xml = defaultAspect });
+
+                        foreach (var aspect in aspects.Where(aspect => !string.IsNullOrEmpty(aspect) && !string.IsNullOrEmpty(serializableDictionary[aspect])))
+                        {
+                            modelDifferenceObject.AspectObjects.Add(new AspectObject(uow) { Name = aspect, Xml = serializableDictionary[aspect] });
+                        }
+                    }
+
+                    uow.CommitChanges();
+                }
+            }
+        }
+
+        private string GetDefaultAspectFromVersion9(SerializableDictionary<string, string> serializableDictionary, List<string> aspects, string defaultAspect)
+        {
+            if (serializableDictionary.ContainsKey("Schema"))
+            {
+                return GetAspectFromXml(aspects, defaultAspect);
+            }
+
+            return defaultAspect;
+        }
+
+        private string GetAspectFromXml(List<string> aspects, string xml)
+        {
+            aspects = aspects.OrderBy(s => s).ToList();
+
+            xml = xml.Replace("&#165;", "¥");
+            xml = removeSpaces(xml);
+            string defaultAspectValuesWhenNoOtherAspectExist = Regex.Replace(xml, "\":([^\"\xA5]*)\"", "\"$1\"");
+            string removedAspectsWithNoDefaultAspects = defaultAspectValuesWhenNoOtherAspectExist;
+            if (!string.IsNullOrEmpty(aspects[0]))
+            {
+                string defaultAspectWhenOtherAspectExists = Regex.Replace(defaultAspectValuesWhenNoOtherAspectExist, @""":([^""\xA5]*)\xA5" + aspects[0] + @":([^""]*)""", "\"$1\"");
+                removedAspectsWithNoDefaultAspects = defaultAspectWhenOtherAspectExists;
+                foreach (var aspect in aspects)
+                {
+                    removedAspectsWithNoDefaultAspects = removeAttributesWithNoDefaultValue(aspect, removedAspectsWithNoDefaultAspects);
+                }
+            }
+            return removedAspectsWithNoDefaultAspects;
+        }
+
+        private string removeSpaces(string aspects)
+        {
+            return aspects.Replace(" >", ">");
+        }
+
+        private string removeAttributesWithNoDefaultValue(string aspect, string value)
+        {
+            return Regex.Replace(value, "( [^=\"]*=\"" + aspect + ":([^\"]*)\")", "");
+        }
+    }
+}
