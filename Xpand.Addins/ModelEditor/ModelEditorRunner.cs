f9a042ef238913f531d7f662e97e904894308211
noxe <mp@prakom.net>
2010-09-19 17:23:59 +0200

    resolving relative path issue when loading xpand modeleditor

--- Xpand.Addins/ModelEditor/ModelEditorRunner.cs
+++ Xpand.Addins/ModelEditor/ModelEditorRunner.cs
@@ -33,7 +33,7 @@ namespace XpandAddIns.ModelEditor {
                 return;
             }
 
-            string arguments = String.Format("'{0}' '{1}' '{2}'", assemblyPath, fullPath, projectWrapper.LocalPath);
+            string arguments = String.Format("'{0}' '{1}' '{2}'", Path.GetFullPath(assemblyPath), fullPath, projectWrapper.LocalPath);
             if (File.Exists(path))
                 Process.Start(path, arguments);
             else
