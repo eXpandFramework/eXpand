62abecee1308ed9e925bd64b3f8a18f158baeb30
noxe <mp@prakom.net>
2010-09-23 10:11:53 +0200

    fixed invalidcastexception

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/NullAble/BooleanPropertyEditor/BooleanHAlignFarPropertyEditor.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/NullAble/BooleanPropertyEditor/BooleanHAlignFarPropertyEditor.cs
@@ -1,26 +1,25 @@
 using System;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Win.Editors;
 using DevExpress.Utils;
 using DevExpress.XtraEditors.Repository;
-using DevExpress.ExpressApp.Editors;
 
 namespace Xpand.ExpressApp.Win.PropertyEditors.NullAble.BooleanPropertyEditor {
     [PropertyEditor(typeof(bool))]
-    public class BooleanHAlignFarPropertyEditor : DevExpress.ExpressApp.Win.Editors.BooleanPropertyEditor
-    {
+    public class BooleanHAlignFarPropertyEditor : DevExpress.ExpressApp.Win.Editors.BooleanPropertyEditor {
         public BooleanHAlignFarPropertyEditor(Type objectType, IModelMemberViewItem member)
-            : base(objectType, member)
-        {
+            : base(objectType, member) {
         }
 
-        protected override void SetupRepositoryItem(RepositoryItem item)
-        {
+        protected override void SetupRepositoryItem(RepositoryItem item) {
             base.SetupRepositoryItem(item);
-            var ri = (RepositoryItemBooleanEdit)item;
-            ri.GlyphAlignment = HorzAlignment.Far;
-            ri.Appearance.TextOptions.HAlignment = HorzAlignment.Far;
-            ri.Appearance.Options.UseTextOptions = true;
+            var ri = item as RepositoryItemBooleanEdit;
+            if (ri != null) {
+                ri.GlyphAlignment = HorzAlignment.Far;
+                ri.Appearance.TextOptions.HAlignment = HorzAlignment.Far;
+                ri.Appearance.Options.UseTextOptions = true;
+            }
         }
     }
 }
