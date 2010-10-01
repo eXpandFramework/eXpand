472b28ab0be02013e527450a4f8e216dbcf30d59
apobekiaris <apostolis.bekiaris@gmail.com>
2010-10-01 15:53:10 +0300

    refactor Model Execution contexts cause of ME incapabilities

--- Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelExecutionContextDomainLogic.cs
+++ Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelExecutionContextDomainLogic.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DevExpress.ExpressApp.DC;
 using Xpand.ExpressApp.Logic.Model;
 
-namespace Xpand.ExpressApp.Logic.DomainLogic
-{
+namespace Xpand.ExpressApp.Logic.DomainLogic {
     [DomainLogic(typeof(IModelExecutionContext))]
-    public class ModelExecutionContextDomainLogic
-    {
-        public static string Get_Name(IModelExecutionContext modelExecutionContext) {
-            string value = modelExecutionContext.GetType().Name.Replace("Model","");
-            return Enum.Parse(typeof(ExecutionContext),value).ToString();
+    public class ModelExecutionContextDomainLogic {
+        public static List<string> Get_ExecutionContexts(IModelExecutionContext modelExecutionContext) {
+            return Enum.GetValues(typeof(ExecutionContext)).OfType<ExecutionContext>().Select(context => context.ToString()).ToList();
+        }
+    }
+    [DomainLogic(typeof(IModelViewContext))]
+    public class ModelViewContextDomainLogic {
+        public static List<string> Get_ExecutionContexts(IModelViewContext modelViewContext) {
+            return Enum.GetValues(typeof(ExecutionContext)).OfType<ExecutionContext>().Select(context => context.ToString()).ToList();
         }
     }
 }
--- Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelGroupContextsDefaultContextDomainLogic.cs
+++ Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelGroupContextsDefaultContextDomainLogic.cs
@@ -4,10 +4,10 @@ using Xpand.ExpressApp.Logic.Model;
 using Xpand.ExpressApp.Logic.NodeUpdaters;
 
 namespace Xpand.ExpressApp.Logic.DomainLogic {
-    [DomainLogic(typeof (IModelGroupContexts))]
+    [DomainLogic(typeof (IModelGroupExecutionContexts))]
     public class ModelGroupContextsDefaultContextDomainLogic {
-        public static IModelExecutionContexts Get_DefaultContext(IModelGroupContexts modelGroupContexts){
-            return modelGroupContexts.Where(context => context.Id == LogicDefaultGroupContextNodeUpdater.Default).SingleOrDefault();
+        public static IModelExecutionContexts Get_DefaultContext(IModelGroupExecutionContexts modelGroupExecutionContexts){
+            return modelGroupExecutionContexts.Where(context => context.Id == LogicDefaultGroupContextNodeUpdater.Default).SingleOrDefault();
         }
     }
 }
