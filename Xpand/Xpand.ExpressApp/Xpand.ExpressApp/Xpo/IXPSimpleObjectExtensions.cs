8c3a0636c5bd42263a8c6740f7dd17c182c5e8f6
noxe <mp@prakom.net>
2010-10-02 13:02:16 +0200

    Added support for runtime collection members (collections without associations)

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/Xpo/IXPSimpleObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using Xpand.ExpressApp.Xpo.MetaData;
+
+namespace Xpand.ExpressApp.Xpo
+{
+    public static class IXPSimpleObjectExtensions
+    {
+        public static XPCustomMemberInfo CreateCollection(this XPClassInfo classInfo, string propertyName, Type elementType, string criteria, params Attribute[] attributes)
+        {
+            var newMemberInfo = new XpandCollectionMemberInfo(classInfo, propertyName, typeof(XPCollection<>).MakeGenericType(elementType), criteria);
+            foreach (Attribute attribute in attributes)
+                newMemberInfo.AddAttribute(attribute);
+            return newMemberInfo;
+        }
+    }
+}
