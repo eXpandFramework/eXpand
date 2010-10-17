c2e10eb0e396fab43b5bb25a14db0a7118ea4abf
apobekiaris <apostolis.bekiaris@gmail.com>
2010-10-17 15:38:30 +0300

    code format

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference.Win/Controllers/ModelEditorTemplateViewController.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference.Win/Controllers/ModelEditorTemplateViewController.cs
@@ -9,86 +9,67 @@ using DevExpress.XtraBars.Ribbon;
 using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 using Xpand.ExpressApp.ModelDifference.Win.PropertyEditors;
 
-namespace Xpand.ExpressApp.ModelDifference.Win.Controllers
-{
-    public class ModelEditorTemplateViewController : ViewController<DetailView>
-    {
-        public ModelEditorTemplateViewController()
-        {
+namespace Xpand.ExpressApp.ModelDifference.Win.Controllers {
+    public class ModelEditorTemplateViewController : ViewController<DetailView> {
+        public ModelEditorTemplateViewController() {
             TargetObjectType = typeof(ModelDifferenceObject);
         }
 
-        protected override void OnViewChanging(View view)
-        {
+        protected override void OnViewChanging(View view) {
             base.OnViewChanging(view);
 
-            if (View != null)
-            {
+            if (View != null) {
                 View.Closing -= View_Closing;
             }
 
-            if (view is DetailView && typeof(ModelDifferenceObject).IsAssignableFrom(view.ObjectTypeInfo.Type))
-            {
+            if (view is DetailView && typeof(ModelDifferenceObject).IsAssignableFrom(view.ObjectTypeInfo.Type)) {
                 view.Closing += View_Closing;
             }
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            try
-            {
-                if (View != null)
-                {
+        protected override void Dispose(bool disposing) {
+            try {
+                if (View != null) {
                     View.Closing -= View_Closing;
                 }
-            }
-            finally
-            {
+            } finally {
                 base.Dispose(disposing);
             }
         }
 
-        void View_Closing(object sender, System.EventArgs e)
-        {
-            HideMainBarActions((DetailView) sender);
+        void View_Closing(object sender, System.EventArgs e) {
+            HideMainBarActions((DetailView)sender);
         }
 
-        void HideMainBarActions(DetailView xpandDetailView)
-        {
+        void HideMainBarActions(DetailView xpandDetailView) {
             var modelEditorViewController = xpandDetailView.GetItems<ModelEditorPropertyEditor>()[0].ModelEditorViewController;
             FieldInfo fieldInfo = modelEditorViewController.GetType().GetField("mainBarActions",
                                                                                BindingFlags.Instance | BindingFlags.NonPublic);
             if (fieldInfo != null) {
                 var actions = (List<ActionBase>)fieldInfo.GetValue(modelEditorViewController);
-                foreach (var actionBase in Frame.Template.DefaultContainer.Actions.Where(actions.Contains))
-                {
+                foreach (var actionBase in Frame.Template.DefaultContainer.Actions.Where(actions.Contains)) {
                     actionBase.Active["Is Not ModelDiffs view"] = false;
                 }
             }
         }
 
-        protected override void OnViewControlsCreated()
-        {
+        protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
             var template = Frame.Template as XtraFormTemplateBase;
-            if (template != null)
-            {
+            if (template != null) {
                 SetTemplate();
-                if (template.FormStyle == RibbonFormStyle.Ribbon)
-                {
+                if (template.FormStyle == RibbonFormStyle.Ribbon) {
                     template.RibbonTransformer.Transformed += RibbonTransformer_Transformed;
                 }
             }
         }
 
-        private void RibbonTransformer_Transformed(object sender, System.EventArgs e)
-        {
+        private void RibbonTransformer_Transformed(object sender, System.EventArgs e) {
             ((ClassicToRibbonTransformer)sender).Transformed -= RibbonTransformer_Transformed;
             SetTemplate();
         }
 
-        private void SetTemplate()
-        {
+        private void SetTemplate() {
 
             View.GetItems<ModelEditorPropertyEditor>()[0].ModelEditorViewController.SetTemplate(Frame.Template);
         }
