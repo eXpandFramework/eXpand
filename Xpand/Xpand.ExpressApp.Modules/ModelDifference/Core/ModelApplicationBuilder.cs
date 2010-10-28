76dad48969543e863d81282ce43a9f61e2d1bef0
apobekiaris <apostolis.bekiaris@gmail.com>
2010-10-28 13:33:04 +0300

    do not remove member that not exist

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/Core/ModelApplicationBuilder.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/Core/ModelApplicationBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Web.Configuration;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
@@ -10,49 +11,45 @@ using DevExpress.ExpressApp.Utils;
 using DevExpress.Persistent.Base;
 using Xpand.ExpressApp.Core;
 using Xpand.ExpressApp.Model;
-using System.Linq;
 
 
 namespace Xpand.ExpressApp.ModelDifference.Core {
-    public class XpoTypeInfoSource : DevExpress.ExpressApp.DC.Xpo.XpoTypeInfoSource
-    {
-        public XpoTypeInfoSource(TypesInfo typesInfo) : base(typesInfo) {
+    public class XpoTypeInfoSource : DevExpress.ExpressApp.DC.Xpo.XpoTypeInfoSource {
+        public XpoTypeInfoSource(TypesInfo typesInfo)
+            : base(typesInfo) {
         }
 
-        public new Type GetFirstRegisteredTypeForEntity(Type from)
-        {
+        public new Type GetFirstRegisteredTypeForEntity(Type from) {
             return base.GetFirstRegisteredTypeForEntity(from);
         }
     }
     public class ModelApplicationBuilder {
         readonly string _executableName;
-        
+
 
         public ModelApplicationBuilder(string executableName) {
             _executableName = executableName;
         }
-        public XpandApplicationModulesManager CreateApplicationModulesManager(XafApplication application, string configFileName, string assembliesPath, ITypesInfo typesInfo)
-        {
+        public XpandApplicationModulesManager CreateApplicationModulesManager(XafApplication application, string configFileName, string assembliesPath, ITypesInfo typesInfo) {
             if (!string.IsNullOrEmpty(configFileName)) {
                 bool isWebApplicationModel =
                     string.Compare(Path.GetFileNameWithoutExtension(configFileName), "web", true) == 0;
                 if (string.IsNullOrEmpty(assembliesPath)) {
                     assembliesPath = Path.GetDirectoryName(configFileName);
                     if (isWebApplicationModel) {
-                        assembliesPath = Path.Combine(assembliesPath+"", "Bin");
+                        assembliesPath = Path.Combine(assembliesPath + "", "Bin");
                     }
                 }
             }
             ReflectionHelper.AddResolvePath(assembliesPath);
             try {
-                var result = new XpandApplicationModulesManager(new XpandControllersManager(), assembliesPath,application.Security);
+                var result = new XpandApplicationModulesManager(new XpandControllersManager(), assembliesPath, application.Security);
                 foreach (ModuleBase module in application.Modules) {
                     result.AddModule(module);
                 }
                 result.Security = application.Security;
                 return result;
-            }
-            finally {
+            } finally {
                 ReflectionHelper.RemoveResolvePath(assembliesPath);
             }
         }
@@ -72,9 +69,11 @@ namespace Xpand.ExpressApp.ModelDifference.Core {
             var modelRuntimeMembers = XpandModuleBase.Application.Model.BOModel.SelectMany(@class => @class.OwnMembers).OfType<IModelRuntimeMember>();
             foreach (var modelRuntimeMember in modelRuntimeMembers) {
                 var modelClass = masterModel.Application.BOModel.GetClass(modelRuntimeMember.ModelClass.TypeInfo.Type);
-                var modelMember = modelClass.FindMember(modelRuntimeMember.Name);
-                if (!modelRuntimeMember.NonPersistent)
-                    modelClass.OwnMembers.Remove(modelMember);
+                if (modelClass != null) {
+                    var modelMember = modelClass.FindMember(modelRuntimeMember.Name);
+                    if (modelClass.FindMember(modelMember.Name)!=null)
+                        modelClass.OwnMembers.Remove(modelMember);
+                }
             }
         }
 
@@ -94,20 +93,20 @@ namespace Xpand.ExpressApp.ModelDifference.Core {
             XpandModuleBase.ModelApplicationCreator = null;
             var modelApplication = modelsManager.CreateModelApplication();
             AddAfterSetupLayer(modelApplication);
-            XpandModuleBase.ModelApplicationCreator=modelApplicationCreator;
+            XpandModuleBase.ModelApplicationCreator = modelApplicationCreator;
             return modelApplication;
         }
 
         ApplicationModelsManager GetModelsManager(XpandApplicationModulesManager modulesManager) {
-            var controllersManager=modulesManager.ControllersManager;
-            var applicationModelsManager = new ApplicationModelsManager(modulesManager.Modules,controllersManager,modulesManager.DomainComponents);
+            var controllersManager = modulesManager.ControllersManager;
+            var applicationModelsManager = new ApplicationModelsManager(modulesManager.Modules, controllersManager, modulesManager.DomainComponents);
             return applicationModelsManager;
         }
 
         XpandApplicationModulesManager GetModulesManager(TypesInfo typesInfo, XafApplication application) {
-            var modulesManager = CreateApplicationModulesManager(application,string.Empty,
-                                                                 AppDomain.CurrentDomain.SetupInformation.ApplicationBase,typesInfo);
-            
+            var modulesManager = CreateApplicationModulesManager(application, string.Empty,
+                                                                 AppDomain.CurrentDomain.SetupInformation.ApplicationBase, typesInfo);
+
             ReadModulesFromConfig(modulesManager, application);
 
             modulesManager.Load(typesInfo);
@@ -115,41 +114,38 @@ namespace Xpand.ExpressApp.ModelDifference.Core {
         }
 
         void AddAfterSetupLayer(IModelApplication modelApplication) {
-            var modelApplicationBase = ((ModelApplicationBase) modelApplication);
+            var modelApplicationBase = ((ModelApplicationBase)modelApplication);
             ModelApplicationBase afterSetup = modelApplicationBase.CreatorInstance.CreateModelApplication();
             afterSetup.Id = "After Setup";
             modelApplicationBase.AddLayer(afterSetup);
         }
 
-        private XafApplication GetApplication(string executableName, TypesInfo typesInfo){
+        private XafApplication GetApplication(string executableName, TypesInfo typesInfo) {
             string assemblyPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-            try{
+            try {
                 ReflectionHelper.AddResolvePath(assemblyPath);
                 var assembly = ReflectionHelper.GetAssembly(Path.GetFileNameWithoutExtension(executableName), assemblyPath);
                 var assemblyInfo = typesInfo.FindAssemblyInfo(assembly);
-                ((ITypesInfo) typesInfo).LoadTypes(assembly);
+                ((ITypesInfo)typesInfo).LoadTypes(assembly);
                 var findTypeInfo = typesInfo.FindTypeInfo(typeof(XafApplication));
                 var findTypeDescendants = ReflectionHelper.FindTypeDescendants(assemblyInfo, findTypeInfo, false);
                 return Enumerator.GetFirst(findTypeDescendants).CreateInstance(new object[0]) as XafApplication;
-            }
-            finally{
+            } finally {
                 ReflectionHelper.RemoveResolvePath(assemblyPath);
             }
         }
 
-        private void ReadModulesFromConfig(ApplicationModulesManager manager, XafApplication application)
-        {
+        private void ReadModulesFromConfig(ApplicationModulesManager manager, XafApplication application) {
             Configuration config;
-            if (application is IWinApplication){
+            if (application is IWinApplication) {
                 config = ConfigurationManager.OpenExeConfiguration(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + _executableName);
-            }
-            else{
+            } else {
                 var mapping = new WebConfigurationFileMap();
                 mapping.VirtualDirectories.Add("/Dummy", new VirtualDirectoryMapping(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, true));
                 config = WebConfigurationManager.OpenMappedWebConfiguration(mapping, "/Dummy");
             }
 
-            if (config.AppSettings.Settings["Modules"] != null){
+            if (config.AppSettings.Settings["Modules"] != null) {
                 manager.AddModuleFromAssemblies(config.AppSettings.Settings["Modules"].Value.Split(';'));
             }
         }
@@ -160,7 +156,7 @@ namespace Xpand.ExpressApp.ModelDifference.Core {
             var layer = masterModel.CreatorInstance.CreateModelApplication();
 
             masterModel.AddLayerBeforeLast(layer);
-            var storeBase =(ModelApplicationFromStreamStoreBase)ReflectionHelper.CreateObject(modelApplicationFromStreamStoreBaseType);
+            var storeBase = (ModelApplicationFromStreamStoreBase)ReflectionHelper.CreateObject(modelApplicationFromStreamStoreBaseType);
             storeBase.Load(layer);
             return layer;
         }
