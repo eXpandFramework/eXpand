6754a76384cb14173a93c1c6b43e9794235ea717
apobekiaris <apostolis.bekiaris@gmail.com>
2010-11-15 18:31:37 +0200

    Rename ExpressionTransformer to ExpressionConverter

--- Xpand/Xpand.Tests/Xpand.Tests/Xpand.Utils/ExpressionTransformerSpecs.cs
+++ Xpand/Xpand.Tests/Xpand.Tests/Xpand.Utils/ExpressionTransformerSpecs.cs
@@ -3,22 +3,21 @@ using System.Linq.Expressions;
 using Machine.Specifications;
 using Xpand.Utils.Linq;
 
-namespace Xpand.Tests.Xpand.Utils
-{
-    [Subject(typeof(ExpressionTransformer),"Transform")]
+namespace Xpand.Tests.Xpand.Utils {
+
+    [Subject(typeof(ExpressionConverter), "Convert")]
     public class When_expression_type_is_not_equal {
         static LambdaExpression _lambdaExpression;
         static BinaryExpression _transform;
         static Expression<Func<ITransformerExpressionClass, bool>> _expression;
 
-        Establish context = () =>
-        {
+        Establish context = () => {
             _expression = info => info.Name != "test";
         };
 
         Because of = () => {
-            _lambdaExpression = ((LambdaExpression) new ExpressionTransformer().Transform(typeof(TransformerExpressionClass), _expression));
-            _transform =_lambdaExpression.Body as BinaryExpression;
+            _lambdaExpression = ((LambdaExpression)new ExpressionConverter().Convert(typeof(TransformerExpressionClass), _expression));
+            _transform = _lambdaExpression.Body as BinaryExpression;
         };
 
         It should_return_a_binary_expression = () => _transform.ShouldNotBeNull();
@@ -27,45 +26,45 @@ namespace Xpand.Tests.Xpand.Utils
         It should_have_as_type_of_the_expression_of_the_memberexpression_the_transformarion_type = () => ((MemberExpression)_transform.Left).Expression.Type.ShouldEqual(typeof(TransformerExpressionClass));
 
         It should_have_as_expression_of_the_memberexpression_the_parameter_of_the_passed_in_lamda =
-            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression) _transform.Left).Expression);
+            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression)_transform.Left).Expression);
     }
-    [Subject(typeof(ExpressionTransformer), "Transform")]
-    public class When_expression_type_is_equal
-    {
+
+
+    [Subject(typeof(ExpressionConverter), "Convert")]
+    public class When_expression_type_is_equal {
         static LambdaExpression _lambdaExpression;
         static BinaryExpression _transform;
         static Expression<Func<ITransformerExpressionClass, bool>> _expression;
 
         Establish context = () => {
-            _expression = info => info.Name == "test" ;
+            _expression = info => info.Name == "test";
         };
 
         Because of = () => {
-            _lambdaExpression = ((LambdaExpression)new ExpressionTransformer().Transform(typeof(TransformerExpressionClass), _expression));
+            _lambdaExpression = ((LambdaExpression)new ExpressionConverter().Convert(typeof(TransformerExpressionClass), _expression));
             _transform = _lambdaExpression.Body as BinaryExpression;
         };
 
         It should_return_a_binary_expression = () => _transform.ShouldNotBeNull();
 
-        It should_have_as_left_a_member_expression = () => _transform.Left.ShouldBeOfType(typeof (MemberExpression));
-        It should_have_as_type_of_the_expression_of_the_memberexpression_the_transformarion_type = () =>((MemberExpression) _transform.Left).Expression.Type.ShouldEqual(typeof(TransformerExpressionClass));
+        It should_have_as_left_a_member_expression = () => _transform.Left.ShouldBeOfType(typeof(MemberExpression));
+        It should_have_as_type_of_the_expression_of_the_memberexpression_the_transformarion_type = () => ((MemberExpression)_transform.Left).Expression.Type.ShouldEqual(typeof(TransformerExpressionClass));
 
         It should_have_as_expression_of_the_memberexpression_the_parameter_of_the_passed_in_lamda =
             () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression)_transform.Left).Expression);
     }
-    [Subject(typeof(ExpressionTransformer), "Transform")]
+    [Subject(typeof(ExpressionConverter), "Convert")]
     public class When_expression_type_is_and_also {
         static LambdaExpression _lambdaExpression;
         static BinaryExpression _transform;
         static Expression<Func<ITransformerExpressionClass, bool>> _expression;
 
-        Establish context = () =>
-        {
+        Establish context = () => {
             _expression = info => info.Name == "test" && info.Name == "test";
         };
 
         Because of = () => {
-            _lambdaExpression = ((LambdaExpression)new ExpressionTransformer().Transform(typeof(TransformerExpressionClass), _expression));
+            _lambdaExpression = ((LambdaExpression)new ExpressionConverter().Convert(typeof(TransformerExpressionClass), _expression));
             _transform = _lambdaExpression.Body as BinaryExpression;
         };
 
@@ -75,23 +74,21 @@ namespace Xpand.Tests.Xpand.Utils
         It should_have_as_right_a_binary_expression = () => _transform.Left.ShouldBeOfType(typeof(BinaryExpression));
 
         It should_have_as_expression_of_the_left_memberexpression_the_parameter_of_the_passed_in_lamda =
-            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression) ((BinaryExpression)_transform.Left).Left).Expression);
+            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression)((BinaryExpression)_transform.Left).Left).Expression);
         It should_have_as_expression_of_the_right_memberexpression_the_parameter_of_the_passed_in_lamda =
-            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression) ((BinaryExpression)_transform.Right).Left).Expression);
+            () => _lambdaExpression.Parameters[0].ShouldEqual(((MemberExpression)((BinaryExpression)_transform.Right).Left).Expression);
     }
-    [Subject(typeof(ExpressionTransformer), "Transform")]
+    [Subject(typeof(ExpressionConverter), "Convert")]
     public class When_expression_type_is_or_else {
         static BinaryExpression _transform;
         static Expression<Func<ITransformerExpressionClass, bool>> _expression;
 
-        Establish context = () =>
-        {
+        Establish context = () => {
             _expression = info => info.Name == "test" || info.Name == "test";
         };
 
-        Because of = () =>
-        {
-            _transform = ((LambdaExpression)new ExpressionTransformer().Transform(typeof(TransformerExpressionClass), _expression)).Body as BinaryExpression;
+        Because of = () => {
+            _transform = ((LambdaExpression)new ExpressionConverter().Convert(typeof(TransformerExpressionClass), _expression)).Body as BinaryExpression;
         };
 
         It should_return_a_binary_expression = () => _transform.ShouldNotBeNull();
@@ -99,30 +96,28 @@ namespace Xpand.Tests.Xpand.Utils
         It should_have_as_left_a_binary_expression = () => _transform.Left.ShouldBeOfType(typeof(BinaryExpression));
         It should_have_as_right_a_binary_expression = () => _transform.Left.ShouldBeOfType(typeof(BinaryExpression));
     }
-    [Subject(typeof(ExpressionTransformer), "Transform")]
+    [Subject(typeof(ExpressionConverter), "Convert")]
     public class When_expression_is_null {
         static Expression _expression;
 
-        Because of = () =>
-        {
-            _expression = new ExpressionTransformer().Transform(typeof(TransformerExpressionClass),null);
+        Because of = () => {
+            _expression = new ExpressionConverter().Convert(typeof(TransformerExpressionClass), null);
         };
 
         It should_return_null_as_expression = () => _expression.ShouldBeNull();
     }
-    [Subject(typeof(ExpressionTransformer), "Transform")]
+    [Subject(typeof(ExpressionConverter), "Convert")]
     internal class When_expression_right_member_is_not_constant {
         static BinaryExpression _transform;
         static Expression<Func<ITransformerExpressionClass, bool>> _expression;
 
-        Establish context = () =>
-        {
+        Establish context = () => {
             var transformerExpressionClass = new TransformerExpressionClass();
             _expression = info => info.ExpressionClass == transformerExpressionClass;
         };
 
-        Because of = () =>{
-            _transform = ((LambdaExpression)new ExpressionTransformer().Transform(typeof(TransformerExpressionClass), _expression)).Body as BinaryExpression;
+        Because of = () => {
+            _transform = ((LambdaExpression)new ExpressionConverter().Convert(typeof(TransformerExpressionClass), _expression)).Body as BinaryExpression;
         };
         It should_not_be_transformed = () => ((MemberExpression)_transform.Right).Member.Name.ShouldEqual("transformerExpressionClass");
     }
