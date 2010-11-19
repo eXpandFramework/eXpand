a671fb0e680317dc2b470b227b40c451b68541a3
apobekiaris <apostolis.bekiaris@gmail.com>
2010-11-19 02:10:04 +0200

    added newobject action grouping support

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/Attributes/NewObjectCreateGroupAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Xpand.ExpressApp.Attributes {
+    public class NewObjectCreateGroupAttribute : Attribute {
+        readonly string _groupPath;
+
+        public NewObjectCreateGroupAttribute(string groupPath) {
+            _groupPath = groupPath;
+        }
+
+        public string GroupPath {
+            get {
+                return _groupPath;
+            }
+        }
+    }
+}
