0daf09bc6b02b9d2eebdeeede5b413c0e22c05e8
apobekiaris <apostolis.bekiaris@gmail.com>
2010-11-25 22:53:46 +0200

    added support for ViewModeDetailView

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/EditModeDetailViewController.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.Persistent.Base;
+
+namespace Xpand.ExpressApp.SystemModule {
+    public interface IModelClassViewModeDetailView : IModelNode {
+        [Category("eXpand")]
+        [Description("This view is going to be used when ViewMode==Edit")]
+        [DataSourceCriteria("ModelClass Is Not Null And ModelClass.Name = '@This.Name'")]
+        [DataSourceProperty("Application.Views")]
+        [ModelValueCalculator("ModelClass", typeof(IModelClass), "DefaultDetailView")]
+        IModelDetailView ViewModeDetailView { get; set; }
+    }
+
+    public interface IModelDetailViewViewModeDetailView : IModelNode {
+        [Category("eXpand")]
+        [Description("This view is going to be used when ViewMode==Edit")]
+        [DataSourceCriteria("ModelClass Is Not Null And ModelClass.Name = '@This.ModelClass.Name'")]
+        [DataSourceProperty("Application.Views")]
+        [ModelValueCalculator("ModelClass", typeof(IModelClassViewModeDetailView), "ViewModeDetailView")]
+        IModelDetailView ViewModeDetailView { get; set; }
+    }
+    [DomainLogic(typeof(IModelClassViewModeDetailView))]
+    public class ModelClassViewModeDetailViewDomainLogic {
+        public static IModelDetailView Get_ViewModeDetailView(IModelClassViewModeDetailView modelClassViewModeDetailView) {
+            if (modelClassViewModeDetailView is IModelClass)
+                return ((IModelClass)modelClassViewModeDetailView).DefaultDetailView;
+            return null;
+        }
+    }
+    public class EditModeDetailViewController : ViewController<XpandDetailView> {
+        protected override void OnViewControllersActivated() {
+            base.OnViewControllersActivated();
+            if (View.ViewEditMode == ViewEditMode.View && IsDefaultModel()) {
+                View.SetInfo(((IModelDetailViewViewModeDetailView)View.Model).ViewModeDetailView);
+                Frame.SetView(View);
+            }
+        }
+
+        bool IsDefaultModel() {
+            ListView listView = Frame.GetController<LinkToListViewController>().Link.ListView;
+            if (listView != null)
+                return listView.DetailViewId == View.Model.Id;
+            return false;
+        }
+    }
+}
---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/ListViewViewModeDetailViewController.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.SystemModule;
+using Xpand.ExpressApp.Model;
+
+namespace Xpand.ExpressApp.SystemModule {
+    public class ListViewViewModeDetailViewController : ViewController<ListView> {
+        protected override void OnActivated() {
+            base.OnActivated();
+            Frame.GetController<ListViewProcessCurrentObjectController>().CustomizeShowViewParameters += OnCustomizeShowViewParameters;
+        }
+        protected override void OnDeactivating() {
+            base.OnDeactivating();
+            Frame.GetController<ListViewProcessCurrentObjectController>().CustomizeShowViewParameters -= OnCustomizeShowViewParameters;
+        }
+        void OnCustomizeShowViewParameters(object sender, CustomizeShowViewParametersEventArgs customizeShowViewParametersEventArgs) {
+            var showViewParameters = customizeShowViewParametersEventArgs.ShowViewParameters;
+            var createdView = showViewParameters.CreatedView as DetailView;
+            if (createdView != null && createdView.ViewEditMode == ViewEditMode.View) {
+                var viewModeDetailView = ((IModelListViewViewModeDetailView)View.Model).ViewModeDetailView;
+                if (View.Model.DetailView != viewModeDetailView)
+                    createdView.SetInfo(viewModeDetailView);
+            }
+        }
+    }
+}
