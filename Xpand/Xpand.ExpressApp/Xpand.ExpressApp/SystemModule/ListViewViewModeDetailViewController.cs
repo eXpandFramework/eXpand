488bc58c3096457747938a6aee1aacfad94faa54
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-11 21:50:51 +0200

    added support for different detailview per view mode

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/ListViewViewModeDetailViewController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/ListViewViewModeDetailViewController.cs
@@ -1,7 +1,6 @@
 ﻿using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.SystemModule;
-using Xpand.ExpressApp.Model;
 
 namespace Xpand.ExpressApp.SystemModule {
     public class ListViewViewModeDetailViewController : ViewController<ListView> {
@@ -9,15 +8,15 @@ namespace Xpand.ExpressApp.SystemModule {
             base.OnActivated();
             Frame.GetController<ListViewProcessCurrentObjectController>().CustomizeShowViewParameters += OnCustomizeShowViewParameters;
         }
-        protected override void OnDeactivating() {
-            base.OnDeactivating();
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
             Frame.GetController<ListViewProcessCurrentObjectController>().CustomizeShowViewParameters -= OnCustomizeShowViewParameters;
         }
         void OnCustomizeShowViewParameters(object sender, CustomizeShowViewParametersEventArgs customizeShowViewParametersEventArgs) {
             var showViewParameters = customizeShowViewParametersEventArgs.ShowViewParameters;
             var createdView = showViewParameters.CreatedView as DetailView;
             if (createdView != null && createdView.ViewEditMode == ViewEditMode.View) {
-                var viewModeDetailView = ((IModelListViewViewModeDetailView)View.Model).ViewModeDetailView;
+                var viewModeDetailView = ((IModelDetailViewViewModeDetailView)View.Model).ViewModeDetailView;
                 if (View.Model.DetailView != viewModeDetailView)
                     createdView.SetInfo(viewModeDetailView);
             }
