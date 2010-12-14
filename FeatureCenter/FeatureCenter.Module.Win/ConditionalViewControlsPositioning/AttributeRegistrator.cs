c777f3a5d47487522a11f8ac87b7e602a91b6a67
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-14 17:19:23 +0200

    FC move ContitionalControlsPositioning to Win platform

---
+++ FeatureCenter/FeatureCenter.Module.Win/ConditionalViewControlsPositioning/AttributeRegistrator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.DC;
+using Xpand.ExpressApp.AdditionalViewControlsProvider.Logic;
+using Xpand.ExpressApp.Attributes;
+
+namespace FeatureCenter.Module.Win.ConditionalViewControlsPositioning {
+    public class AttributeRegistrator : Xpand.ExpressApp.Core.AttributeRegistrator {
+        public override IEnumerable<Attribute> GetAttributes(ITypeInfo typesInfo) {
+            if (typesInfo.Type != typeof(Customer)) yield break;
+            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.ViewMessage + " " + Module.Captions.HeaderConditionalViewControlsPositioning, "1=1",
+                "1=1", Module.Captions.ViewMessageConditionalViewControlsPositioning, Position.Bottom) { View = "ConditionalViewControlsPositioning_DetailView" };
+            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.Header + " " + Module.Captions.HeaderConditionalViewControlsPositioning, "1=1", "1=1",
+                Module.Captions.HeaderConditionalViewControlsPositioning, Position.Top) { View = "ConditionalViewControlsPositioning_DetailView" };
+            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.ConditionalViewControlsPositioningForCustomerName, "1=1", "1=1", null, Position.DetailViewItem) { MessageProperty = "NameWarning", View = "ConditionalViewControlsPositioning_DetailView" };
+            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.ConditionalViewControlsPositioningForCustomerCity, "1=1", "1=1", null, Position.DetailViewItem) { MessageProperty = "CityWarning", View = "ConditionalViewControlsPositioning_DetailView" };
+            yield return new CloneViewAttribute(CloneViewType.DetailView, "ConditionalViewControlsPositioning_DetailView");
+            yield return new XpandNavigationItemAttribute("Additional View Controls/Conditional View Controls Positioning",
+                "ConditionalViewControlsPositioning_DetailView");
+            yield return new DisplayFeatureModelAttribute("ConditionalViewControlsPositioning_DetailView");
+        }
+    }
+}
