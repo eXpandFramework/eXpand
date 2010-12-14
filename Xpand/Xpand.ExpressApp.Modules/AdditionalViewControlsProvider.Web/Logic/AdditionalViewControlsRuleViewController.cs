eccf123ed251fb695213369a85d454b12f22e809
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-14 17:21:56 +0200

    FC- support conditionalcontroland message

--- Xpand/Xpand.ExpressApp.Modules/AdditionalViewControlsProvider.Web/Logic/AdditionalViewControlsRuleViewController.cs
+++ Xpand/Xpand.ExpressApp.Modules/AdditionalViewControlsProvider.Web/Logic/AdditionalViewControlsRuleViewController.cs
@@ -1,46 +1,40 @@
 ﻿using System.Web.UI;
-using System.Web.UI.WebControls;
-using DevExpress.ExpressApp.Templates;
-using DevExpress.ExpressApp.Web.Controls;
+using DevExpress.Web.ASPxCallbackPanel;
+using DevExpress.Web.ASPxGlobalEvents;
 using Xpand.ExpressApp.AdditionalViewControlsProvider.Logic;
 using Xpand.ExpressApp.Logic;
 
 namespace Xpand.ExpressApp.AdditionalViewControlsProvider.Web.Logic {
+    public interface ISupportAdditionalViewControls {
+        ASPxCallbackPanel TopPanel { get; }
+        ASPxCallbackPanel BottomPanel { get; }
+        ASPxGlobalEvents AsPxGlobalEvents { get; }
+    }
+
     public class AdditionalViewControlsRuleViewController : AdditionalViewControlsProvider.Logic.AdditionalViewControlsRuleViewController {
         protected override void AddControl(object control, object controls, LogicRuleInfo<IAdditionalViewControlsRule> info) {
-            var viewSiteControl = GetContainerControl((IViewSiteTemplate)Frame.Template, info.Rule) as ViewSiteControl;
-            if (viewSiteControl != null) {
-                //                if (info.Rule.Position!=Position.DetailViewItem) {
-                //                    var hintPanel = (HintPanel) control;
-                //                    hintPanel.Label.Text = "sdfsdfsdf";
-                //                    hintPanel.Label.Changed+=LabelOnChanged;
-                //                    if (!(viewSiteControl.Parent is Panel))
-                //                        throw new NotSupportedException("Your ViewSiteControl should be inside a panel. Modify default.aspx page");
-                //                    viewSiteControl.Control.Controls.Add((Control) control);
-                //                    XpandModuleBase.Control = viewSiteControl;
-                //                }
-
-                var parent = ((ViewSiteControl)GetContainerControl((IViewSiteTemplate)Frame.Template, info.Rule)).Parent;
-                var tableCell = (TableCell)parent;
-                var tableRow = ((TableRow)tableCell.Parent);
-                var row = new TableRow();
-                var tableRowCollection = ((Table)tableRow.Parent).Rows;
-                var cellCollection = row.Cells;
-                ((Control)control).Visible = true;
-                var cell = new TableCell();
-                cell.Controls.Add((Control)control);
-                cellCollection.Add(cell);
-                switch (info.Rule.Position) {
-                    case Position.Top: {
-                            tableRowCollection.AddAt(0, row);
-                            break;
-                        }
-                    default:
-                        tableRowCollection.Add(row);
-                        break;
-                }
+            var supportAdditionalViewControls = Frame.Template as ISupportAdditionalViewControls;
+            if (supportAdditionalViewControls!=null) {
+                var asPxCallbackPanel = supportAdditionalViewControls.TopPanel;
+                if (info.Rule.Position == Position.Bottom)
+                    asPxCallbackPanel = supportAdditionalViewControls.BottomPanel;
+                var control1 = ((Control)control);
+                control1.Visible = info.Active;
+                asPxCallbackPanel.Controls.Add(control1);
+                ModifyClientEvent(supportAdditionalViewControls.AsPxGlobalEvents, info);
             }
 
         }
+
+        void ModifyClientEvent(ASPxGlobalEvents asPxGlobalEvents, LogicRuleInfo<IAdditionalViewControlsRule> info) {
+            if (info.Active)
+                asPxGlobalEvents.ClientSideEvents.EndCallback =
+                    "function(s, e) { DXUpdateSplitterSize();DXMoveFooter(); if(s != TopCallBackPanel&&s != BottomCallBackPanel) {TopCallBackPanel.PerformCallback();BottomCallBackPanel.PerformCallback();};  }";
+            else {
+                asPxGlobalEvents.ClientSideEvents.EndCallback =
+                    "function(s, e) { DXUpdateSplitterSize();DXMoveFooter(); }";
+            }
+        }
+
     }
 }
