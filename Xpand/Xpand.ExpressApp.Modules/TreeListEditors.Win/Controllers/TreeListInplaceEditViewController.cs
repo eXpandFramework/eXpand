bcfabd7c403ddd1e0f07150b86af424bf6630134
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-21 01:53:25 +0200

    update TreeListInplaceEditViewController

--- Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Win/Controllers/TreeListInplaceEditViewController.cs
+++ Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Win/Controllers/TreeListInplaceEditViewController.cs
@@ -1,35 +1,57 @@
-﻿using System.ComponentModel;
+﻿using System;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.TreeListEditors.Win;
 using DevExpress.ExpressApp.Win.Controls;
-using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Win.Core;
+using DevExpress.Xpo;
 using DevExpress.XtraEditors.Repository;
 using DevExpress.XtraTreeList;
-using DevExpress.XtraTreeList.Columns;
 
 namespace Xpand.ExpressApp.TreeListEditors.Win.Controllers {
-    public class TreeListInplaceEditViewController : ViewController<XpandListView> {
-        void treeList_CellValueChanged(object sender, CellValueChangedEventArgs cellValueChangedEventArgs) {
-            ReflectionHelper.SetMemberValue(((ObjectTreeList) sender).FocusedObject,
-                                            cellValueChangedEventArgs.Column.FieldName, cellValueChangedEventArgs.Value);
-            ObjectSpace.CommitChanges();
-        }
-
-        void TreeListOnShowingEditor(object sender, CancelEventArgs cancelEventArgs) {
-            TreeListColumn treeListColumn = ((TreeList) sender).FocusedColumn;
-            cancelEventArgs.Cancel = !View.Model.Columns[treeListColumn.FieldName].AllowEdit;
-        }
-
+    public class TreeListInplaceEditViewController : ViewController<ListView> {
+        private ObjectTreeList treeList;
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
-            if ((View.Editor is TreeListEditor) && View.Model.AllowEdit) {
-                TreeList treeList = ((TreeListEditor) View.Editor).TreeList;
-                foreach (RepositoryItem ri in treeList.RepositoryItems) {
-                    ri.ReadOnly = false;
+            var treeListEditor = View.Editor as TreeListEditor;
+            if (treeListEditor != null) {
+                treeList = (ObjectTreeList)treeListEditor.TreeList;
+                if (View.Model.AllowEdit) {
+                    foreach (RepositoryItem ri in treeList.RepositoryItems)
+                        ri.ReadOnly = false;
+                    treeList.CellValueChanged += treeList_CellValueChanged;
+                    treeList.ShownEditor += treeList_ShownEditor;
+                    treeList.OptionsBehavior.Editable = true;
+                    treeList.OptionsBehavior.ImmediateEditor = false;
                 }
-                treeList.CellValueChanged += treeList_CellValueChanged;
-                treeList.ShowingEditor += TreeListOnShowingEditor;
-                treeList.OptionsBehavior.Editable = true;
+                treeList.OptionsView.EnableAppearanceEvenRow = true;
+                treeList.OptionsView.EnableAppearanceOddRow = true;
+            }
+        }
+        protected override void OnDeactivated() {
+            if (treeList != null) {
+                treeList.CellValueChanged -= treeList_CellValueChanged;
+                treeList.ShownEditor -= treeList_ShownEditor;
+            }
+            base.OnDeactivated();
+        }
+        private void treeList_ShownEditor(object sender, EventArgs e) {
+            var activeEditor = treeList.ActiveEditor as IGridInplaceEdit;
+            if (activeEditor != null && treeList.FocusedObject is IXPSimpleObject) {
+                activeEditor.GridEditingObject = treeList.FocusedObject;
+            }
+        }
+        private void treeList_CellValueChanged(object sender, CellValueChangedEventArgs e) {
+            object newValue = e.Value;
+            if (e.Value is IXPSimpleObject)
+                newValue = ObjectSpace.GetObject(e.Value);
+            object focusedObject = treeList.FocusedObject;
+            if (focusedObject != null) {
+                IMemberInfo focusedColumnMemberInfo =
+ObjectSpace.TypesInfo.FindTypeInfo(focusedObject.GetType()).FindMember(e.Column.FieldName);
+                if (focusedColumnMemberInfo != null)
+                    focusedColumnMemberInfo.SetValue(focusedObject, Convert.ChangeType(newValue,
+focusedColumnMemberInfo.MemberType));
             }
         }
     }
