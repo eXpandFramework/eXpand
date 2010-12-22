c24a17981c2db73a1d0af15612a80610df5e4960
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-22 14:55:09 +0200

    Collection vieweditmode

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/CollectionsEditModeController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/CollectionsEditModeController.cs
@@ -1,33 +1,38 @@
-﻿using System;
-using DevExpress.ExpressApp;
+﻿using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Web;
 using Xpand.ExpressApp.SystemModule;
 
 namespace Xpand.ExpressApp.Web.SystemModule {
     public class CollectionsEditModeController : ViewEditModeController{
-        ViewEditMode _collectionsEditMode;
-        View _view;
-
         protected override void OnFrameAssigned() {
             base.OnFrameAssigned();
             Frame.ViewChanging+=FrameOnViewChanging;
-            Frame.ViewChanged+=FrameOnViewChanged;
         }
 
-        void FrameOnViewChanged(object sender, ViewChangedEventArgs viewChangedEventArgs) {
-            if (((Frame) sender).View == _view) {
-                ((ShowViewStrategy)Application.ShowViewStrategy).CollectionsEditMode=_collectionsEditMode;
-                _view = null;
+
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            var detailView = View;
+            foreach (ListPropertyEditor listPropertyEditor in detailView.GetItems<ListPropertyEditor>()) {
+                var control = listPropertyEditor.Control as System.Web.UI.Control;
+                if (control != null) {
+                    control.Visible = GetViewEditMode(listPropertyEditor) || (detailView.ViewEditMode == ViewEditMode.View);
+                }
             }
+        }
+
+        bool GetViewEditMode(ListPropertyEditor listPropertyEditor) {
+            var modelPropertyEditor = ((IModelPropertyEditor)listPropertyEditor.Model);
+            var viewEditMode = ((IModelViewEditMode) modelPropertyEditor.View).ViewEditMode;
+            return viewEditMode.HasValue && viewEditMode.Value == ViewEditMode.Edit;
         }
 
         void FrameOnViewChanging(object sender, ViewChangingEventArgs viewChangingEventArgs) {
             var viewEditMode = ((IModelViewEditMode) viewChangingEventArgs.View.Model).ViewEditMode;
             if (viewEditMode.HasValue&&viewChangingEventArgs.View is ListView) {
                 var showViewStrategy = ((ShowViewStrategy)Application.ShowViewStrategy);
-                _collectionsEditMode = showViewStrategy.CollectionsEditMode;
-                _view = viewChangingEventArgs.View;
                 showViewStrategy.CollectionsEditMode = viewEditMode.Value;
             }
         }
