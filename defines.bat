b2e5e783c8a9ff0cf28dff26f3b474312367d13c
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-28 00:44:30 +0200

    added one more contrain to scripts as suggested by bvsimmons

--- defines.bat
+++ defines.bat
@@ -12,6 +12,6 @@ set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\gacutil.exe"
 set sdkDir=%ProgramFiles%\Microsoft SDKs\Windows\v7.0A
 IF NOT EXIST sdkDir set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin\gacutil.exe"
 IF NOT EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin" set gacutil="%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe"
-
+IF NOT EXIST "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\" set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\gacutil.exe"
 
 set msbuild="%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
