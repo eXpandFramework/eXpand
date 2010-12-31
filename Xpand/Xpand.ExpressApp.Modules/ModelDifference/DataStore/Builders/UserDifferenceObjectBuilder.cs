8bc570f19fee69e1fd38a835100f7ec87355caa8
apobekiaris <apostolis.bekiaris@gmail.com>
2010-12-31 10:36:33 +0200

    diffs intermediate table indexes

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/DataStore/Builders/UserDifferenceObjectBuilder.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/DataStore/Builders/UserDifferenceObjectBuilder.cs
@@ -4,19 +4,17 @@ using DevExpress.Persistent.Base.Security;
 using Xpand.ExpressApp.Core;
 using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 
-namespace Xpand.ExpressApp.ModelDifference.DataStore.Builders{
-    public class UserDifferenceObjectBuilder 
-    {
-        public static void SetUp(UserModelDifferenceObject userModelDifferenceObject)
-        {
+namespace Xpand.ExpressApp.ModelDifference.DataStore.Builders {
+    public class UserDifferenceObjectBuilder {
+        public static void SetUp(UserModelDifferenceObject userModelDifferenceObject) {
             userModelDifferenceObject.Name = string.Format("AutoCreated for {0} {1}", ((IAuthenticationStandardUser)SecuritySystem.CurrentUser).UserName, DateTime.Now);
         }
 
-        public static void CreateDynamicMembers(Type userType){
-            if (userType != null){
-                XafTypesInfo.Instance.CreateBothPartMembers(userType, typeof (UserModelDifferenceObject),
+        public static void CreateDynamicMembers(Type userType) {
+            if (userType != null) {
+                XafTypesInfo.Instance.CreateBothPartMembers(userType, typeof(UserModelDifferenceObject),
                                                             XafTypesInfo.XpoTypeInfoSource.XPDictionary, true,
-                                                            "UserUsers_UserModelDifferenceObjectUserModelDifferenceObjects", "UserModels", "Users");
+                                                            "UserUsers_UserModelDifferenceObjectUserModelDifferenceObjects", "UserModelDifferenceObjects", "Users");
             }
         }
     }
