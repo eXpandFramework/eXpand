a5ced4a17831a9a2aa7b2825aeb6beb70fe7f0a7
apobekiaris <apostolis.bekiaris@gmail.com>
2011-02-05 17:47:11 +0200

    pessimistic locking support for web

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/PessimisticLockingViewController.cs
@@ -0,0 +1,20 @@
+﻿
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Web.SystemModule;
+
+namespace Xpand.ExpressApp.Web.SystemModule {
+    public class PessimisticLockingViewController : ExpressApp.SystemModule.PessimisticLockingViewController {
+        protected override void SubscribeToEvents() {
+            base.SubscribeToEvents();
+            Frame.GetController<WebDetailViewController>().EditAction.Execute += EditActionOnExecute;
+        }
+
+        void EditActionOnExecute(object sender, SimpleActionExecuteEventArgs simpleActionExecuteEventArgs) {
+            UpdateViewAllowEditState();
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            Frame.GetController<WebDetailViewController>().EditAction.Execute -= EditActionOnExecute;
+        }
+    }
+}
