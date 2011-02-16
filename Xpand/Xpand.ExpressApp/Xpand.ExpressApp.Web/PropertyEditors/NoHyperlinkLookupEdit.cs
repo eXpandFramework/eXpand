610cdff80e7083e18992b10e3d696c04c1f9e9d8
apobekiaris <apostolis.bekiaris@gmail.com>
2011-02-16 16:49:24 +0200

    Added NoHyperlinkLookup web editor

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/PropertyEditors/NoHyperlinkLookupEdit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+using System.Web.UI.WebControls;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+
+namespace Xpand.ExpressApp.Web.PropertyEditors {
+    [PropertyEditor(typeof(object), false)]
+    public class NoHyperlinkLookupEdit : ASPxLookupPropertyEditor {
+        /// <summary>
+        /// Initializes a new instance of the NoHyperlinkLookupEdit class.
+        /// </summary>
+        public NoHyperlinkLookupEdit(Type objectType, IModelMemberViewItem info)
+            : base(objectType, info) {
+        }
+
+        protected override WebControl CreateViewModeControlCore() {
+            var result = new Label {CssClass = ViewModeControlCssClass, ID = Guid.NewGuid().ToString()};
+            return result;
+        }
+
+        protected override void ReadViewModeValueCore() {
+            if (InplaceViewModeEditor != null) {
+                ((Label) InplaceViewModeEditor).Text = HttpUtility.HtmlEncode(GetPropertyDisplayValue());
+            }
+        }
+    }
+}
