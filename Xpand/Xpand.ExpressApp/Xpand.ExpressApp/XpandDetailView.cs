127206d1c184e7e9d5329c1c1f26d47a37b3ab6c
apobekiaris <apostolis.bekiaris@gmail.com>
2011-02-22 15:03:54 +0200

    bug:readonly propertyeditors for sessionless persistent objects

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/XpandDetailView.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/XpandDetailView.cs
@@ -3,12 +3,23 @@ using System.Linq;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
+using Xpand.Persistent.Base.General;
 using Xpand.Persistent.Base.MemberLevelSecurity;
 
 namespace Xpand.ExpressApp {
     public class XpandDetailView : DetailView {
 
-
+        public override object CurrentObject {
+            get {
+                return base.CurrentObject;
+            }
+            set {
+                if (value!=null&& XafTypesInfo.Instance.FindTypeInfo(value.GetType()).FindAttribute<SessionLessPersistentAttribute>()!=null)
+                    ((XpandObjectSpace) ObjectSpace).ObjectAction = o => o;
+                base.CurrentObject = value;
+                ((XpandObjectSpace) ObjectSpace).ObjectAction = null;
+            }
+        }
         public XpandDetailView(IModelDetailView info, IObjectSpace objectSpace, object obj, XafApplication application, bool isRoot)
             : base(info, objectSpace, obj, application, isRoot) {
         }
