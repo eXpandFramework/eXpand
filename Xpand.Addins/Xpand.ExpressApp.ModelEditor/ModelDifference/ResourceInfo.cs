abce030814fd74a52a614150bf1dc01e63d75347
apobekiaris <apostolis.bekiaris@gmail.com>
2011-02-23 18:16:59 +0200

    remove Xpand.ExpressApp.Base reference from model editor

---
+++ Xpand.Addins/Xpand.ExpressApp.ModelEditor/ModelDifference/ResourceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Xpand.Persistent.Base.ModelDifference {
+    public class ResourceInfo {
+        public ResourceInfo(string name, string assemblyName) {
+            Name = name;
+            AssemblyName = assemblyName;
+        }
+
+        public string Name { get; set; }
+        public string AssemblyName { get; set; }
+        private readonly List<AspectInfo> _aspectInfos=new List<AspectInfo>();
+        public List<AspectInfo> AspectInfos
+        {
+            get { return _aspectInfos; }
+        }
+        
+    }
+}
