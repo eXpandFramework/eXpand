9560f02e9cadc8fe8fbe60152c16f4b4da9492ee
apobekiaris <apostolis.bekiaris@gmail.com>
2011-02-23 18:32:03 +0200

    added IFastManyToMany classes

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/HideNewActionInNestedListViewController.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using Xpand.Persistent.Base.General;
+
+namespace Xpand.ExpressApp.SystemModule {
+    public class HideNewActionInNestedListViewController : ViewController {
+        public HideNewActionInNestedListViewController() {
+            TargetViewType = ViewType.ListView;
+            TargetViewNesting = Nesting.Nested;
+            TargetObjectType = typeof(IFastManyToMany);
+        }
+        protected override void OnActivated() {
+            base.OnActivated();
+            if (!View.Id.EndsWith("LookupListView")) {
+                Frame.GetController<NewObjectViewController>().NewObjectAction.Active.SetItemValue("IFastManyToMany", false);
+            }
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            if (!View.Id.EndsWith("LookupListView")) {
+                Frame.GetController<NewObjectViewController>().NewObjectAction.Active.SetItemValue("IFastManyToMany", true);
+            }
+        }
+    }
+}
