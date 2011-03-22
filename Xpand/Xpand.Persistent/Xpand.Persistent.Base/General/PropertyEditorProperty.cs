e7f153b0153968201a85632f46d61485cb01f865
apobekiaris <apostolis.bekiaris@gmail.com>
2011-03-22 20:15:11 +0200

    added attribute to assign property editors based on an interface

---
+++ Xpand/Xpand.Persistent/Xpand.Persistent.Base/General/PropertyEditorProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xpand.Persistent.Base.General {
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false)]
+    public class PropertyEditorProperty:Attribute {
+        readonly string _propertyName;
+
+        public PropertyEditorProperty(string propertyName) {
+            _propertyName = propertyName;
+        }
+
+        public string PropertyName {
+            get { return _propertyName; }
+        }
+    }
+}
