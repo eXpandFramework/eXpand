fa11334c1a43e2f5e1936e9b3866a7ff5fc653df
apobekiaris <apostolis.bekiaris@gmail.com>
2011-03-22 20:13:09 +0200

    add extension method to IRole for calculating if permission exists

--- Xpand/Xpand.ExpressApp.Modules/Security/Core/SecuritySystemExtensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/Security/Core/SecuritySystemExtensions.cs
@@ -1,19 +1,25 @@
 ﻿using System.Security;
+using System.Security.Permissions;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.Base.Security;
+using Xpand.Utils.Helpers;
 
-namespace Xpand.ExpressApp.Security.Core
-{
-    public class SecuritySystemExtensions
-    {
-        public static bool IsGranted(IPermission permission, bool isGrantedForNonExistent)
-        {
+namespace Xpand.ExpressApp.Security.Core {
+    public static class SecuritySystemExtensions {
+        public static bool IsGranted(IPermission permission, bool isGrantedForNonExistent) {
             var securityComplex = ((SecurityBase)SecuritySystem.Instance);
             bool isGrantedForNonExistentPermission = securityComplex.IsGrantedForNonExistentPermission;
             securityComplex.IsGrantedForNonExistentPermission = isGrantedForNonExistent;
             bool granted = SecuritySystem.IsGranted(permission);
             securityComplex.IsGrantedForNonExistentPermission = isGrantedForNonExistentPermission;
             return granted;
+        }
+        public static bool IsGranted(this IRole role,IPermission permission) {
+            var permissionSet = new PermissionSet(PermissionState.None);
+            role.Permissions.Each(perm => permissionSet.AddPermission(perm));
+            var getPermission = permissionSet.GetPermission(typeof(ObjectAccessPermission));
+            return getPermission!=null && permission.IsSubsetOf(getPermission);
         }
     }
 }
