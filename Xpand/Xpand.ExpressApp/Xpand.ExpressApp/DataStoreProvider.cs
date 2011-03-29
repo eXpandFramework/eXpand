c85ce6cb1490aea582922fae16721d39eb4956d9
apobekiaris <apostolis.bekiaris@gmail.com>
2011-03-30 01:31:07 +0300

    code formatting

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/DataStoreProvider.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/DataStoreProvider.cs
@@ -3,39 +3,30 @@ using DevExpress.Xpo.DB;
 using DevExpress.Xpo.Metadata;
 using Xpand.Xpo.DB;
 
-namespace Xpand.ExpressApp
-{
-    public class DataStoreProvider :  IXpoDataStoreProxy
-    {
+namespace Xpand.ExpressApp {
+    public class DataStoreProvider : IXpoDataStoreProxy {
         private readonly SqlDataStoreProxy proxyCore;
         private readonly string connectionStringCore;
-        public DataStoreProvider(string connectionString)
-        {
+        public DataStoreProvider(string connectionString) {
             connectionStringCore = connectionString;
             proxyCore = new SqlDataStoreProxy(connectionString);
         }
-        public string ConnectionString
-        {
+        public string ConnectionString {
             get { return connectionStringCore; }
         }
-        public virtual IDataStore CreateUpdatingStore(out IDisposable[] disposableObjects)
-        {
+        public virtual IDataStore CreateUpdatingStore(out IDisposable[] disposableObjects) {
             disposableObjects = null;
             return Proxy;
         }
-        public IDataStore CreateWorkingStore(out IDisposable[] disposableObjects)
-        {
+        public IDataStore CreateWorkingStore(out IDisposable[] disposableObjects) {
             disposableObjects = null;
             return Proxy;
         }
-        public XPDictionary XPDictionary
-        {
+        public XPDictionary XPDictionary {
             get { return null; }
         }
-        public virtual SqlDataStoreProxy Proxy
-        {
-            get
-            {
+        public virtual SqlDataStoreProxy Proxy {
+            get {
                 return proxyCore;
             }
         }
