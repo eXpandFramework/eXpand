16a3fbcd59d100fdf3e0a38dff38f3389a029054
noxe <mp@prakom.net>
2011-04-28 21:28:05 +0200

    removed Logoffcontroller and moved code to module / loggedoff

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/ModelDifferenceBaseModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/ModelDifferenceBaseModule.cs
@@ -15,20 +15,40 @@ namespace Xpand.ExpressApp.ModelDifference {
         }
         public override void Setup(ApplicationModulesManager moduleManager) {
             base.Setup(moduleManager);
-            if (Application != null)
+            if (Application != null) {
                 Application.LoggingOn += (sender, args) => {
                     if (ModelsLoaded.HasValue)
                         LoadModels(!ModelsLoaded.Value);
                     ModelsLoaded = true;
                 };
+                Application.LoggedOff += Application_LoggedOff;
+                Application.Disposed += Application_Disposed;
+            }
+        }
+
+        void Application_Disposed(object sender, EventArgs e) {
+            ((XafApplication)sender).Disposed -= Application_Disposed;
+            ((XafApplication)sender).LoggedOff -= Application_LoggedOff;
+        }
+
+        void Application_LoggedOff(object sender, EventArgs e) {
+            var modelApplicationBase = ((ModelApplicationBase)((XafApplication)sender).Model);
+            var lastLayer = modelApplicationBase.LastLayer;
+            while (lastLayer.Id != "Unchanged Master Part") {
+                modelApplicationBase.RemoveLayer(lastLayer);
+                lastLayer = modelApplicationBase.LastLayer;
+            }
+            var afterSetupLayer = modelApplicationBase.CreatorInstance.CreateModelApplication();
+            afterSetupLayer.Id = "After Setup";
+            modelApplicationBase.AddLayer(afterSetupLayer);
         }
 
         public void LoadModels(bool loadResources) {
-            ((ModelApplicationBase) Application.Model).ReInitLayers();
+            ((ModelApplicationBase)Application.Model).ReInitLayers();
             var customModelDifferenceStoreEventArgs = new CreateCustomModelDifferenceStoreEventArgs();
             OnCreateCustomModelDifferenceStore(customModelDifferenceStoreEventArgs);
             if (!customModelDifferenceStoreEventArgs.Handled)
-                new XpoModelDictionaryDifferenceStore(Application, GetPath(), customModelDifferenceStoreEventArgs.ExtraDiffStores,loadResources).Load((ModelApplicationBase)Application.Model);
+                new XpoModelDictionaryDifferenceStore(Application, GetPath(), customModelDifferenceStoreEventArgs.ExtraDiffStores, loadResources).Load((ModelApplicationBase)Application.Model);
         }
 
         public abstract string GetPath();
