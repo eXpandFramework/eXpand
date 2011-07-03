2836d20dc0101f6d083ee58e0f8f8a82c0800d03
apobekiaris <apostolis.bekiaris@gmail.com>
2011-07-03 12:17:45 +0300

    upgrade to 11.1.5

--- Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using Xpand;
 [assembly: AssemblyFileVersion(AssemblyInfo.Version)]
 namespace Xpand {
     public class AssemblyInfo {
-        public const string Version = "11.1.4.0";
+        public const string Version = "11.1.5.0";
 
 
     }
