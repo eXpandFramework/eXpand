002af2ad4b03b3d35c17ad47a1ceb9ed74ecf967
apobekiaris <apostolis.bekiaris@gmail.com>
2011-07-11 23:49:19 +0300

    code formating

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/CursorPositionController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/CursorPositionController.cs
@@ -6,34 +6,31 @@ using System.Linq;
 using DevExpress.ExpressApp.Model;
 using DevExpress.XtraEditors;
 
-namespace Xpand.ExpressApp.Win.SystemModule
-{
+namespace Xpand.ExpressApp.Win.SystemModule {
     public interface IModelMemberCursorPosition {
         [Category("eXpand")]
         [Description("Controls the position of the cursor after the control gets focused")]
-        CursorPosition CursorPosition { get; set; }    
+        CursorPosition CursorPosition { get; set; }
     }
     [ModelInterfaceImplementor(typeof(IModelMemberCursorPosition), "ModelMember")]
-    public interface IModelPropertyEditorCursorPosition : IModelMemberCursorPosition
-    {
+    public interface IModelPropertyEditorCursorPosition : IModelMemberCursorPosition {
     }
-    public class CursorPositionController : ViewController<DetailView>,IModelExtender{
+    public class CursorPositionController : ViewController<DetailView>, IModelExtender {
         public const string CursorPosition = "CursorPosition";
 
-        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders)
-        {
-            extenders.Add<IModelMember,IModelMemberCursorPosition>();
-            extenders.Add<IModelPropertyEditor,IModelPropertyEditorCursorPosition>();
+        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelMember, IModelMemberCursorPosition>();
+            extenders.Add<IModelPropertyEditor, IModelPropertyEditorCursorPosition>();
         }
 
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
             IEnumerable<PropertyEditor> textEdits = GetTextEdits();
 
-            foreach (var textEdit in textEdits){
-                CursorPosition cursorPosition = ((IModelPropertyEditorCursorPosition) textEdit.Model).CursorPosition;
-                var edit = ((TextEdit) textEdit.Control);
-                edit.GotFocus+=(sender, args) => {
+            foreach (var textEdit in textEdits) {
+                CursorPosition cursorPosition = ((IModelPropertyEditorCursorPosition)textEdit.Model).CursorPosition;
+                var edit = ((TextEdit)textEdit.Control);
+                edit.GotFocus += (sender, args) => {
                     var length = cursorPosition == SystemModule.CursorPosition.End ? (edit.EditValue + "").Length : 0;
                     edit.Select(length, length);
                 };
@@ -44,7 +41,7 @@ namespace Xpand.ExpressApp.Win.SystemModule
             return View.GetItems<PropertyEditor>().Where(
                 editor =>
                 editor.Control is TextEdit &&
-                ((IModelPropertyEditorCursorPosition) editor.Model).CursorPosition!=SystemModule.CursorPosition.Default);
+                ((IModelPropertyEditorCursorPosition)editor.Model).CursorPosition != SystemModule.CursorPosition.Default);
         }
     }
 
