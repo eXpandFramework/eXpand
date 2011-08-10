d7edbda5d2c02dcd4cdb951773481954c22a3fe0
apobekiaris <apostolis.bekiaris@gmail.com>
2011-08-10 16:30:07 +0300

    support aggregation in standalong filtercontrol

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/Editors/XpandFilterControl.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/Editors/XpandFilterControl.cs
@@ -3,37 +3,31 @@ using System.Windows.Forms;
 using DevExpress.XtraEditors;
 using DevExpress.XtraEditors.Filtering;
 
-namespace Xpand.ExpressApp.Win.Editors
-{
-    public class XpandFilterControl : DevExpress.XtraEditors.FilterControl
-    {
+namespace Xpand.ExpressApp.Win.Editors {
+    public class XpandFilterControl : FilterControl {
 
         public event Action<BaseEdit> EditorActivated;
 
-        protected void InvokeEditorActivated(BaseEdit baseEdit)
-        {
+        protected void InvokeEditorActivated(BaseEdit baseEdit) {
             Action<BaseEdit> activated = EditorActivated;
             if (activated != null && baseEdit != null) activated(baseEdit);
         }
 
 
-        protected override void OnFocusedElementChanged()
-        {
+        protected override void OnFocusedElementChanged() {
             base.OnFocusedElementChanged();
             InvokeEditorActivated(ActiveEditor);
         }
 
 
 
-        protected override void ShowElementMenu(ElementType type)
-        {
+        protected override void ShowElementMenu(ElementType type) {
             base.ShowElementMenu(type);
             InvokeEditorActivated(ActiveEditor);
 
         }
 
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
+        protected override void OnKeyDown(KeyEventArgs e) {
             base.OnKeyDown(e);
             InvokeEditorActivated(ActiveEditor);
 
