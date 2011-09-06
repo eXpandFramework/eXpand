97f9aff46e549b31edc408ca3dd3d8a9f98d5c2c
apobekiaris <apostolis.bekiaris@gmail.com>
2011-09-06 05:26:42 +0300

    populatecontroller should not dirty the model

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/PopulateController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/PopulateController.cs
@@ -4,34 +4,55 @@ using System.Linq.Expressions;
 using System.Reflection;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Model.Core;
 using Xpand.Utils.Helpers;
 
 namespace Xpand.ExpressApp.SystemModule {
     public abstract class PopulateController<T> : ViewController<ObjectView> {
-        private IModelMember modelMember;
+        string _oldPredefinedValues;
 
         protected PopulateController() {
             TargetObjectType = typeof(T);
         }
         protected override void OnActivated() {
             base.OnActivated();
-            Populate();
+            Populate(member => {
+                _oldPredefinedValues = member.PredefinedValues;
+                return GetPredefinedValues(member);
+            });
         }
         protected override void OnDeactivated() {
             base.OnDeactivated();
-            if (modelMember != null) {
-                modelMember.PredefinedValues = string.Empty;
+            Populate(member => _oldPredefinedValues);
+        }
+        protected virtual void Populate(Func<IModelMember, string> collect) {
+            var name = PropertyName;
+            var model = ((ModelApplicationBase)Application.Model);
+            ModelApplicationBase lastLayer = model.LastLayer;
+            model.RemoveLayer(lastLayer);
+            if (name != null) {
+                IModelMember modelMember = View.Model.ModelClass.AllMembers.Where(wrapper => wrapper.Name == name).FirstOrDefault();
+                if (modelMember != null) {
+                    string invoke = collect.Invoke(modelMember);
+                    modelMember.PredefinedValues = invoke;
+                }
+                model.AddLayer(lastLayer);
             }
         }
-        protected virtual void Populate() {
-            var lambdaExpression = GetPropertyName();
+
+
+        protected string PropertyName {
+            get {
+                var lambdaExpression = GetPropertyName();
+                return GetPropertyName(lambdaExpression);
+            }
+        }
+
+        string GetPropertyName(Expression<Func<T, object>> lambdaExpression) {
             var propertyInfo = ReflectionExtensions.GetExpression(lambdaExpression) as PropertyInfo;
-            if (propertyInfo != null)
-                modelMember = (View.Model.ModelClass.AllMembers.Where(wrapper => wrapper.Name == propertyInfo.Name)).FirstOrDefault();
-            if (modelMember != null) {
-                modelMember.PredefinedValues = GetPredefinedValues(modelMember);
-            }
+            return propertyInfo != null ? propertyInfo.Name : null;
         }
+
 
         protected abstract string GetPredefinedValues(IModelMember wrapper);
 
--- Xpand/Xpand.Utils/Helpers/EnumerableExtensions.cs
+++ Xpand/Xpand.Utils/Helpers/EnumerableExtensions.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Xpand.Utils.Helpers {
     public static class EnumerableExtensions {
+        public static string AggregateWith(this IEnumerable<string> instance, string accumulator) {
+            return instance.Aggregate("", (current, result) => current + (result + accumulator)).TrimEnd(accumulator.ToCharArray());
+        }
+
         public static void Each<T>(this IEnumerable<T> instance, Action<T> action) {
             foreach (T item in instance) {
                 action(item);
