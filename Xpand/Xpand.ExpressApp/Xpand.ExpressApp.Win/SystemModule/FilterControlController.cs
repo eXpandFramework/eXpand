838d492ff0b37d7d8e7b0739160866f97a74b041
noxe <mp@prakom.net>
2011-11-30 13:34:06 +0100

    fixed NullReference with delayeditems initialization

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/FilterControlController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/FilterControlController.cs
@@ -1,9 +1,9 @@
+using System;
 using System.ComponentModel;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Win.Editors;
 using DevExpress.XtraEditors;
-using Xpand.Utils.Helpers;
 
 namespace Xpand.ExpressApp.Win.SystemModule {
     public interface IModelMemberFilterControl : IModelNode {
@@ -23,7 +23,27 @@ namespace Xpand.ExpressApp.Win.SystemModule {
         }
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
-            View.GetItems<CriteriaPropertyEditor>().Each(editor => editor.Control.ViewMode = ((IModelPropertyEditorFilterControl)editor.Model).FilterEditorViewMode);
+            foreach (var editor in View.GetItems<CriteriaPropertyEditor>()) {
+                if (editor.Control != null)
+                    this.SetViewMode(editor); 
+                else
+                    editor.ControlCreated += Editor_ControlCreated;
+            }
+        }
+
+        private void Editor_ControlCreated(object sender, EventArgs e) {
+            this.SetViewMode((CriteriaPropertyEditor)sender);
+        }
+
+        private void SetViewMode(CriteriaPropertyEditor editor) {
+            editor.Control.ViewMode = ((IModelPropertyEditorFilterControl)editor.Model).FilterEditorViewMode;
+        }
+
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            foreach (var editor in View.GetItems<CriteriaPropertyEditor>()) {
+                editor.ControlCreated -= this.Editor_ControlCreated;
+            }
         }
     }
 }
