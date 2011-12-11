997f5e9e3700eee662d285c4480bd7cb150f46a4
apobekiaris <apostolis.bekiaris@gmail.com>
2011-12-11 19:56:22 +0200

    BaseImpl-->Updater support the new security system

--- Xpand/Xpand.Persistent/Xpand.Persistent.BaseImpl/Updater.cs
+++ Xpand/Xpand.Persistent/Xpand.Persistent.BaseImpl/Updater.cs
@@ -6,10 +6,12 @@ using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
 using DevExpress.ExpressApp.Updating;
 using DevExpress.Persistent.Base.Security;
+using Xpand.ExpressApp.Security;
+using Xpand.ExpressApp.Security.Core;
+using System.Linq;
 
 namespace Xpand.Persistent.BaseImpl {
     public abstract class Updater : ModuleUpdater {
-        public const string Administrators = "Administrators";
         public const string UserRole = "UserRole";
         public const string Admin = "Admin";
 
@@ -18,65 +20,145 @@ namespace Xpand.Persistent.BaseImpl {
         }
 
 
-        public virtual List<IPermission> GetPermissions(ICustomizableRole role) {
-            var permissions = new List<IPermission>();
-            if (role.Name != Administrators) {
-                permissions.Add(new ObjectAccessPermission(((ISecurityComplex)SecuritySystem.Instance).RoleType, ObjectAccess.AllAccess, ObjectAccessModifier.Deny));
+        public virtual List<object> GetPermissions(object role) {
+            return !IsNewSecuritySystem ? GetPermissions((ICustomizableRole)role, new List<object>()) : GetPermissions(((SecurityRole)role), new List<object>());
+        }
+
+        private List<object> GetPermissions(ICustomizableRole role, List<object> permissions) {
+            if (role.Name != SecurityStrategy.AdministratorRoleName) {
+                permissions.Add(new ObjectAccessPermission(RoleType, ObjectAccess.AllAccess, ObjectAccessModifier.Deny));
             } else {
                 permissions.Add(new EditModelPermission(ModelAccessModifier.Allow));
             }
             return permissions;
         }
 
-        protected virtual bool InitializeSecurity() {
-            ICustomizableRole admins = EnsureRoleExists(Administrators, GetPermissions);
-            EnsureUserExists(Admin, "Administrator", admins);
-            if (!ObjectSpace.IsNewObject(admins))
-                return false;
-            ICustomizableRole userRole = EnsureRoleExists(UserRole, GetPermissions);
-            EnsureUserExists("user", "user", userRole);
-            ObjectSpace.CommitChanges();
-            return true;
+        public virtual Type RoleType {
+            get { return ((ISecurityComplex)SecuritySystem.Instance).RoleType; }
         }
 
 
-        public virtual IUserWithRoles EnsureUserExists(string userName, string firstName, ICustomizableRole role, Type userType) {
-            var type = userType;
-            var user = (IUserWithRoles)((ObjectSpace)ObjectSpace).Session.FindObject(type, new BinaryOperator("UserName", userName));
+        protected virtual bool InitializeSecurity() {
+            EnsureAnonymousUser();
+            var admins = EnsureRoleExists(SecurityStrategy.AdministratorRoleName, GetPermissions);
+            EnsureUserExists(Admin, "Administrator", admins);
+            if (!ObjectSpace.IsNewObject(admins))
+                return false;
+            var userRole = EnsureRoleExists(UserRole, GetPermissions);
+            EnsureUserExists("user", "user", userRole);
+            ObjectSpace.CommitChanges();
+            return true;
+
+        }
+
+        public void EnsureAnonymousUser() {
+            if (IsNewSecuritySystem) {
+                var anonymousUser = ObjectSpace.FindObject<SecurityUser>(
+                    new BinaryOperator("UserName", SecurityStrategy.AnonymousUserName));
+                if (anonymousUser == null) {
+                    anonymousUser = ObjectSpace.CreateObject<SecurityUser>();
+                    anonymousUser.UserName = SecurityStrategy.AnonymousUserName;
+                    anonymousUser.IsActive = true;
+                    anonymousUser.SetPassword("");
+                    anonymousUser.Save();
+                }
+            }
+        }
+
+
+        public virtual object EnsureUserExists(string userName, string firstName, object role, Type userType) {
+            return IsNewSecuritySystem ? EnsureUserExists(userName, role) : EnsureUserExists(userName, role, userType);
+        }
+
+        private object EnsureUserExists(string userName, object role, Type userType) {
+            var user =
+                (IUserWithRoles)
+                ((ObjectSpace)ObjectSpace).Session.FindObject(userType, new BinaryOperator("UserName", userName));
             if (user == null) {
-                user = (IUserWithRoles)ObjectSpace.CreateObject(type);
-                var typeInfo = XafTypesInfo.Instance.FindTypeInfo(type);
+                user = (IUserWithRoles)ObjectSpace.CreateObject(userType);
+                var typeInfo = XafTypesInfo.Instance.FindTypeInfo(userType);
                 typeInfo.FindMember("UserName").SetValue(user, userName);
                 var memberInfo = typeInfo.FindMember("FirstName");
                 if (memberInfo != null) memberInfo.SetValue(user, userName);
-                user.Roles.Add(role);
+                user.Roles.Add((IRole)role);
             }
             return user;
         }
 
-        public virtual IUserWithRoles EnsureUserExists(string userName, string firstName, ICustomizableRole role) {
-            return EnsureUserExists(userName, firstName, role, SecuritySystem.UserType);
+        private object EnsureUserExists(string userName, object role) {
+            var securityUser = ObjectSpace.FindObject<SecurityUser>(new BinaryOperator("UserName", userName));
+            if (securityUser == null) {
+                var strategyRole = (SecurityStrategyRole)role;
+                securityUser = ObjectSpace.CreateObject<SecurityUser>();
+                securityUser.UserName = userName;
+                securityUser.IsActive = true;
+                securityUser.SetPassword("");
+                securityUser.Roles.Add(strategyRole);
+                securityUser.Save();
+            }
+            return securityUser;
         }
 
-        public virtual ICustomizableRole EnsureRoleExists(string roleName, Func<ICustomizableRole, List<IPermission>> permissionAddFunc) {
-            var role = (ICustomizableRole)((ObjectSpace)ObjectSpace).Session.FindObject(((ISecurityComplex)SecuritySystem.Instance).RoleType, new BinaryOperator("Name", roleName));
+        public virtual object EnsureUserExists(string userName, string firstName, object role) {
+            return EnsureUserExists(userName, firstName, role, UserType);
+        }
+
+        public virtual Type UserType {
+            get { return SecuritySystem.UserType; }
+        }
+
+        private List<object> GetPermissions(SecurityRole securityRole, List<object> permissions) {
+            if (securityRole.Name == SecurityStrategy.AdministratorRoleName) {
+                var modelPermission = ObjectSpace.CreateObject<ModelOperationPermissionData>();
+                modelPermission.Save();
+                securityRole.BeginUpdate();
+                securityRole.Permissions.GrantRecursive(typeof(object), SecurityOperations.Read);
+                securityRole.Permissions.GrantRecursive(typeof(object), SecurityOperations.Write);
+                securityRole.Permissions.GrantRecursive(typeof(object), SecurityOperations.Create);
+                securityRole.Permissions.GrantRecursive(typeof(object), SecurityOperations.Delete);
+                securityRole.Permissions.GrantRecursive(typeof(object), SecurityOperations.Navigate);
+                securityRole.EndUpdate();
+                permissions.Add(modelPermission);
+            }
+            return permissions;
+        }
+
+        public virtual object EnsureRoleExists(string roleName, Func<object, List<object>> permissionAddFunc) {
+            if (IsNewSecuritySystem) {
+                var securityRole = ObjectSpace.FindObject<SecurityRole>(new BinaryOperator("Name", SecurityStrategy.AdministratorRoleName));
+                return EnsureRoleExists(roleName, permissionAddFunc, securityRole);
+            }
+            var role = (ICustomizableRole)((ObjectSpace)ObjectSpace).Session.FindObject(RoleType, new BinaryOperator("Name", roleName));
+            return EnsureRoleExists(roleName, permissionAddFunc, role);
+        }
+
+        private object EnsureRoleExists(string roleName, Func<object, List<object>> permissionAddFunc, ICustomizableRole role) {
             if (role == null) {
-                role = (ICustomizableRole)ObjectSpace.CreateObject(((ISecurityComplex)SecuritySystem.Instance).RoleType);
+                role = (ICustomizableRole)ObjectSpace.CreateObject(RoleType);
                 role.Name = roleName;
                 role.AddPermission(new ObjectAccessPermission(typeof(object), ObjectAccess.AllAccess));
-                foreach (var permission in permissionAddFunc.Invoke(role)) {
+                foreach (var permission in permissionAddFunc.Invoke(role).OfType<IPermission>()) {
                     role.AddPermission(permission);
                 }
             }
-
             return role;
-
         }
 
+        private object EnsureRoleExists(string roleName, Func<object, List<object>> permissionAddFunc, SecurityRole securityRole) {
+            if (securityRole == null) {
+                securityRole = ObjectSpace.CreateObject<SecurityRole>();
+                securityRole.Name = roleName;
+                if (permissionAddFunc != null)
+                    foreach (var permission in permissionAddFunc.Invoke(securityRole).OfType<PermissionData>()) {
+                        securityRole.PersistentPermissions.Add(permission);
+                    }
+                securityRole.Save();
+            }
+            return securityRole;
+        }
 
-
-
-
-
+        public virtual bool IsNewSecuritySystem {
+            get { return ((ISecurityComplex)SecuritySystem.Instance).IsNewSecuritySystem(); }
+        }
     }
 }
