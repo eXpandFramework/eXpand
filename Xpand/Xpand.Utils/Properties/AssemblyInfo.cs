899ed6692ee7cf9a664ff45778bc65956799c955
apobekiaris <apostolis.bekiaris@gmail.com>
2011-12-27 20:30:42 +0200

    upgrade to 11.2.7

--- Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using Xpand;
 [assembly: AllowPartiallyTrustedCallers]
 namespace Xpand {
     public class AssemblyInfo {
-        public const string Version = "11.2.6.0";
+        public const string Version = "11.2.7.0";
 
 
     }
