cf00f495dc0cc5cbb722c92b7ccd393b3676d1dc
noxe <mp@prakom.net>
2011-12-28 10:55:10 +0100

    new custom member type, which allows to specify an callback which is called in the getter of the member

---
+++ Xpand/Xpand.Xpo/MetaData/XpandCodeMemberInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+
+namespace Xpand.Xpo.MetaData {
+    public class XpandCodeMemberInfo : XpandCustomMemberInfo {
+        readonly Func<XPBaseObject, object> _callBack;
+
+        public XpandCodeMemberInfo(XPClassInfo owner, string propertyName, Type propertyType, XPClassInfo referenceType, bool nonPersistent, bool nonPublic, Func<XPBaseObject, object> callBack)
+            : base(owner, propertyName, propertyType, referenceType, nonPersistent, nonPublic, true) {
+                _callBack = callBack;
+        }
+
+        public override object GetValue(object theObject) {
+            var xpBaseObject = ((XPBaseObject)theObject);
+            return !xpBaseObject.Session.IsObjectsLoading && !xpBaseObject.Session.IsObjectsSaving
+                       ? _callBack(xpBaseObject)
+                       : base.GetValue(theObject);
+        }
+
+        protected override bool CanPersist {
+            get { return false; }
+        }
+    }
+}
