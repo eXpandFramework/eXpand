73a55d9967d231bbd1aa70afb516901e0777c9a0
noxe <mp@prakom.net>
2011-12-28 10:54:37 +0100

    new Controller to validate objects edited in an child listview

---
+++ Xpand/Xpand.ExpressApp.Modules/MasterDetail.Win/MasterDetailValidationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Validation;
+using Xpand.ExpressApp.MasterDetail.Logic;
+using Xpand.ExpressApp.Win.ListEditors;
+
+namespace Xpand.ExpressApp.MasterDetail.Win {
+    public class MasterDetailValidationController : ViewController<ListView> {
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            if (IsMasterDetail) {
+                var validationController = this.Frame.GetController<PersistenceValidationController>();
+                if (validationController != null) {
+                    validationController.NeedToValidateObject += ValidationController_NeedToValidateObject;
+                }
+            }
+        }
+
+        protected override void OnDeactivated() {
+            if (GridListEditor != null && GridListEditor.GridView != null && GridListEditor.GridView.MasterFrame != null) {
+                var validationController = this.Frame.GetController<PersistenceValidationController>();
+                if (validationController != null) {
+                    validationController.NeedToValidateObject -= ValidationController_NeedToValidateObject;
+                }
+            }
+
+            base.OnDeactivated();
+        }
+
+        private void ValidationController_NeedToValidateObject(object sender, DevExpress.ExpressApp.Validation.NeedToValidateObjectEventArgs e) {
+            e.NeedToValidate = (!ObjectSpace.IsDeletedObject(e.CurrentObject) && !ObjectSpace.IsObjectToDelete(e.CurrentObject));
+        }
+
+        XpandGridListEditor GridListEditor {
+            get { return View != null ? View.Editor as XpandGridListEditor : null; }
+        }
+
+        public bool IsMasterDetail {
+            get {
+                return GridListEditor != null && Frame.GetController<MasterDetailRuleController>().MasterDetailRules.FirstOrDefault() != null;
+            }
+        }
+    }
+}
