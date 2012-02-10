3bda0ed17e7427f88b9e8b19be45a1c74d13cf3e
apobekiaris <apostolis.bekiaris@gmail.com>
2012-02-10 21:39:30 +0200

    Exception thrown when combining user model with app diff on logoff
    http://www.expandframework.com/forum/6-bugs/1917-exception-thrown-when-combining-user-model-with-app-diff-on-logoff.html#1917

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/DictionaryStores/XpoUserModelDictionaryDifferenceStore.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/DictionaryStores/XpoUserModelDictionaryDifferenceStore.cs
@@ -111,7 +111,7 @@ namespace Xpand.ExpressApp.ModelDifference.DictionaryStores {
 
         private IEnumerable<string> GetNames() {
             return ((ISecurityComplex)SecuritySystem.Instance).IsNewSecuritySystem()
-                       ? ((ISecurityUserWithRoles)SecuritySystem.CurrentUser).GetPermissions().OfType<ModelCombinePermission>().Select(permission => permission.Difference)
+                       ? ((ISecurityUserWithRoles)SecuritySystem.CurrentUser).GetPermissions().OfType<Security.Improved.ModelCombinePermission>().Select(permission => permission.Difference)
                        : ((IUser)SecuritySystem.CurrentUser).Permissions.OfType<ModelCombinePermission>().Select(permission => permission.Difference);
         }
 
