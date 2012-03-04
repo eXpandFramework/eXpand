bd79298b70a4127b0833250ee2ceeedf76ff0331
apobekiaris <apostolis.bekiaris@gmail.com>
2012-03-04 05:05:49 +0200

    Reload Sequence objects after objectspace commit.Useful in MiddleTier because the client is not aware of the sequance assigment from the server
    http://www.expandframework.com/forum/6-bugs/1845-use-of-sequencegenerator-with-middletier-fails-to-get-a-working-datalayer.html?limit=6&start=12#1939

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/ReloadObjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using Xpand.Persistent.Base.General;
+
+namespace Xpand.ExpressApp.SystemModule {
+    public interface IModelOptionsReloadSequenceObject {
+        [Category("Xpand")]
+        [Description("Reload Sequence objects after objectspace commit.Useful in MiddleTier because the client is not aware of the sequance assigment from the server")]
+        bool ReloadSequenceObject { get; set; }
+    }
+    public class ReloadObjectController : ViewController, IModelExtender {
+        public ReloadObjectController() {
+            TargetObjectType = typeof(ISupportSequenceObject);
+        }
+
+        protected override void OnActivated() {
+            base.OnActivated();
+            if (((IModelOptionsReloadSequenceObject)Application.Model.Options).ReloadSequenceObject)
+                ObjectSpace.Committed += ObjectSpaceOnCommitted;
+        }
+
+        void ObjectSpaceOnCommitted(object sender, EventArgs eventArgs) {
+            ObjectSpace.Refresh();
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            if (((IModelOptionsReloadSequenceObject)Application.Model.Options).ReloadSequenceObject)
+                ObjectSpace.Committed -= ObjectSpaceOnCommitted;
+        }
+
+        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelOptions, IModelOptionsReloadSequenceObject>();
+        }
+    }
+}
