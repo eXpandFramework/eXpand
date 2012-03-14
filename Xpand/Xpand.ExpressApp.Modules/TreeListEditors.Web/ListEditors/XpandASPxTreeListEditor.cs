b687e0eef5603c47897dc620c682e8987bdec8c0
jstrehan <jaspreet.punjab@gmail.com>
2012-03-15 04:23:18 +0530

    Update Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Web/ListEditors/XpandASPxTreeListEditor.cs

--- Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Web/ListEditors/XpandASPxTreeListEditor.cs
+++ Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Web/ListEditors/XpandASPxTreeListEditor.cs
@@ -3,8 +3,11 @@ using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.TreeListEditors.Web;
 using Xpand.ExpressApp.TreeListEditors.Web.Core;
 using Xpand.ExpressApp.TreeListEditors.Win.Core;
+using DevExpress.Persistent.Base.General;
+using DevExpress.ExpressApp.Editors;
 
 namespace Xpand.ExpressApp.TreeListEditors.Web.ListEditors {
+    [ListEditor(typeof(ITreeNode),true)]
     public class XpandASPxTreeListEditor : ASPxTreeListEditor,ITreeListEditor {
         public XpandASPxTreeListEditor(IModelListView model) : base(model) {
         }
