a27244f5ac11fe07c1d09209a9e349431ba3a413
apobekiaris <apostolis.bekiaris@gmail.com>
2012-04-23 14:26:00 +0300

    Master Detail Controls Disappearing
    http://www.expandframework.com/forum/6-bugs/2160-master-detail-controls-disappearing.html#2160

--- Xpand/Xpand.ExpressApp.Modules/MasterDetail/Logic/MasterDetailRuleController.cs
+++ Xpand/Xpand.ExpressApp.Modules/MasterDetail/Logic/MasterDetailRuleController.cs
@@ -11,21 +11,10 @@ namespace Xpand.ExpressApp.MasterDetail.Logic {
     public class MasterDetailRuleController : ConditionalLogicRuleViewController<IMasterDetailRule> {
         readonly List<IMasterDetailRule> _masterDetailRules = new List<IMasterDetailRule>();
 
-        protected override void OnDeactivated() {
-            base.OnDeactivated();
-            _masterDetailRules.Clear();
-        }
-
         protected override IModelLogic GetModelLogic() {
             return ((IModelApplicationMasterDetail)Application.Model).MasterDetail;
         }
 
-        protected override IEnumerable<LogicRuleInfo<IMasterDetailRule>> GetContextValidLogicRuleInfos(View view, IEnumerable<IMasterDetailRule> modelLogicRules, object currentObject, ExecutionContext executionContext, bool invertCustomization, ActionBase action) {
-            var contextValidLogicRuleInfos = base.GetContextValidLogicRuleInfos(view, modelLogicRules, currentObject, executionContext, invertCustomization, action);
-            if (contextValidLogicRuleInfos.Count() > 0)
-                _masterDetailRules.Clear();
-            return contextValidLogicRuleInfos;
-        }
 
         public override void ExecuteRule(LogicRuleInfo<IMasterDetailRule> info, ExecutionContext executionContext) {
 
