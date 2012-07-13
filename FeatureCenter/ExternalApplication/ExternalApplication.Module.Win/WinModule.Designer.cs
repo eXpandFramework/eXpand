39e967291acd744e4d72d822fc84762ba4db0be9
apobekiaris <apostolis.bekiaris@gmail.com>
2012-07-13 04:01:14 +0300

    ModelDifference-->Problems when loading modules that contain rules (for external application models)
        http://www.expandframework.com/forum/6-bugs/2327-121410-web-model-differences.html#2372

--- FeatureCenter/ExternalApplication/ExternalApplication.Module.Win/WinModule.Designer.cs
+++ FeatureCenter/ExternalApplication/ExternalApplication.Module.Win/WinModule.Designer.cs
@@ -1,14 +1,13 @@
 
+using DevExpress.ExpressApp.Kpi;
 using Xpand.ExpressApp.ModelDifference;
 using Xpand.ExpressApp.ModelDifference.Win;
 using Xpand.ExpressApp.SystemModule;
 using Xpand.ExpressApp.Win.SystemModule;
 using Xpand.ExpressApp.WorldCreator.Win;
 
-namespace ExternalApplication.Module.Win
-{
-    partial class ExternalApplicationWindowsFormsModule
-    {
+namespace ExternalApplication.Module.Win {
+    partial class ExternalApplicationWindowsFormsModule {
         /// <summary> 
         /// Required designer variable.
         /// </summary>
@@ -18,10 +17,8 @@ namespace ExternalApplication.Module.Win
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -33,12 +30,11 @@ namespace ExternalApplication.Module.Win
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             // 
             // ExternalApplicationWindowsFormsModule
             // 
-            
+
             this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
             this.RequiredModuleTypes.Add(typeof(ModelDifferenceModule));
             this.RequiredModuleTypes.Add(typeof(ModelDifferenceWindowsFormsModule));
@@ -46,6 +42,7 @@ namespace ExternalApplication.Module.Win
             this.RequiredModuleTypes.Add(typeof(XpandSystemWindowsFormsModule));
             this.RequiredModuleTypes.Add(typeof(Xpand.ExpressApp.Validation.XpandValidationModule));
             this.RequiredModuleTypes.Add(typeof(WorldCreatorWinModule));
+            this.RequiredModuleTypes.Add(typeof(KpiModule));
         }
 
         #endregion
