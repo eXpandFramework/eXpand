2e7319ba48eb1d85005c1c9dea5cd186c45bbada
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-02 03:19:34 +0300

    ModelDifference:removed HideMainBarActions as it is not needed anymore

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference.Win/Controllers/ModelEditorTemplateViewController.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference.Win/Controllers/ModelEditorTemplateViewController.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Actions;
 using DevExpress.ExpressApp.Win.Controls;
 using DevExpress.ExpressApp.Win.Templates;
-using DevExpress.ExpressApp.Win.Templates.ActionContainers;
 using DevExpress.XtraBars;
-using DevExpress.XtraBars.Ribbon;
 using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 using Xpand.ExpressApp.ModelDifference.Win.PropertyEditors;
 
@@ -18,60 +13,12 @@ namespace Xpand.ExpressApp.ModelDifference.Win.Controllers {
             TargetObjectType = typeof(ModelDifferenceObject);
         }
 
-        protected override void OnViewChanging(View view) {
-            base.OnViewChanging(view);
-
-            if (View != null) {
-                View.Closing -= View_Closing;
-            }
-
-            if (view is DetailView && typeof(ModelDifferenceObject).IsAssignableFrom(view.ObjectTypeInfo.Type)) {
-                view.Closing += View_Closing;
-            }
-        }
-
-        protected override void Dispose(bool disposing) {
-            try {
-                if (View != null) {
-                    View.Closing -= View_Closing;
-                }
-            } finally {
-                base.Dispose(disposing);
-            }
-        }
-
-        void View_Closing(object sender, EventArgs e) {
-            HideMainBarActions((DetailView)sender);
-        }
-
-        void HideMainBarActions(DetailView xpandDetailView) {
-            var modelEditorPropertyEditors = xpandDetailView.GetItems<ModelEditorPropertyEditor>();
-            if (modelEditorPropertyEditors.Count > 0) {
-                var modelEditorViewController = modelEditorPropertyEditors[0].ModelEditorViewModelEditorViewController;
-                FieldInfo fieldInfo = modelEditorViewController.GetType().GetField("mainBarActions", BindingFlags.Instance | BindingFlags.NonPublic);
-                if (fieldInfo != null) {
-                    var actions = (Dictionary<ActionBase, string>)fieldInfo.GetValue(modelEditorViewController);
-                    foreach (var actionBase in Frame.Template.DefaultContainer.Actions.Where(actions.Keys.Contains)) {
-                        actionBase.Active["Is Not ModelDiffs view"] = false;
-                    }
-                }
-            }
-        }
-
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
             var template = Frame.Template as XtraFormTemplateBase;
             if (template != null) {
                 SetTemplate();
-                if (template.FormStyle == RibbonFormStyle.Ribbon) {
-                    template.RibbonTransformer.Transformed += RibbonTransformer_Transformed;
-                }
             }
-        }
-
-        private void RibbonTransformer_Transformed(object sender, EventArgs e) {
-            ((ClassicToRibbonTransformer)sender).Transformed -= RibbonTransformer_Transformed;
-            SetTemplate();
         }
 
         private void SetTemplate() {
