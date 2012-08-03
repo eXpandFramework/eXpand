096663267d84b30379e6d66a3386ebc22cdda3c2
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-03 20:30:15 +0300

    IO: Bug: Fix exception raized when member is a persistent enumeration with null value

--- Xpand/Xpand.ExpressApp.Modules/IO/Core/ExportEngine.cs
+++ Xpand/Xpand.ExpressApp.Modules/IO/Core/ExportEngine.cs
@@ -14,13 +14,13 @@ using Xpand.Utils.Helpers;
 namespace Xpand.ExpressApp.IO.Core {
     public class ExportEngine {
         readonly Dictionary<ObjectInfo, object> exportedObjecs = new Dictionary<ObjectInfo, object>();
-        SerializeClassInfoGraphNodesCalculator _serializeClassInfoGraphNodesCalculator ;
+        SerializeClassInfoGraphNodesCalculator _serializeClassInfoGraphNodesCalculator;
 
-        public XDocument Export(IEnumerable<XPBaseObject> baseCollection, ISerializationConfigurationGroup serializationConfigurationGroup){
+        public XDocument Export(IEnumerable<XPBaseObject> baseCollection, ISerializationConfigurationGroup serializationConfigurationGroup) {
             var xDocument = new XDocument();
             var root = new XElement("SerializedObjects");
             xDocument.Add(root);
-            _serializeClassInfoGraphNodesCalculator =new SerializeClassInfoGraphNodesCalculator(serializationConfigurationGroup);    
+            _serializeClassInfoGraphNodesCalculator = new SerializeClassInfoGraphNodesCalculator(serializationConfigurationGroup);
             foreach (var baseObject in baseCollection) {
                 IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes =
                     _serializeClassInfoGraphNodesCalculator.GetSerializedClassInfoGraphNodes(baseObject);
@@ -32,13 +32,13 @@ namespace Xpand.ExpressApp.IO.Core {
 
 
         void ExportCore(XPBaseObject selectedObject, IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes, XElement root) {
-            var objectInfo = new ObjectInfo(selectedObject.GetType(),selectedObject.ClassInfo.KeyProperty.GetValue(selectedObject));
-            if (!(exportedObjecs.ContainsKey(objectInfo))){
+            var objectInfo = new ObjectInfo(selectedObject.GetType(), selectedObject.ClassInfo.KeyProperty.GetValue(selectedObject));
+            if (!(exportedObjecs.ContainsKey(objectInfo))) {
                 exportedObjecs.Add(objectInfo, null);
                 var serializedObjectElement = new XElement("SerializedObject");
                 serializedObjectElement.Add(new XAttribute("type", selectedObject.GetType().Name));
                 root.Add(serializedObjectElement);
-                foreach (var classInfoGraphNode in serializedClassInfoGraphNodes.Where(node => node.SerializationStrategy!=SerializationStrategy.DoNotSerialize)) {
+                foreach (var classInfoGraphNode in serializedClassInfoGraphNodes.Where(node => node.SerializationStrategy != SerializationStrategy.DoNotSerialize)) {
                     XElement propertyElement = GetPropertyElement(serializedObjectElement, classInfoGraphNode);
                     switch (classInfoGraphNode.NodeType) {
                         case NodeType.Simple:
@@ -57,7 +57,7 @@ namespace Xpand.ExpressApp.IO.Core {
 
         void SetMemberValue(XPBaseObject selectedObject, IClassInfoGraphNode classInfoGraphNode, XElement propertyElement) {
             var xpMemberInfo = selectedObject.ClassInfo.FindMember(classInfoGraphNode.Name);
-            if (xpMemberInfo != null ) {
+            if (xpMemberInfo != null) {
                 var memberValue = selectedObject.GetMemberValue(classInfoGraphNode.Name);
                 if (xpMemberInfo.Converter != null)
                     memberValue = (xpMemberInfo.Converter.ConvertToStorageType(memberValue));
@@ -67,7 +67,7 @@ namespace Xpand.ExpressApp.IO.Core {
                     memberValue = ((DateTime)memberValue).Ticks;
                 if (memberValue is string)
                     propertyElement.Add(new XCData(memberValue.ToString()));
-                else                {
+                else {
                     propertyElement.Value = GetInvariantValue(memberValue);
                 }
             }
@@ -75,8 +75,8 @@ namespace Xpand.ExpressApp.IO.Core {
 
         string GetInvariantValue(object memberValue) {
             double result;
-            var parse = Double.TryParse(memberValue+"",out result);
-            return parse ? ((IConvertible) memberValue).ToString(CultureInfo.InvariantCulture) : memberValue + "";
+            var parse = Double.TryParse(memberValue + "", out result);
+            return parse ? ((IConvertible)memberValue).ToString(CultureInfo.InvariantCulture) : memberValue + "";
         }
 
         XElement GetPropertyElement(XElement serializedObjectElement, IClassInfoGraphNode classInfoGraphNode) {
@@ -92,14 +92,15 @@ namespace Xpand.ExpressApp.IO.Core {
                                       XElement propertyElement) {
             XPMemberInfo memberInfo = selectedObject.ClassInfo.FindMember(classInfoGraphNode.Name);
             if (memberInfo != null) {
-                var theObjects = (IEnumerable)memberInfo.GetValue(selectedObject);
-                foreach (XPBaseObject theObject in theObjects) {
-                    CreateRefElelement(classInfoGraphNode,theObject.GetType().Name, root,  theObject, propertyElement);
-                }
+                var theObjects = memberInfo.GetValue(selectedObject) as IEnumerable;
+                if (theObjects != null)
+                    foreach (XPBaseObject theObject in theObjects) {
+                        CreateRefElelement(classInfoGraphNode, theObject.GetType().Name, root, theObject, propertyElement);
+                    }
             }
         }
 
-        void CreateRefElelement(IClassInfoGraphNode classInfoGraphNode, string typeName, XElement root,  XPBaseObject theObject, XElement propertyElement) {
+        void CreateRefElelement(IClassInfoGraphNode classInfoGraphNode, string typeName, XElement root, XPBaseObject theObject, XElement propertyElement) {
             var serializedObjectRefElement = new XElement("SerializedObjectRef");
             propertyElement.Add(serializedObjectRefElement);
             serializedObjectRefElement.Add(new XAttribute("type", typeName));
@@ -117,7 +118,7 @@ namespace Xpand.ExpressApp.IO.Core {
         void CreateRefKeyElements(IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes, XPBaseObject theObject, XElement serializedObjectRefElement) {
             foreach (var infoGraphNode in serializedClassInfoGraphNodes.Where(node => node.Key)) {
                 var serializedObjectRefKeyElement = new XElement("Key");
-                serializedObjectRefKeyElement.Add(new XAttribute("name",infoGraphNode.Name));
+                serializedObjectRefKeyElement.Add(new XAttribute("name", infoGraphNode.Name));
                 serializedObjectRefKeyElement.Value = theObject.GetMemberValue(infoGraphNode.Name).ToString();
                 serializedObjectRefElement.Add(serializedObjectRefKeyElement);
             }
@@ -126,8 +127,8 @@ namespace Xpand.ExpressApp.IO.Core {
         void CreateObjectProperty(XPBaseObject selectedObject, XElement propertyElement, IClassInfoGraphNode classInfoGraphNode, XElement root) {
             XPMemberInfo memberInfo = selectedObject.ClassInfo.FindMember(classInfoGraphNode.Name);
             if (memberInfo != null) {
-                var theObject = (XPBaseObject) memberInfo.GetValue(selectedObject);
-                CreateRefElelement(classInfoGraphNode,theObject!= null?theObject.GetType().Name:memberInfo.MemberType.Name, root,  theObject, propertyElement);
+                var theObject = (XPBaseObject)memberInfo.GetValue(selectedObject);
+                CreateRefElelement(classInfoGraphNode, theObject != null ? theObject.GetType().Name : memberInfo.MemberType.Name, root, theObject, propertyElement);
             }
         }
 
