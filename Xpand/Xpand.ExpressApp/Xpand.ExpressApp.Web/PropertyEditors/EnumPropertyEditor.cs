518897faf8e7bc11120cd52c3068db6e61e25c9c
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-06 14:33:59 +0300

    Core-->Include Enum propertyeditors with flag support as in
    http://www.devexpress.com/Support/Center/p/S30849.aspx

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/PropertyEditors/EnumPropertyEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Web.Editors;
+using DevExpress.Web.ASPxEditors;
+
+namespace Xpand.ExpressApp.Web.PropertyEditors {
+    public class CheckboxEnumPropertyEditor : WebPropertyEditor {
+        readonly Dictionary<ASPxCheckBox, int> controlsHash = new Dictionary<ASPxCheckBox, int>();
+        readonly EnumDescriptor enumDescriptor;
+
+        public CheckboxEnumPropertyEditor(Type objectType, IModelMemberViewItem info)
+            : base(objectType, info) {
+            enumDescriptor = new EnumDescriptor(MemberInfo.MemberType);
+        }
+
+        public new Panel Editor {
+            get { return (Panel)base.Editor; }
+        }
+
+        protected override WebControl CreateEditModeControlCore() {
+            var placeHolder = new Panel();
+            controlsHash.Clear();
+            foreach (object enumValue in enumDescriptor.Values) {
+                if ((int)enumValue != 0) {
+                    var checkBox = new ASPxCheckBox();
+                    controlsHash.Add(checkBox, (int)enumValue);
+                    checkBox.Text = enumDescriptor.GetCaption(enumValue);
+                    checkBox.CheckedChanged += checkBox_CheckedChanged;
+                    placeHolder.Controls.Add(checkBox);
+                }
+            }
+            return placeHolder;
+        }
+
+        void checkBox_CheckedChanged(object sender, EventArgs e) {
+            EditValueChangedHandler(sender, e);
+        }
+
+        protected override string GetPropertyDisplayValue() {
+            return enumDescriptor.GetCaption(PropertyValue);
+        }
+
+        protected override void ReadEditModeValueCore() {
+            object value = PropertyValue;
+            if (value != null) {
+                foreach (ASPxCheckBox checkBox in Editor.Controls) {
+                    int currentValue = controlsHash[checkBox];
+                    checkBox.Checked = ((int)value & currentValue) != 0;
+                }
+            }
+        }
+
+        protected override object GetControlValueCore() {
+            return Editor.Controls.Cast<ASPxCheckBox>().Where(checkBox => checkBox.Checked).Aggregate<ASPxCheckBox, object>(0, (current, checkBox) => (int)current | controlsHash[checkBox]);
+        }
+
+        public override void BreakLinksToControl(bool unwireEventsOnly) {
+            if (Editor != null) {
+                foreach (ASPxCheckBox checkBox in Editor.Controls) {
+                    checkBox.CheckedChanged -= checkBox_CheckedChanged;
+                }
+                if (!unwireEventsOnly) {
+                    controlsHash.Clear();
+                }
+            }
+            base.BreakLinksToControl(unwireEventsOnly);
+        }
+    }
+}
+
---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/EnumPropertyEditor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Repository;
+
+namespace Xpand.ExpressApp.Win.PropertyEditors {
+    [PropertyEditor(typeof(Enum), true)]
+    public class EnumPropertyEditor : DXPropertyEditor {
+        EnumDescriptor enumDescriptor;
+        object noneValue;
+
+        public EnumPropertyEditor(Type objectType, IModelMemberViewItem model)
+            : base(objectType, model) {
+            ImmediatePostData = model.ImmediatePostData;
+        }
+
+        bool TypeHasFlagsAttribute() {
+            return GetUnderlyingType().GetCustomAttributes(typeof(FlagsAttribute), true).Length > 0;
+        }
+
+        public new PopupBaseEdit Control {
+            get { return (PopupBaseEdit)base.Control; }
+        }
+        protected override object CreateControlCore() {
+            return TypeHasFlagsAttribute() ? (object)new CheckedComboBoxEdit() : new EnumEdit(MemberInfo.MemberType);
+        }
+
+        protected override RepositoryItem CreateRepositoryItem() {
+            return TypeHasFlagsAttribute()
+                       ? (RepositoryItem)new RepositoryItemCheckedComboBoxEdit()
+                       : new RepositoryItemEnumEdit(MemberInfo.MemberType);
+        }
+
+        protected override void SetupRepositoryItem(RepositoryItem item) {
+            base.SetupRepositoryItem(item);
+            if (TypeHasFlagsAttribute()) {
+                enumDescriptor = new EnumDescriptor(GetUnderlyingType());
+                var checkedItem = ((RepositoryItemCheckedComboBoxEdit)item);
+                checkedItem.BeginUpdate();
+                checkedItem.Items.Clear();
+                noneValue = GetNoneValue();
+                //checkedItem.SelectAllItemVisible = false;
+                //Dennis: this is required to show localized items in the editor.
+                foreach (object value in enumDescriptor.Values)
+                    if (!IsNoneValue(value))
+                        checkedItem.Items.Add(value, enumDescriptor.GetCaption(value), CheckState.Unchecked, true);
+                //Dennis: use this method if you don't to show localized items in the editor.
+                //checkedItem.SetFlags(GetUnderlyingType());
+                checkedItem.EndUpdate();
+                checkedItem.ParseEditValue += checkedEdit_ParseEditValue;
+                checkedItem.CustomDisplayText += checkedItem_CustomDisplayText;
+            }
+        }
+
+        void checkedEdit_ParseEditValue(object sender, ConvertEditValueEventArgs e) {
+            if (string.IsNullOrEmpty(Convert.ToString(e.Value))) {
+                ((CheckedComboBoxEdit)sender).EditValue = noneValue;
+                e.Handled = true;
+            }
+        }
+
+        void checkedItem_CustomDisplayText(object sender, CustomDisplayTextEventArgs e) {
+            if (!IsNoneValue(e.Value) || enumDescriptor == null) return;
+            e.DisplayText = enumDescriptor.GetCaption(e.Value);
+        }
+
+        bool IsNoneValue(object value) {
+            if (value is string) return false;
+            int result = int.MinValue;
+            try {
+                result = Convert.ToInt32(value);
+            } catch {
+            }
+            return 0.Equals(result);
+        }
+
+        object GetNoneValue() {
+            return Enum.ToObject(GetUnderlyingType(), 0);
+        }
+    }
+}
