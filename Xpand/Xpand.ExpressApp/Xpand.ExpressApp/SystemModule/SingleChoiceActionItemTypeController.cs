db35f3b5f2c14970553407858136074b0fcbc36e
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-13 19:54:36 +0300

    core: Changing singleaction ItemType through model

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/SingleChoiceActionItemTypeController.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Model;
+
+namespace Xpand.ExpressApp.SystemModule {
+    [ModelAbstractClass]
+    public interface IModelActionSingleChoiceActionItemType : IModelAction {
+        [Category("Appearance")]
+        SingleChoiceActionItemType? ItemType { get; set; }
+    }
+
+    public class SingleChoiceActionItemTypeController : ViewController, IModelExtender {
+        protected override void OnActivated() {
+            base.OnActivated();
+            var modelActions = Application.Model.ActionDesign.Actions.OfType<IModelActionSingleChoiceActionItemType>().Where(type => type.ItemType.HasValue);
+            var singleChoiceActions = Frame.Controllers.Cast<Controller>().SelectMany(controller => controller.Actions).OfType<SingleChoiceAction>().ToDictionary(action => action.Id, action => action);
+            foreach (var modelAction in modelActions) {
+                if (modelAction.ItemType != null)
+                    singleChoiceActions[modelAction.Id].ItemType = modelAction.ItemType.Value;
+            }
+        }
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelAction, IModelActionSingleChoiceActionItemType>();
+        }
+    }
+}
