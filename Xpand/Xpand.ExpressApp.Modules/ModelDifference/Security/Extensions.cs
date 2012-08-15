77b44e0d09b2497f9f4b5cd835c14670b47e02a9
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-15 03:09:48 +0300

    ModelDifference:Provide a set of minimal permissions for a user when using the new security system

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/Security/Extensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/Security/Extensions.cs
@@ -2,20 +2,46 @@
 using System.Collections.Generic;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
-using DevExpress.Xpo;
+using DevExpress.ExpressApp.Security.Strategy;
 using DevExpress.Xpo.Metadata.Helpers;
 using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 using Xpand.ExpressApp.Security;
+using Xpand.ExpressApp.Security.Core;
 
 namespace Xpand.ExpressApp.ModelDifference.Security {
     public static class Extensions {
+        static readonly List<Type> Types = new List<Type> {
+                                        typeof (ModelDifferenceObject), typeof (UserModelDifferenceObject),
+                                          typeof (RoleModelDifferenceObject), typeof (IntermediateObject), typeof (AspectObject),
+                                          typeof (PersistentApplication)
+                                      };
+
+        public static SecuritySystemRole GetDefaultModelRole(this IObjectSpace objectSpace, string roleName) {
+            var modelRole = objectSpace.GetRole(roleName);
+            if (objectSpace.IsNewObject(modelRole)) {
+                modelRole.CreateTypePermission<PersistentApplication>(o => {
+                    o.AllowDelete = false;
+                    o.AllowNavigate = false;
+                });
+                modelRole.CreateTypePermission<ModelDifferenceObject>(o => {
+                    o.AllowCreate = false;
+                    o.AllowNavigate = false;
+                });
+                modelRole.CreateTypePermission<UserModelDifferenceObject>(o => {
+                    o.AllowDelete = false;
+                    o.AllowNavigate = false;
+                });
+                modelRole.CreateTypePermission<AspectObject>(o => {
+                    o.AllowDelete = false;
+                    o.AllowNavigate = false;
+                    o.AllowCreate = false;
+                });
+            }
+            return modelRole;
+        }
+
         public static void GrantPermissionsForModelDifferenceObjects(this ISecurityRole securityRole) {
-            var types = new List<Type> {
-                                typeof (ModelDifferenceObject), typeof (UserModelDifferenceObject),
-                                  typeof (RoleModelDifferenceObject), typeof (IntermediateObject), typeof (AspectObject),
-                                  typeof (PersistentApplication), typeof (XPObjectType),SecuritySystem.UserType
-                              };
-            securityRole.GrantPermissionsObjects(types);
+            securityRole.GrantPermissionsObjects(Types);
         }
     }
 }
