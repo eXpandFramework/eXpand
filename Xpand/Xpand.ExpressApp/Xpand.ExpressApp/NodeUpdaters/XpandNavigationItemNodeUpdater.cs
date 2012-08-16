b27d950367ae0c1004c62ac52b0b0acc179d8df4
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-16 06:06:36 +0300

    Core :XpandNavigationItemNodeUpdater Improve error handling info

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/NodeUpdaters/XpandNavigationItemNodeUpdater.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/NodeUpdaters/XpandNavigationItemNodeUpdater.cs
@@ -7,31 +7,30 @@ using System.Linq;
 using Xpand.ExpressApp.Attributes;
 
 namespace Xpand.ExpressApp.NodeUpdaters {
-    public class XpandNavigationItemNodeUpdater : ModelNodesGeneratorUpdater<NavigationItemNodeGenerator>
-    {
+    public class XpandNavigationItemNodeUpdater : ModelNodesGeneratorUpdater<NavigationItemNodeGenerator> {
         public override void UpdateNode(ModelNode node) {
-            var modelClasses = node.Application.BOModel.Where(modelClass => modelClass.TypeInfo.FindAttribute<XpandNavigationItemAttribute>()!=null);
+            var modelClasses = node.Application.BOModel.Where(modelClass => modelClass.TypeInfo.FindAttribute<XpandNavigationItemAttribute>() != null);
             foreach (var modelClass in modelClasses) {
                 var navigationItemAttributes = modelClass.TypeInfo.FindAttributes<XpandNavigationItemAttribute>();
                 foreach (var itemAttribute in navigationItemAttributes) {
                     var paths = itemAttribute.Path.Split('/');
-                    AddNodes(((IModelRootNavigationItems)node).Items, paths.ToList(), itemAttribute.ViewId,itemAttribute.ObjectKey);   
+                    AddNodes(((IModelRootNavigationItems)node).Items, paths.ToList(), itemAttribute.ViewId, itemAttribute.ObjectKey);
                 }
             }
         }
 
         void AddNodes(IModelNavigationItems navigationItems, List<string> strings, string viewId, string objectKey) {
             if (strings.Count == 0) {
-                var modelView = navigationItems.Application.Views.Where(view => view.Id == viewId).FirstOrDefault();
-                if (modelView== null)
-                    throw new NullReferenceException(viewId);
-                ((IModelNavigationItem) navigationItems.Parent).View =modelView;
+                var modelView = navigationItems.Application.Views.FirstOrDefault(view => view.Id == viewId);
+                if (modelView == null)
+                    throw new NullReferenceException(viewId + " not found in Application.Views");
+                ((IModelNavigationItem)navigationItems.Parent).View = modelView;
                 return;
             }
             var id = strings[0];
-            IModelNavigationItem navigationItem = GetNavigationItem(navigationItems, id,objectKey);
+            IModelNavigationItem navigationItem = GetNavigationItem(navigationItems, id, objectKey);
             strings.RemoveAt(0);
-            AddNodes(navigationItem.Items, strings,viewId,objectKey);
+            AddNodes(navigationItem.Items, strings, viewId, objectKey);
         }
 
         IModelNavigationItem GetNavigationItem(IModelNavigationItems navigationItems, string id, string objectKey) {
