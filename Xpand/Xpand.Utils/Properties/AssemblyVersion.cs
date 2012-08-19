0c68e13e08a6a481ab597ea727c3c81264a9bdee
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-19 18:34:30 +0300

    Utils: Support link file versioning

---
+++ Xpand/Xpand.Utils/Properties/AssemblyVersion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xpand {
+    public class AssemblyVersion {
+        public const string Version = AssemblyInfo.Version;
+    }
+}
