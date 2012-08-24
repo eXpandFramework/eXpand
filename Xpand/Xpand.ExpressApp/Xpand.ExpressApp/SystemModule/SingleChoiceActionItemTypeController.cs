c25e194456644bc989db9364f497577d4486f2d2
apobekiaris <apostolis.bekiaris@gmail.com>
2012-08-24 22:24:23 +0300

    Core:An Item With The Same Key Has Already Been Added
    http://goo.gl/Ir3mt

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/SingleChoiceActionItemTypeController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/SingleChoiceActionItemTypeController.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Actions;
@@ -15,12 +16,22 @@ namespace Xpand.ExpressApp.SystemModule {
         protected override void OnActivated() {
             base.OnActivated();
             var modelActions = Application.Model.ActionDesign.Actions.OfType<IModelActionSingleChoiceActionItemType>().Where(type => type.ItemType.HasValue);
-            var singleChoiceActions = Frame.Controllers.Cast<Controller>().SelectMany(controller => controller.Actions).OfType<SingleChoiceAction>().ToDictionary(action => action.Id, action => action);
+            var singleChoiceActions = SingleChoiceActions();
             foreach (var modelAction in modelActions) {
                 if (modelAction.ItemType != null)
                     singleChoiceActions[modelAction.Id].ItemType = modelAction.ItemType.Value;
             }
         }
+
+        Dictionary<string, SingleChoiceAction> SingleChoiceActions() {
+            var singleChoiceActions = Frame.Controllers.Cast<Controller>().SelectMany(controller => controller.Actions).OfType<SingleChoiceAction>();
+            var choiceActions = new Dictionary<string, SingleChoiceAction>();
+            foreach (var singleChoiceAction in singleChoiceActions.Where(singleChoiceAction => !choiceActions.ContainsKey(singleChoiceAction.Id))) {
+                choiceActions.Add(singleChoiceAction.Id, singleChoiceAction);
+            }
+            return choiceActions;
+        }
+
         public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
             extenders.Add<IModelAction, IModelActionSingleChoiceActionItemType>();
         }
