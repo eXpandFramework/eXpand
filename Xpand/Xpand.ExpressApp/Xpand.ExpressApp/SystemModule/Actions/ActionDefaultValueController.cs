650b23865d4cf9efb2fcc12d84f072312122feb9
apobekiaris <apostolis.bekiaris@gmail.com>
2012-10-16 21:51:52 +0300

    Action related controllers

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/Actions/ActionDefaultValueController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+
+namespace Xpand.ExpressApp.SystemModule.Actions {
+    [ModelAbstractClass]
+    public interface IModelActionDefaultValue : IModelAction {
+        IActionDefaultValue DefaultValue { get; }
+    }
+
+    public interface IActionDefaultValue : IModelNode {
+        string DefaultValue { get; set; }
+        int? DefaultIndex { get; set; }
+        [DefaultValue(true)]
+        bool Synchronize { get; set; }
+    }
+
+    public class ActionDefaultValueController : ViewController, IModelExtender {
+
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            foreach (var action in GetActions()) {
+                var actionDefaultValue = ((IModelActionDefaultValue)Application.Model.ActionDesign.Actions[action.Id]).DefaultValue;
+                var parametrizedAction = action as ParametrizedAction;
+                if (parametrizedAction != null && !string.IsNullOrEmpty(actionDefaultValue.DefaultValue)) {
+                    parametrizedAction.Value = ReflectionHelper.Convert(actionDefaultValue.DefaultValue, parametrizedAction.ValueType);
+                    parametrizedAction.DoExecute(parametrizedAction.Value);
+                }
+                var singleChoiceAction = action as SingleChoiceAction;
+                if (singleChoiceAction != null && actionDefaultValue.DefaultIndex.HasValue) {
+                    singleChoiceAction.SelectedIndex = actionDefaultValue.DefaultIndex.Value;
+                    singleChoiceAction.DoExecute(singleChoiceAction.SelectedItem);
+                }
+            }
+        }
+
+        IEnumerable<ActionBase> GetActions() {
+            return Frame.Controllers.Cast<Controller>().SelectMany(controller => controller.Actions).Where(@base => @base.Active);
+        }
+
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            foreach (var action in GetActions()) {
+                var actionDefaultValue = ((IModelActionDefaultValue)Application.Model.ActionDesign.Actions[action.Id]).DefaultValue;
+                if (actionDefaultValue.Synchronize) {
+                    var parametrizedAction = action as ParametrizedAction;
+                    if (parametrizedAction != null) {
+                        actionDefaultValue.DefaultValue = parametrizedAction.Value == null ? null : parametrizedAction.Value.ToString();
+                    }
+                    var singleChoiceAction = action as SingleChoiceAction;
+                    if (singleChoiceAction != null) {
+                        if (singleChoiceAction.SelectedIndex == -1)
+                            actionDefaultValue.DefaultIndex = null;
+                        else
+                            actionDefaultValue.DefaultIndex = singleChoiceAction.SelectedIndex;
+                    }
+                }
+            }
+        }
+        #region Implementation of IModelExtender
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelAction, IModelActionDefaultValue>();
+        }
+        #endregion
+    }
+}
---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/Actions/DisableActionsLinqListViewController.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using Xpand.ExpressApp.Core;
+
+namespace Xpand.ExpressApp.SystemModule.Actions {
+    public class DisableActionsLinqListViewController : ViewController<XpandListView> {
+        private const string DefaultReason = "LinqListViewController is active";
+        protected override void OnActivated() {
+            base.OnActivated();
+            bool flag = !View.Id.EndsWith(LinqCollectionSource.DefaultSuffix);
+            Frame.GetController<ListViewProcessCurrentObjectController>().Active[DefaultReason] = flag;
+            Frame.GetController<DeleteObjectsViewController>().Active[DefaultReason] = flag;
+            Frame.GetController<NewObjectViewController>().Active[DefaultReason] = flag;
+            Frame.GetController<FilterController>().Active[DefaultReason] = flag;
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            Frame.GetController<ListViewProcessCurrentObjectController>().Active.RemoveItem(DefaultReason);
+            Frame.GetController<DeleteObjectsViewController>().Active.RemoveItem(DefaultReason);
+            Frame.GetController<NewObjectViewController>().Active.RemoveItem(DefaultReason);
+            Frame.GetController<FilterController>().Active.RemoveItem(DefaultReason);
+        }
+    }
+}
