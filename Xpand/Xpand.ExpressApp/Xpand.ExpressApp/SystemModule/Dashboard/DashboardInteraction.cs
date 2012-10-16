fba15655ab06298aae0848da6a8a5103b81dbd8b
apobekiaris <apostolis.bekiaris@gmail.com>
2012-10-16 21:52:47 +0300

    DashBoardInteraction

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/Dashboard/DashboardInteraction.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Filtering;
+using DevExpress.ExpressApp.Model;
+using Xpand.ExpressApp.SystemModule.Dashboard;
+using Xpand.Persistent.Base.General;
+
+namespace Xpand.ExpressApp.SystemModule.DashBoard {
+
+    public class DashboardInteractionController : ViewController<DashboardView>, IModelExtender {
+        readonly Dictionary<IModelListView, MasterDetailMode> _masterDetailModes = new Dictionary<IModelListView, MasterDetailMode>();
+
+        protected override void OnActivated() {
+            base.OnActivated();
+            View.LayoutManager.CustomizeAppearance += LayoutManagerOnCustomizeAppearance;
+            foreach (var item in View.GetItems<DashboardViewItem>()) {
+                var modelDashboardViewItem = (item.GetModel(View));
+                if (!(modelDashboardViewItem is IModelDashboardReportViewItem))
+                    AssignMasterDetailModes((IModelDashboardViewItemEx)modelDashboardViewItem);
+            }
+        }
+
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            foreach (var result in View.Items.OfType<DashboardViewItem>()) {
+                var frame1 = result.Frame;
+                if (frame1 != null && frame1.View is ListView) {
+                    var listView = ((ListView)frame1.View);
+                    DashboardViewItem result1 = result;
+                    listView.SelectionChanged += (sender, args) => OnSelectionChanged(new SelectionChangedArgs(listView, result1));
+                }
+            }
+            ResetMasterDetailModes();
+        }
+
+        void AssignMasterDetailModes(IModelDashboardViewItemEx modelDashboardViewItem) {
+            if (modelDashboardViewItem.MasterDetailMode.HasValue) {
+                var modelListView = modelDashboardViewItem.View as IModelListView;
+                if (modelListView != null) {
+                    _masterDetailModes.Add(modelListView, modelListView.MasterDetailMode);
+                    modelListView.MasterDetailMode = modelDashboardViewItem.MasterDetailMode.Value;
+                }
+            }
+        }
+
+        void ResetMasterDetailModes() {
+            foreach (var masterDetailMode in _masterDetailModes) {
+                masterDetailMode.Key.MasterDetailMode = masterDetailMode.Value;
+            }
+            _masterDetailModes.Clear();
+        }
+
+        void OnSelectionChanged(SelectionChangedArgs selectionChangedArgs) {
+            var dataSourceListView = DataSourceListView((IModelListView)selectionChangedArgs.DashboardViewItemModel.View);
+            if (dataSourceListView != null) {
+                var dashboardViewItems = View.Items.OfType<DashboardViewItem>();
+                foreach (var dashboardViewItem in dashboardViewItems) {
+                    var modelDashboardViewItemEx = (IModelDashboardViewItemEx)dashboardViewItem.GetModel(View);
+                    if (modelDashboardViewItemEx.Filter.DataSourceView == dataSourceListView.Model) {
+                        throw new NotImplementedException();
+                        //                        if (dashboardViewItem is DashboardReportViewItem) {
+                        //                            var report = ((DashboardReportViewItem)dashboardViewItem).Report;
+                        //                            //                            var propertyName = PropertyName(report, modelDashboardViewItemEx.Filter.ReportDataTypeMember);
+                        //                            //                            var criteria = new InOperator(propertyName, Getkeys(dataSourceListView));
+                        //                            throw new NotImplementedException();
+                        //                            //                            report.SetFilteringObject(new LocalizedCriteriaWrapper(report.DataType, criteria));
+                        //                            //                            report.CreateDocument(true);
+                        //                        } else {
+                        //                            var filterListView = FilteredListView(dataSourceListView, dashboardViewItem, modelDashboardViewItemEx);
+                        //                            CreateSummaryDataSource(filterListView);
+                        //                        }
+                    }
+                }
+            }
+            NotifyControllers(selectionChangedArgs.ListView);
+        }
+
+        //        string PropertyName(XafReport report, string reportDataTypeMember) {
+        //            var typeInfo = XafTypesInfo.Instance.FindTypeInfo(report.DataType);
+        //            return string.IsNullOrEmpty(reportDataTypeMember) ? typeInfo.KeyMember.Name : typeInfo.FindMember(reportDataTypeMember).Name;
+        //        }
+
+        void NotifyControllers(ListView listView) {
+            if (View != null) {
+                var selectionChangeds = View.Items.OfType<DashboardViewItem>().SelectMany(Controllers);
+                foreach (var selectionChanged in selectionChangeds) {
+                    selectionChanged.SelectedObjects = listView.SelectedObjects;
+                }
+            }
+        }
+
+        IEnumerable<IDataSourceSelectionChanged> Controllers(IFrameContainer item) {
+            return item.Frame == null ? Enumerable.Empty<IDataSourceSelectionChanged>()
+                       : item.Frame.Controllers.Cast<Controller>().Where(controller => controller.Active).OfType<IDataSourceSelectionChanged>();
+        }
+
+        ListView DataSourceListView(IModelListView dataSourceView) {
+            return View != null ? (dataSourceView != null ?
+                       View.Items.OfType<DashboardViewItem>().Where(item => ViewMatch(item, dataSourceView)).Select(item => item.Frame.View).Cast<ListView>().Single() : null)
+                       : null;
+        }
+
+        bool ViewMatch(DashboardViewItem item, IModelListView dataSourceView) {
+            return item.Frame != null && item.Frame.View != null && item.Frame.View.Model == dataSourceView;
+        }
+
+        void LayoutManagerOnCustomizeAppearance(object sender, CustomizeAppearanceEventArgs customizeAppearanceEventArgs) {
+            var modelDashboardViewItem = View.Model.Items.OfType<IModelDashboardViewItemEx>().FirstOrDefault(item => item.Id == customizeAppearanceEventArgs.Name);
+            if (modelDashboardViewItem != null)
+                ((IAppearanceVisibility)customizeAppearanceEventArgs.Item).Visibility = modelDashboardViewItem.Visibility;
+        }
+
+        void CreateSummaryDataSource(ListView filterListView) {
+            throw new NotImplementedException();
+            //            var pivotGridListEditor = filterListView.Editor as PivotGridListEditor;
+            //            if (pivotGridListEditor != null) {
+            //                if (!filterListView.IsControlCreated) {
+            //                    filterListView.CreateControls();
+            //                }
+            //                var pivotSummaryDataSource = pivotGridListEditor.PivotGridControl.CreateSummaryDataSource();
+            //                foreach (ListView summaryDataSourceView in GetSummaryDataSourceViews(filterListView)) {
+            //                    ((GridControl)summaryDataSourceView.Control).DataSource = pivotSummaryDataSource;
+            //                }
+            //            }
+        }
+
+        IEnumerable<ListView> GetSummaryDataSourceViews(ListView filterListView) {
+            var dashboardViewItems = View.Items.OfType<DashboardViewItem>();
+            var viewItems = dashboardViewItems.Where(item => ((IModelDashboardViewItemEx)item.GetModel(View)).Filter.SummaryDataSourceView == filterListView.Model);
+            return viewItems.Select(item => item.Frame.View).OfType<ListView>();
+        }
+
+        ListView FilteredListView(ListView listView, DashboardViewItem dashboardViewItem, IModelDashboardViewItemEx modelDashboardViewItemFiltered) {
+            var filteredColumn = modelDashboardViewItemFiltered.Filter.FilteredColumn;
+            var filteredListView = ((ListView)dashboardViewItem.Frame.View);
+            var collectionSourceBase = filteredListView.CollectionSource;
+            collectionSourceBase.Criteria[modelDashboardViewItemFiltered.Filter.DataSourceView.Id] = CriteriaSelectionOperator(listView, filteredColumn);
+            return filteredListView;
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            View.LayoutManager.CustomizeAppearance -= LayoutManagerOnCustomizeAppearance;
+        }
+
+        CriteriaOperator CriteriaSelectionOperator(ListView listView, IModelColumn filteredColumn) {
+            var keyName = filteredColumn.ModelMember.MemberInfo.MemberTypeInfo.KeyMember.Name;
+            throw new NotImplementedException();
+            //            var selectionCriteria = listView.Editor as ISelectionCriteria;
+            //            return selectionCriteria != null ? CriteriaOperator.Parse(filteredColumn.PropertyName + "." + (selectionCriteria).SelectionCriteria.ToString())
+            //                       : new InOperator(filteredColumn.PropertyName + "." + keyName, Getkeys(listView));
+        }
+
+        IEnumerable Getkeys(ListView listView) {
+            return listView.SelectedObjects.OfType<object>().Select(o => ObjectSpace.GetKeyValue(o));
+        }
+        #region Implementation of IModelExtender
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelDashboardViewItem, IModelDashboardViewItemEx>();
+        }
+        #endregion
+    }
+
+    public class SelectionChangedArgs : EventArgs {
+        readonly ListView _listView;
+        readonly DashboardViewItem _dashboardViewItem;
+
+        public SelectionChangedArgs(ListView listView, DashboardViewItem dashboardViewItem) {
+            _listView = listView;
+            _dashboardViewItem = dashboardViewItem;
+        }
+
+        public ListView ListView {
+            get { return _listView; }
+        }
+
+        public DashboardViewItem DashboardViewItem {
+            get {
+                return _dashboardViewItem;
+            }
+        }
+
+        public IModelDashboardViewItemEx DashboardViewItemModel {
+            get {
+                return (IModelDashboardViewItemEx)_dashboardViewItem.GetModel((DashboardView)_dashboardViewItem.View);
+            }
+        }
+    }
+}
+
---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/Dashboard/Model.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.Persistent.Base;
+
+namespace Xpand.ExpressApp.SystemModule.Dashboard {
+    public interface IModelDashboardViewFilter : IModelNode {
+        [DataSourceProperty("FilteredColumns")]
+        [ModelBrowsable(typeof(DashboardViewFilterVisibilityCalculator))]
+        IModelColumn FilteredColumn { get; set; }
+        string ReportDataTypeMember { get; set; }
+        [Browsable(false)]
+        IModelList<IModelColumn> FilteredColumns { get; }
+        [DataSourceProperty("DataSourceViews")]
+        IModelListView DataSourceView { get; set; }
+        [Browsable(false)]
+        IModelList<IModelListView> DataSourceViews { get; }
+
+        [DataSourceProperty("SummaryDataSourceViews")]
+        [ModelBrowsable(typeof(DashboardViewFilterVisibilityCalculator))]
+        IModelListView SummaryDataSourceView { get; set; }
+        [Browsable(false)]
+        IModelList<IModelListView> SummaryDataSourceViews { get; }
+    }
+    public class DashboardViewFilterVisibilityCalculator : IModelIsVisible {
+        #region Implementation of IModelIsVisible
+        public bool IsVisible(IModelNode node, string propertyName) {
+            return !(node.Parent is IModelDashboardReportViewItem);
+        }
+        #endregion
+    }
+
+    [DomainLogic(typeof(IModelDashboardViewFilter))]
+    public static class DashboardViewFilteredDomainLogic {
+        public static IModelList<IModelListView> Get_SummaryDataSourceViews(IModelDashboardViewFilter modelDashboardViewFilter) {
+            var calculatedModelNodeList = new CalculatedModelNodeList<IModelListView>();
+            var modelView = ((IModelDashboardViewItemEx)modelDashboardViewFilter.Parent).View;
+            var dashboardViewItemFiltereds = modelDashboardViewFilter.AllDashBoardViewItems().Where(filtered => filtered.View is IModelListView && modelView != filtered.View);
+            throw new NotImplementedException();
+            //            var modelListViews = dashboardViewItemFiltereds.Select(filtered => filtered.View).OfType<IModelListView>().Where(view => typeof(PivotGridListEditorBase).IsAssignableFrom(view.EditorType));
+            //            calculatedModelNodeList.AddRange(modelListViews);
+            //            return calculatedModelNodeList;
+        }
+
+        public static IModelList<IModelListView> Get_DataSourceViews(IModelDashboardViewFilter modelDashboardViewFilter) {
+            if (modelDashboardViewFilter.FilteredColumn != null) {
+                var modelClass = modelDashboardViewFilter.Application.BOModel.GetClass(modelDashboardViewFilter.FilteredColumn.ModelMember.Type);
+                var dashBoardViewItems = modelDashboardViewFilter.AllDashBoardViewItems();
+                var modelListViews = dashBoardViewItems.Where(item => item.View != null && item.View.AsObjectView.ModelClass == modelClass).Select(filtered => filtered.View).OfType<IModelListView>();
+                return new CalculatedModelNodeList<IModelListView>(modelListViews);
+            }
+            if (modelDashboardViewFilter.Parent is IModelDashboardViewItemEx)
+                return new CalculatedModelNodeList<IModelListView>(modelDashboardViewFilter.AllDashBoardViewItems().Select(ex => ex.View).OfType<IModelListView>());
+            return new CalculatedModelNodeList<IModelListView>();
+        }
+
+        public static IEnumerable<IModelDashboardViewItemEx> AllDashBoardViewItems(this IModelDashboardViewFilter modelDashboardViewFilter) {
+            return ((IModelDashboardView)modelDashboardViewFilter.Parent.Parent.Parent).Items.OfType<IModelDashboardViewItemEx>();
+        }
+
+        public static IModelList<IModelColumn> Get_FilteredColumns(IModelDashboardViewFilter modelDashboardViewFilter) {
+            var calculatedModelNodeList = new CalculatedModelNodeList<IModelColumn>();
+            var modelListView = ((IModelDashboardViewItemEx)modelDashboardViewFilter.Parent).View as IModelListView;
+            if (modelListView != null) {
+                calculatedModelNodeList.AddRange(modelListView.Columns.Where(column => column.ModelMember.MemberInfo.MemberTypeInfo.IsDomainComponent));
+            }
+            return calculatedModelNodeList;
+        }
+
+    }
+    public static class DashboardViewItemExtensions {
+        public static IModelDashboardViewItem GetModel(this DashboardViewItem item, DashboardView view) {
+            return (IModelDashboardViewItem)view.Model.Items[item.Id];
+        }
+    }
+    public interface IModelDashboardReportViewItem : IModelDashboardViewItem {
+        string ReportName { get; set; }
+        bool CreateDocumentOnLoad { get; set; }
+        [Browsable(false)]
+        new IModelView View { get; set; }
+        [Browsable(false)]
+        new string Criteria { get; set; }
+        [Browsable(false)]
+        [DefaultValue(ActionsToolbarVisibility.Hide)]
+        new ActionsToolbarVisibility ActionsToolbarVisibility { get; set; }
+        [DefaultValue(ViewItemVisibility.Show)]
+        [Browsable(false)]
+        ViewItemVisibility Visibility { get; set; }
+        [Browsable(false)]
+        MasterDetailMode? MasterDetailMode { get; set; }
+    }
+
+    [ModelAbstractClass]
+    public interface IModelDashboardViewItemEx : IModelDashboardViewItem {
+        IModelDashboardViewFilter Filter { get; }
+        [DefaultValue(ViewItemVisibility.Show)]
+        ViewItemVisibility Visibility { get; set; }
+        MasterDetailMode? MasterDetailMode { get; set; }
+    }
+}
