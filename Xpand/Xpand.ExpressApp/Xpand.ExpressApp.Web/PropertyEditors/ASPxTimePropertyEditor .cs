43919cee018253218e1db137620cc4d2cbde84e3
apobekiaris <apostolis.bekiaris@gmail.com>
2012-10-24 12:25:33 +0300

    Web.Core: ASPxTimePropertyEditor
    http://goo.gl/O4cAq

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/PropertyEditors/ASPxTimePropertyEditor .cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Web.ASPxEditors;
+
+namespace Xpand.ExpressApp.Web.PropertyEditors {
+    public class ASPxTimePropertyEditor : ASPxPropertyEditor {
+        private const string TimeFormat = "HH:mm";
+
+        public ASPxTimePropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+
+        public new ASPxTimeEdit Editor {
+            get { return (ASPxTimeEdit)base.Editor; }
+        }
+        private void SelectedDateChangedHandler(object source, EventArgs e) {
+            FixYear(source as ASPxTimeEdit);
+
+            EditValueChangedHandler(source, e);
+        }
+        protected override string GetPropertyDisplayValue() {
+            if (Equals(PropertyValue, DateTime.MinValue) || !(PropertyValue is DateTime)) {
+                return string.Empty;
+            }
+            return ((DateTime)PropertyValue).ToString(TimeFormat);
+        }
+        protected override void SetupControl(WebControl control) {
+            base.SetupControl(control);
+            var aSPxDateEdit = control as ASPxTimeEdit;
+            if (aSPxDateEdit != null) {
+                aSPxDateEdit.EditFormat = EditFormat.Custom;
+                aSPxDateEdit.EditFormatString = EditMask;
+                aSPxDateEdit.DisplayFormatString = TimeFormat;
+                aSPxDateEdit.DateChanged += SelectedDateChangedHandler;
+            }
+        }
+        protected override void SetImmediatePostDataScript(string script) {
+            Editor.ClientSideEvents.ValueChanged = script;
+        }
+        protected override WebControl CreateEditModeControlCore() {
+            return new ASPxTimeEdit();
+        }
+        public override void BreakLinksToControl(bool unwireEventsOnly) {
+            if (Editor != null) {
+                Editor.DateChanged -= SelectedDateChangedHandler;
+            }
+            base.BreakLinksToControl(unwireEventsOnly);
+        }
+
+        private void FixYear(ASPxTimeEdit editor) {
+            if (editor == null)
+                return;
+
+            if (editor.Value is DateTime) {
+                var time = (DateTime)editor.Value;
+                editor.Value = time.AddYears(Math.Max(2000 - time.Year, 0));
+            }
+        }
+    }
+}
