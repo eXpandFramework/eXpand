19186db9e1b4dbf5d40db0130569c0af375f86b9
apobekiaris <apostolis.bekiaris@gmail.com>
2012-11-14 21:31:27 +0200

    Enumeration Edit - Drop-down Filtering - Win / Web solution.
    http://www.expandframework.com/forum/11-news/2881-enumeration-edit-drop-down-filtering-win--web-solution.html#2883

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/PropertyEditors/EnumPropertyEditor.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/PropertyEditors/EnumPropertyEditor.cs
@@ -1,13 +1,151 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Web.UI.WebControls;
+using DevExpress.Data.Filtering;
+using DevExpress.Data.Filtering.Helpers;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.ExpressApp.Web.Editors;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+using DevExpress.Persistent.Base;
 using DevExpress.Web.ASPxEditors;
+using Xpand.ExpressApp.PropertyEditors;
 
 namespace Xpand.ExpressApp.Web.PropertyEditors {
+    [PropertyEditor(typeof(Enum), EditorAliases.EnumPropertyEditor, false)]
+    public class WebFilterableEnumPropertyEditor : ASPxEnumPropertyEditor, IComplexPropertyEditor {
+        static PropertyDescriptorCollection _propertyDescriptorCollection;
+        readonly PropertyInfo _dataSourceProperty;
+        readonly string _isNullCriteria;
+        readonly DataSourcePropertyIsNullMode _isNullMode = DataSourcePropertyIsNullMode.SelectAll;
+        readonly Type _propertyType;
+
+        IObjectSpace _objectSpace;
+
+        public WebFilterableEnumPropertyEditor(Type objectType, IModelMemberViewItem model)
+            : base(objectType, model) {
+            PropertyInfo propertyInfo = ObjectType.GetProperty(PropertyName);
+            if (propertyInfo != null) {
+                _propertyType = propertyInfo.PropertyType;
+                foreach (object item in propertyInfo.GetCustomAttributes(false)) {
+                    var propAttr = item as DataSourcePropertyAttribute;
+                    if (propAttr != null && !string.IsNullOrEmpty(propAttr.DataSourceProperty)) {
+                        PropertyInfo dataSourceProperty = ObjectType.GetProperty(propAttr.DataSourceProperty);
+                        _isNullMode = propAttr.DataSourcePropertyIsNullMode;
+                        _isNullCriteria = propAttr.DataSourcePropertyIsNullCriteria;
+                        if (dataSourceProperty != null) {
+                            if (typeof(IEnumerable).IsAssignableFrom(dataSourceProperty.PropertyType) &&
+                                dataSourceProperty.PropertyType.IsGenericType &&
+                                dataSourceProperty.PropertyType.GetGenericArguments()[0].IsAssignableFrom(
+                                    propertyInfo.PropertyType))
+                                _dataSourceProperty = dataSourceProperty;
+                        }
+                    }
+
+                    var criteriaAttr = item as DataSourceCriteriaAttribute;
+                    if (criteriaAttr != null)
+                        _isNullCriteria = criteriaAttr.DataSourceCriteria;
+                }
+            }
+        }
+
+        protected static PropertyDescriptorCollection PropertyDescriptorCollection {
+            get {
+                return _propertyDescriptorCollection ??
+                       (_propertyDescriptorCollection = TypeDescriptor.GetProperties(typeof(ListEditItem)));
+            }
+        }
+        #region IComplexPropertyEditor Members
+        public void Setup(IObjectSpace objectSpace, XafApplication application) {
+            if (_objectSpace != null)
+                _objectSpace.ObjectChanged -= ObjectSpace_ObjectChanged;
+            _objectSpace = objectSpace;
+            _objectSpace.ObjectChanged += ObjectSpace_ObjectChanged;
+        }
+        #endregion
+        protected override void SetupControl(WebControl control) {
+            var editor = control as ASPxComboBox;
+            if (editor != null) {
+                editor.ShowImageInEditBox = true;
+                editor.SelectedIndexChanged += ExtendedEditValueChangedHandler;
+                FillEditor(editor);
+            }
+        }
+
+        void FillEditor(ASPxComboBox editor) {
+            editor.Items.Clear();
+            editor.ValueType = GetComboBoxValueType();
+
+            IEnumerable dataSource = GetDataSource();
+
+            if (_propertyType.IsGenericType && _propertyType.GetGenericTypeDefinition() == typeof(Nullable<>)) {
+                editor.Items.Add(CaptionHelper.NullValueText, null);
+            }
+
+            if (dataSource != null)
+                foreach (object value in dataSource) {
+                    if (value is ListEditItem)
+                        editor.Items.Add(value as ListEditItem);
+                    else
+                        editor.Items.Add(CreateEditItem(value));
+                }
+        }
+
+        IEnumerable GetDataSource() {
+            IEnumerable dataSource = null;
+            if (_dataSourceProperty != null) {
+                dataSource = _dataSourceProperty.GetValue(CurrentObject, null) as IEnumerable;
+                if (dataSource != null) {
+                    bool hasItems = (dataSource).GetEnumerator().MoveNext();
+                    if (!hasItems)
+                        dataSource = null;
+                }
+            }
+            if (dataSource == null) {
+                if (string.IsNullOrEmpty(_isNullCriteria)) {
+                    if (_isNullMode == DataSourcePropertyIsNullMode.SelectAll)
+                        return descriptor.Values;
+                    if (_isNullMode == DataSourcePropertyIsNullMode.SelectNothing)
+                        return null;
+                } else {
+                    CriteriaOperator criteriaOperator = CriteriaOperator.Parse(_isNullCriteria);
+                    if (!ReferenceEquals(criteriaOperator, null)) {
+                        dataSource = new List<object>();
+                        foreach (object item in descriptor.Values)
+                            ((IList)dataSource).Add(CreateEditItem(item));
+                        criteriaOperator.Accept(new EnumCriteriaParser(PropertyName, _propertyType));
+                        var filteredDataSource = new ExpressionEvaluator(PropertyDescriptorCollection, criteriaOperator, true).Filter(dataSource);
+                        ((IList)dataSource).Clear();
+                        foreach (ListEditItem item in filteredDataSource)
+                            ((IList)dataSource).Add(item);
+                    }
+                }
+            }
+            return dataSource;
+        }
+
+        void ObjectSpace_ObjectChanged(object sender, ObjectChangedEventArgs e) {
+            if (e.PropertyName == _dataSourceProperty.Name) {
+                FillEditor(Editor);
+            }
+        }
+
+        ListEditItem CreateEditItem(object enumValue) {
+            object value = ConvertEnumValueForComboBox(enumValue);
+            ImageInfo imageInfo = GetImageInfo(enumValue);
+            if (imageInfo.IsUrlEmpty) {
+                return new ListEditItem(descriptor.GetCaption(enumValue), value);
+            }
+            return new ListEditItem(descriptor.GetCaption(enumValue), value, imageInfo.ImageUrl);
+        }
+    }
+
     public class CheckboxEnumPropertyEditor : WebPropertyEditor {
         readonly Dictionary<ASPxCheckBox, int> controlsHash = new Dictionary<ASPxCheckBox, int>();
         readonly EnumDescriptor enumDescriptor;
---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/PropertyEditors/EnumCriteriaParser.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.Data.Filtering;
+
+namespace Xpand.ExpressApp.PropertyEditors {
+    public class EnumCriteriaParser : ICriteriaVisitor {
+        /* Private */
+        readonly Dictionary<string, OperandValue> _values = new Dictionary<string, OperandValue>();
+
+        /* Internal */
+
+        /* Constructor */
+
+        public EnumCriteriaParser(string propertyName, Type enumType) {
+            PropertyName = propertyName;
+            if (enumType.IsGenericType) {
+                Type[] types = enumType.GetGenericArguments();
+                if (types.Length == 1 && typeof(Nullable<>).MakeGenericType(types[0]) == enumType)
+                    enumType = types[0];
+            }
+            EnumType = enumType;
+            foreach (object value in Enum.GetValues(enumType))
+                _values.Add(value.ToString(), new OperandValue(value));
+        }
+
+        public string PropertyName { get; private set; }
+        public Type EnumType { get; private set; }
+
+        /* Operand visitors */
+        #region ICriteriaVisitor Members
+        public object Visit(FunctionOperator theOperator) {
+            /* Nothing special to do here.. */
+            return theOperator;
+        }
+
+        public object Visit(OperandValue theOperand) {
+            /* Nothing special to do here.. */
+            return theOperand;
+        }
+
+        public object Visit(GroupOperator theOperator) {
+            foreach (CriteriaOperator operand in theOperator.Operands)
+                operand.Accept(this);
+
+            return theOperator;
+        }
+
+        public object Visit(InOperator theOperator) {
+            UpdatePropertyName(theOperator.LeftOperand);
+            ToValue(theOperator.Operands);
+
+            return theOperator;
+        }
+
+        public object Visit(UnaryOperator theOperator) {
+            switch (theOperator.OperatorType) {
+                case UnaryOperatorType.IsNull:
+                    UpdatePropertyName(theOperator.Operand);
+                    break;
+                case UnaryOperatorType.Not:
+                    theOperator.Operand.Accept(this);
+                    break;
+            }
+
+            return theOperator;
+        }
+
+        public object Visit(BinaryOperator theOperator) {
+            UpdatePropertyName(theOperator.LeftOperand);
+
+            CriteriaOperator operandValue;
+
+            if (ToValue(theOperator.RightOperand, out operandValue))
+                theOperator.RightOperand = operandValue;
+            else
+                theOperator.RightOperand.Accept(this);
+
+            return theOperator;
+        }
+
+        public object Visit(BetweenOperator theOperator) {
+            UpdatePropertyName(theOperator.TestExpression);
+
+            CriteriaOperator operandValue;
+
+            if (ToValue(theOperator.BeginExpression, out operandValue))
+                theOperator.BeginExpression = operandValue;
+            else
+                theOperator.BeginExpression.Accept(this);
+
+            if (ToValue(theOperator.EndExpression, out operandValue))
+                theOperator.EndExpression = operandValue;
+            else
+                theOperator.EndExpression.Accept(this);
+
+            return theOperator;
+        }
+        #endregion
+        static void UnsupportedCriteria() {
+            throw new InvalidEnumArgumentException("Unsupported criteria.");
+        }
+
+        void UpdatePropertyName(CriteriaOperator operand) {
+            var operandProperty = operand as OperandProperty;
+            if ((ReferenceEquals(operandProperty, null)) ||
+                (!operandProperty.PropertyName.Equals(PropertyName)))
+                UnsupportedCriteria();
+            if (operandProperty != null) operandProperty.PropertyName = "Value";
+        }
+
+        void ToValue(IList<CriteriaOperator> operands) {
+            CriteriaOperator operandValue;
+            for (int i = 0; i < operands.Count; i++)
+                if (ToValue(operands[i], out operandValue))
+                    operands[i] = operandValue;
+        }
+
+        bool ToValue(CriteriaOperator operand, out CriteriaOperator operandValue) {
+            operandValue = null;
+
+            string valueName;
+
+            if (operand is OperandProperty)
+                valueName = ((OperandProperty)operand).PropertyName;
+            else if (operand is OperandValue && ((OperandValue)operand).Value is string)
+                valueName = (string)((OperandValue)operand).Value;
+            else
+                return false;
+
+            operandValue = _values[valueName];
+
+            return true;
+        }
+
+        /* Meta */
+    }
+}
