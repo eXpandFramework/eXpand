8f7fae0d13ab91b787a5679b79eb60dc1dca73fa
apobekiaris <apostolis.bekiaris@gmail.com>
2012-11-29 21:34:16 +0200

    IO Xml Import Export Error Special Char
    http://www.expandframework.com/forum/6-bugs/2442-io-xml-import-export-error-special-char.html#2934

--- Xpand/Xpand.ExpressApp.Modules/IO/Core/ExportEngine.cs
+++ Xpand/Xpand.ExpressApp.Modules/IO/Core/ExportEngine.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using DevExpress.Xpo;
@@ -16,11 +17,13 @@ namespace Xpand.ExpressApp.IO.Core {
         readonly Dictionary<ObjectInfo, object> exportedObjecs = new Dictionary<ObjectInfo, object>();
         SerializeClassInfoGraphNodesCalculator _serializeClassInfoGraphNodesCalculator;
 
-        public XDocument Export(IEnumerable<XPBaseObject> baseCollection, ISerializationConfigurationGroup serializationConfigurationGroup) {
+        public XDocument Export(IEnumerable<XPBaseObject> baseCollection,
+                                ISerializationConfigurationGroup serializationConfigurationGroup) {
             var xDocument = new XDocument();
             var root = new XElement("SerializedObjects");
             xDocument.Add(root);
-            _serializeClassInfoGraphNodesCalculator = new SerializeClassInfoGraphNodesCalculator(serializationConfigurationGroup);
+            _serializeClassInfoGraphNodesCalculator =
+                new SerializeClassInfoGraphNodesCalculator(serializationConfigurationGroup);
             foreach (var baseObject in baseCollection) {
                 IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes =
                     _serializeClassInfoGraphNodesCalculator.GetSerializedClassInfoGraphNodes(baseObject);
@@ -31,14 +34,19 @@ namespace Xpand.ExpressApp.IO.Core {
         }
 
 
-        void ExportCore(XPBaseObject selectedObject, IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes, XElement root) {
-            var objectInfo = new ObjectInfo(selectedObject.GetType(), selectedObject.ClassInfo.KeyProperty.GetValue(selectedObject));
+        void ExportCore(XPBaseObject selectedObject, IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes,
+                        XElement root) {
+            var objectInfo = new ObjectInfo(selectedObject.GetType(),
+                                            selectedObject.ClassInfo.KeyProperty.GetValue(selectedObject));
             if (!(exportedObjecs.ContainsKey(objectInfo))) {
                 exportedObjecs.Add(objectInfo, null);
                 var serializedObjectElement = new XElement("SerializedObject");
                 serializedObjectElement.Add(new XAttribute("type", selectedObject.GetType().Name));
                 root.Add(serializedObjectElement);
-                foreach (var classInfoGraphNode in serializedClassInfoGraphNodes.Where(node => node.SerializationStrategy != SerializationStrategy.DoNotSerialize)) {
+                foreach (
+                    var classInfoGraphNode in
+                        serializedClassInfoGraphNodes.Where(
+                            node => node.SerializationStrategy != SerializationStrategy.DoNotSerialize)) {
                     XElement propertyElement = GetPropertyElement(serializedObjectElement, classInfoGraphNode);
                     switch (classInfoGraphNode.NodeType) {
                         case NodeType.Simple:
@@ -55,7 +63,8 @@ namespace Xpand.ExpressApp.IO.Core {
             }
         }
 
-        void SetMemberValue(XPBaseObject selectedObject, IClassInfoGraphNode classInfoGraphNode, XElement propertyElement) {
+        void SetMemberValue(XPBaseObject selectedObject, IClassInfoGraphNode classInfoGraphNode,
+                            XElement propertyElement) {
             var xpMemberInfo = selectedObject.ClassInfo.FindMember(classInfoGraphNode.Name);
             if (xpMemberInfo != null) {
                 var memberValue = selectedObject.GetMemberValue(classInfoGraphNode.Name);
@@ -65,9 +74,11 @@ namespace Xpand.ExpressApp.IO.Core {
                     memberValue = Convert.ToBase64String((byte[])memberValue);
                 if (memberValue is DateTime)
                     memberValue = ((DateTime)memberValue).Ticks;
-                if (memberValue is string)
+
+                if (memberValue is string) {
+                    memberValue = IAFModule.SanitizeXmlString((string)memberValue);
                     propertyElement.Add(new XCData(memberValue.ToString()));
-                else {
+                } else {
                     propertyElement.Value = GetInvariantValue(memberValue);
                 }
             }
@@ -95,27 +106,29 @@ namespace Xpand.ExpressApp.IO.Core {
                 var theObjects = memberInfo.GetValue(selectedObject) as IEnumerable;
                 if (theObjects != null)
                     foreach (XPBaseObject theObject in theObjects) {
-                        CreateRefElelement(classInfoGraphNode, theObject.GetType().Name, root, theObject, propertyElement);
+                        CreateRefElelement(classInfoGraphNode, theObject.GetType().Name, root, theObject,
+                                           propertyElement);
                     }
             }
         }
 
-        void CreateRefElelement(IClassInfoGraphNode classInfoGraphNode, string typeName, XElement root, XPBaseObject theObject, XElement propertyElement) {
+        void CreateRefElelement(IClassInfoGraphNode classInfoGraphNode, string typeName, XElement root,
+                                XPBaseObject theObject, XElement propertyElement) {
             var serializedObjectRefElement = new XElement("SerializedObjectRef");
             propertyElement.Add(serializedObjectRefElement);
             serializedObjectRefElement.Add(new XAttribute("type", typeName));
             serializedObjectRefElement.Add(new XAttribute("strategy", classInfoGraphNode.SerializationStrategy));
             if (theObject != null) {
-                IEnumerable<IClassInfoGraphNode> classInfoGraphNodes = _serializeClassInfoGraphNodesCalculator.GetSerializedClassInfoGraphNodes(theObject, typeName);
+                IEnumerable<IClassInfoGraphNode> classInfoGraphNodes =
+                    _serializeClassInfoGraphNodesCalculator.GetSerializedClassInfoGraphNodes(theObject, typeName);
                 CreateRefKeyElements(classInfoGraphNodes, theObject, serializedObjectRefElement);
                 if (classInfoGraphNode.SerializationStrategy == SerializationStrategy.SerializeAsObject)
                     ExportCore(theObject, classInfoGraphNodes, root);
             }
         }
 
-
-
-        void CreateRefKeyElements(IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes, XPBaseObject theObject, XElement serializedObjectRefElement) {
+        void CreateRefKeyElements(IEnumerable<IClassInfoGraphNode> serializedClassInfoGraphNodes, XPBaseObject theObject,
+                                  XElement serializedObjectRefElement) {
             foreach (var infoGraphNode in serializedClassInfoGraphNodes.Where(node => node.Key)) {
                 var serializedObjectRefKeyElement = new XElement("Key");
                 serializedObjectRefKeyElement.Add(new XAttribute("name", infoGraphNode.Name));
@@ -124,26 +137,59 @@ namespace Xpand.ExpressApp.IO.Core {
             }
         }
 
-        void CreateObjectProperty(XPBaseObject selectedObject, XElement propertyElement, IClassInfoGraphNode classInfoGraphNode, XElement root) {
+        void CreateObjectProperty(XPBaseObject selectedObject, XElement propertyElement,
+                                  IClassInfoGraphNode classInfoGraphNode, XElement root) {
             XPMemberInfo memberInfo = selectedObject.ClassInfo.FindMember(classInfoGraphNode.Name);
             if (memberInfo != null) {
                 var theObject = (XPBaseObject)memberInfo.GetValue(selectedObject);
-                CreateRefElelement(classInfoGraphNode, theObject != null ? theObject.GetType().Name : memberInfo.MemberType.Name, root, theObject, propertyElement);
+                CreateRefElelement(classInfoGraphNode,
+                                   theObject != null ? theObject.GetType().Name : memberInfo.MemberType.Name, root,
+                                   theObject, propertyElement);
             }
         }
 
 
-        public void Export(IEnumerable<XPBaseObject> xpBaseObjects, ISerializationConfigurationGroup serializationConfigurationGroup, string fileName) {
+        public void Export(IEnumerable<XPBaseObject> xpBaseObjects,
+                           ISerializationConfigurationGroup serializationConfigurationGroup, string fileName) {
             var document = Export(xpBaseObjects, serializationConfigurationGroup);
             if (fileName != null) {
                 var xmlWriterSettings = new XmlWriterSettings {
-                    OmitXmlDeclaration = true, Indent = true, NewLineChars = "\r\n", CloseOutput = true,
+                    OmitXmlDeclaration = true,
+                    Indent = true,
+                    NewLineChars = "\r\n",
+                    CloseOutput = true,
                 };
-                using (XmlWriter textWriter = XmlWriter.Create(new FileStream(fileName, FileMode.Create), xmlWriterSettings)) {
+                using (
+                    XmlWriter textWriter = XmlWriter.Create(new FileStream(fileName, FileMode.Create), xmlWriterSettings)
+                    ) {
                     document.Save(textWriter);
                     textWriter.Close();
                 }
             }
         }
     }
+
+    public class IAFModule {
+        public static string SanitizeXmlString(string xml) {
+            if (xml == null) {
+                throw new ArgumentNullException("xml");
+            }
+            var buffer = new StringBuilder(xml.Length);
+            foreach (char c in xml.Where(c => IsLegalXmlChar(c))) {
+                buffer.Append(c);
+            }
+            return buffer.ToString().Trim();
+        }
+        public static bool IsLegalXmlChar(int character) {
+            return (
+            character == 0x9 /* == '\t' == 9 */ ||
+            character == 0xA /* == '\n' == 10 */ ||
+            character == 0xD /* == '\r' == 13 */ ||
+            (character >= 0x20 && character <= 0xD7FF) ||
+            (character >= 0xE000 && character <= 0xFFFD) ||
+            (character >= 0x10000 && character <= 0x10FFFF)
+            );
+        }
+    }
+
 }
