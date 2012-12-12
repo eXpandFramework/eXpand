58b44cbcdb7603ae4d8d68ed052ce69630da4462
apobekiaris <apostolis.bekiaris@gmail.com>
2012-12-12 03:00:50 +0200

    Cannot start EntlibConfig
    http://www.expandframework.com/forum/6-bugs/2992-cannot-start-entlibconfigexe.html#2992

--- Demos/FeatureCenter/FeatureCenter.Module/Miscellaneous/ExceptionHandling/ShowELConfigurationToolController.cs
+++ Demos/FeatureCenter/FeatureCenter.Module/Miscellaneous/ExceptionHandling/ShowELConfigurationToolController.cs
@@ -6,7 +6,7 @@ using DevExpress.ExpressApp.Actions;
 using DevExpress.Persistent.Base;
 
 namespace FeatureCenter.Module.Miscellaneous.ExceptionHandling {
-    public class ShowELConfigurationToolController:ViewController<DetailView> {
+    public class ShowELConfigurationToolController : ViewController<DetailView> {
         public ShowELConfigurationToolController() {
             TargetViewId = "ExceptionHandling_DetailView";
             var simpleAction = new SimpleAction(this, "Show EL Config Tool", PredefinedCategory.ObjectsCreation);
@@ -14,7 +14,7 @@ namespace FeatureCenter.Module.Miscellaneous.ExceptionHandling {
         }
 
         void SimpleActionOnExecute(object sender, SimpleActionExecuteEventArgs e) {
-            var fullPath = Path.GetFullPath("../../../../_third_party_assemblies/EntLibConfig.exe");
+            var fullPath = Path.GetFullPath("../../../../../_third_party_assemblies/EntLibConfig.exe");
             string arguments = Path.GetFullPath("../../app.config");
             Process.Start(fullPath, arguments);
         }
