fd88ddae7249b78c4e96ec633bf2fa9eb271e038
apobekiaris <apostolis.bekiaris@gmail.com>
2012-12-28 20:51:42 +0200

    Runtime Fields not working
    http://www.expandframework.com/forum/6-bugs/3022-runtime-fields-not-working-in-fc.html#3106

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/ModelEditorViewController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/ModelEditorViewController.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Actions;
 using DevExpress.ExpressApp.Model;
 using System.Linq;
+using DevExpress.ExpressApp.Win.Core.ModelEditor;
 
 
 namespace Xpand.ExpressApp.Win {
@@ -13,13 +15,20 @@ namespace Xpand.ExpressApp.Win {
 
         void AddNodeActionOnItemsChanged(object sender, ItemsChangedEventArgs itemsChangedEventArgs) {
             var singleChoiceAction = (sender) as SingleChoiceAction;
-            if (singleChoiceAction != null && (singleChoiceAction.Id == "Add" && itemsChangedEventArgs.ChangedItemsInfo.Values.Contains(ChoiceActionItemChangesType.ItemsAdd))) {
-                if (CurrentModelNode.ModelNode.GetType().Name == "ModelLogicRules") {
+            if (singleChoiceAction != null && (singleChoiceAction.Id == "Add" && itemsChangedEventArgs.ChangedItemsInfo.Values.Contains(ChoiceActionItemChangesType.ItemsAdd | ChoiceActionItemChangesType.ItemsRemove))) {
+                string name = CurrentModelNode.ModelNode.GetType().Name;
+                if (name == "ModelLogicRules") {
                     var modelTreeListNode = CurrentModelNode.Parent;
                     for (int i = singleChoiceAction.Items.Count - 1; i > -1; i--) {
                         var value = modelTreeListNode.ModelNode.Id.Replace("Conditional", "");
                         if (!singleChoiceAction.Items[i].Id.StartsWith(value))
                             singleChoiceAction.Items.RemoveAt(i);
+                    }
+                } else if (name == "ModelBOModelClassMembers") {
+                    var adapter = (ExtendModelInterfaceAdapter)GetType().GetProperty("Adapter", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this, null);
+                    var childNodeTypes = adapter.fastModelEditorHelper.GetListChildNodeTypes(CurrentModelNode.ModelNode.NodeInfo);
+                    foreach (var childNodeType in childNodeTypes) {
+                        AddNodeAction.Items.Add(new ChoiceActionItem(childNodeType.Key, childNodeType.Value));
                     }
                 }
             }
