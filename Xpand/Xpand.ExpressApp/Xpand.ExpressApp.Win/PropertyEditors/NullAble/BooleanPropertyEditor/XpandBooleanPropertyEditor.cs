306795311d5af50ef4e46f43635d84461525dda0
apobekiaris <apostolis.bekiaris@gmail.com>
2013-01-27 15:32:02 +0200

    XpandBooleanPropertyEditor wrong binding
    http://www.expandframework.com/forum/6-bugs/3211-confusing-behavior-of-xpandbooleanpropertyeditor.html#3233

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/NullAble/BooleanPropertyEditor/XpandBooleanPropertyEditor.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/NullAble/BooleanPropertyEditor/XpandBooleanPropertyEditor.cs
@@ -6,17 +6,19 @@ using DevExpress.XtraEditors.Repository;
 using Xpand.Utils.Helpers;
 
 namespace Xpand.ExpressApp.Win.PropertyEditors.NullAble.BooleanPropertyEditor {
-    [PropertyEditor(typeof(bool?),true)]
-    [PropertyEditor(typeof(bool),true)]
+    [PropertyEditor(typeof(bool?), true)]
+    [PropertyEditor(typeof(bool), true)]
     public class XpandBooleanPropertyEditor : DevExpress.ExpressApp.Win.Editors.BooleanPropertyEditor {
         public XpandBooleanPropertyEditor(Type objectType, IModelMemberViewItem model)
             : base(objectType, model) {
+            ControlBindingProperty = "EditValue";
         }
 
         protected override void SetupRepositoryItem(RepositoryItem item) {
             base.SetupRepositoryItem(item);
-            if (item is RepositoryItemBooleanEdit)
-                ((RepositoryItemBooleanEdit)item).AllowGrayed = MemberInfo.MemberType.IsNullableType();
+            var edit = item as RepositoryItemBooleanEdit;
+            if (edit != null)
+                edit.AllowGrayed = MemberInfo.MemberType.IsNullableType();
         }
     }
 }
