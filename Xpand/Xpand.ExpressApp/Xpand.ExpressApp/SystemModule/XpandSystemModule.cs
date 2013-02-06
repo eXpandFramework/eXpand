5960a5e7216e24a1f1c6bddc9a7d9556045635a0
apobekiaris <apostolis.bekiaris@gmail.com>
2013-02-06 14:53:39 +0200

    Do not create XPandXpoInfoDataSource in tests

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/XpandSystemModule.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/XpandSystemModule.cs
@@ -28,7 +28,7 @@ namespace Xpand.ExpressApp.SystemModule {
     [Browsable(true)]
     [EditorBrowsable(EditorBrowsableState.Always)]
     [ToolboxBitmap(typeof(XafApplication), "Resources.SystemModule.ico")]
-    public sealed class XpandSystemModule : XpandModuleBase, IModelXmlConverter, IModelNodeUpdater<IModelMemberEx> {
+    public sealed class XpandSystemModule : XpandModuleBase, IModelXmlConverter, IModelNodeUpdater<IModelMemberEx>, ITestSupport {
         public XpandSystemModule() {
             RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
             RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Security.SecurityModule));
@@ -60,7 +60,7 @@ namespace Xpand.ExpressApp.SystemModule {
         }
 
         public override void Setup(XafApplication application) {
-            if (RuntimeMode && (XafTypesInfo.PersistentEntityStore is XpandXpoTypeInfoSource))
+            if (RuntimeMode && (XafTypesInfo.PersistentEntityStore is XpandXpoTypeInfoSource) && !((ITestSupport)this).IsTesting)
                 XafTypesInfo.SetPersistentEntityStore(new XpandXpoTypeInfoSource((TypesInfo)TypesInfo));
             base.Setup(application);
             application.CreateCustomCollectionSource += LinqCollectionSourceHelper.CreateCustomCollectionSource;
@@ -165,6 +165,11 @@ namespace Xpand.ExpressApp.SystemModule {
                 parameters.NodeType = typeof(IModelRuntimeCalculatedMember);
             }
         }
+
+        bool ITestSupport.IsTesting { get; set; }
     }
 
+    public interface ITestSupport {
+        bool IsTesting { get; set; }
+    }
 }
