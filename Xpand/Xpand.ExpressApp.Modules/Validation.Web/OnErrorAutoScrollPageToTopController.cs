384656092b068ac2fbdee28b45ed68baf86866f5
apobekiaris <apostolis.bekiaris@gmail.com>
2013-03-29 19:13:50 +0200

    [On Error Auto Scroll Web Page to Top]
    http://www.expandframework.com/forum/11-news/3327-auto-scroll-web-window-on-top-on-error.html#3501

---
+++ Xpand/Xpand.ExpressApp.Modules/Validation.Web/OnErrorAutoScrollPageToTopController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Web;
+using DevExpress.Persistent.Validation;
+
+namespace Xpand.ExpressApp.Validation.Web {
+    public interface IModelDetailViewOnErrorAutoScrollPageToTop : IModelNode {
+        [Category("eXpand.Validation")]
+        [DefaultValue(true)]
+        bool OnErrorAutoScrollPageToTop { get; set; }
+    }
+
+    public class OnErrorAutoScrollPageToTopController : ViewController<DetailView>, IModelExtender {
+        protected override void OnActivated() {
+            base.OnActivated();
+            if (AutoScrollEnabled())
+                Validator.RuleSet.ValidationCompleted += RuleSetOnValidationCompleted;
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            if (AutoScrollEnabled())
+                Validator.RuleSet.ValidationCompleted -= RuleSetOnValidationCompleted;
+        }
+
+        bool AutoScrollEnabled() {
+            return ((IModelDetailViewOnErrorAutoScrollPageToTop)View.Model).OnErrorAutoScrollPageToTop;
+        }
+
+        void RuleSetOnValidationCompleted(object sender, ValidationCompletedEventArgs validationCompletedEventArgs) {
+            if (validationCompletedEventArgs.Exception != null) {
+                var scriptKey = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+                const string script = "window.scrollTo(0, 0);";
+                WebWindow.CurrentRequestWindow.RegisterClientScript(scriptKey, script);
+            }
+        }
+
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            extenders.Add<IModelDetailView, IModelDetailViewOnErrorAutoScrollPageToTop>();
+        }
+    }
+}
