4bcff176b54c63445bad591db5ee58f10bc5335e
apobekiaris <apostolis.bekiaris@gmail.com>
2013-03-29 15:02:23 +0200

    raise version to 12.2.7.4

--- Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using Xpand;
 [assembly: AllowPartiallyTrustedCallers]
 
 public class XpandAssemblyInfo {
-    public const string Version = "12.2.7.3";
+    public const string Version = "12.2.7.4";
     public const string FileVersion = Version;
 }
