3f42e63b02cd5883d5fd1d7c04b5dfc3111487a8
apobekiaris <apostolis.bekiaris@gmail.com>
2013-04-18 17:22:42 +0300

    12.2.8.6

--- Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using Xpand;
 [assembly: AllowPartiallyTrustedCallers]
 
 public class XpandAssemblyInfo {
-    public const string Version = "12.2.8.5";
+    public const string Version = "12.2.8.6";
     public const string FileVersion = Version;
 }
