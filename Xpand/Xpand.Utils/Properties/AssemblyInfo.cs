9803dc6c794a72d86e12743448bcbf92fc7d5643
apobekiaris <apostolis.bekiaris@gmail.com>
2013-06-04 12:49:03 +0300

    12.2.10.1

--- Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Security;
 [assembly: AllowPartiallyTrustedCallers]
 
 public class XpandAssemblyInfo {
-    public const string Version = "12.2.9.1";
+    public const string Version = "12.2.10.1";
     public const string FileVersion = Version;
     public const string TabAspNetModules = "eXpand: Web Modules";
     public const string TabWinModules = "eXpand: Win Modules";
