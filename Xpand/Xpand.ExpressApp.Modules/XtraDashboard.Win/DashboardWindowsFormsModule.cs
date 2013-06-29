8a0a7f9610c33bbbf829c3be0df3ee0e36092d44
apobekiaris <apostolis.bekiaris@gmail.com>
2013-06-30 02:05:31 +0300

    [Dashboard support for print by diebuche]
    http://expandframework.com/forum/14-samples/3425-small-improvement-on-dashboard-module.html#3932

--- Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/DashboardWindowsFormsModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/DashboardWindowsFormsModule.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel;
 using System.Drawing;
+using DevExpress.ExpressApp.Model;
 using DevExpress.Utils;
 using Xpand.ExpressApp.Dashboard;
+using Xpand.ExpressApp.XtraDashboard.Win.PropertyEditors;
 
 namespace Xpand.ExpressApp.XtraDashboard.Win {
     [ToolboxBitmap(typeof(DashboardWindowsFormsModule))]
@@ -10,6 +12,10 @@ namespace Xpand.ExpressApp.XtraDashboard.Win {
     public sealed class DashboardWindowsFormsModule : XpandModuleBase {
         public DashboardWindowsFormsModule() {
             RequiredModuleTypes.Add(typeof(DashboardModule));
+        }
+        public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            base.ExtendModelInterfaces(extenders);
+            extenders.Add<IModelPropertyEditor, IModelPropertyEditorDashboardViewEditor>();
         }
     }
 }
