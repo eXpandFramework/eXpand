0cc15ce128d6279abd9315a2b28df7ffa81fd759
apobekiaris <apostolis.bekiaris@gmail.com>
2013-07-12 00:20:59 +0300

    XpandNavigationItemAttribute throws when CloneViewAttribute is used

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/NodeUpdaters/XpandNavigationItemNodeUpdater.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/NodeUpdaters/XpandNavigationItemNodeUpdater.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Model.Core;
-using DevExpress.ExpressApp.Model.NodeGenerators;
 using DevExpress.ExpressApp.SystemModule;
 using System.Linq;
 using Xpand.ExpressApp.Attributes;
@@ -15,27 +14,30 @@ namespace Xpand.ExpressApp.NodeUpdaters {
                 var navigationItemAttributes = modelClass.TypeInfo.FindAttributes<XpandNavigationItemAttribute>();
                 foreach (var itemAttribute in navigationItemAttributes) {
                     var paths = itemAttribute.Path.Split('/');
-                    AddNodes(((IModelRootNavigationItems)node).Items, paths.ToList(), ViewId(itemAttribute, modelClass.TypeInfo.Type.Namespace), itemAttribute.ObjectKey, itemAttribute.Index);
+                    AddNodes(((IModelRootNavigationItems)node).Items, paths.ToList(), ViewIds(itemAttribute, modelClass.TypeInfo.Type.Namespace), itemAttribute.ObjectKey, itemAttribute.Index);
                 }
             }
         }
 
-        string ViewId(XpandNavigationItemAttribute itemAttribute, string ns) {
-            return !ModelNodeIdHelper.ProcessShortViewIDs && itemAttribute.ViewId.IndexOf(".", StringComparison.Ordinal)==-1 ? string.Format("{0}.{1}", ns, itemAttribute.ViewId) : itemAttribute.ViewId;
+        string[] ViewIds(XpandNavigationItemAttribute itemAttribute, string ns) {
+            return new[] { string.Format("{0}.{1}", ns, itemAttribute.ViewId), itemAttribute.ViewId };
         }
 
-        void AddNodes(IModelNavigationItems navigationItems, List<string> strings, string viewId, string objectKey, int index) {
+        void AddNodes(IModelNavigationItems navigationItems, List<string> strings, string[] viewIds, string objectKey, int index) {
             if (strings.Count == 0) {
-                var modelView = navigationItems.Application.Views.FirstOrDefault(view => view.Id == viewId);
-                if (modelView == null)
-                    throw new NullReferenceException(viewId + " not found in Application.Views");
+                var modelView = navigationItems.Application.Views[viewIds[0]];
+                if (modelView == null) {
+                    modelView = navigationItems.Application.Views[viewIds[1]];
+                    if (modelView==null)
+                        throw new NullReferenceException(viewIds + " not found in Application.Views");
+                }
                 ((IModelNavigationItem)navigationItems.Parent).View = modelView;
                 return;
             }
             var id = strings[0];
             IModelNavigationItem navigationItem = GetNavigationItem(navigationItems, id, objectKey, strings.Count == 1 ? index : -1);
             strings.RemoveAt(0);
-            AddNodes(navigationItem.Items, strings, viewId, objectKey, index);
+            AddNodes(navigationItem.Items, strings, viewIds, objectKey, index);
         }
 
         IModelNavigationItem GetNavigationItem(IModelNavigationItems navigationItems, string id, string objectKey, int index) {
