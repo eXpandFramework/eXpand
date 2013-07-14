6e96f4db8353f1315917b43906a0700cd2d6dbab
apobekiaris <apostolis.bekiaris@gmail.com>
2013-07-14 22:55:13 +0300

    [IsAdministratorPermission by Marcel  ]
    http://www.devexpress.com/Support/Center/Question/Details/Q438941

---
+++ Xpand/Xpand.ExpressApp.Modules/Security/Permissions/IsAdministratorPermission.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.ExpressApp.Security;
+
+namespace Xpand.ExpressApp.Security.Permissions {
+    public class IsAdministratorPermissionRequest : IPermissionRequest {
+        public object GetHashObject() {
+            return GetType().FullName;
+        }
+    }
+
+    public class IsAdministratorPermissionRequestProcessor : PermissionRequestProcessorBase<IsAdministratorPermissionRequest> {
+        private readonly IPermissionDictionary permissions;
+
+        public IsAdministratorPermissionRequestProcessor(IPermissionDictionary permissions) {
+            this.permissions = permissions;
+        }
+
+        public override bool IsGranted(IsAdministratorPermissionRequest permissionRequest) {
+            return (permissions.FindFirst<IsAdministratorPermission>() != null);
+        }
+    }
+}
