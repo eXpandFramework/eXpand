9a4e5c36ebf8f4fcb86dbb0d85cfcabd780f3600
apobekiaris <apostolis.bekiaris@gmail.com>
2013-07-30 16:58:33 +0300

    [StateMachineTester-->use XpandRole to utilize StateMachine Permission]

--- Xpand/Xpand.ExpressApp.Modules/StateMachine/Security/StatePermissionController.cs
+++ Xpand/Xpand.ExpressApp.Modules/StateMachine/Security/StatePermissionController.cs
@@ -12,6 +12,13 @@ namespace Xpand.ExpressApp.StateMachine.Security {
     public interface IModelOptionsStateMachine {
         bool PermissionsForActionState { get; set; }
     }
+
+    public class StateMachineController : DevExpress.ExpressApp.StateMachine.StateMachineController {
+        protected override void UpdateActionState() {
+            base.UpdateActionState();
+
+        }
+    }
     public class StatePermissionController : ViewController, IModelExtender {
         void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
             extenders.Add<IModelOptions, IModelOptionsStateMachine>();
@@ -20,8 +27,10 @@ namespace Xpand.ExpressApp.StateMachine.Security {
         protected override void OnActivated() {
             base.OnActivated();
             var stateMachineController = Frame.GetController<StateMachineController>();
-            if (((IModelOptionsStateMachine)Application.Model.Options).PermissionsForActionState)
-                ((SingleChoiceAction)stateMachineController.Actions["ChangeStateAction"]).ItemsChanged += OnItemsChanged;
+            if (((IModelOptionsStateMachine)Application.Model.Options).PermissionsForActionState) {
+                var singleChoiceAction = stateMachineController.ChangeStateAction;
+                singleChoiceAction.ItemsChanged += OnItemsChanged;
+            }
             stateMachineController.TransitionExecuting += OnTransitionExecuting;
         }
 
@@ -32,29 +41,29 @@ namespace Xpand.ExpressApp.StateMachine.Security {
                     var choiceActionItem = ((ChoiceActionItem)actionItemChangesType.Key);
                     var transition = choiceActionItem.Data as ITransition;
                     if (transition != null)
-                        choiceActionItem.Active["Permission"] = !IsNotGranted(transition.TargetState);
+                        choiceActionItem.Active["Permission"] = !IsGranted(transition.TargetState);
                 }
             }
         }
 
         void OnTransitionExecuting(object sender, ExecuteTransitionEventArgs executeTransitionEventArgs) {
             var targetState = executeTransitionEventArgs.Transition.TargetState;
-            if (IsNotGranted(targetState))
+            if (!IsGranted(targetState))
                 throw new UserFriendlyException("Permissions are not granted for transitioning to the " + targetState.Caption);
         }
 
-        bool IsNotGranted(IState state) {
+        bool IsGranted(IState state) {
             if (!((IRoleTypeProvider)SecuritySystem.Instance).IsNewSecuritySystem())
-                return IsNotGranted(new StateMachineTransitionPermission(StateMachineTransitionModifier.Deny, state.Caption, state.StateMachine.Name));
+                return IsGranted(new StateMachineTransitionPermission(StateMachineTransitionModifier.Allow, state.Caption, state.StateMachine.Name));
             var stateMachineTransitionPermission = new Improved.StateMachineTransitionPermission {
-                Modifier = StateMachineTransitionModifier.Deny,
+                Modifier = StateMachineTransitionModifier.Allow,
                 StateCaption = state.Caption,
                 StateMachineName = state.StateMachine.Name
             };
-            return !SecuritySystem.IsGranted(new StateMachineTransitionOperationRequest(stateMachineTransitionPermission));
+            return SecuritySystem.IsGranted(new StateMachineTransitionOperationRequest(stateMachineTransitionPermission));
         }
 
-        bool IsNotGranted(IPermission permission) {
+        bool IsGranted(IPermission permission) {
             var securityComplex = ((SecurityBase)SecuritySystem.Instance);
             bool isGrantedForNonExistentPermission = securityComplex.IsGrantedForNonExistentPermission;
             securityComplex.IsGrantedForNonExistentPermission = true;
