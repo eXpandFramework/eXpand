64c63dc3802cc9bc47b2e51a59f127b1887f54a1
apobekiaris <apostolis.bekiaris@gmail.com>
2013-08-13 14:18:35 +0300

    [Merge Logic.Conditional, Logic.Win into Logic assembly]

--- Xpand/Xpand.ExpressApp.Modules/Logic.Conditional/Security/ConditionalLogicRulePermission.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xpand.ExpressApp.Logic.Security;
-using Xpand.Persistent.Base.Logic;
-
-namespace Xpand.ExpressApp.Logic.Conditional.Security {
-    public abstract class ConditionalLogicRulePermission:LogicRulePermission,IConditionalLogicRule {
-        public string NormalCriteria { get; set; }
-        public string EmptyCriteria { get; set; }
-    }
-}
--- Xpand/Xpand.ExpressApp.Modules/Logic.Conditional/_ConditionalLogicRuleInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace eXpand.ExpressApp.Logic.Conditional {
-    public abstract class ConditionalLogicRuleInfo<TConditionalLogicRule> : LogicRuleInfo<TConditionalLogicRule> where TConditionalLogicRule:IConditionalLogicRule
-    {
-        
-    }
-}
