ffad940fae020213f7a89351bb290fa7a865f155
apobekiaris <apostolis.bekiaris@gmail.com>
2013-08-17 20:11:21 +0300

    FIlter ExecutionContexts without the LogicInstaller attribute

--- Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelExecutionContextDomainLogic.cs
+++ Xpand/Xpand.ExpressApp.Modules/Logic/DomainLogic/ModelExecutionContextDomainLogic.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Xpand.Persistent.Base.Logic;
 using Xpand.Persistent.Base.Logic.Model;
 using Xpand.Persistent.Base.ModelDifference;
 
@@ -12,10 +11,13 @@ namespace Xpand.ExpressApp.Logic.DomainLogic {
                 modelLogic = modelLogic.Parent;
             }
             var typeInfo = ((ITypesInfoProvider) modelLogic.Application).TypesInfo.FindTypeInfo(modelLogic.GetType());
-            var modelLogicInterface = typeInfo.ImplementedInterfaces.First(info => typeof (IModelLogic) != info.Type && typeof (IModelLogic).IsAssignableFrom(info.Type));
-            var installerType = modelLogicInterface.FindAttribute<LogicInstallerAttribute>().InstallerType;
-            var executionContexts = LogicInstallerManager.Instance.LogicInstallers.First(installerType.IsInstanceOfType).ExecutionContexts;
-            return executionContexts.Select(context => context.ToString()).ToList();
+            var modelLogicInterface = typeInfo.ImplementedInterfaces.First(info 
+                => typeof (IModelLogic) != info.Type && typeof (IModelLogic).IsAssignableFrom(info.Type));
+            return LogicInstallerManager.Instance.LogicInstallers.First(logicInstaller
+                => ReferenceEquals(logicInstaller.GetModelLogic().GetType(), modelLogicInterface)).ExecutionContexts.Select(context 
+                    => context.ToString()).ToList();
+
         }
+
     }
 }
