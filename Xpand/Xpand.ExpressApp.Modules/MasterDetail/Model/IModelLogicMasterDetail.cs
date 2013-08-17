d3f8344632caa1e3529adfd7e16c485603ba4bec
apobekiaris <apostolis.bekiaris@gmail.com>
2013-08-17 22:11:33 +0300

    remove logicinstaller attribute

--- Xpand/Xpand.ExpressApp.Modules/MasterDetail/Model/IModelLogicMasterDetail.cs
+++ Xpand/Xpand.ExpressApp.Modules/MasterDetail/Model/IModelLogicMasterDetail.cs
@@ -1,12 +1,10 @@
 ﻿using Xpand.ExpressApp.MasterDetail.Logic;
 using Xpand.Persistent.Base.General;
-using Xpand.Persistent.Base.Logic;
 using Xpand.Persistent.Base.Logic.Model;
 
 namespace Xpand.ExpressApp.MasterDetail.Model {
 
     [ModelLogicValidRule(typeof(IMasterDetailRule))]
-    [LogicInstaller(typeof(MasterDetailLogicInstaller))]
     public interface IModelLogicMasterDetail : IModelLogic {
 
     }
