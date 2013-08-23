f3812899512a2946c907efd73641cb94ad24d37e
apobekiaris <apostolis.bekiaris@gmail.com>
2013-08-23 19:07:53 +0300

    Update AuditTrialTester
    http://www.expandframework.com/forum/6-bugs/3513-13-1-6-5-audittraildemo-problems/4328.html

--- Demos/Modules/AuditTrail/AuditTrailTester.Web/WebApplication.cs
+++ Demos/Modules/AuditTrail/AuditTrailTester.Web/WebApplication.cs
@@ -1,48 +1,70 @@
 using System;
-using DevExpress.ExpressApp;
 using System.ComponentModel;
-using DevExpress.ExpressApp.Xpo;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using AuditTrailTester.Module;
+using AuditTrailTester.Module.Web;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.AuditTrail;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.Strategy;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Validation;
 using DevExpress.ExpressApp.Web;
-using System.Collections.Generic;
+using DevExpress.ExpressApp.Web.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using Xpand.ExpressApp.AuditTrail;
+using Xpand.ExpressApp.Logic;
+
 //using DevExpress.ExpressApp.Security;
 
 namespace AuditTrailTester.Web {
     // You can override various virtual methods and handle corresponding events to manage various aspects of your XAF application UI and behavior.
-    public partial class AuditTrailTesterAspNetApplication : WebApplication { // http://documentation.devexpress.com/#Xaf/DevExpressExpressAppWebWebApplicationMembersTopicAll
-        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
-        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
-        private AuditTrailTester.Module.AuditTrailTesterModule module3;
-        private AuditTrailTester.Module.Web.AuditTrailTesterAspNetModule module4;
-        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    public class AuditTrailTesterAspNetApplication : WebApplication {
+        // http://documentation.devexpress.com/#Xaf/DevExpressExpressAppWebWebApplicationMembersTopicAll
+        AuditTrailModule auditTrailModule1;
+        AuthenticationStandard authenticationStandard1;
+        ConditionalAppearanceModule conditionalAppearanceModule1;
+        LogicModule logicModule1;
+        SystemModule module1;
+        SystemAspNetModule module2;
+        AuditTrailTesterModule module3;
+        AuditTrailTesterAspNetModule module4;
+        SecurityModule securityModule1;
+        SecurityStrategyComplex securityStrategyComplex1;
+        SqlConnection sqlConnection1;
+        ValidationModule validationModule1;
+        XpandAuditTrailModule xpandAuditTrailModule1;
 
         public AuditTrailTesterAspNetApplication() {
             InitializeComponent();
+            LastLogonParametersRead += OnLastLogonParametersRead;
         }
 
-        // Override to execute custom code after a logon has been performed, the SecuritySystem object is initialized, logon parameters have been saved and user model differences are loaded.
-        protected override void OnLoggedOn(LogonEventArgs args) { // http://documentation.devexpress.com/#Xaf/DevExpressExpressAppXafApplication_LoggedOntopic
-            base.OnLoggedOn(args);
-        }
-
-        // Override to execute custom code after a user has logged off.
-        protected override void OnLoggedOff() { // http://documentation.devexpress.com/#Xaf/DevExpressExpressAppXafApplication_LoggedOfftopic
-            base.OnLoggedOff();
+        void OnLastLogonParametersRead(object sender, LastLogonParametersReadEventArgs lastLogonParametersReadEventArgs) {
+            var parameters = ((AuthenticationStandardLogonParameters) lastLogonParametersReadEventArgs.LogonObject);
+            if (string.IsNullOrEmpty(parameters.UserName))
+                parameters.UserName = "User";
         }
 
         protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
             args.ObjectSpaceProvider = new XPObjectSpaceProviderThreadSafe(args.ConnectionString, args.Connection);
         }
 
-        private void AuditTrailTesterAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+        void AuditTrailTesterAspNetApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
 #if EASYTEST
 			e.Updater.Update();
 			e.Handled = true;
 #else
-            if (System.Diagnostics.Debugger.IsAttached) {
+            if (Debugger.IsAttached) {
                 e.Updater.Update();
                 e.Handled = true;
-            } else {
-                string message = "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
+            }
+            else {
+                string message =
+                    "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
                     "This error occurred  because the automatic database update was disabled when the application was started without debugging.\r\n" +
                     "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " +
                     "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " +
@@ -60,31 +82,64 @@ namespace AuditTrailTester.Web {
 #endif
         }
 
-        private void InitializeComponent() {
-            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
-            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
-            this.module3 = new AuditTrailTester.Module.AuditTrailTesterModule();
-            this.module4 = new AuditTrailTester.Module.Web.AuditTrailTesterAspNetModule();
-            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
-            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+        void InitializeComponent() {
+            module1 = new SystemModule();
+            module2 = new SystemAspNetModule();
+            module3 = new AuditTrailTesterModule();
+            module4 = new AuditTrailTesterAspNetModule();
+            sqlConnection1 = new SqlConnection();
+            auditTrailModule1 = new AuditTrailModule();
+            securityModule1 = new SecurityModule();
+            validationModule1 = new ValidationModule();
+            conditionalAppearanceModule1 = new ConditionalAppearanceModule();
+            logicModule1 = new LogicModule();
+            xpandAuditTrailModule1 = new XpandAuditTrailModule();
+            securityStrategyComplex1 = new SecurityStrategyComplex();
+            authenticationStandard1 = new AuthenticationStandard();
+            ((ISupportInitialize) (this)).BeginInit();
             // 
             // sqlConnection1
             // 
-            this.sqlConnection1.ConnectionString = @"Integrated Security=SSPI;Pooling=false;Data Source=.\SQLEXPRESS;Initial Catalog=AuditTrailTester";
-            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            sqlConnection1.ConnectionString =
+                "Integrated Security=SSPI;Pooling=false;Data Source=.\\SQLEXPRESS;Initial Catalog=A" +
+                "uditTrailTester";
+            sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // auditTrailModule1
+            // 
+            auditTrailModule1.AuditDataItemPersistentType = typeof (AuditDataItemPersistent);
+            // 
+            // validationModule1
+            // 
+            validationModule1.AllowValidationDetailsAccess = true;
+            // 
+            // securityStrategyComplex1
+            // 
+            securityStrategyComplex1.Authentication = authenticationStandard1;
+            securityStrategyComplex1.RoleType = typeof (SecuritySystemRole);
+            securityStrategyComplex1.UserType = typeof (SecuritySystemUser);
+            // 
+            // authenticationStandard1
+            // 
+            authenticationStandard1.LogonParametersType = typeof (AuthenticationStandardLogonParameters);
             // 
             // AuditTrailTesterAspNetApplication
             // 
-            this.ApplicationName = "AuditTrailTester";
-            this.Connection = this.sqlConnection1;
-            this.Modules.Add(this.module1);
-            this.Modules.Add(this.module2);
-            this.Modules.Add(this.module3);
-            this.Modules.Add(this.module4);
-
-            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.AuditTrailTesterAspNetApplication_DatabaseVersionMismatch);
-            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
-
+            ApplicationName = "AuditTrailTester";
+            Connection = sqlConnection1;
+            Modules.Add(module1);
+            Modules.Add(module2);
+            Modules.Add(auditTrailModule1);
+            Modules.Add(securityModule1);
+            Modules.Add(validationModule1);
+            Modules.Add(conditionalAppearanceModule1);
+            Modules.Add(logicModule1);
+            Modules.Add(xpandAuditTrailModule1);
+            Modules.Add(module3);
+            Modules.Add(module4);
+            Security = securityStrategyComplex1;
+            DatabaseVersionMismatch += AuditTrailTesterAspNetApplication_DatabaseVersionMismatch;
+            ((ISupportInitialize) (this)).EndInit();
         }
     }
 }
