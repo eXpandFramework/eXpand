09bca26c0ad0005c732c53a6a5a31ee3287e7126
apobekiaris <apostolis.bekiaris@gmail.com>
2013-09-13 10:43:25 +0300

    ViewVariantsTester --> sample data

---
+++ Demos/Modules/ViewVariants/ViewVariantsTester.Module/BusinessObjects/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ViewVariantsTester.Module.BusinessObjects {
+    [DefaultClassOptions]
+    public class TestClass:Person {
+        public TestClass(Session session) : base(session) {
+        }
+    }
+}
