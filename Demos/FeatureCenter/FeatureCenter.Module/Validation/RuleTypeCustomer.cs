5c55ece3f8844e56dad7244d9db7c83cea62fdf5
apobekiaris <apostolis.bekiaris@gmail.com>
2013-09-18 00:15:01 +0300

    13.1.6.24

--- Demos/FeatureCenter/FeatureCenter.Module/Validation/RuleTypeCustomer.cs
+++ Demos/FeatureCenter/FeatureCenter.Module/Validation/RuleTypeCustomer.cs
@@ -2,19 +2,19 @@
 using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
 using FeatureCenter.Base;
-using Xpand.ExpressApp.Validation;
+using Xpand.ExpressApp.Validation.RuleType;
 
 namespace FeatureCenter.Module.Validation {
 
     [RuleCombinationOfPropertiesIsUnique("aaa", DefaultContexts.Save, "WarningProperty,Information")]
-    [RuleErrorType("aaa", RuleType.Warning)]
+    [RuleType("aaa", RuleType.Warning)]
     public class RuleTypeCustomer : CustomerBase {
         public RuleTypeCustomer(Session session)
             : base(session) {
         }
         private string _warningProperty;
         [RuleRequiredField("Required_WarningProperty_for_Warning_Customer", DefaultContexts.Save)]
-        [RuleErrorType("Required_WarningProperty_for_Warning_Customer", RuleType.Warning)]
+        [RuleType("Required_WarningProperty_for_Warning_Customer", RuleType.Warning)]
         public string WarningProperty {
             get {
                 return _warningProperty;
@@ -25,7 +25,7 @@ namespace FeatureCenter.Module.Validation {
         }
         private string _information;
         [RuleRequiredField("Required_InformationProperty_for_Warning_Customer", DefaultContexts.Save)]
-        [RuleErrorType("Required_InformationProperty_for_Warning_Customer", RuleType.Information)]
+        [RuleType("Required_InformationProperty_for_Warning_Customer", RuleType.Information)]
         public string Information {
             get {
                 return _information;
@@ -38,7 +38,7 @@ namespace FeatureCenter.Module.Validation {
 
         [RuleValueComparison("RuleStringComparison_InfoOnControlValueChanged_for_Warning_Customer",
             RuleTypeController.ObjectSpaceObjectChanged, ValueComparisonType.NotEquals, "value")]
-        [RuleErrorType("RuleStringComparison_InfoOnControlValueChanged_for_Warning_Customer", RuleType.Information)]
+        [RuleType("RuleStringComparison_InfoOnControlValueChanged_for_Warning_Customer", RuleType.Information)]
         [ImmediatePostData]
         public string InfoOnControlValueChanged {
             get { return _infoOnControlValueChanged; }
