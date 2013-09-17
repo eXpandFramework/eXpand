6e677adcac11d0bddb00a61b7d70e3c86aecbe34
apobekiaris <apostolis.bekiaris@gmail.com>
2013-09-18 00:25:25 +0300

    13.1.6.25

--- Demos/Modules/Validation/ValidationTester.Module/BusinessObjects/Customer.cs
+++ Demos/Modules/Validation/ValidationTester.Module/BusinessObjects/Customer.cs
@@ -2,9 +2,9 @@
 using System;
 using DevExpress.Xpo;
 using DevExpress.Persistent.Validation;
-using Xpand.ExpressApp.Validation;
 using DevExpress.Persistent.Base;
 using System.ComponentModel;
+using Xpand.ExpressApp.Validation.RuleType;
 
 namespace ValidationTester.Module.BusinessObjects {
     [DefaultClassOptions]
@@ -27,14 +27,14 @@ namespace ValidationTester.Module.BusinessObjects {
         }
 
         [RuleRequiredField("City_WarningProperty", DefaultContexts.Save)]
-        [RuleErrorType("City_WarningProperty", RuleType.Warning)]
+        [RuleType("City_WarningProperty", RuleType.Warning)]
         public string City {
             get { return _city; }
             set { SetPropertyValue("City", ref _city, value); }
         }
 
         [RuleRange("Age_InformationProperty", DefaultContexts.Save, 10, 60, CustomMessageTemplate = "Too old or too young.")]
-        [RuleErrorType("Age_InformationProperty", RuleType.Information)]
+        [RuleType("Age_InformationProperty", RuleType.Information)]
         public int Age {
             get { return _age; }
             set { SetPropertyValue("Age", ref _age, value); }
@@ -42,7 +42,7 @@ namespace ValidationTester.Module.BusinessObjects {
 
         [RuleValueComparison("Registration_ValueChangedProperty", RuleTypeController.ObjectSpaceObjectChanged,
             ValueComparisonType.GreaterThanOrEqual, "LocalDateTimeToday()", ParametersMode.Expression, CustomMessageTemplate = "Registration before today?",SkipNullOrEmptyValues = false)]
-        [RuleErrorType("Registration_ValueChangedProperty", RuleType.Information)]
+        [RuleType("Registration_ValueChangedProperty", RuleType.Information)]
         [ImmediatePostData]
         public DateTime Registration {
             get { return _registration; }
