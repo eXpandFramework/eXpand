89a1f659ff886c9c0303ea7da6c6177df6c38914
apobekiaris <apostolis.bekiaris@gmail.com>
2013-09-17 14:11:39 +0300

    twitter tasked fixed

---
+++ Resource/Build/Twitter/XTwitter/Program.cs
@@ -0,0 +1,20 @@
+﻿using LinqToTwitter;
+
+namespace XTwitter {
+    class Program {
+
+        static void Main(string[] args) {
+            var auth = new SingleUserAuthorizer {
+                Credentials = new SingleUserInMemoryCredentials {
+                    ConsumerKey = args[0],
+                    ConsumerSecret = args[1],
+                    TwitterAccessToken = args[2],
+                    TwitterAccessTokenSecret = args[3]
+                }
+            };
+
+            var twitterCtx = new TwitterContext(auth);
+            twitterCtx.UpdateStatus(args[4]);
+        }
+    }
+}
