77c6b8580f27c72ec12ecd3be72421e07300e452
apobekiaris <apostolis.bekiaris@gmail.com>
2013-10-01 13:58:17 +0300

    do to not fetch data when there is no criteria
    http://www.expandframework.com/forum/8-discussion/3587-does-expand-have-functionality-to-not-fetch-data-when-there-is-no-criteria.html#4785

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/LoadWhenFilteredController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/LoadWhenFilteredController.cs
@@ -1,9 +1,4 @@
-﻿namespace Xpand.ExpressApp.Web.SystemModule
-{
-    public class LoadWhenFilteredController : ExpressApp.SystemModule.LoadWhenFilteredController
-    {
-        protected override string GetActiveFilter() {
-            return View.Model.Filter;
-        }
+﻿namespace Xpand.ExpressApp.Web.SystemModule {
+    public class LoadWhenFilteredController : ExpressApp.SystemModule.LoadWhenFilteredController {
     }
 }
--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/LoadWhenFilteredController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp/SystemModule/LoadWhenFilteredController.cs
@@ -20,9 +20,9 @@ namespace Xpand.ExpressApp.SystemModule {
 
     public abstract class LoadWhenFilteredController : ViewController<ListView> {
         protected const string LoadWhenFiltered = "LoadWhenFiltered";
+
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
-
             if (IsReady()) {
                 var filterController = Frame.GetController<FilterController>();
                 filterController.FullTextFilterAction.Execute += FullTextFilterAction_Execute;
@@ -32,10 +32,8 @@ namespace Xpand.ExpressApp.SystemModule {
 
         protected bool IsReady() {
             var modelListViewGridViewOptions = ((IModelListViewLoadWhenFiltered)View.Model);
-            return modelListViewGridViewOptions.LoadWhenFiltered && string.IsNullOrEmpty(GetActiveFilter());
+            return modelListViewGridViewOptions.LoadWhenFiltered && (string.IsNullOrEmpty(View.Model.Filter)&&string.IsNullOrEmpty(View.Model.Criteria));
         }
-
-        protected abstract string GetActiveFilter();
 
         void FullTextFilterAction_Execute(object sender, ParametrizedActionExecuteEventArgs e) {
             if (string.IsNullOrEmpty(e.ParameterCurrentValue as string))
@@ -45,9 +43,9 @@ namespace Xpand.ExpressApp.SystemModule {
         }
 
         protected BinaryOperator GetDoNotLoadWhenFilterExistsCriteria() {
-            IMemberInfo memberInfo = View.ObjectTypeInfo.KeyMember;
-            Type memberType = memberInfo.MemberType;
-            object o = memberType.IsValueType ? memberType.CreateInstance() : null;
+            var memberInfo = View.ObjectTypeInfo.KeyMember;
+            var memberType = memberInfo.MemberType;
+            var o = memberType.IsValueType ? memberType.CreateInstance() : null;
             return new BinaryOperator(memberInfo.Name, o);
         }
 
