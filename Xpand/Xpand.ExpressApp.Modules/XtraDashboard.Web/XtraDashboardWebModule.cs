72700ef98bdc4f682a44bb56d1dce743e3721b8d
apobekiaris <apostolis.bekiaris@gmail.com>
2013-10-06 01:09:09 +0300

    add xtradashboardweb to toolbox

--- Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/XtraDashboardWebModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/XtraDashboardWebModule.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.Utils;
 using Xpand.ExpressApp.Dashboard;
 using Xpand.Persistent.Base.General;
 
 namespace Xpand.ExpressApp.XtraDashboard.Web {
+    [ToolboxBitmap(typeof(XtraDashboardWebModule))]
+    [ToolboxItem(true)]
+    [ToolboxTabName(XpandAssemblyInfo.TabAspNetModules)]
     public sealed class XtraDashboardWebModule : XpandModuleBase { 
         public XtraDashboardWebModule() {
             RequiredModuleTypes.Add(typeof(DashboardModule));
