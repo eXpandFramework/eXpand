bbd389454cf540c6728a4bc6a0cfc6d0de3ee668
apobekiaris <apostolis.bekiaris@gmail.com>
2013-10-22 00:54:07 +0300

    [add model AlwaysVisibleCalculator]

---
+++ Xpand/Xpand.Persistent/Xpand.Persistent.Base/General/Model/VisibilityCalculators/AlwaysVisibleCalculator.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.ExpressApp.Model;
+
+namespace Xpand.Persistent.Base.General.Model.VisibilityCalculators {
+    public class AlwaysVisibleCalculator:IModelIsVisible {
+        public bool IsVisible(IModelNode node, string propertyName) {
+            return true;
+        }
+    }
+}
