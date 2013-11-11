aa8efb0e601e234c40428bc6a037cb410b14f27c
apobekiaris <apostolis.bekiaris@gmail.com>
2013-11-11 19:41:17 +0200

    Exception when FileAttachment and runtime model members
    http://www.expandframework.com/forum/6-bugs/3688-calculatedproperty-adding-exception-in-13-1-8-0-version/5157.html

--- Xpand/Xpand.ExpressApp.Modules/FileAttachment/XpandFileAttachmentsModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/FileAttachment/XpandFileAttachmentsModule.cs
@@ -2,6 +2,9 @@
 using System.ComponentModel;
 using System.Configuration;
 using System.Drawing;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.FileAttachments.Win;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.Persistent.Base;
@@ -10,8 +13,39 @@ using DevExpress.Xpo;
 using Xpand.ExpressApp.FileAttachment.BusinessObjects;
 using Xpand.Persistent.Base.General;
 using System.Linq;
+using AggregatedAttribute = DevExpress.Xpo.AggregatedAttribute;
 
 namespace Xpand.ExpressApp.FileAttachment {
+    public interface IModelCommonFileTypeFiltersEx : IModelCommonFileTypeFilters {
+        [ModelBrowsableAttribute(typeof(FileTypeFiltersVisibilityCalculator))]
+        new IModelFileTypeFilters FileTypeFilters { get; }
+    }
+
+    public class FileTypeFiltersVisibilityCalculator : IModelIsVisible {
+        public bool IsVisible(IModelNode node, String propertyName) {
+            var modelClass = node as IModelClass;
+            if (modelClass != null) {
+                ITypeInfo classTypeInfo = modelClass.TypeInfo;
+                if (classTypeInfo != null) {
+                    if (classTypeInfo.IsListType) {
+                        Type[] genericArguments = classTypeInfo.Type.GetGenericArguments();
+                        if (genericArguments.Length == 1) {
+                            classTypeInfo = XafTypesInfo.Instance.FindTypeInfo(genericArguments[0].FullName);
+                        }
+                    }
+                    if (classTypeInfo != null && (classTypeInfo.Implements<IFileData>() || classTypeInfo.FindAttribute<FileAttachmentAttribute>() != null)) {
+                        return true;
+                    }
+                }
+            }
+            var member = node as IModelMember;
+            if (member != null) {
+                return member.MemberInfo != null && typeof(IFileData).IsAssignableFrom(member.MemberInfo.MemberType);
+            }
+            return false;
+        }
+    }
+
     [ToolboxTabName(XpandAssemblyInfo.TabWinModules), ToolboxBitmap(typeof (XpandFileAttachmentsModule)), ToolboxItem(true)]
     public sealed class XpandFileAttachmentsModule : XpandModuleBase {
         public const string FileDataFolderName = "FileData";
@@ -20,9 +54,12 @@ namespace Xpand.ExpressApp.FileAttachment {
         public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
             base.ExtendModelInterfaces(extenders);
             extenders.Add<IModelOptions,IModelOptionsFileSystemStoreLocation>();
+            if (IsHosted&&RuntimeMode)
+                return;
+            extenders.Add<IModelCommonFileTypeFilters, IModelCommonFileTypeFiltersEx>();
         }
 
-        public override void CustomizeTypesInfo(DevExpress.ExpressApp.DC.ITypesInfo typesInfo) {
+        public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
             base.CustomizeTypesInfo(typesInfo);
             foreach (var memberInfo in typesInfo.PersistentTypes.SelectMany(info => info.Members)) {
                 if (typeof(IFileSystemObject).IsAssignableFrom(memberInfo.MemberType)) {
