6af39e97ba201d21fe4935bd21430def486da11b
apobekiaris <apostolis.bekiaris@gmail.com>
2013-11-23 23:46:05 +0200

    [DashboardTypesEditor for web base on the ChooseFromListCollectionEditor]

---
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/PropertyEditors/DashboardTypesEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using Xpand.ExpressApp.Dashboard.BusinessObjects;
+using Xpand.ExpressApp.Web.PropertyEditors;
+
+namespace Xpand.ExpressApp.XtraDashboard.Web.PropertyEditors {
+    [PropertyEditor(typeof(IList<ITypeWrapper>), true)]
+    public class DashboardTypesEditor : ChooseFromListCollectionEditor {
+        public DashboardTypesEditor(Type objectType, IModelMemberViewItem info)
+            : base(objectType, info) {
+        }
+    }
+}
