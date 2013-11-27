d80b807892fe2f6da54bd4898ef8640daafc7e7f
apobekiaris <apostolis.bekiaris@gmail.com>
2013-11-27 16:17:32 +0200

    untracked files on master: ca97fa6 ModelDifference editor problem - XML difrent is not create and changes are not saved

---
+++ Demos/Modules/FileAttachment/FileAttachmentTester.Module/BusinessObjects/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using Xpand.ExpressApp.FileAttachment.BusinessObjects;
+
+namespace FileAttachmentTester.Module.BusinessObjects {
+[DefaultClassOptions]
+  public partial class BusinessObject1 : DevExpress.Persistent.BaseImpl.BaseObject
+  {
+
+    public BusinessObject1(DevExpress.Xpo.Session session)
+      : base(session)
+    {
+    }
+    [DevExpress.Xpo.Aggregated, ExpandObjectMembers(ExpandObjectMembers.Never), NoForeignKey, ImmediatePostData]
+    public FileSystemStoreObject File
+    {
+        get { return GetPropertyValue<FileSystemStoreObject>("File"); }
+        set { SetPropertyValue("File", value); }
+    }
+  }
+}
