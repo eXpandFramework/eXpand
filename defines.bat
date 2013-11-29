d8bdd9a737e92fdce9e53b81ec049786a40e0c66
apobekiaris <apostolis.bekiaris@gmail.com>
2013-11-29 04:54:43 +0200

    support for VS2013
    http://www.expandframework.com/forum/5-installation-configuration-a-updates/3721-vs-2013-installation-problem.html#5346

--- defines.bat
+++ defines.bat
@@ -5,6 +5,7 @@ rem uncomment the line bellow if you not use vs2010
 rem set vsver=vs2008
 rem uncomment the line bellow if you not use vs2012
 rem set vsver=vs2012
+rem set vsver=vs2013
 
 set msbuild="%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 
@@ -14,6 +15,16 @@ set Gac4path="%WinDir%\Microsoft.NET\assembly\GAC_MSIL\"
 if '%vsver%'=='vs2008' goto vs2008
 if '%vsver%'=='vs2010' goto vs2010
 if '%vsver%'=='vs2012' goto vs2012
+if '%vsver%'=='vs2013' goto vs2013
+
+:vs2013
+set sn="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\sn.exe"
+set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\gacutil.exe"
+set csharptemplates="%ProgramFiles%\Microsoft Visual Studio 12.0\Common7\IDE\ProjectTemplates\CSharp\DevExpress XAF\"
+set vbtemplates="%ProgramFiles%\Microsoft Visual Studio 12.0\Common7\IDE\ProjectTemplates\VisualBasic\DevExpress XAF\"
+set devenv="%ProgramFiles%\Microsoft Visual Studio 12.0\Common7\IDE\"
+goto end
+goto end
 
 :vs2012
 set sn="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\sn.exe"
@@ -22,7 +33,6 @@ set csharptemplates="%ProgramFiles%\Microsoft Visual Studio 11.0\Common7\IDE\Pro
 set vbtemplates="%ProgramFiles%\Microsoft Visual Studio 11.0\Common7\IDE\ProjectTemplates\VisualBasic\DevExpress XAF\"
 set devenv="%ProgramFiles%\Microsoft Visual Studio 11.0\Common7\IDE\"
 goto end
-
 goto end
 
 :vs2008
