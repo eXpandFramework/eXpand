14cbda15ec57f4926d1ff8f6c56e3808c7fc961b
apobekiaris <apostolis.bekiaris@gmail.com>
2013-12-01 22:06:41 +0200

    StateMachine-->populate changestateaction with all transitions when adminroles
    http://www.expandframework.com/forum/13-suggestions/3735-state-machine-enhancements.html?start=6#5408

--- Xpand/Xpand.ExpressApp.Modules/StateMachine/Controllers/XpoStateMachineExtensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/StateMachine/Controllers/XpoStateMachineExtensions.cs
@@ -1,13 +1,14 @@
 using System.Linq;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.StateMachine;
 using DevExpress.ExpressApp.StateMachine.Xpo;
 using DevExpress.Xpo;
 
 namespace Xpand.ExpressApp.StateMachine.Controllers {
     public static class XpoStateMachineExtensions {
-        public static bool CanExecuteTransition(this XpoStateMachine stateMachine) {
-            var collection = (XPBaseCollection)stateMachine.GetMemberValue(XpandStateMachineModule.AdminRoles);
+        public static bool CanExecuteTransition(this IStateMachine stateMachine) {
+            var collection = (XPBaseCollection)((XpoStateMachine) stateMachine).GetMemberValue(XpandStateMachineModule.AdminRoles);
             return collection.OfType<ISecurityRole>().Any(IsInRole);
         }
         static bool IsInRole(ISecurityRole securityRole) {
