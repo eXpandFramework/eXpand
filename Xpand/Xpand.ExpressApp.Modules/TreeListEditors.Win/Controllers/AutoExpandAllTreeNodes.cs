2ea04c9b780c3225f728ab7248e424aa5a8928fc
apobekiaris <apostolis.bekiaris@gmail.com>
2013-12-31 06:10:36 +0200

    [AutoExpandAllTreeNodes]
    http://www.expandframework.com/forum/8-discussion/3795-auto-expand-all-nodes-on-xpandtreelisteditor/5623.html

---
+++ Xpand/Xpand.ExpressApp.Modules/TreeListEditors.Win/Controllers/AutoExpandAllTreeNodes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.TreeListEditors.Win;
+using DevExpress.Persistent.Base.General;
+using DevExpress.XtraTreeList;
+using ListView = DevExpress.ExpressApp.ListView;
+
+namespace Xpand.ExpressApp.TreeListEditors.Win.Controllers{
+    public interface IModelListViewAutoExpandAllTreeNodes {
+        [Category("eXpand.TreeList")]
+        bool AutoExpandAllTreeNodes { get; set; }
+    }
+
+    public class AutoExpandAllTreeNodesVisibilityCalculator:IModelIsVisible{
+        public bool IsVisible(IModelNode node, string propertyName){
+            return typeof (TreeListEditor).IsAssignableFrom(((IModelListView) node).EditorType);
+        }
+    }
+
+    public class AutoExpandAllTreeNodesController : ObjectViewController<ListView, ITreeNode>{
+        protected override void OnViewControlsCreated(){
+            base.OnViewControlsCreated();
+            var treeListEditor = View.Editor as TreeListEditor;
+            if (treeListEditor != null&& ((IModelListViewAutoExpandAllTreeNodes) View.Model).AutoExpandAllTreeNodes){
+                treeListEditor.TreeList.VisibleChanged += TreeList_VisibleChanged;
+                treeListEditor.TreeList.HandleCreated += TreeList_HandleCreated;
+            }
+        }
+
+        private void TreeList_VisibleChanged(object sender, EventArgs e){
+            CustomizeTreeList((TreeList) sender);
+        }
+
+        private void TreeList_HandleCreated(object sender, EventArgs e){
+            CustomizeTreeList((TreeList) sender);
+        }
+
+        private void CustomizeTreeList(TreeList treeList){
+            if (treeList.IsHandleCreated & treeList.Visible){
+                treeList.BeginInvoke(new MethodInvoker(treeList.ExpandAll));
+                treeList.VisibleChanged -= TreeList_VisibleChanged;
+                treeList.HandleCreated -= TreeList_HandleCreated;
+            }
+        }
+    }
+}
