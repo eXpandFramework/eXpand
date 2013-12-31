8a8fe0e126f3de08851143e65058f3fe3fc657f9
apobekiaris <apostolis.bekiaris@gmail.com>
2013-12-31 06:08:42 +0200

    code formatting

--- Demos/FeatureCenter/FeatureCenter.Module.Win/Wizard/AttributeRegistrator.cs
+++ Demos/FeatureCenter/FeatureCenter.Module.Win/Wizard/AttributeRegistrator.cs
@@ -7,19 +7,19 @@ using Xpand.Persistent.Base.General.Model;
 
 namespace FeatureCenter.Module.Win.Wizard {
     public class AttributeRegistrator : Xpand.ExpressApp.Core.AttributeRegistrator {
-        public static readonly string WizardCustomer_DetailView = "WizardCustomer_DetailView";
+        public static readonly string WizardCustomerDetailView = "WizardCustomer_DetailView";
 
         public override IEnumerable<Attribute> GetAttributes(ITypeInfo typesInfo) {
             if (typesInfo.Type != typeof(WizardCustomer)) yield break;
-            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.Header + " " + Captions.HeaderWizard, "1=1", "1=1", Captions.HeaderWizard, Position.Top) { View = WizardCustomer_DetailView + 1 };
+            yield return new AdditionalViewControlsRuleAttribute(Module.Captions.Header + " " + Captions.HeaderWizard, "1=1", "1=1", Captions.HeaderWizard, Position.Top) { View = WizardCustomerDetailView + 1 };
             yield return new AdditionalViewControlsRuleAttribute(Module.Captions.ViewMessage + " " + Captions.HeaderWizard, "1=1", "1=1",
-                Captions.ViewMessageWizard, Position.Bottom) { View = WizardCustomer_DetailView + 1 };
-            yield return new CloneViewAttribute(CloneViewType.DetailView, WizardCustomer_DetailView + 1);
-            yield return new CloneViewAttribute(CloneViewType.DetailView, WizardCustomer_DetailView + 2);
-            var navigationItemAttribute = new XpandNavigationItemAttribute(Module.Captions.DetailViewCotrol + "Wizard", WizardCustomer_DetailView);
+                Captions.ViewMessageWizard, Position.Bottom) { View = WizardCustomerDetailView + 1 };
+            yield return new CloneViewAttribute(CloneViewType.DetailView, WizardCustomerDetailView + 1);
+            yield return new CloneViewAttribute(CloneViewType.DetailView, WizardCustomerDetailView + 2);
+            var navigationItemAttribute = new XpandNavigationItemAttribute(Module.Captions.DetailViewCotrol + "Wizard", WizardCustomerDetailView);
             yield return navigationItemAttribute;
             yield return new WhatsNewAttribute(new DateTime(2012,3,16), navigationItemAttribute);
-            yield return new DisplayFeatureModelAttribute(WizardCustomer_DetailView);
+            yield return new DisplayFeatureModelAttribute(WizardCustomerDetailView);
         }
     }
 }
