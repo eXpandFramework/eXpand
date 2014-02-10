4778ea7255fd963522b34f678952a88087e2c4d5
apobekiaris <apostolis.bekiaris@gmail.com>
2014-02-10 17:30:30 +0200

    MasterDetailKeyFieldController
    http://www.expandframework.com/forum/6-bugs/3847-exception-when-clicking-select-all-in-web-masterdetailview/5835.html

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/MasterDetail/MasterDetailKeyFieldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Web.Editors.ASPx;
+
+namespace Xpand.ExpressApp.Web.SystemModule.MasterDetail {
+    public class MasterDetailKeyFieldController:ViewController<ListView>{
+        protected override void OnActivated(){
+            base.OnActivated();
+            var editFrame = View.EditFrame;
+            if (editFrame != null)
+                foreach (var item in ((DetailView) editFrame.View).GetItems<ListPropertyEditor>()){
+                    item.ControlCreated+=OnControlsCreated;
+                }            
+        }
+
+        private void OnControlsCreated(object sender, EventArgs eventArgs){
+            var asPxGridListEditor = ((ListPropertyEditor) sender).ListView.Editor as ASPxGridListEditor;
+            if (asPxGridListEditor != null) asPxGridListEditor.ControlsCreated+=ASPxGridListEditorOnControlsCreated;
+        }
+
+        private void ASPxGridListEditorOnControlsCreated(object sender, EventArgs eventArgs){
+            var asPxGridListEditor = ((ASPxGridListEditor) sender);
+            asPxGridListEditor.Grid.KeyFieldName = asPxGridListEditor.Model.ModelClass.KeyProperty;
+        }
+    }
+}
