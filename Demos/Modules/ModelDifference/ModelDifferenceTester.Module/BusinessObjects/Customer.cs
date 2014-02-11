264b0e3d8cf624cc00af2f9346384ab1753b7620
apobekiaris <apostolis.bekiaris@gmail.com>
2014-02-11 11:56:41 +0200

    add customer

---
+++ Demos/Modules/ModelDifference/ModelDifferenceTester.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ModelDifferenceTester.Module.BusinessObjects {
+    [DefaultClassOptions]
+    public class Customer:Person {
+        public Customer(Session session) : base(session){
+        }
+    }
+}
