d97c8093368d6ac805d811fd6d69e60bc7893fb4
apobekiaris <apostolis.bekiaris@gmail.com>
2014-02-20 11:45:34 +0200

    Chart.Win

---
+++ Demos/Modules/Chart/ChartTester.Module.Win/BussinessObjects/Customer.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+
+namespace ChartTester.Module.Win.BussinessObjects {
+    [DomainComponent]
+    [DefaultClassOptions]
+    public class Customer {
+    }
+}
