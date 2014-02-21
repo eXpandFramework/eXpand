b693d7958962541bb549cb5a248cf6a2dacfd638
apobekiaris <apostolis.bekiaris@gmail.com>
2014-02-21 15:33:45 +0200

    exceptionhandling

---
+++ Demos/Modules/ExceptionHanding/ExceptionHandingTester.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+
+namespace ExceptionHandingTester.Module.BusinessObjects {
+    [DomainComponent]
+    [DefaultClassOptions]
+    public class Customer {
+    }
+}
