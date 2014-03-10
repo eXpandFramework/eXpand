1a5abed7cf7099e1b65a5369020d1bd058fa629f
apobekiaris <apostolis.bekiaris@gmail.com>
2014-03-10 12:52:52 +0200

    DurationPropertyEditor binidg issue
    http://www.expandframework.com/forum/8-discussion/3891-durationpropertyeditor-problem.html

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/DurationPropertyEditor.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/DurationPropertyEditor.cs
@@ -28,12 +28,11 @@ namespace Xpand.ExpressApp.Win.PropertyEditors {
 
             Control.ShowToolTips = true;
             Control.ToolTip =
-                " Examples:  " + Environment.NewLine +
-                " 1d                     = 1 Day" + Environment.NewLine +
-                " 1 day                  = 1 Day" + Environment.NewLine +
-                " 2d 5h 45 m             = 2 Days 5 Hours 45 minutes" + Environment.NewLine +
-                " 2 days 4 hours 25 min  = 2 Days 4 Hours 25 minutes" + Environment.NewLine
-                ;
+                @" Examples:  " + Environment.NewLine +
+                @" 1d                     = 1 Day" + Environment.NewLine +
+                @" 1 day                  = 1 Day" + Environment.NewLine +
+                @" 2d 5h 45 m             = 2 Days 5 Hours 45 minutes" + Environment.NewLine +
+                @" 2 days 4 hours 25 min  = 2 Days 4 Hours 25 minutes" + Environment.NewLine;
             Control.EditValueChanged += Control_EditValueChanged;
         }
 
@@ -47,42 +46,43 @@ namespace Xpand.ExpressApp.Win.PropertyEditors {
         }
 
         protected override void ReadValueCore() {
+            base.ReadValueCore();
             Control.EditValue = DecodeTimeSpan((TimeSpan) PropertyValue);
         }
 
 
         public static TimeSpan ParseTimeSpan(string s) {
-            const string Quantity = "quantity";
-            const string Unit = "unit";
+            const string quantity = "quantity";
+            const string unit = "unit";
 
-            const string Days = @"(d(ays?)?)";
-            const string Hours = @"(h((ours?)|(rs?))?)";
-            const string Minutes = @"(m((inutes?)|(ins?))?)";
-            const string Seconds = @"(s((econds?)|(ecs?))?)";
+            const string days = @"(d(ays?)?)";
+            const string hours = @"(h((ours?)|(rs?))?)";
+            const string minutes = @"(m((inutes?)|(ins?))?)";
+            const string seconds = @"(s((econds?)|(ecs?))?)";
 
             var timeSpanRegex = new Regex(
                 string.Format(@"\s*(?<{0}>\d+)\s*(?<{1}>({2}|{3}|{4}|{5}|\Z))",
-                              Quantity, Unit, Days, Hours, Minutes, Seconds),
+                              quantity, unit, days, hours, minutes, seconds),
                 RegexOptions.IgnoreCase);
             MatchCollection matches = timeSpanRegex.Matches(s);
 
             var ts = new TimeSpan();
             foreach (Match match in matches) {
-                if (Regex.IsMatch(match.Groups[Unit].Value, @"\A" + Days)) {
-                    ts = ts.Add(TimeSpan.FromDays(double.Parse(match.Groups[Quantity].Value)));
+                if (Regex.IsMatch(match.Groups[unit].Value, @"\A" + days)) {
+                    ts = ts.Add(TimeSpan.FromDays(double.Parse(match.Groups[quantity].Value)));
                 }
-                else if (Regex.IsMatch(match.Groups[Unit].Value, Hours)) {
-                    ts = ts.Add(TimeSpan.FromHours(double.Parse(match.Groups[Quantity].Value)));
+                else if (Regex.IsMatch(match.Groups[unit].Value, hours)) {
+                    ts = ts.Add(TimeSpan.FromHours(double.Parse(match.Groups[quantity].Value)));
                 }
-                else if (Regex.IsMatch(match.Groups[Unit].Value, Minutes)) {
-                    ts = ts.Add(TimeSpan.FromMinutes(double.Parse(match.Groups[Quantity].Value)));
+                else if (Regex.IsMatch(match.Groups[unit].Value, minutes)) {
+                    ts = ts.Add(TimeSpan.FromMinutes(double.Parse(match.Groups[quantity].Value)));
                 }
-                else if (Regex.IsMatch(match.Groups[Unit].Value, Seconds)) {
-                    ts = ts.Add(TimeSpan.FromSeconds(double.Parse(match.Groups[Quantity].Value)));
+                else if (Regex.IsMatch(match.Groups[unit].Value, seconds)) {
+                    ts = ts.Add(TimeSpan.FromSeconds(double.Parse(match.Groups[quantity].Value)));
                 }
                 else {
                     // Quantity given but no unit, default to Hours
-                    ts = ts.Add(TimeSpan.FromHours(double.Parse(match.Groups[Quantity].Value)));
+                    ts = ts.Add(TimeSpan.FromHours(double.Parse(match.Groups[quantity].Value)));
                 }
             }
             return ts;
