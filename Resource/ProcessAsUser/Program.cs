8071cc420ad2cf74ca730a3addaa3acb1f2be16b
apobekiaris <apostolis.bekiaris@gmail.com>
2014-03-18 17:49:04 +0200

    [ProcessAsAUser to allow interactive easytest execution from build server]

---
+++ Resource/ProcessAsUser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProcessAsUser {
+    class Program {
+        static void Main(string[] args) {
+            
+            if (args.Length!=4)
+                throw new ArgumentException( "UserName, Passowrd, TestExecutorPath,TestExecutorArgs");
+            ProcessAsUser.Launch("Tolis", "4450@p0m", @"C:\eXpandFrameWork\eXpand\Demos\Modules\AuditTrail\AuditTrailTester.Win\bin\EasyTest\TestExecutor.v13.2.exe", @"C:\eXpandFrameWork\eXpand\Demos\Modules\AuditTrail\AuditTrailTester.Module\FunctionalTests\AutoTest.ets");
+            ProcessAsUser.Launch(args[0],args[1],args[2],args[3]);
+        }
+    }
+}
---
+++ Resource/ProcessAsUser/RDCClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace ProcessAsUser {
+    public class RDCClient {
+        public static bool Connect(string userName, string password){
+            var done = new ManualResetEventSlim();
+            bool connect = false;
+            var processCreationThread = new Thread(() => {
+                var form = new Form1();
+                connect = form.Connect(userName, password);
+                done.Set();
+            });
+            processCreationThread.SetApartmentState(ApartmentState.STA);
+            processCreationThread.Start();
+            done.Wait();
+            return connect;
+        }
+    }
+}
