187e3cfd07fd4fd6b08638eb955ce8f89ff42fdf
apobekiaris <apostolis.bekiaris@gmail.com>
2014-03-19 19:22:14 +0200

    [AllowFocusReadonlyEditors]
    Q536569

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/ReadOnlyTabStopController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/ReadOnlyTabStopController.cs
@@ -4,35 +4,40 @@ using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Win.Layout;
 using DevExpress.XtraEditors;
+using DevExpress.XtraLayout;
 
 namespace Xpand.ExpressApp.Win.SystemModule {
     public interface IModelClassTabStopForReadOnly : IModelNode {
         [Category("eXpand")]
         [Description("If a detailview editor is readonly then you can not navigate to it using the TAB key")]
         bool TabOverReadOnlyEditors { get; set; }
+        [Category("eXpand")]
+        [Description("Set LayoutControl AllowFocusReadonlyEditors property value")]
+        [DefaultValue(true)]
+        bool AllowFocusReadonlyEditors { get; set; }
     }
     [ModelInterfaceImplementor(typeof(IModelClassTabStopForReadOnly), "ModelClass")]
     public interface IModelDetailViewTabStopForReadOnly : IModelClassTabStopForReadOnly {
+        
     }
 
     public class ReadOnlyTabStopController : ViewController<DetailView>, IModelExtender {
-
-
         void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
             extenders.Add<IModelClass, IModelClassTabStopForReadOnly>();
             extenders.Add<IModelDetailView, IModelDetailViewTabStopForReadOnly>();
         }
+
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
-
-
-
             if (((IModelDetailViewTabStopForReadOnly)View.Model).TabOverReadOnlyEditors) {
                 CheckControlsVisibility(View);
             }
-
+            var layoutControl = (LayoutControl) ((WinLayoutManager) (View.LayoutManager)).Container;
+            layoutControl.OptionsFocus.AllowFocusReadonlyEditors = ((IModelDetailViewTabStopForReadOnly)View.Model).AllowFocusReadonlyEditors;
         }
+
         private void CheckControlsVisibility(DetailView xpandDetailView) {
             Guard.ArgumentNotNull(xpandDetailView, "detailView");
             foreach (PropertyEditor propertyEditor in xpandDetailView.GetItems<PropertyEditor>()) {
@@ -44,9 +49,7 @@ namespace Xpand.ExpressApp.Win.SystemModule {
                         editor.TabStop = false;
                     }
                 }
-
             }
         }
-
     }
 }
