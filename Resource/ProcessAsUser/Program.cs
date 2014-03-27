104ed57526269231300d17fb5c61ec53633393fb
apobekiaris <apostolis.bekiaris@gmail.com>
2014-03-27 19:02:25 +0200

    easytest CI updates

--- Resource/ProcessAsUser/Program.cs
+++ Resource/ProcessAsUser/Program.cs
@@ -5,8 +5,7 @@ namespace ProcessAsUser {
         static void Main(string[] args) {
             
             if (args.Length!=4)
-                throw new ArgumentException( "UserName, Passowrd, TestExecutorPath,TestExecutorArgs");
-            ProcessAsUser.Launch("Tolis", "4450@p0m", @"C:\eXpandFrameWork\eXpand\Demos\Modules\AuditTrail\AuditTrailTester.Win\bin\EasyTest\TestExecutor.v13.2.exe", @"C:\eXpandFrameWork\eXpand\Demos\Modules\AuditTrail\AuditTrailTester.Module\FunctionalTests\AutoTest.ets");
+                throw new ArgumentException( "Args count=" +args.Length+ "Expected args--> UserName, Passowrd, TestExecutorPath,TestExecutorArgs");
             ProcessAsUser.Launch(args[0],args[1],args[2],args[3]);
         }
     }
