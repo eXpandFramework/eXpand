f94190b03d38ea948f59782e65db8a4c46e92c02
apobekiaris <apostolis.bekiaris@gmail.com>
2014-03-29 15:27:17 +0200

    Dashboard doesn't display data from a linked table
    http://www.expandframework.com/forum/6-bugs/3778-dashboard-doesn-t-display-data-from-a-linked-table/6217.html

--- Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
@@ -4,7 +4,7 @@ using DevExpress.ExpressApp;
 namespace Xpand.ExpressApp.Dashboard {
     public static class Extensions {
         public static object CreateDashboardDataSource(this IObjectSpace objectSpace,Type objectType) {
-            return objectSpace.CreateServerCollection(objectType, null);
+            return objectSpace.GetObjects(objectType);
         }
     }
 }
