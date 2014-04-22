c60f014f5d24cf135d39560db63f17f44f4f166a
apobekiaris <apostolis.bekiaris@gmail.com>
2014-04-23 02:21:39 +0300

    support configuration for the reference member object creation

--- Demos/Modules/ImportWizard/ImportWizardTester.Module/BusinessObjects/Customer.cs
+++ Demos/Modules/ImportWizard/ImportWizardTester.Module/BusinessObjects/Customer.cs
@@ -1,11 +1,43 @@
-﻿using DevExpress.Persistent.Base;
+﻿using System.ComponentModel;
+using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.Xpo;
 
-namespace ImportWizardTester.Module.BusinessObjects {
+namespace ImportWizardTester.Module.BusinessObjects{
     [DefaultClassOptions]
-    public class Customer:Person {
+    public class Customer : Person{
+        // Fields...
+        private GenderObject _gender;
+        private string _importName;
+
         public Customer(Session session) : base(session){
+        }
+
+        public GenderObject Gender{
+            get { return _gender; }
+            set { SetPropertyValue("Gender", ref _gender, value); }
+        }
+
+        public string ImportName{
+            get { return _importName; }
+            set { SetPropertyValue("ImportName", ref _importName, value); }
+        }
+    }
+
+//    [DefaultProperty("Gender")]
+//    [DefaultClassOptions]
+    public class GenderObject : BaseObject{
+        private string _gender;
+
+        public GenderObject(Session session) : base(session){
+        }
+
+        // Fields...
+
+
+        public string Gender {
+            get { return _gender; }
+            set { SetPropertyValue("Gender", ref _gender, value); }
         }
     }
 }
