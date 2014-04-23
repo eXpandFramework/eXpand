2d040b696e791050d879484a1f72660c0f00fcce
apobekiaris <apostolis.bekiaris@gmail.com>
2014-04-23 19:32:02 +0300

    Object reference

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/FullTextAutoFilterRowController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/FullTextAutoFilterRowController.cs
@@ -20,9 +20,13 @@ namespace Xpand.ExpressApp.Win.SystemModule {
         private void GridViewOnColumnFilterChanged(object sender, EventArgs eventArgs){
             var gridView = ((GridView) sender);
             var activeFilterCriteria = gridView.ActiveFilterCriteria;
-            var memberInfos = View.Model.GetFullTextMembers().Select(member => member.GetXpmemberInfo());
-            FullTextOperatorProcessor.Process(activeFilterCriteria,memberInfos.ToList());
-            gridView.ActiveFilterCriteria=activeFilterCriteria;
+            if (activeFilterCriteria!=null){
+                var memberInfos = View.Model.GetFullTextMembers().Select(member => member.GetXpmemberInfo());
+                if (memberInfos.Any()){
+                    FullTextOperatorProcessor.Process(activeFilterCriteria, memberInfos.ToList());
+                    gridView.ActiveFilterCriteria = activeFilterCriteria;
+                }
+            }
         }
     }
 }
