d970fd6cbe240d574201236aae00bf9115b0159b
apobekiaris <apostolis.bekiaris@gmail.com>
2014-05-18 19:22:49 +0300

    [SelectedItemSumController for GridView Win ListEditor]
    http://www.expandframework.com/forum/13-suggestions/3981-proposed-simple-and-handy-win-controller/6540.html

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/SelectedItemSumController.cs
@@ -0,0 +1,131 @@
+using System;
+using System.ComponentModel;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Utils;
+using DevExpress.Xpo;
+using DevExpress.XtraGrid.Views.Grid;
+using Xpand.ExpressApp.Win.ListEditors.GridListEditors.ColumnView;
+
+namespace Xpand.ExpressApp.Win.SystemModule{
+    public interface IModelMemberSeletectItemSum{
+        [Category("eXpand")]
+        bool SelectedItemSum { get; set; } 
+    }
+
+    [ModelInterfaceImplementor(typeof(IModelMemberSeletectItemSum),"ModelMember")]
+    public interface IModelColumnSelectedItemSum : IModelMemberSeletectItemSum {
+         
+    }
+    public class SelectedItemSumController : ViewController<ListView>,IModelExtender {
+        private ToolTipController _toolTipController;
+        private GridView _gridView;
+
+
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            var columnsListEditor = View.Editor as ColumnsListEditor;
+            if (columnsListEditor != null) {
+                _gridView = columnsListEditor.GridView();
+                View.Editor.SelectionChanged += Editor_SelectionChanged;
+                _toolTipController = new ToolTipController { ShowShadow = true, ShowBeak = true, AllowHtmlText = true };
+            }
+        }
+
+        protected override void OnDeactivated() {
+            _gridView = null;
+            if (_toolTipController != null)
+                _toolTipController.Dispose();
+            _toolTipController = null;
+            View.Editor.SelectionChanged -= Editor_SelectionChanged;
+            base.OnDeactivated();
+        }
+
+        private void Editor_SelectionChanged(object sender, EventArgs e) {
+            if (View.SelectedObjects.Count > 1 && _gridView != null && SelectedItemSumEnabled()) {
+                var isUnboundColumn = !string.IsNullOrEmpty(_gridView.FocusedColumn.UnboundExpression);
+                var columnName = isUnboundColumn ? _gridView.FocusedColumn.Caption : _gridView.FocusedColumn.FieldName;
+                if (!columnName.Contains("!")) {
+                    var columnType = GetColumnType();
+                    switch (columnType.FullName) {
+                        case "System.Int32":
+                        case "System.Decimal":
+                        case "System.Int64":
+                        case "System.Single":
+                        case "System.Double":
+                            var info = Calculate(isUnboundColumn, columnName);
+                            string columnMask = GetColumnMask();
+                            _toolTipController.ShowHint(string.Format(columnMask, info.Total, columnName, info.MinTotal, info.Count, info.MaxTotal));
+                            if (!_gridView.OptionsView.ShowFooter) {
+                                _gridView.OptionsView.ShowFooter = true;
+                                View.Model.IsFooterVisible = true;
+                                View.SaveModel();
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        private bool SelectedItemSumEnabled(){
+            return _gridView.FocusedColumn != null && ((IModelColumnSelectedItemSum)_gridView.FocusedColumn.Model()).SelectedItemSum;
+        }
+
+        private Type GetColumnType() {
+            var columnType = _gridView.FocusedColumn.ColumnType;
+            return columnType.IsGenericType ? Nullable.GetUnderlyingType(columnType) : columnType;
+        }
+
+        private string GetColumnMask() {
+            string mask = _gridView.FocusedColumn.DisplayFormat.FormatString;
+            mask = mask.Length > 3 ? mask.Substring(3, mask.Length - 4) : "";
+            var localizedText = CaptionHelper.GetLocalizedText(XpandSystemWindowsFormsModule.XpandWin, "SelectedItemSumFormatString");
+            return string.Format(localizedText, mask);
+        }
+
+        private CalcInfo Calculate(bool isUnboundColumn, string columnName) {
+            var calcInfo = new CalcInfo();
+            foreach (XPBaseObject selectedObject in View.SelectedObjects) {
+                var tempvalue = GetTempvalue(isUnboundColumn, selectedObject, columnName);
+                calcInfo.Total += tempvalue;
+                calcInfo.Count += 1;
+                if (calcInfo.Count == 1) {
+                    calcInfo.MaxTotal = tempvalue;
+                    calcInfo.MinTotal = tempvalue;
+                }
+                else {
+                    if (calcInfo.MaxTotal < tempvalue)
+                        calcInfo.MaxTotal = tempvalue;
+                    if (calcInfo.MinTotal > tempvalue)
+                        calcInfo.MinTotal = tempvalue;
+                }
+            }
+            return calcInfo;
+        }
+
+        private decimal GetTempvalue(bool isUnboundColumn, XPBaseObject selectedObject, string columnName) {
+            return isUnboundColumn
+                ? +Convert.ToDecimal(selectedObject.Evaluate(_gridView.FocusedColumn.UnboundExpression))
+                : (columnName.Contains(".")
+                    ? +Convert.ToDecimal(selectedObject.Evaluate(CriteriaOperator.Parse(columnName)))
+                    : +Convert.ToDecimal(selectedObject.GetMemberValue(columnName)));
+        }
+        class CalcInfo {
+            public decimal Total { get; set; }
+
+            public int Count { get; set; }
+
+            public decimal MaxTotal { get; set; }
+
+            public decimal MinTotal { get; set; }
+        }
+
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders){
+            extenders.Add<IModelMember,IModelMemberSeletectItemSum>();
+            extenders.Add<IModelColumn,IModelColumnSelectedItemSum>();
+        }
+    }
+}
