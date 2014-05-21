54af019ef8667a8806188f830dc8ad7f1043764c
apobekiaris <apostolis.bekiaris@gmail.com>
2014-05-21 13:59:48 +0300

    AudiTrial object reference

--- Xpand/Xpand.ExpressApp.Modules/AuditTrail/Model/Member/AuditTrailCollectionMemberInfo.cs
+++ Xpand/Xpand.ExpressApp.Modules/AuditTrail/Model/Member/AuditTrailCollectionMemberInfo.cs
@@ -2,17 +2,15 @@ using System;
 using DevExpress.Data.Filtering;
 using DevExpress.Xpo;
 using DevExpress.Xpo.Metadata;
+using Xpand.ExpressApp.AuditTrail.BusinessObjects;
 using Xpand.Persistent.Base.General;
 using Xpand.Xpo.MetaData;
 
 namespace Xpand.ExpressApp.AuditTrail.Model.Member {
     public class AuditTrailCollectionMemberInfo : XpandCustomMemberInfo {
-        
-
         public AuditTrailCollectionMemberInfo(XPClassInfo owner, string propertyName, Type propertyType)
             : base(owner, propertyName, typeof(XPCollection<>).MakeGenericType(propertyType), null, true, false) {
         }
-
 
         public override object GetValue(object theObject) {
             var xpBaseObject = ((XPBaseObject)theObject);
@@ -22,15 +20,14 @@ namespace Xpand.ExpressApp.AuditTrail.Model.Member {
         }
 
         object CreateInstance(XPBaseObject xpBaseObject) {
-            var auditedObjectWeakReferenceType = ApplicationHelper.Instance.Application.TypesInfo.FindTypeInfo("DevExpress.Persistent.BaseImpl.AuditedObjectWeakReference").Type;
+            var auditedObjectWeakReferenceType = ApplicationHelper.Instance.Application.TypesInfo.FindTypeInfo(typeof(XpandAuditedObjectWeakReference)).Type;
             return GetAuditTrail(xpBaseObject.Session, xpBaseObject, auditedObjectWeakReferenceType);
         }
 
         XPBaseCollection GetAuditTrail(Session session, XPBaseObject xpBaseObject, Type auditedObjectWeakReferenceType) {
             var binaryOperator = new BinaryOperator("TargetType", session.GetObjectType(xpBaseObject));
             var operands = new BinaryOperator("TargetKey", XPWeakReference.KeyToString(session.GetKeyValue(xpBaseObject)));
-            var auditObjectWR = (XPWeakReference) session.FindObject(auditedObjectWeakReferenceType,
-                                                                     new GroupOperator(binaryOperator,operands));
+            var auditObjectWR = (XPWeakReference) session.FindObject(auditedObjectWeakReferenceType,new GroupOperator(binaryOperator,operands));
             if (auditObjectWR != null) {
                 var baseCollection = (XPBaseCollection) auditObjectWR.ClassInfo.GetMember("AuditDataItems").GetValue(auditObjectWR);
                 baseCollection.BindingBehavior = CollectionBindingBehavior.AllowNone;
@@ -38,7 +35,6 @@ namespace Xpand.ExpressApp.AuditTrail.Model.Member {
             }
             return null;
         }
-
 
         protected override bool CanPersist {
             get { return false; }
