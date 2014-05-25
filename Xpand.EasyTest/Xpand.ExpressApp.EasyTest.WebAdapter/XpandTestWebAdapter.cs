8ef1c252a23d4ca9b5f09599b64c89b93b7067c7
apobekiaris <apostolis.bekiaris@gmail.com>
2014-05-25 15:45:30 +0300

    Kill issexpress site after test finished

--- Xpand.EasyTest/Xpand.ExpressApp.EasyTest.WebAdapter/XpandTestWebAdapter.cs
+++ Xpand.EasyTest/Xpand.ExpressApp.EasyTest.WebAdapter/XpandTestWebAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using DevExpress.EasyTest.Framework;
 using DevExpress.ExpressApp.EasyTest.WebAdapter;
 using Fasterflect;
@@ -11,6 +12,8 @@ using Xpand.ExpressApp.EasyTest.WebAdapter;
 
 namespace Xpand.ExpressApp.EasyTest.WebAdapter{
     public class XpandTestWebAdapter : DevExpress.ExpressApp.EasyTest.WebAdapter.WebAdapter{
+        private Process _process;
+        private const string SingleWebDevParamName = "SingleWebDev";
         private const string UrlParamName = "Url";
         public override void RunApplication(TestApplication testApplication){
             if (!GetParamValue("UseIISExpress", false, testApplication)){
@@ -22,12 +25,27 @@ namespace Xpand.ExpressApp.EasyTest.WebAdapter{
                 string webBrowserType = testApplication.FindParamValue("WebBrowserType");
                 webBrowsers = string.IsNullOrEmpty(webBrowserType) ? (IWebBrowserCollection) new WebBrowserCollection() : new StandaloneWebBrowserCollection();
                 
-                if (!WebDevWebServerHelper.IsWebDevServerStarted(uri))
-                    IISExpressServerHelper.Run(testApplication,uri);
+                if (!WebDevWebServerHelper.IsWebDevServerStarted(uri)){
+                    _process = IISExpressServerHelper.Run(testApplication,uri);
+                }
                 if (testApplication.FindParamValue("DefaultWindowSize") != null) {
                     WebBrowserCollection.DefaultFormSize = GetWindowSize(testApplication.GetParamValue("DefaultWindowSize"));
                 }
                 this.CallMethod("CreateBrowser", url);
+            }
+        }
+
+        public override void KillApplication(TestApplication testApplication, KillApplicationConext context){
+            bool isSingleWebDev = testApplication.FindParamValue(SingleWebDevParamName) != null;
+            if (testApplication.FindParamValue("DontKillWebDev") == null) {
+                if (isSingleWebDev) {
+                    if (context != KillApplicationConext.TestNormalEnded) {
+                        IISExpressServerHelper.Stop(_process);
+                    }
+                }
+                else {
+                    IISExpressServerHelper.Stop(_process);
+                }
             }
         }
 
@@ -47,13 +65,46 @@ namespace Xpand.ExpressApp.EasyTest.WebAdapter{
     }
 
     public class IISExpressServerHelper{
+        internal class NativeMethods {
+            // Methods
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern IntPtr GetTopWindow(IntPtr hWnd);
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint lpdwProcessId);
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern bool PostMessage(HandleRef hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+        }
+
+        static void SendStopMessageToProcess(int pid) {
+            try {
+                for (IntPtr ptr = NativeMethods.GetTopWindow(IntPtr.Zero); ptr != IntPtr.Zero; ptr = NativeMethods.GetWindow(ptr, 2)) {
+                    uint num;
+                    NativeMethods.GetWindowThreadProcessId(ptr, out num);
+                    if (pid == num) {
+                        var hWnd = new HandleRef(null, ptr);
+                        NativeMethods.PostMessage(hWnd, 0x12, IntPtr.Zero, IntPtr.Zero);
+                        return;
+                    }
+                }
+            }
+            catch (ArgumentException) {
+            }
+        }
+
+        public static void Stop(Process process) {
+            SendStopMessageToProcess(process.Id);
+            process.Close();
+        }
+
         public static Process Run(TestApplication testApplication, Uri uri) {
             string physicalPath = Path.GetFullPath(testApplication.FindParamValue("PhysicalPath"));
-            string arguments = string.Format(@"/path:""{0}"" /port:{1}", physicalPath, uri.Port);
-            EasyTestTracer.Tracer.InProcedure(string.Format("RunIISExpressServer({0})", arguments));
+            string arguments = String.Format(@"/path:""{0}"" /port:{1}", physicalPath, uri.Port);
+            EasyTestTracer.Tracer.InProcedure(String.Format("RunIISExpressServer({0})", arguments));
             try{
                 string serverPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"IIS Express\iisexpress.exe");
-                EasyTestTracer.Tracer.InProcedure(string.Format("IISExpressServerPath= {0}", serverPath));
+                EasyTestTracer.Tracer.InProcedure(String.Format("IISExpressServerPath= {0}", serverPath));
                 var serverProcess = new Process{
                     StartInfo ={
                         FileName = serverPath,
@@ -66,9 +117,8 @@ namespace Xpand.ExpressApp.EasyTest.WebAdapter{
                 return serverProcess;
             }
             finally{
-                EasyTestTracer.Tracer.OutProcedure(string.Format("RunWebDevWebServer({0})", arguments));
+                EasyTestTracer.Tracer.OutProcedure(String.Format("RunWebDevWebServer({0})", arguments));
             }
         }
-
     }
 }
