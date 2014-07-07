36d2dfe4bb53951f70f3e47a4ec53a28d051a8cd
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-07 09:45:52 +0300

    Remember me web bug
    http://www.expandframework.com/forum/8-discussion/3452-auto-authenticate-and-remember-me/6990.html

--- Xpand/Xpand.ExpressApp.Modules/Security/Controllers/RememberMeController.cs
+++ Xpand/Xpand.ExpressApp.Modules/Security/Controllers/RememberMeController.cs
@@ -1,8 +1,10 @@
 ﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Utils;
+using Xpand.Persistent.Base.General;
 
 namespace Xpand.ExpressApp.Security.Controllers {
-    public abstract class RememberMeController : WindowController {
-        protected RememberMeController() {
+    public class RememberMeController : WindowController {
+        public RememberMeController() {
             TargetWindowType = WindowType.Main;
         }
         protected override void OnActivated() {
@@ -14,6 +16,18 @@ namespace Xpand.ExpressApp.Security.Controllers {
             Application.LoggingOff -= ApplicationOnLoggingOff;
         }
 
-        protected abstract void ApplicationOnLoggingOff(object sender, LoggingOffEventArgs loggingOffEventArgs);
+        protected virtual void ApplicationOnLoggingOff(object sender, LoggingOffEventArgs loggingOffEventArgs){
+            if (!loggingOffEventArgs.CanCancel)
+                return;
+            const string rememberMePropertyName = "RememberMe";
+
+            var logonParameters = SecuritySystem.LogonParameters as IXpandLogonParameters;
+            if (logonParameters != null) {
+                var typeInfo = XafTypesInfo.Instance.FindTypeInfo(logonParameters.GetType());
+                var memberInfo = typeInfo.FindMember(rememberMePropertyName);
+                if (memberInfo != null) memberInfo.SetValue(logonParameters, false);
+                ObjectSerializer.WriteObjectPropertyValues(null, logonParameters.Storage, logonParameters);
+            }
+        }
     }
 }
