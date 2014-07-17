30f4e5a90cd6d85127d125ae0d851f8aa5da75f9
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-17 19:14:59 +0300

    Modelartifact controller state does not work
    http://www.expandframework.com/forum/8-discussion/4090-modelartifact-actionstate-and-controller-state/7044.html

---
+++ Demos/Modules/ModelArtifactState/ModelArtifactStateTester.Module/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+
+namespace ModelArtifactStateTester.Module.Controllers {
+    public class TestController:ViewController {
+        public TestController(){
+            var simpleAction = new SimpleAction(this,"TestAction",PredefinedCategory.ObjectsCreation);
+            simpleAction.Execute+=SimpleActionOnExecute;
+        }
+
+        private void SimpleActionOnExecute(object sender, SimpleActionExecuteEventArgs simpleActionExecuteEventArgs){
+            
+        }
+    }
+}
