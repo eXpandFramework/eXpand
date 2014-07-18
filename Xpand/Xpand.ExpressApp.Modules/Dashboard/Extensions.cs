52a2ca05457f094607d3c7ca9b25efa0ffa816e4
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-18 09:45:11 +0300

    Dashboard and Entity Framework support
    http://www.expandframework.com/forum/13-suggestions/4050-dashboard-and-entity-framework/6911.html

--- Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
@@ -3,9 +3,9 @@ using DevExpress.ExpressApp;
 
 namespace Xpand.ExpressApp.Dashboard {
     public static class Extensions {
-        public static object CreateDashboardDataSource(this XafApplication application,Type objectType) {
+        public static object CreateDashboardDataSource(this XafApplication application, Type objectType) {
             var space = application.CreateObjectSpace(objectType);
-            return space.GetObjects(objectType);
+            return new ProxyCollection(space, space.TypesInfo.FindTypeInfo(objectType), space.GetObjects(objectType));
         }
     }
 }
