be3471c0fc88fa1a10cc0adf70b359c60dced492
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-23 17:54:52 +0300

    [End user appearance rule customization]
    https://www.devexpress.com/Support/Center/Question/Details/Q381881

---
+++ Xpand/Xpand.Xpo/Converters/ValueConverters/ColorValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace Xpand.Xpo.Converters.ValueConverters{
+    public class ColorValueConverter : DevExpress.Xpo.Metadata.ValueConverter {
+        public override Type StorageType {
+            get {
+                return typeof(Int32);
+            }
+        }
+        public override object ConvertToStorageType(object value) {
+            if (!(value is Color)) {
+                return null;
+            }
+            return ((Color)value).ToArgb();
+        }
+        public override object ConvertFromStorageType(object value) {
+            if (!(value is Int32)) {
+                return null;
+            }
+            return Color.FromArgb((Int32)value);
+        }
+    }
+}
