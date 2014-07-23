597bc9758f1c131a47cd419f367bbdee1c7156da
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-23 18:33:29 +0300

    [E380 - How to hide individual navigation items and groups for certain users]
    http://www.expandframework.com/forum/13-suggestions/4096-security-hide-individual-navigation-items-and-groups-for-certain-users/7106.html

---
+++ Xpand/Xpand.ExpressApp.Modules/Security/Permissions/NavigationItemPermission.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using DevExpress.ExpressApp.Security;
+
+namespace Xpand.ExpressApp.Security.Permissions{
+    public class NavigationItemPermission : IOperationPermission{
+        public NavigationItemPermission(string hiddenNavigationItem){
+            HiddenNavigationItem = hiddenNavigationItem;
+        }
+
+        public string HiddenNavigationItem { get; set; }
+
+        public string Operation{
+            get { return "NavigateToItem"; }
+        }
+    }
+
+    public class NavigationItemPermissionRequest : IPermissionRequest{
+        public NavigationItemPermissionRequest(string navigationItem){
+            NavigationItem = navigationItem;
+        }
+
+        public string NavigationItem { get; set; }
+
+        public object GetHashObject(){
+            return String.Format("{0} - {1}", GetType().FullName, NavigationItem);
+        }
+    }
+
+    public class NavigationItemPermissionRequestProcessor :
+        PermissionRequestProcessorBase<NavigationItemPermissionRequest>{
+        private readonly IPermissionDictionary _permissions;
+
+        public NavigationItemPermissionRequestProcessor(IPermissionDictionary permissions){
+            _permissions = permissions;
+        }
+
+        public override bool IsGranted(NavigationItemPermissionRequest permissionRequest){
+            return _permissions.GetPermissions<NavigationItemPermission>().All(permission => permission.HiddenNavigationItem != permissionRequest.NavigationItem);
+        }
+    }
+}
