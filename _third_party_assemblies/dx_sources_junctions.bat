12cdc3f407bd03d40b823376a81c15c2f1deaf14
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-29 16:09:10 +0300

    wrong xaf modules path

--- _third_party_assemblies/dx_sources_junctions.bat
+++ _third_party_assemblies/dx_sources_junctions.bat
@@ -16,8 +16,11 @@ set xafJunctionDir=%junctionDir%\X
 rd /s /q %xafJunctionDir%
 mkdir %xafJunctionDir%
 
-set XAFDirs=DevExpress.ExpressApp DevExpress.ExpressApp.Modules DevExpress.ExpressApp.Workflow DevExpress.ExpressApp.Design DevExpress.ExpressApp.Tools DevExpress.Persistent
+set XAFDirs=DevExpress.ExpressApp  DevExpress.ExpressApp.Workflow DevExpress.ExpressApp.Design DevExpress.ExpressApp.Tools DevExpress.Persistent
 for %%x in (%XAFDirs%) do for /f %%l in ('dir /b "%targetDir%"\%%x') do junction -s %xafJunctionDir%\%%l "%targetDir%"\%%x\%%l
+
+junction -s %xafJunctionDir%\DevExpress.ExpressApp.Modules "%targetDir%\DevExpress.ExpressApp.Modules"
+
 
 
 
