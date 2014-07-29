343a532bca7fe02994a831ee9c8645c55b37cba6
apobekiaris <apostolis.bekiaris@gmail.com>
2014-07-29 17:52:29 +0300

    [EasyTests for HideOnExit]

---
+++ Demos/Modules/XpandSystem/XpandSystemTester.Module.Win/FunctionalTests/ApplicationExit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Win.Templates;
+using DevExpress.Persistent.Base;
+using Xpand.ExpressApp.Win.SystemModule;
+
+namespace XpandSystemTester.Module.Win.FunctionalTests {
+    public class ApplicationExit:ViewController {
+        private readonly SingleChoiceAction _singleChoiceAction;
+        private MainForm _mainForm;
+
+        public ApplicationExit(){
+            _singleChoiceAction = new SingleChoiceAction(this,GetType().Name,PredefinedCategory.ObjectsCreation){
+                ItemType = SingleChoiceActionItemType.ItemIsOperation
+            };
+            _singleChoiceAction.Items.Add(new ChoiceActionItem("MinimizeOnExit", "MinimizeOnExit"));
+            _singleChoiceAction.Items.Add(new ChoiceActionItem("HideOnExit", "HideOnExit"));
+            _singleChoiceAction.Items.Add(new ChoiceActionItem("PromptOnExit", "PromptOnExit"));
+            _singleChoiceAction.Execute+=SingleChoiceActionOnExecute;
+        }
+
+        public SingleChoiceAction SingleChoiceAction{
+            get { return _singleChoiceAction; }
+        }
+
+        protected override void OnFrameAssigned() {
+            base.OnFrameAssigned();
+            Frame.TemplateChanged+=FrameOnTemplateChanged;
+        }
+
+        private void FrameOnTemplateChanged(object sender, EventArgs eventArgs){
+            if (Frame.Template is MainForm)
+                _mainForm = Frame.Template as MainForm;
+        }
+
+        private void SingleChoiceActionOnExecute(object sender, SimpleActionExecuteEventArgs simpleActionExecuteEventArgs){
+            var id = SingleChoiceAction.SelectedItem.Id;
+            ((IModelOptionsApplicationExit)Application.Model.Options).SetValue(id, true);
+            _mainForm.Close();
+            var value = new[] { "PromptOnExit", "MinimizeOnExit" }.Contains(id) ? _mainForm.WindowState != FormWindowState.Minimized : _mainForm.Visible;
+            _singleChoiceAction.Enabled.SetItemValue("ActionOnFormIsNot", value);
+            if (!_mainForm.Disposing&&!_mainForm.IsDisposed)
+                _mainForm.Show();
+        }
+    }
+}
