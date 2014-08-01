dcb2d1911cc7af30f8f89322aa0c8a57636bbbbb
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-01 20:32:26 +0300

    derive from CompareScreenshotCommand

--- Xpand.EasyTest/Xpand.EasyTest/Commands/XpandCompareScreenshotCommand.cs
+++ Xpand.EasyTest/Xpand.EasyTest/Commands/XpandCompareScreenshotCommand.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Runtime.InteropServices;
 using DevExpress.EasyTest.Framework;
 using DevExpress.EasyTest.Framework.Commands;
+using Xpand.Utils.Automation;
+using Xpand.Utils.Win32;
 
 namespace Xpand.EasyTest.Commands{
-    public class XpandCompareScreenshotCommand : FilesCommand{
+    public class XpandCompareScreenshotCommand : CompareScreenshotCommand {
         public const string Name = "XpandCompareScreenshot";
         protected override void InternalExecute(ICommandAdapter adapter){
             ITestControl activeWindowControl = adapter.CreateTestControl(TestControlType.Dialog, null);
@@ -20,18 +20,21 @@ namespace Xpand.EasyTest.Commands{
             if (Parameters["ScreenMainWindow"] != null){
                 Boolean.TryParse(Parameters["ScreenMainWindow"].Value, out screenMainWindow);
             }
+
+
             IntPtr windowHandle = activeWindowControl.GetInterface<ITestWindow>().GetActiveWindowHandle();
             if (screenMainWindow){
                 windowHandle = GetRootWindow(windowHandle.ToInt32());
             }
 
-
             Parameter windowNameParameter = Parameters["WindowTitle"];
             if (windowNameParameter != null){
                 windowHandle = Win32.FindWindowByCaption(IntPtr.Zero, windowNameParameter.Value);
                 if (windowHandle == IntPtr.Zero)
-                    throw new CommandException(String.Format("Cannot find window {0}", windowNameParameter.Value), StartPosition);
+                    throw new CommandException(String.Format("Cannot find window {0}", windowNameParameter.Value),StartPosition);
             }
+
+
             Image testImage;
             try{
 
@@ -61,43 +64,5 @@ namespace Xpand.EasyTest.Commands{
             }
         }
 
-        protected void SaveExpectedImage(Image img, string fileName){
-            var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
-            var etalonImageStream = (MemoryStream) ImageHelper.GetStreamForImage(img, "image/png", 100L);
-            etalonImageStream.WriteTo(fileStream);
-            fileStream.Close();
-        }
-
-        protected void SaveDiffImage(Image img, string fileName){
-            var view = new ApplicationView{
-                fileNamePostfix = Path.GetFileNameWithoutExtension(fileName),
-                fileExtension = "png",
-                applicationViewStream = ImageHelper.GetStreamForImage(img, "image/png", 100L)
-            };
-            Logger.Instance.SaveApplicationViews(new[]{view});
-        }
-
-        protected void SaveActualImage(Image actualImage, string etalonFileName){
-            string actualCopyFileNameWithoutExtension = Path.Combine(Path.GetDirectoryName(etalonFileName)+"",
-                Path.GetFileNameWithoutExtension(etalonFileName)+"");
-            string actualCopyFileName = string.Format("{0}.Actual{1}", actualCopyFileNameWithoutExtension,
-                Path.GetExtension(etalonFileName));
-            actualImage.Save(actualCopyFileName, ImageFormat.Png);
-        }
-
-        public static IntPtr GetRootWindow(int hWnd){
-            int currentWindowHandle = hWnd;
-            int result = currentWindowHandle;
-            do{
-                currentWindowHandle = GetParent(currentWindowHandle);
-                if (currentWindowHandle != 0){
-                    result = currentWindowHandle;
-                }
-            } while (currentWindowHandle != 0);
-            return new IntPtr(result);
-        }
-
-        [DllImport("USER32.DLL", EntryPoint = "GetParent")]
-        public static extern int GetParent(int hWnd);
     }
 }
