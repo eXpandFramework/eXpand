cc8137aff6d7eb06f0aa3ffed4cdd5718e0d92d1
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-04 01:27:10 +0300

    EasyTest for HyperLinkPropertyEditor for both platforms

---
+++ Demos/Modules/XpandSystem/XpandSystemTester.Module/FunctionalTests/HyperLinkPropertyEditor/HyperLinkObject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using Xpand.Persistent.Base.General;
+
+namespace XpandSystemTester.Module.FunctionalTests.HyperLinkPropertyEditor {
+    [DefaultClassOptions]
+    [DefaultProperty("Url")]
+    public class HyperLinkObject:BaseObject {
+        public HyperLinkObject(Session session) : base(session){
+        }
+        [EditorAlias(EditorAliases.HyperLinkPropertyEditor)]
+        public string Url { get; set; }
+        
+
+        
+    }
+}
