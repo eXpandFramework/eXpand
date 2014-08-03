4a7ef1d061454aa29d842437d179f6b43242da9f
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-04 01:30:37 +0300

    [New EasyTest KillFocus command]

---
+++ Xpand.EasyTest/Xpand.EasyTest/Commands/KillFocusCommand.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xpand.Utils.Automation;
+
+namespace Xpand.EasyTest.Commands{
+    public class KillFocusCommand:Command{
+        public const string Name = "KillFocus";
+        protected override void InternalExecute(ICommandAdapter adapter){
+            WindowAutomation.KillFocus();
+        }
+    }
+}
