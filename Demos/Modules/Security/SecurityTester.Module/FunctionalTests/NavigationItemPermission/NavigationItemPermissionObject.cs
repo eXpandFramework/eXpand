3a92fbec7679935f2b304b588e6f320da0d2b71b
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-05 01:10:59 +0300

    [NavigationItemPermission EasyTested]
    http://www.expandframework.com/forum/13-suggestions/4096-security-hide-individual-navigation-items-and-groups-for-certain-users.html#7169

---
+++ Demos/Modules/Security/SecurityTester.Module/FunctionalTests/NavigationItemPermission/NavigationItemPermissionObject.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+
+namespace SecurityTester.Module.FunctionalTests.NavigationItemPermission {
+    [NavigationItem("NavigationItemPermission")]
+    public class NavigationItemPermissionObject:BaseObject {
+
+    }
+}
