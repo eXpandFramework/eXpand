802dd31c1a6792537416a4c86a997d1ab2b2022e
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-04 17:32:02 +0300

    [New XpandFillFormCommand with SendKeys support]

---
+++ Xpand.EasyTest/Xpand.EasyTest/Commands/XpandFillFormCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using DevExpress.EasyTest.Framework;
+using DevExpress.EasyTest.Framework.Commands;
+
+namespace Xpand.EasyTest.Commands{
+    public class XpandFillFormCommand : Command{
+        public const string Name = "XpandFillForm";
+        public const string SendKeysX = "SendKeysX";
+        protected override void InternalExecute(ICommandAdapter adapter){
+            var sendKeysParameter = Parameters[SendKeysX];
+            if (sendKeysParameter != null && (adapter.IsWinAdapter())) {
+                var sendKeysCommand = new SendKeysCommand().SynchWith(this);
+                sendKeysCommand.Parameters.MainParameter = new MainParameter(Parameters.First().Value);
+                sendKeysCommand.Execute(adapter);
+            }
+            else {
+                var processRecordCommand = new FillFormCommand();
+                processRecordCommand.SynchWith(this);
+                if (sendKeysParameter != null)
+                    processRecordCommand.Parameters.Remove(sendKeysParameter);
+                processRecordCommand.Execute(adapter);
+            }
+        }
+    }
+}
