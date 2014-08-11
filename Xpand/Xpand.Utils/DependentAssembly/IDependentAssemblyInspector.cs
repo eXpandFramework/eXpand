665cd5692e0c351e7db492574d8fc649805fab62
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-12 00:31:35 +0300

    remove obsoletes

--- Xpand/Xpand.Utils/DependentAssembly/IDependentAssemblyInspector.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Xpand.Utils.DependentAssembly {
-    public interface IDependentAssemblyInspector
-    {
-        List<string> GetAssemblies(string assembly);
-    }
-}
