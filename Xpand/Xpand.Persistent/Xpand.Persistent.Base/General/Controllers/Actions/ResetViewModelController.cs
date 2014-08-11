66c7839019cd197d328c98fb2fb3976d17084bd5
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-12 00:47:16 +0300

    [EasyTested action for end user reset of a model view]

---
+++ Xpand/Xpand.Persistent/Xpand.Persistent.Base/General/Controllers/Actions/ResetViewModelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Model.Core;
+
+namespace Xpand.Persistent.Base.General.Controllers.Actions {
+    public class ResetViewModelController : ModifyModelActionControllerBase {
+        private const string ResetViewModel = "Reset View Model";
+        protected override IEnumerable<ChoiceActionItem> GetChoiceActionItems(){
+            yield return new ChoiceActionItem(ResetViewModel,null);
+        }
+
+        private void ViewOnCustomModelSaving(object sender, HandledEventArgs handledEventArgs){
+            handledEventArgs.Handled = true;
+        }
+
+        protected override void ModifyModelActionOnExecute(object sender, SingleChoiceActionExecuteEventArgs e){
+            var choiceActionItem = e.SelectedChoiceActionItem;
+            if (choiceActionItem.Id == ResetViewModel){
+                var modelApplicationBase = (ModelApplicationBase)Application.Model;
+                var modelApplication = modelApplicationBase.CreatorInstance.CreateModelApplication();
+                modelApplication.Id = modelApplicationBase.LastLayer.Id;
+                new ModelXmlReader().ReadFromModel(modelApplication, modelApplicationBase.LastLayer);
+                var modelViews = ((IModelApplication) modelApplication).Views;
+                if (modelViews != null){
+                    var modelView = modelViews[View.Id];
+                    if (modelView != null){
+                        if (!modelView.IsNewNode()){
+                            modelView.Remove();       
+                            ModelApplicationHelper.RemoveLayer(modelApplicationBase);
+                            ModelApplicationHelper.AddLayer(modelApplicationBase, modelApplication);
+                        }
+                        else{
+                            throw new UserFriendlyException("Cannot reset new views");
+                        }
+                    }
+                }
+                View.CustomModelSaving += ViewOnCustomModelSaving;
+                View.SetModel(View.Model, true);
+                View.CustomModelSaving -= ViewOnCustomModelSaving;
+            }
+        }
+    }
+}
