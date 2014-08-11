89a61441b8ace645ebba8c763e04dedd0b227df7
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-12 00:54:51 +0300

    helper files for actions easytests

---
+++ Demos/Modules/XpandSystem/XpandSystemTester.Module/FunctionalTests/Actions/Actions.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+
+namespace XpandSystemTester.Module.FunctionalTests.Actions {
+    public class Actions:ObjectViewController<ObjectView,ActionsObject> {
+        private const string ChangeColumnCaption = "ChangeColumnCaption";
+        private const string RestoreColumnCaption = "RestoreColumnCaption";
+        private readonly SingleChoiceAction _actionsAction;
+        private string _columnCaption;
+
+        public Actions(){
+            ChangeModelAction();
+            HiddenAction();
+            ItemsFromModelAction();
+            _actionsAction = new SingleChoiceAction(this,"Actions",PredefinedCategory.View);
+            ActionsAction.Items.Add(new ChoiceActionItem(ChangeColumnCaption, null));
+            ActionsAction.Items.Add(new ChoiceActionItem(RestoreColumnCaption, null));
+            ActionsAction.ItemType = SingleChoiceActionItemType.ItemIsOperation;
+            _actionsAction.Execute+=ActionsActionOnExecute;
+        }
+
+        public SingleChoiceAction ActionsAction{
+            get { return _actionsAction; }
+        }
+
+        public XpandEasyTestController EasyTestControler{
+            get { return Frame.GetController<XpandEasyTestController>(); }
+        }
+
+        private void ActionsActionOnExecute(object sender, SingleChoiceActionExecuteEventArgs e){
+            if (e.SelectedChoiceActionItem.Id == RestoreColumnCaption){
+                EasyTestControler.ChangeColumnCaption(_columnCaption);
+                _columnCaption = null;
+            }
+            else if (e.SelectedChoiceActionItem.Id == ChangeColumnCaption){
+                _columnCaption = EasyTestControler.ChangeColumnCaption("Changed");
+            }
+        }
+
+        private void ItemsFromModelAction(){
+            var itemsFromModelAction = new SingleChoiceAction(this, "ItemsFromModelAction", PredefinedCategory.View){
+                ItemType = SingleChoiceActionItemType.ItemIsOperation
+            };
+            itemsFromModelAction.Execute += SingleChoiceActionOnExecute;
+        }
+
+        private void HiddenAction(){
+            var hiddenAction = new SingleChoiceAction(this, "HiddenAction", PredefinedCategory.View){
+                ItemType = SingleChoiceActionItemType.ItemIsOperation
+            };
+            hiddenAction.Items.Add(new ChoiceActionItem("CodeItem", null));
+            hiddenAction.Execute += SingleChoiceActionOnExecute;
+        }
+
+        private void ChangeModelAction(){
+            var changeModelAction = new SingleChoiceAction(this, "ChangeModelAction", PredefinedCategory.View){
+                ItemType = SingleChoiceActionItemType.ItemIsOperation
+            };
+            changeModelAction.Execute += SingleChoiceActionOnExecute;
+        }
+
+        private void SingleChoiceActionOnExecute(object sender, SingleChoiceActionExecuteEventArgs singleChoiceActionExecuteEventArgs){
+            
+        }
+    }
+}
---
+++ Demos/Modules/XpandSystem/XpandSystemTester.Module/FunctionalTests/Actions/ActionsObject.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace XpandSystemTester.Module.FunctionalTests.Actions {
+    [DefaultClassOptions]    
+    public class ActionsObject:BaseObject {
+        public ActionsObject(Session session) : base(session){
+        }
+        
+
+        public string PropertyName { get; set; }
+        public string PropertyName2 { get; set; }
+    }
+}
