b860a5433dcc63c6e567133d38606f5b5df2f9e3
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-12 15:15:30 +0300

    [End User Reset View Variants Model together with EasyTests]
    http://www.expandframework.com/forum/8-discussion/4105-viewvariants-reset-function/7175.html

---
+++ Demos/Modules/ViewVariants/ViewVariantsTester.Module/FunctionalTests/Actions/ActionsObject.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ViewVariantsTester.Module.FunctionalTests.Actions {
+    [DefaultClassOptions]
+    public class ActionsObject:BaseObject {
+        public ActionsObject(Session session) : base(session){
+        }
+
+        public string PropertyName { get; set; }
+        public string PropertyName2 { get; set; }
+    }
+}
---
+++ Demos/Modules/ViewVariants/ViewVariantsTester.Module/FunctionalTests/XpandEasyTestController.cs
@@ -0,0 +1,9 @@
+﻿using DevExpress.ExpressApp;
+
+namespace ViewVariantsTester.Module.FunctionalTests {
+    public class XpandEasyTestController : ViewController {
+        public virtual string ChangeColumnCaption(string caption) {
+            return null;
+        }
+    }
+}
