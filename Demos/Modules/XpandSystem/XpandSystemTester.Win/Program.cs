fc756ad537cfb0d1624401c9de80eb101d50b099
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-14 21:51:49 +0300

    minor update

--- Demos/Modules/XpandSystem/XpandSystemTester.Win/Program.cs
+++ Demos/Modules/XpandSystem/XpandSystemTester.Win/Program.cs
@@ -3,7 +3,6 @@ using System.Configuration;
 using System.Windows.Forms;
 using DevExpress.ExpressApp.Security;
 using Xpand.ExpressApp.Security.Core;
-using Xpand.Persistent.Base.General;
 
 namespace XpandSystemTester.Win {
     static class Program {
@@ -15,14 +14,12 @@ namespace XpandSystemTester.Win {
             
 #if EASYTEST
 			DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
-            XpandModuleBase.IsEasyTesting = true;
 #endif
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             EditModelPermission.AlwaysGranted = true;
             var winApplication = new XpandSystemTesterWindowsFormsApplication();
-            winApplication.DoStartupActions(XpandModuleBase.IsEasyTesting);
 #if EASYTEST
 			if(ConfigurationManager.ConnectionStrings["EasyTestConnectionString"] != null) {
 				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["EasyTestConnectionString"].ConnectionString;
