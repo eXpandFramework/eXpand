54b2a37995233c4d62a3cfab1ddd8e9352355da3
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-22 16:18:08 +0300

    hide console window

--- Resource/ProcessAsUser/ProcessAsUserWrapper/Program.cs
+++ Resource/ProcessAsUser/ProcessAsUserWrapper/Program.cs
@@ -14,7 +14,7 @@ namespace ProcessAsUserWrapper {
             Application.SetCompatibleTextRenderingDefault(false);
             var mainForm = new Form1();
             mainForm.Load+= (sender, eventArgs) =>{
-                var processStartInfo = new ProcessStartInfo(args[0], args.Length == 2 ? args[1] : null) {UseShellExecute=false, RedirectStandardOutput = true,WindowStyle = ProcessWindowStyle.Minimized};
+                var processStartInfo = new ProcessStartInfo(args[0], args.Length == 2 ? args[1] : null) {UseShellExecute=false, RedirectStandardOutput = true,CreateNoWindow = true};
                 var process = Process.Start(processStartInfo);
                 
                 Debug.Assert(process != null, "process != null");
