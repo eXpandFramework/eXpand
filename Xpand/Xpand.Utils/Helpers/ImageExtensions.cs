c74d476171f22f209bff1e4f610731e78dc94c70
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-28 02:03:59 +0300

    restore mistaken removed method

--- Xpand/Xpand.Utils/Helpers/ImageExtensions.cs
+++ Xpand/Xpand.Utils/Helpers/ImageExtensions.cs
@@ -17,6 +17,27 @@ namespace Xpand.Utils.Helpers{
             }
         }
 
+        public static Image CreateImage(this Image srcImage, int maxWidth, int maxHeight) {
+            if (srcImage == null) return null;
+            Size size = GetPhotoSize(srcImage, maxWidth, maxHeight);
+            Image ret = new Bitmap(size.Width, size.Height);
+            using (Graphics gr = Graphics.FromImage(ret)) {
+                gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                gr.DrawImage(srcImage, new Rectangle(0, 0, size.Width, size.Height));
+            }
+            return ret;
+        }
+
+        private static Size GetPhotoSize(Image image, int maxWidth, int maxHeight) {
+            int width = Math.Min(maxWidth, image.Width);
+            int height = width * image.Height / image.Width;
+            if (height > maxHeight) {
+                height = maxHeight;
+                width = height * image.Width / image.Height;
+            }
+            return new Size(width, height);
+        }
+
         public static Bitmap ToNonIndexedImage(this Image image) {
             var newBmp = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppArgb);
             using (Graphics gfx = Graphics.FromImage(newBmp)) {
