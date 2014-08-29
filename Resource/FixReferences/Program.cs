99e03d325cd11031d6f3e1140eac562f2c0cb2a2
apobekiaris <apostolis.bekiaris@gmail.com>
2014-08-29 19:59:19 +0300

    build fail

--- Resource/FixReferences/Program.cs
+++ Resource/FixReferences/Program.cs
@@ -25,7 +25,7 @@ namespace FixReferences {
             }
 
             
-            files = Directory.GetFiles(Path.Combine(rootDir, @"Resource\Nuget"), "*.nuspec");
+            files = Directory.GetFiles(Path.Combine(rootDir, @"Resource\Nuspec"), "*.nuspec");
             foreach (var file in files) {
                 var projectReferencesUpdater = new NugetUpdater(documentHelper, rootDir, version);
                 projectReferencesUpdater.Update(file);
