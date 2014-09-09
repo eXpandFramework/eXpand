861325f5eedb4be3d06f04ff503b64584934d0a4
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-09 20:02:58 +0300

    minor update

--- Demos/Modules/SystemTester/SystemTester.Module/Module.cs
+++ Demos/Modules/SystemTester/SystemTester.Module/Module.cs
@@ -1,32 +1,16 @@
 using System;
-using System.Text;
-using System.Linq;
 using DevExpress.ExpressApp;
-using System.ComponentModel;
-using DevExpress.ExpressApp.DC;
 using System.Collections.Generic;
-using DevExpress.Persistent.Base;
-using DevExpress.ExpressApp.Model;
-using DevExpress.ExpressApp.Actions;
-using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Updating;
-using DevExpress.ExpressApp.Model.Core;
-using DevExpress.ExpressApp.Model.DomainLogics;
-using DevExpress.ExpressApp.Model.NodeGenerators;
 
 namespace SystemTester.Module {
-    // For more typical usage scenarios, be sure to check out http://documentation.devexpress.com/#Xaf/clsDevExpressExpressAppModuleBasetopic.
     public sealed partial class SystemTesterModule : ModuleBase {
         public SystemTesterModule() {
             InitializeComponent();
         }
         public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
             ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
-            return new ModuleUpdater[] { updater };
-        }
-        public override void Setup(XafApplication application) {
-            base.Setup(application);
-            // Manage various aspects of the application UI and behavior at the module level.
+            return new[] { updater };
         }
     }
 }
