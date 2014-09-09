0bdc2be200b6fec7a3a7d3a7f21aaf00dea7a5a7
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-09 20:26:35 +0300

    RememberMe needs only XpandLogonParameters and no model modifications

--- Xpand/Xpand.ExpressApp.Modules/Security/AuthenticationProviders/XpandAuthenticationStandard.cs
+++ Xpand/Xpand.ExpressApp.Modules/Security/AuthenticationProviders/XpandAuthenticationStandard.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
-using DevExpress.ExpressApp.Utils;
 using DevExpress.Utils;
 using Xpand.Persistent.Base.General;
 using Xpand.Persistent.Base.Security;
@@ -21,13 +20,11 @@ namespace Xpand.ExpressApp.Security.AuthenticationProviders {
                 var application = ApplicationHelper.Instance.Application;
                 application.ReadLastLogonParameters();
                 var xpandLogonParameters = LogonParameters as XpandLogonParameters;
-                if (((IModelOptionsAuthentication) CaptionHelper.ApplicationModel.Options).Athentication.AutoAthentication.Enabled){
-                    var ask = xpandLogonParameters == null || (!xpandLogonParameters.RememberMe || !(!(string.IsNullOrEmpty(
-                    xpandLogonParameters.Password)) && !(string.IsNullOrEmpty(xpandLogonParameters.UserName))));
-                    if (!ask){
-                        var authenticationStandard =((SecurityStrategyBase) SecuritySystem.Instance).Authentication as XpandAuthenticationStandard;
-                        return authenticationStandard != null && !authenticationStandard.CanAuthenticate();
-                    }
+                var ask = xpandLogonParameters == null || (!xpandLogonParameters.RememberMe || !(!(string.IsNullOrEmpty(
+                xpandLogonParameters.Password)) && !(string.IsNullOrEmpty(xpandLogonParameters.UserName))));
+                if (!ask){
+                    var authenticationStandard =((SecurityStrategyBase) SecuritySystem.Instance).Authentication as XpandAuthenticationStandard;
+                    return authenticationStandard != null && !authenticationStandard.CanAuthenticate();
                 }
                 return true;
             }
