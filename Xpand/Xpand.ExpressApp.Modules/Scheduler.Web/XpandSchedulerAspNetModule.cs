5765db21687d2838e8472b7475183313382e4d9c
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-16 13:32:22 +0300

    douplicate node exceptions because non platform controllers are loaded
    http://www.expandframework.com/forum/6-bugs/4166-xpandwebapplication-non-threadsafe-xpandobjectspaceprovider.html#7456

--- Xpand/Xpand.ExpressApp.Modules/Scheduler.Web/XpandSchedulerAspNetModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/Scheduler.Web/XpandSchedulerAspNetModule.cs
@@ -6,7 +6,7 @@ using Xpand.Persistent.Base.General;
 namespace Xpand.ExpressApp.Scheduler.Web {
     [ToolboxBitmap(typeof(XpandSchedulerAspNetModule))]
     [ToolboxItem(true)]
-    [ToolboxTabName(XpandAssemblyInfo.TabWinModules)]
+    [ToolboxTabName(XpandAssemblyInfo.TabAspNetModules)]
     public sealed class XpandSchedulerAspNetModule : XpandModuleBase {
         public XpandSchedulerAspNetModule() {
             RequiredModuleTypes.Add(typeof (XpandSchedulerModule));
