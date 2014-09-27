bcbf9289354730635d2c752387080ecd4ab8459e
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-27 20:26:04 +0300

    [WorldCreator easytests for MergeWithExistingType]

---
+++ Demos/Modules/WorldCreator/WorldCreatorTester.Module/FunctionalTests/MergeWithExistingType/ExistingTypeToMergeWith.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using Xpand.Persistent.Base.General;
+
+namespace WorldCreatorTester.Module.FunctionalTests.MergeWithExistingType {
+    [XpandNavigationItem("Merge/ExistingTypeToMergeWith", "ExistingTypeToMergeWith_ListView")]
+    public class ExistingTypeToMergeWith : BaseObject {
+        string _firstName;
+        string _lastName;
+
+        public ExistingTypeToMergeWith(Session session)
+            : base(session) {
+        }
+
+        public string FirstName {
+            get { return _firstName; }
+            set { SetPropertyValue("FirstName", ref _firstName, value); }
+        }
+
+        public string LastName {
+            get { return _lastName; }
+            set { SetPropertyValue("LastName", ref _lastName, value); }
+        }
+    }
+
+}
