59ea1b9f52ecba67621f848879e856350efc697f
apobekiaris <apostolis.bekiaris@gmail.com>
2014-12-01 20:20:36 +0200

    [EasyTests for SequenceGenerator]

---
+++ Demos/Modules/SystemTester/SystemTester.Module/FunctionalTests/SequenceGenerator/SequenceGeneratorObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using Xpand.Persistent.Base.General;
+
+namespace SystemTester.Module.FunctionalTests.SequenceGenerator {
+    [DefaultClassOptions]
+    public class SequenceGeneratorObject:BaseObject,ISupportSequenceObject {
+        public SequenceGeneratorObject(Session session) : base(session){
+        }
+
+        protected override void OnSaving() {
+            base.OnSaving();
+            Xpand.Persistent.Base.General.SequenceGenerator.GenerateSequence(this);
+        }
+        [Action]
+        public void UpdateSequence(){
+            var guid = Oid;
+            ClassInfo.KeyProperty.SetValue(this,Guid.Empty);
+            Xpand.Persistent.Base.General.SequenceGenerator.GenerateSequence(this);
+            ClassInfo.KeyProperty.SetValue(this, guid);
+        }
+        public long Sequence { get; set; }
+
+        public string Prefix { get; set; }
+    }
+}
