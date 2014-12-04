8009e9049d246cf427da6d6bf3ef9312edfa7d24
apobekiaris <apostolis.bekiaris@gmail.com>
2014-12-04 17:14:49 +0200

    [AuditTrail support for unlimited fields auditing]
    http://www.expandframework.com/forum/13-suggestions/4190-xpand-audittrail-change-field.html#7552

---
+++ Xpand/Xpand.ExpressApp.Modules/AuditTrail/XpandAuditDataStore.cs
@@ -0,0 +1,36 @@
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.AuditTrail;
+using DevExpress.Xpo;
+using Xpand.ExpressApp.AuditTrail.BusinessObjects;
+
+namespace Xpand.ExpressApp.AuditTrail{
+    public class XpandAuditDataStore : AuditDataStore<IAuditDataItemPersistent<XpandAuditedObjectWeakReference>, XpandAuditedObjectWeakReference>{
+        protected override string GetDefaultStringRepresentation(object value){
+            if (value == null) {
+                return NullValueString;
+            }
+            if (value is XPWeakReference) {
+                if (!(value as XPWeakReference).IsAlive) {
+                    return NullValueString;
+                }
+                return (value as XPWeakReference).Target.ToString();
+            }
+            ITypeInfo ti = XafTypesInfo.Instance.FindTypeInfo(value.GetType());
+            IMemberInfo defaultMember = (ti != null) ? ti.DefaultMember : null;
+            string result;
+            if (defaultMember != null){
+                object memberValue = defaultMember.GetValue(value);
+                result = memberValue == null ? NullValueString : memberValue.ToString();
+            }
+            else {
+                result = value.ToString();
+            }
+            if (!(value is string) && result.Length > MaxLengthOfValue) {
+                result = BlobDataString;
+            }
+            return result;
+
+        }
+    }
+}
