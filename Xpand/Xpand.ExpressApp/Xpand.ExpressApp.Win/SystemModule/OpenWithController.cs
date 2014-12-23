5fb7564f4cf2397b839c22526b9d39b3a5986b28
apobekiaris <apostolis.bekiaris@gmail.com>
2014-12-23 16:38:38 +0200

    [OpenWirth functionality to handle path data]

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/OpenWithController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/OpenWithController.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Views.Base;
@@ -11,39 +13,88 @@ using ListView = DevExpress.ExpressApp.ListView;
 
 namespace Xpand.ExpressApp.Win.SystemModule {
     [ModelAbstractClass]
-    public interface IModelColumnOpenWith {
+    public interface IModelCommonMemberViewItemOpenWith {
         [Category("eXpand")]
         bool OpenWithAssociatedProgram { get; set; }
         [Category("eXpand")]
         bool OpenPathWithExplorer { get; set; }
     }
 
-    public class OpenWithController : ViewController<ListView>, IModelExtender {
-        protected override void OnViewControlsCreated() {
-            base.OnViewControlsCreated();
-            var gridControl = View.Editor.Control as GridControl;
-            if (gridControl != null) gridControl.KeyDown += GridControlOnKeyDown;
+    public class OpenWithController : ViewController<ObjectView>, IModelExtender {
+        protected override void OnActivated() {
+            base.OnActivated();
+            var detailView = View as DetailView;
+            if (detailView != null) {
+                OpenWith(detailView);
+            }
+
         }
 
-        private void GridControlOnKeyDown(object sender, KeyEventArgs keyEventArgs) {
-            if (keyEventArgs.KeyCode == Keys.Space) {
-                var focusedView = ((GridControl)sender).FocusedView as ColumnView;
-                if (focusedView != null) {
-                    var modelColumnOpenInExplorer = ((IModelColumnOpenWith)focusedView.FocusedColumn.Model());
-                    var path = focusedView.GetRowCellDisplayText(focusedView.FocusedRowHandle, focusedView.FocusedColumn);
-                    if (modelColumnOpenInExplorer.OpenWithAssociatedProgram) {
-                        Process.Start(path);
-                    }
-                    if (modelColumnOpenInExplorer.OpenPathWithExplorer) {
-                        var directoryName = Path.GetDirectoryName(path) + "";
-                        Process.Start(directoryName);
-                    }
+        private void OpenWith(DetailView detailView) {
+            var propertyEditors = detailView.GetItems<PropertyEditor>();
+            foreach (var propertyEditor in propertyEditors) {
+                propertyEditor.ControlCreated += PropertyEditorOnControlCreated;
+            }
+        }
+
+        private void PropertyEditorOnControlCreated(object sender, EventArgs eventArgs) {
+            var editor = (PropertyEditor)sender;
+            ((Control)editor.Control).KeyUp += (ender, args) => {
+                if (args.KeyCode == Keys.Enter) {
+                    var modelCommonMemberViewItemOpenWith = ((IModelCommonMemberViewItemOpenWith)editor.Model);
+                    OpenWith(modelCommonMemberViewItemOpenWith, editor.PropertyValue.ToString());
                 }
+            };
+        }
+
+        protected override void OnViewControlsCreated() {
+            base.OnViewControlsCreated();
+            var listView = View as ListView;
+            if (listView != null) {
+                OpenWith(listView);
+            }
+        }
+
+        private void OpenWith(ListView listView) {
+            var gridControl = listView.Editor.Control as GridControl;
+            if (gridControl != null) {
+                gridControl.KeyUp += GridControlOnKeyUp;
+                gridControl.MouseDoubleClick += GridControlOnMouseDoubleClick;
+            }
+        }
+
+        private void GridControlOnMouseDoubleClick(object sender, MouseEventArgs mouseEventArgs) {
+            OpenWith((GridControl)(sender));
+        }
+
+        private void GridControlOnKeyUp(object sender, KeyEventArgs keyEventArgs) {
+            if (keyEventArgs.KeyCode == Keys.Enter) {
+                var gridControl = ((GridControl)sender);
+                OpenWith(gridControl);
+            }
+        }
+
+        private void OpenWith(GridControl gridControl) {
+            var focusedView = gridControl.FocusedView as ColumnView;
+            if (focusedView != null) {
+                var path = focusedView.GetRowCellDisplayText(focusedView.FocusedRowHandle, focusedView.FocusedColumn);
+                var modelCommonMemberViewItemOpenWith = ((IModelCommonMemberViewItemOpenWith)focusedView.FocusedColumn.Model());
+                OpenWith(modelCommonMemberViewItemOpenWith, path);
+            }
+        }
+
+        private void OpenWith(IModelCommonMemberViewItemOpenWith modelCommonMemberViewItemOpenWith, string path) {
+            if (modelCommonMemberViewItemOpenWith.OpenWithAssociatedProgram) {
+                Process.Start(path);
+            }
+            if (modelCommonMemberViewItemOpenWith.OpenPathWithExplorer) {
+                var directoryName = Path.GetDirectoryName(path) + "";
+                Process.Start(directoryName);
             }
         }
 
         public void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
-            extenders.Add<IModelColumn, IModelColumnOpenWith>();
+            extenders.Add<IModelCommonMemberViewItem, IModelCommonMemberViewItemOpenWith>();
         }
     }
 }
