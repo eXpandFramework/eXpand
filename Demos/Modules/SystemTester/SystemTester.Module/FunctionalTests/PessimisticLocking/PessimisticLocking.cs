56695cf806d158fbcdddf470e40751058d5397d4
apobekiaris <apostolis.bekiaris@gmail.com>
2014-12-29 13:11:03 +0200

    [Pessimistic locking EasyTests ]

---
+++ Demos/Modules/SystemTester/SystemTester.Module/FunctionalTests/PessimisticLocking/PessimisticLocking.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using Xpand.ExpressApp.Security.Core;
+using Xpand.Persistent.Base.General;
+using Xpand.Persistent.Base.General.Controllers;
+
+namespace SystemTester.Module.FunctionalTests.PessimisticLocking {
+    public class PessimisticLocking:ObjectViewController<ListView,PessimisticLockingObject>{
+        public PessimisticLocking(){
+            var simpleAction = new SimpleAction(this,"MarkModified",PredefinedCategory.View){Caption = "Modify"};
+            simpleAction.Execute+=SimpleActionOnExecute;
+        }
+
+        private void SimpleActionOnExecute(object sender, SimpleActionExecuteEventArgs simpleActionExecuteEventArgs){
+            var selectedObject = View.SelectedObjects[0];
+            var member = View.ObjectTypeInfo.FindMember(PessimisticLockingViewController.LockedUser);
+            member.SetValue(selectedObject,ObjectSpace.FindObject<XpandUser>(user => user.UserName=="User"));
+            ObjectSpace.CommitChanges();
+        }
+    }
+}
---
+++ Demos/Modules/SystemTester/SystemTester.Module/FunctionalTests/PessimisticLocking/PessimisticLockingObject.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using Xpand.Persistent.Base.General.Controllers;
+
+namespace SystemTester.Module.FunctionalTests.PessimisticLocking{
+    [PessimisticLocking]
+    [DefaultClassOptions]
+    public class PessimisticLockingObject : XPLiteObject{
+        private int _key;
+        private string _name;
+
+        public PessimisticLockingObject(Session session) : base(session){
+        }
+
+        [Key(true)]
+        [Browsable(false)]
+        public int Key{
+            get { return _key; }
+            set { SetPropertyValue("Key", ref _key, value); }
+        }
+
+        public string Name{
+            get { return _name; }
+            set { SetPropertyValue("Name", ref _name, value); }
+        }
+    }
+}
