3b6fa4acfd990053198b84bc830de73e2adf90d6
apobekiaris <apostolis.bekiaris@gmail.com>
2015-01-03 10:15:46 +0200

    [EasyTests for hiding a lookup action using the model]

--- Demos/Modules/ModelArtifactState/ModelArtifactStateTester.Module/FunctionalTests/Actions/ActionsObject.cs
+++ Demos/Modules/ModelArtifactState/ModelArtifactStateTester.Module/FunctionalTests/Actions/ActionsObject.cs
@@ -2,10 +2,17 @@
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.Xpo;
 
-namespace ModelArtifactStateTester.Module.FunctionalTests.Actions {
+namespace ModelArtifactStateTester.Module.FunctionalTests.Actions{
     [DefaultClassOptions]
-    public class ActionsObject:BaseObject {
+    public class ActionsObject : BaseObject{
+        private ActionsObject _actionsObject;
+
         public ActionsObject(Session session) : base(session){
+        }
+
+        public ActionsObject ActiObject{
+            get { return _actionsObject; }
+            set { SetPropertyValue("ActiObject", ref _actionsObject, value); }
         }
     }
 }
