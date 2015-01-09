cd49247dfd4248048dafd247db9375c44266d738
apobekiaris <apostolis.bekiaris@gmail.com>
2015-01-09 05:01:25 +0200

    adapter terminates running tests

--- Xpand.EasyTest/Xpand.ExpressApp.EasyTest.WinAdapter/XpandTestWinAdapter.cs
+++ Xpand.EasyTest/Xpand.ExpressApp.EasyTest.WinAdapter/XpandTestWinAdapter.cs
@@ -24,15 +24,72 @@ namespace Xpand.ExpressApp.EasyTest.WinAdapter {
             registrator.RegisterCommand(HideScrollBarCommand.Name, typeof(Commands.HideScrollBarCommand));
         }
 
+        void CloseApplication(IEnumerable<Process> appProcesses, bool force) {
+            int totalTimeout = force ? 1 : 10000;
+            foreach (Process appProcess in appProcesses) {
+                var processName = appProcess.ProcessName;
+                DateTime start = DateTime.Now;
+                TimeSpan spendTime = TimeSpan.Zero;
+                bool closed = false;
+                IntPtr lastWindowForClose = IntPtr.Zero;
+                while (spendTime.TotalMilliseconds < totalTimeout) {
+                    Trace.WriteLine(DateTime.Now + "\tCall Process.Refresh() at WinApplicationAdapter.CloseApplication");
+                    appProcess.Refresh();
+                    Trace.WriteLine(DateTime.Now + "\tCall Process.Refresh() success");
+                    Trace.WriteLine(DateTime.Now + "\tCall Process.MainWindowHandle at WinApplicationAdapter.CloseApplication");
+                    IntPtr processWindowHandle = IntPtr.Zero;
+                    try {
+                        processWindowHandle = appProcess.MainWindowHandle;
+                    }
+                    catch (InvalidOperationException) { }
+                    if (processWindowHandle != lastWindowForClose && processWindowHandle != IntPtr.Zero) {
+                        lastWindowForClose = processWindowHandle;
+                        try {
+                            appProcess.CloseMainWindow();
+                        }
+                        catch (Exception e) {
+                            Logger.Instance.AddMessage(string.Format("Process.CloseMainWindow return error:\n'{0}'", e.Message));
+                        }
+                    }
+                    Trace.WriteLine(DateTime.Now + "\tCall Process.MainWindowHandle success");
+                    try {
+                        if (appProcess.WaitForExit(Math.Min(1000, (int)(totalTimeout - spendTime.TotalMilliseconds)))) {
+                            closed = true;
+                            break;
+                        }
+                    }
+                    catch (Exception e) {
+                        Logger.Instance.AddMessage(string.Format("Process.WaitForExit return error:\n'{0}'", e.Message));
+                    }
+                    spendTime = DateTime.Now - start;
+                }
+                if (!closed) {
+                    if (!force) {
+                        Logger.Instance.AddMessage(string.Format(
+                            "The process '{0}' was not closed in '{1}' millisecond after " +
+                            "the Process.CloseMainWindow was invoked, trying to kill this process",
+                            processName, totalTimeout));
+                    }
+                    try {
+                        appProcess.Kill();
+                    }
+                    catch (Exception e) {
+                        Logger.Instance.AddMessage(string.Format("Process name: '{0}' is not killed.\nReason:\n'{1}'", processName, e.Message));
+                    }
+                    if (!appProcess.WaitForExit(10000)) {
+                        throw new WarningException(string.Format("Process name: '{0}' doesn't exited in 10 seconds after the Process.Kill was invoked", processName));
+                    }
+                }
+            }
+        }
+
         public override void KillApplication(TestApplication testApplication, KillApplicationConext context) {
             ScreenCaptureCommand.Stop();
             if (_easyTestCommandAdapter != null) {
                 _easyTestCommandAdapter.Disconnect();
             }
-            CloseApplication(mainProcess.ProcessName, true);
+            CloseApplication(new[]{mainProcess}, true);
             CloseApplication(_additionalProcesses.Where(process => !process.HasExited).ToArray(), true);
-                CloseApplication(additionalProcess.ProcessName, true);
-            }
         }
 
         public override void RunApplication(TestApplication testApplication) {
