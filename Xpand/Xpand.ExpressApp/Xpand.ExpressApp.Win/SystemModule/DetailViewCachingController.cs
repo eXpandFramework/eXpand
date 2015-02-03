5f96103da176a6481019e76c8f120e067a49b98a
apobekiaris <apostolis.bekiaris@gmail.com>
2015-02-03 09:17:45 +0200

    App not closed when DetailViewCaching and TabbedMDI
    http://www.expandframework.com/forum/6-bugs/4343-detailviewcaching-application-doesnt-close-and-new-record-creation/8266.html

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/DetailViewCachingController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/DetailViewCachingController.cs
@@ -29,74 +29,86 @@ namespace Xpand.ExpressApp.Win.SystemModule {
         }
     }
 
-    public class DetailViewCachingController:ViewController<DetailView>, IModelExtender{
+    public class DetailViewCachingController : ViewController<DetailView>, IModelExtender {
         private bool _isVisible;
         private static bool _applicationWindowClosing;
+        private CloseReason _closeReason;
 
-        protected override void OnActivated(){
+        protected override void OnActivated() {
             base.OnActivated();
             if (_isVisible && ((IModelDetailViewCaching)View.Model).DetailViewCaching) {
-                View.QueryCanClose+=ViewOnQueryCanClose;
+                View.QueryCanClose += ViewOnQueryCanClose;
             }
         }
 
-        protected override void OnDeactivated(){
+        protected override void OnDeactivated() {
             base.OnDeactivated();
             View.QueryCanClose -= ViewOnQueryCanClose;
         }
 
-        protected override void OnFrameAssigned(){
+        private void OnCancel(object sender, CancelEventArgs cancelEventArgs) {
+            _applicationWindowClosing = Frame.Template == Application.MainWindow.Template;
+        }
+
+        protected override void OnFrameAssigned() {
             base.OnFrameAssigned();
             var modelOptions = Application.Model.Options;
             _isVisible = new ModelOptionsDetailViewCachingVisibilityCalculator().IsVisible(modelOptions, null);
+            var winWindow = Frame as WinWindow;
+            if (winWindow != null) winWindow.Closing += OnClosing;
             if (_isVisible && Frame.Context == TemplateContext.ApplicationWindow) {
                 Application.DetailViewCreated += ApplicationOnDetailViewCreated;
+                Frame.GetController<CloseFormController>().Cancel += OnCancel;
                 Frame.TemplateChanged += FrameOnTemplateChanged;
+
             }
-            Frame.Disposing+=FrameOnDisposing;
+            Frame.Disposing += FrameOnDisposing;
         }
 
-        private void FrameOnDisposing(object sender, EventArgs eventArgs){
-            Frame.Disposing-=FrameOnDisposing;
-            Application.DetailViewCreated-=ApplicationOnDetailViewCreated;
-            Frame.TemplateChanged-=FrameOnTemplateChanged;
+        private void FrameOnDisposing(object sender, EventArgs eventArgs) {
+            Frame.Disposing -= FrameOnDisposing;
+            Application.DetailViewCreated -= ApplicationOnDetailViewCreated;
+            Frame.TemplateChanged -= FrameOnTemplateChanged;
+            var winWindow = Frame as WinWindow;
+            if (winWindow != null) winWindow.Closing -= OnClosing;
         }
 
-        private void FrameOnTemplateChanged(object sender, EventArgs eventArgs){
-            ((Form) Frame.Template).Closing+=OnClosing;
+        private void FrameOnTemplateChanged(object sender, EventArgs eventArgs) {
+            var form = ((Form)Frame.Template);
+            form.Closing += OnClosing;
         }
 
-        private void OnClosing(object sender, CancelEventArgs cancelEventArgs){
-            _applicationWindowClosing = !cancelEventArgs.Cancel;
+        private void OnClosing(object sender, CancelEventArgs cancelEventArgs) {
+            _closeReason = ((FormClosingEventArgs)cancelEventArgs).CloseReason;
         }
 
-        private void ViewOnQueryCanClose(object sender, CancelEventArgs e){
-            if (CloseFormController.IsNotLoggingOffOrModelEditing&&!_applicationWindowClosing) {
+        private void ViewOnQueryCanClose(object sender, CancelEventArgs e) {
+            if (CloseFormController.IsNotLoggingOffOrModelEditing && !_applicationWindowClosing && _closeReason == CloseReason.UserClosing) {
                 var cancelEventArgs = new CancelEventArgs(false);
                 Frame.GetController<WinModificationsController>().CallMethod("OnViewQueryCanClose", cancelEventArgs);
                 e.Cancel = true;
-                if (!cancelEventArgs.Cancel){
+                if (!cancelEventArgs.Cancel) {
                     if (ObjectSpace.IsNewObject(View.CurrentObject))
                         ObjectSpace.SetIsModified(false);
-                    ((Form) Frame.Template).Hide();
+                    ((Form)Frame.Template).Hide();
                 }
             }
         }
 
         private void ApplicationOnDetailViewCreated(object sender, DetailViewCreatedEventArgs e) {
-            var strategy = ((WinShowViewStrategyBase)Application.ShowViewStrategy);
-            var winWindow = strategy.Windows.FirstOrDefault(window => window.View != null && window.View.Model == e.View.Model);
-            if (winWindow != null) {
-                if (((IModelDetailViewCaching)e.View.Model).DetailViewCaching) {
+            if (((IModelDetailViewCaching)e.View.Model).DetailViewCaching) {
+                var strategy = ((WinShowViewStrategyBase)Application.ShowViewStrategy);
+                var winWindow = strategy.Windows.FirstOrDefault(window => window.View != null && window.View.Model == e.View.Model);
+                if (winWindow != null) {
                     if (winWindow.View.ObjectSpace.IsNewObject(winWindow.View.CurrentObject))
                         winWindow.View.ObjectSpace.RollbackSilent();
-                    winWindow.View.CurrentObject=GetCurrentObject(e, winWindow);
+                    winWindow.View.CurrentObject = GetCurrentObject(e, winWindow);
                 }
             }
         }
 
-        private object GetCurrentObject(DetailViewCreatedEventArgs e, WinWindow winWindow){
-            if (e.View.ObjectSpace.IsNewObject(e.View.CurrentObject)){
+        private object GetCurrentObject(DetailViewCreatedEventArgs e, WinWindow winWindow) {
+            if (e.View.ObjectSpace.IsNewObject(e.View.CurrentObject)) {
                 var newViewShortcut = e.View.CreateShortcut();
                 var temporaryObjectKey = newViewShortcut[ViewShortcut.TemporaryObjectKeyParamName];
                 var newObj = winWindow.View.ObjectSpace.CreateObject(winWindow.View.ObjectTypeInfo.Type);
@@ -124,7 +136,7 @@ namespace Xpand.ExpressApp.Win.SystemModule {
             };
         }
 
-        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders){
+        void IModelExtender.ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
             extenders.Add<IModelOptions, IModelOptionsDetailViewCaching>();
             extenders.Add<IModelDetailView, IModelDetailViewCaching>();
         }
