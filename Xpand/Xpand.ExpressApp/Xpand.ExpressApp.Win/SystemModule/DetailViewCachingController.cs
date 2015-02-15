98440ae9b89b2778c879f2ef35bfdd1046b7c9a9
apobekiaris <apostolis.bekiaris@gmail.com>
2015-02-15 11:46:31 +0200

    Use Cloner for DetailViewCaching newobjects
    http://www.expandframework.com/forum/6-bugs/4414-detailviewcaching-and-clone-possible-bug/8270.html

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/DetailViewCachingController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/SystemModule/DetailViewCachingController.cs
@@ -6,9 +6,13 @@ using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Win;
 using DevExpress.ExpressApp.Win.SystemModule;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
 using Fasterflect;
 using Xpand.Persistent.Base.General;
 using Xpand.Persistent.Base.General.Model;
+using Xpand.Persistent.Base.Xpo;
+using Xpand.Xpo;
 
 namespace Xpand.ExpressApp.Win.SystemModule {
     public interface IModelOptionsDetailViewCaching : IModelNode {
@@ -33,6 +37,12 @@ namespace Xpand.ExpressApp.Win.SystemModule {
         private bool _isVisible;
         private static bool _applicationWindowClosing;
         private CloseReason _closeReason;
+        public event EventHandler<CustomNewObjectArgs> CustomNewObject;
+
+        protected virtual void OnCustomNewObject(CustomNewObjectArgs e){
+            var handler = CustomNewObject;
+            if (handler != null) handler(this, e);
+        }
 
         protected override void OnActivated() {
             base.OnActivated();
@@ -102,17 +112,41 @@ namespace Xpand.ExpressApp.Win.SystemModule {
                 if (winWindow != null) {
                     if (winWindow.View.ObjectSpace.IsNewObject(winWindow.View.CurrentObject))
                         winWindow.View.ObjectSpace.RollbackSilent();
-                    winWindow.View.CurrentObject = GetCurrentObject(e, winWindow);
+                    var currentObject = GetCurrentObject(e, winWindow);
+                    winWindow.View.CurrentObject = currentObject;
                 }
             }
         }
 
+        class Cloner : DevExpress.Persistent.Base.Cloner {
+            public override IXPSimpleObject CreateObject(Session session, Type type){
+                return NewObject(type);
+            }
+
+            public override void CopyMemberValue(XPMemberInfo memberInfo, IXPSimpleObject sourceObject, IXPSimpleObject targetObject){
+                CheckMemberInObjects(memberInfo, sourceObject, targetObject);
+                object memberValue = this.CallMethod("GetSourceMemberValue",memberInfo, sourceObject);
+                if (memberValue is IXPSimpleObject) {
+                    this.CallMethod("ClearTargetObjectProperty",memberInfo, targetObject, memberInfo.IsAggregated);
+                    var clonedObjectReference = (IXPSimpleObject) this.CallMethod("CloneReferenceMemberValue",targetObject, memberInfo, (IXPSimpleObject)memberValue, memberValue.GetType(), memberInfo.IsAggregated);
+                    if (!clonedObjectReference.IsNewObject())
+                        clonedObjectReference = targetObject.XPObjectSpace().GetObject(clonedObjectReference);
+                    memberInfo.SetValue(targetObject, clonedObjectReference);
+                }
+                else {
+                    memberInfo.SetValue(targetObject, memberValue);
+                }
+            }
+
+
+            public Func<Type,IXPSimpleObject> NewObject;
+        }
         private object GetCurrentObject(DetailViewCreatedEventArgs e, WinWindow winWindow) {
             if (e.View.ObjectSpace.IsNewObject(e.View.CurrentObject)) {
                 var newViewShortcut = e.View.CreateShortcut();
                 var temporaryObjectKey = newViewShortcut[ViewShortcut.TemporaryObjectKeyParamName];
-                var newObj = winWindow.View.ObjectSpace.CreateObject(winWindow.View.ObjectTypeInfo.Type);
-                e.View.CurrentObject.Map(newObj, Flags.Public);
+                var newObj =new Cloner{NewObject =type =>(IXPSimpleObject)winWindow.View.ObjectSpace.CreateObject(type)}.
+                    CloneTo (e.View.CurrentObject, winWindow.View.ObjectTypeInfo.Type);
                 CustomizeViewShortcut(e, winWindow, temporaryObjectKey);
                 return newObj;
             }
@@ -140,5 +174,8 @@ namespace Xpand.ExpressApp.Win.SystemModule {
             extenders.Add<IModelOptions, IModelOptionsDetailViewCaching>();
             extenders.Add<IModelDetailView, IModelDetailViewCaching>();
         }
+    }
+
+    public class CustomNewObjectArgs : EventArgs{
     }
 }
