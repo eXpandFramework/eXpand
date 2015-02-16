3ac3ed455cb3740a0a1778a6ce42ed3a91301826
apobekiaris <apostolis.bekiaris@gmail.com>
2015-02-16 04:12:44 +0200

    [EasyTests For LayoutStyle]

---
+++ Demos/Modules/SystemTester/SystemTester.Module.Web/FunctionalTests/LayoutStyle/LayoutStyleObjects.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace SystemTester.Module.Web.FunctionalTests.LayoutStyle{
+    [DefaultClassOptions]
+    public class LayoutStyleObject : BaseObject{
+        public LayoutStyleObject(Session session) : base(session){
+        }
+
+        public string Name { get; set; }
+
+    }
+}
