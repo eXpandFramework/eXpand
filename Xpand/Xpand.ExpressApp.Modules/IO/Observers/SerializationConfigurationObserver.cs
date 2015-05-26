681c33249674caf48e77730fc188d6619836f444
Michael <Michael@3Way.be>
2015-05-26 15:29:49 +0200

    Removed Session Property from ISerializationConfiguration
    (cherry picked from commit 5f973d9820a43b990ff1c8b0fcda05c68e6028b9)

--- Xpand/Xpand.ExpressApp.Modules/IO/Observers/SerializationConfigurationObserver.cs
+++ Xpand/Xpand.ExpressApp.Modules/IO/Observers/SerializationConfigurationObserver.cs
@@ -2,6 +2,7 @@
 using Xpand.ExpressApp.IO.PersistentTypesHelpers;
 using Xpand.Persistent.Base.General;
 using Xpand.Persistent.Base.ImportExport;
+using Xpand.Persistent.Base.Xpo;
 using Xpand.Utils.Helpers;
 
 namespace Xpand.ExpressApp.IO.Observers {
@@ -18,7 +19,9 @@ namespace Xpand.ExpressApp.IO.Observers {
             if (e.PropertyName == serializationConfiguration.GetPropertyName(x => x.TypeToSerialize) && e.NewValue != null) {
                 if (!_serializing) {
                     _serializing = true;
-                    serializationConfiguration.Session.Delete(serializationConfiguration.SerializationGraph);
+                    ObjectSpace.Delete(serializationConfiguration.SerializationGraph);
+                    //serializationConfiguration.
+                        //.Session.Delete(serializationConfiguration.SerializationGraph);
                     new ClassInfoGraphNodeBuilder().Generate(serializationConfiguration);
                     _serializing = false;
                 }
--- Xpand/Xpand.Persistent/Xpand.Persistent.Base/ImportExport/ISerializationConfiguration.cs
+++ Xpand/Xpand.Persistent/Xpand.Persistent.Base/ImportExport/ISerializationConfiguration.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using DevExpress.Xpo;
 
 namespace Xpand.Persistent.Base.ImportExport {
-    public interface ISerializationConfiguration
+    public interface ISerializationConfiguration 
     {
-        Session Session { get; }
         Type TypeToSerialize { get; set; }
         IList<IClassInfoGraphNode> SerializationGraph { get; }
         ISerializationConfigurationGroup SerializationConfigurationGroup { get; set; }
