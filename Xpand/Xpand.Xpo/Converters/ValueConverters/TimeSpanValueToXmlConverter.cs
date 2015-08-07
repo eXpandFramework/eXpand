c48f32f3975bbb58dbac0139dab1ad08e74a155f
apobekiaris <apostolis.bekiaris@gmail.com>
2015-08-07 17:55:47 +0300

    TimeSpan valueconverter for remoting scenarios
    http://www.expandframework.com/forum/13-suggestions/4577-a-timespan-valueconverter-is-required-for-wcf-to-work-with-expand.html#8959

---
+++ Xpand/Xpand.Xpo/Converters/ValueConverters/TimeSpanValueToXmlConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml;
+using DevExpress.Xpo.Metadata;
+
+namespace Xpand.Xpo.Converters.ValueConverters{
+    public class TimeSpanValueToXmlConverter : ValueConverter {
+        public override object ConvertFromStorageType(object value) {
+            return value == null ? TimeSpan.Zero : XmlConvert.ToTimeSpan((String)value);
+        }
+        public override object ConvertToStorageType(object value) {
+            return value == null ? null : XmlConvert.ToString((TimeSpan)value);
+        }
+        public override Type StorageType { get { return typeof(String); } }
+    }
+}
