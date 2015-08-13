9cd9e6c96a7665b92bf760da6d03170de68616fd
Jim Horvath <jim.horvath@gmail.com>
2015-08-13 14:07:46 -0400

    add event to allow customizing the DashboardDesigner before opening

--- Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/Controllers/DashboardDesignerController.cs
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/Controllers/DashboardDesignerController.cs
@@ -17,6 +17,8 @@ namespace Xpand.ExpressApp.XtraDashboard.Win.Controllers {
             TargetObjectType = typeof(IDashboardDefinition);
         }
 
+        public event EventHandler<DashboardDesignerOpeningEventArgs> DashboardDesignerOpening;
+
         public SimpleAction DashboardEdit {
             get { return _dashboardEdit; }
         }
@@ -77,6 +79,9 @@ namespace Xpand.ExpressApp.XtraDashboard.Win.Controllers {
         void dashboardEdit_Execute(object sender, SimpleActionExecuteEventArgs e) {
             using (var form = new DashboardDesignerForm { ObjectSpace = ObjectSpace }) {
                 form.LoadTemplate(((IDashboardDefinition)View.CurrentObject), Application);
+                var eventHandler = DashboardDesignerOpening;
+                if (eventHandler != null)
+                    eventHandler(this, new DashboardDesignerOpeningEventArgs(form.Designer));
                 form.ShowDialog();
                 if (View is ListView)
                     ObjectSpace.CommitChanges();
