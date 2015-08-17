baa06d7a295998542e78a1f2b1c66cf412ca424a
apobekiaris <apostolis.bekiaris@gmail.com>
2015-08-17 20:13:51 +0300

    [Support for VS2015]

--- defines.bat
+++ defines.bat
@@ -7,7 +7,6 @@ rem uncomment the line bellow if you not use vs2012
 rem set vsver=vs2012
 rem set vsver=vs2013
 set ProgramFiles=%ProgramFiles(x86)%
-set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 
 set GACPATH="%WinDir%\assembly\GAC_MSIL\"
 set Gac4path="%WinDir%\Microsoft.NET\assembly\GAC_MSIL\"
@@ -16,8 +15,20 @@ if '%vsver%'=='vs2008' goto vs2008
 if '%vsver%'=='vs2010' goto vs2010
 if '%vsver%'=='vs2012' goto vs2012
 if '%vsver%'=='vs2013' goto vs2013
+if '%vsver%'=='vs2015' goto vs2015
+
+:vs2015
+set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+set sn="%ProgramFiles%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe"
+set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\gacutil.exe"
+set csharptemplates="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\IDE\ProjectTemplates\CSharp\DevExpress XAF\"
+set vbtemplates="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\IDE\ProjectTemplates\VisualBasic\DevExpress XAF\"
+set devenv="%ProgramFiles%\Microsoft Visual Studio 14.0\Common7\IDE\"
+goto end
+goto end
 
 :vs2013
+set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 set sn="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\sn.exe"
 set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\gacutil.exe"
 set csharptemplates="%ProgramFiles%\Microsoft Visual Studio 12.0\Common7\IDE\ProjectTemplates\CSharp\DevExpress XAF\"
@@ -27,6 +38,7 @@ goto end
 goto end
 
 :vs2012
+set msbuild="%ProgramFiles%\MSBuild\10.0\Bin\MSBuild.exe"
 set sn="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\sn.exe"
 set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\gacutil.exe"
 set csharptemplates="%ProgramFiles%\Microsoft Visual Studio 11.0\Common7\IDE\ProjectTemplates\CSharp\DevExpress XAF\"
