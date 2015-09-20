0e08c41220663c2ffc7b9a83279bec25db7fe7f1
apobekiaris <apostolis.bekiaris@gmail.com>
2015-09-20 20:06:44 +0300

    remove Xpand.Addins folder

--- Xpand.Addins/ModelEditor/IDependedAttributeInspector.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace eXpandAddIns.ModelEditor {
-    public interface IDependedAttributeInspector
-    {
-        List<string> GetAssemblies(string assembly);
-    }
-}
