056942f0d9e1eaf4a4f94b81161db06069fadff1
apobekiaris <apostolis.bekiaris@gmail.com>
2015-10-01 15:32:51 +0300

    Dashboard hide duplicate properties
    T280140

--- Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
+++ Xpand/Xpand.ExpressApp.Modules/Dashboard/Extensions.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Linq;
 using DevExpress.ExpressApp;
 
 namespace Xpand.ExpressApp.Dashboard {
     public static class Extensions {
         public static object CreateDashboardDataSource(this XafApplication application, Type objectType) {
             var space = application.CreateObjectSpace(objectType);
-            return new ProxyCollection(space, space.TypesInfo.FindTypeInfo(objectType), space.GetObjects(objectType));
+            var proxyCollection = new ProxyCollection(space, space.TypesInfo.FindTypeInfo(objectType), space.GetObjects(objectType));
+            proxyCollection.DisplayableMembers = string.Join(";", proxyCollection.DisplayableMembers.Split(';').Where(s => !s.EndsWith("!")));
+            return proxyCollection;
         }
     }
 }
