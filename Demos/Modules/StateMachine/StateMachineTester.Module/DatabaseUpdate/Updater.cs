1048672ce6f5d8f8d44ee8034e133c203a6ef25d
apobekiaris <apostolis.bekiaris@gmail.com>
2015-10-04 17:09:15 +0300

    Refactor ChangeStateAction StateMachine Easytest to HideIfCriteriaDoNotFit

--- Demos/Modules/StateMachine/StateMachineTester.Module/DatabaseUpdate/Updater.cs
+++ Demos/Modules/StateMachine/StateMachineTester.Module/DatabaseUpdate/Updater.cs
@@ -1,17 +1,16 @@
 using System;
-using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.Linq;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
 using DevExpress.ExpressApp.Security.Strategy;
+using DevExpress.ExpressApp.StateMachine.Utils;
 using DevExpress.ExpressApp.StateMachine.Xpo;
 using DevExpress.ExpressApp.Updating;
 using DevExpress.ExpressApp.Utils;
-using DevExpress.Persistent.Base.General;
 using StateMachineTester.Module.BusinessObjects;
 using Xpand.ExpressApp.Security.Core;
-using System.Linq;
-using Xpand.ExpressApp.StateMachine.Security;
-using Xpand.ExpressApp.StateMachine.Security.Improved;
+using Xpand.Persistent.Base.General;
 
 namespace StateMachineTester.Module.DatabaseUpdate {
     public class Updater : ModuleUpdater {
@@ -25,51 +24,93 @@ namespace StateMachineTester.Module.DatabaseUpdate {
             
 
             var userRole = (XpandRole) ObjectSpace.GetRole("User");
-            if (ObjectSpace.IsNewObject(userRole)) {
-                var permissionData = ObjectSpace.CreateObject<StateMachineTransitionOperationPermissionData>();
-                permissionData.StateCaption = "InProgress";
-                permissionData.StateMachineName = "Test";
-                permissionData.Modifier = StateMachineTransitionModifier.Deny;
-                userRole.Permissions.Add(permissionData);
-            }
-
-            userRole.SetTypePermissions<TestTask>( SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
+            var user = (SecuritySystemUser)userRole.GetUser("user");
+            user.Roles.Add(defaultRole);
             userRole.SetTypePermissions<XpoStateMachine>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
             userRole.SetTypePermissions<XpoState>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
             userRole.SetTypePermissions<XpoTransition>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
-            var user = (SecuritySystemUser)userRole.GetUser("user");
-            user.Roles.Add(defaultRole);
-
-            var stateMachine = ObjectSpace.FindObject<XpoStateMachine>(CriteriaOperator.Parse("Name=?", "Test")) ??
-                               ObjectSpace.CreateObject<XpoStateMachine>();
-            stateMachine.Active = true;
-            stateMachine.Name = "Test";
-            stateMachine.TargetObjectType = typeof (TestTask);
-            stateMachine.StatePropertyName = new StringObject("Status");
-            stateMachine.StartState = GetXpoState(TaskStatus.NotStarted,stateMachine);
-            foreach (var taskStatus in Enum.GetValues(typeof(TaskStatus)).Cast<TaskStatus>()) {
-                GetXpoState(taskStatus, stateMachine);
-            }
-            CreateTransition(TaskStatus.NotStarted, TaskStatus.InProgress);
-            CreateTransition(TaskStatus.InProgress, TaskStatus.Completed);
-            ObjectSpace.FindObject<XpoState>(CriteriaOperator.Parse("Caption=?", TaskStatus.Completed.ToString())).TargetObjectCriteria = IsCurrentUserInRoleOperator.OperatorName + "('Admin')";
+            userRole.SetTypePermissions<PaymentTask>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
+            userRole.SetTypePermissions<Status>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
+            userRole.SetTypePermissions<BillStatus>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
+            CreateStatuses();
+            ObjectSpace.CommitChanges();
+            CreateStateMachines();
             ObjectSpace.CommitChanges();
         }
 
-        void CreateTransition(TaskStatus startStatus, TaskStatus endStatus) {
-            var caption = startStatus.ToString() + endStatus;
-            var transition = ObjectSpace.FindObject<XpoTransition>(CriteriaOperator.Parse("Caption=?", caption)) ?? ObjectSpace.CreateObject<XpoTransition>();
-            transition.Caption = caption;
-            transition.SourceState = ObjectSpace.FindObject<XpoState>(CriteriaOperator.Parse("Caption=?", startStatus.ToString()));
-            transition.TargetState =ObjectSpace.FindObject<XpoState>(CriteriaOperator.Parse("Caption=?", endStatus.ToString()));
+        private void CreateStatuses(){
+            if (ObjectSpace.FindObject<Status>(null)==null){
+                var captions = new[]{"Paid","Canceled","Pending","New"};
+                foreach (var caption in captions){
+                    var status = ObjectSpace.CreateObject<Status>();
+                    status.Caption = caption;
+                }
+            }
         }
 
-        XpoState GetXpoState(TaskStatus taskStatus, XpoStateMachine stateMachine) {
-            var xpoState = ObjectSpace.FindObject<XpoState>(CriteriaOperator.Parse("Caption=?", taskStatus.ToString()))??ObjectSpace.CreateObject<XpoState>();
-            xpoState.Caption = taskStatus.ToString();
-            xpoState.StateMachine=stateMachine;
-            xpoState.MarkerValue = taskStatus.ToString();
-            return xpoState;
+        private void CreateStateMachines(){
+            if (ObjectSpace.FindObject<XpoStateMachine>(machine => machine.Name=="Payment")==null){
+                var stateMachine = ObjectSpace.CreateObject<XpoStateMachine>();
+                stateMachine.Name = "Payment";
+                stateMachine.Active = true;
+                stateMachine.TargetObjectType = typeof (PaymentTask);
+                stateMachine.StatePropertyName = new StringObject("PaymentStatus");
+                stateMachine.States.AddRange(CreatePaymentStatusStates());
+                stateMachine.StartState = stateMachine.States.First(state => state.Caption=="New");
+                
+                stateMachine = ObjectSpace.CreateObject<XpoStateMachine>();
+                stateMachine.Name = "Bill";
+                stateMachine.Active = true;
+                stateMachine.TargetObjectType = typeof (PaymentTask);
+                stateMachine.StatePropertyName = new StringObject("BillStatus");
+                stateMachine.States.AddRange(CreateBillStatusStates());
+                stateMachine.StartState = stateMachine.States.First(state => state.Caption=="Active");
+            }
         }
+
+        private IEnumerable<XpoState> CreateBillStatusStates(){
+            var states = new List<XpoState>();
+
+            var activeState = CreateState("Active", BillStatus.Active);
+            states.Add(activeState);
+            var inDisputeState = CreateState("InDispute", BillStatus.InDispute);
+            states.Add(inDisputeState);
+            var overdueState = CreateState("Overdue", BillStatus.Overdue);
+            states.Add(overdueState);
+
+            activeState.AddTransition(inDisputeState);
+            inDisputeState.AddTransition(overdueState);
+
+            return states;
+        }
+
+        private List<XpoState> CreatePaymentStatusStates(){
+            var states = new List<XpoState>();
+            var canceled = CreateState("Canceled", ObjectSpace.FindObject<Status>(status => status.Caption == "Canceled"));
+            states.Add(canceled);
+            var pending = CreateState("Pending", ObjectSpace.FindObject<Status>(status => status.Caption == "Pending"));
+            states.Add(pending);
+            var paid = CreateState("Paid", ObjectSpace.FindObject<Status>(status => status.Caption == "Paid"));
+            states.Add(paid);
+            var newState = CreateState("New", ObjectSpace.FindObject<Status>(status => status.Caption == "New"));
+            states.Add(newState);
+
+            newState.AddTransition(canceled);
+            newState.AddTransition(paid);
+            newState.AddTransition(pending);
+
+            pending.AddTransition(paid);
+            pending.AddTransition(canceled);
+
+            return states;
+        }
+
+        private XpoState CreateState(string caption, object marker){
+            var state = ObjectSpace.CreateObject<XpoState>();
+            state.Caption = caption;
+            state.Marker=new MarkerObject(marker);
+            return state;
+        }
+
     }
 }
