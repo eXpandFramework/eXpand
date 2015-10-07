473ad6faeb05699ce04421262260ecf1ec64d434
apobekiaris <apostolis.bekiaris@gmail.com>
2015-10-07 17:27:29 +0300

    [EasyTests for StateMachine AdminRoles]

---
+++ Xpand/Xpand.ExpressApp.Modules/StateMachine/Controllers/AdminRolesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DevExpress.ExpressApp;
+
+namespace Xpand.ExpressApp.StateMachine.Controllers {
+    public class AdminRolesController:ViewController {
+        protected override void OnActivated(){
+            base.OnActivated();
+            var enableStatePropertyController = Frame.GetController<StatePropertyController>();
+            enableStatePropertyController.CustomStatePropertyIsEnabled+=CustomStatePropertyIsEnabled;
+            enableStatePropertyController.CustomFilterEditorItems+=CustomFilterEditorItems;
+        }
+
+        private void CustomFilterEditorItems(object sender, StatePropertyFilterEditorItemsEventArgs e){
+            e.Handled = e.StateMachine.CanExecuteAllTransitions();
+        }
+
+        protected override void OnDeactivated(){
+            base.OnDeactivated();
+            var enableStatePropertyController = Frame.GetController<StatePropertyController>();
+            enableStatePropertyController.CustomStatePropertyIsEnabled -= CustomStatePropertyIsEnabled;
+        }
+
+        private void CustomStatePropertyIsEnabled(object sender, StatePropertyEventArgs e){
+            var canExecuteAllTransitions = e.StateMachine.CanExecuteAllTransitions();
+            if (canExecuteAllTransitions){
+                e.Handled = true;
+                e.Enable = e.Handled;
+            }
+        }
+    }
+}
--- Xpand/Xpand.ExpressApp.Modules/StateMachine/Controllers/StateMachineAdminRolesController.cs
+++ Xpand/Xpand.ExpressApp.Modules/StateMachine/Controllers/StateMachineAdminRolesController.cs
@@ -1,67 +1,60 @@
-using System;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Actions;
-using DevExpress.ExpressApp.StateMachine;
-using Xpand.Persistent.Base.General;
-using System.Linq;
-
-namespace Xpand.ExpressApp.StateMachine.Controllers {
-    public class StateMachineAdminRolesController : StateMachineControllerBase<ObjectView> {
-        bool _changeStateActionOnItemsChanged;
-        StateMachineController _stateMachineController;
-
-        protected override void OnFrameAssigned(){
-            base.OnFrameAssigned();
-            if (Application.CanBuildSecurityObjects()) {
-                _stateMachineController = Frame.GetController<StateMachineController>();
-                _stateMachineController.TransitionExecuting += StateMachineControllerOnTransitionExecuting;
-                _stateMachineController.ChangeStateAction.ItemsChanged += ChangeStateActionOnItemsChanged;
-                Frame.Disposing += FrameOnDisposing;
-            }
-        }
-
-        private void FrameOnDisposing(object sender, EventArgs eventArgs){
-            Frame.Disposing-=FrameOnDisposing;
-            _stateMachineController.TransitionExecuting -= StateMachineControllerOnTransitionExecuting;
-            _stateMachineController.ChangeStateAction.ItemsChanged -= ChangeStateActionOnItemsChanged;
-        }
-
-        void ChangeStateActionOnItemsChanged(object sender, ItemsChangedEventArgs itemsChangedEventArgs) {
-            if (!_changeStateActionOnItemsChanged&&View!=null) {
-                var keyValuePairs =itemsChangedEventArgs.ChangedItemsInfo.Any(pair =>
-                        pair.Value == ChoiceActionItemChangesType.ItemsAdd ||
-                        pair.Value == ChoiceActionItemChangesType.ItemsRemove);
-                if (keyValuePairs) {
-                    _changeStateActionOnItemsChanged = true;
-                    var stateMachineAction = _stateMachineController.ChangeStateAction;
-                    Object viewCurrentObject = (View.SelectedObjects.Count == 1) ? View.SelectedObjects[0] : null;
-                    if (viewCurrentObject != null) {
-                        var stateMachines = GetStateMachines().Where(machine => machine.CanExecuteTransition());
-                        foreach (var stateMachine in stateMachines) {
-                            var item = stateMachineAction.Items.Find(stateMachine);
-                            if (item != null){
-                                item.Items.Clear();
-                                foreach (var transition in stateMachine.States.SelectMany(state => state.Transitions)) {
-                                    item.Items.Add(new ChoiceActionItem(transition.Caption, transition));
-                                }
-                            }
-                        }
-                    }
-                    _changeStateActionOnItemsChanged = false;
-                }
-            }
-        }
-
-        void StateMachineControllerOnTransitionExecuting(object sender, ExecuteTransitionEventArgs executeTransitionEventArgs) {
-            if (executeTransitionEventArgs.Transition.TargetState.StateMachine.CanExecuteTransition()){
-//                executeTransitionEventArgs.Cancel = true;
-//                new StateMachineLogic(ObjectSpace).ProcessTransition(View.CurrentObject,
-//                    executeTransitionEventArgs.Transition.TargetState.StateMachine.StatePropertyName,
-//                    ObjectSpace.GetObject(executeTransitionEventArgs.Transition.TargetState));
-            }
-            
-        }
-
-
-    }
-}
+//using System;
+//using System.ComponentModel;
+//using System.Linq;
+//using DevExpress.ExpressApp;
+//using DevExpress.ExpressApp.Actions;
+//using DevExpress.ExpressApp.StateMachine;
+//using Fasterflect;
+//using Xpand.Persistent.Base.General;
+//
+//namespace Xpand.ExpressApp.StateMachine.Controllers {
+//    public class StateMachineAdminRolesController : StateMachineControllerBase<ObjectView> {
+//        StateMachineController _stateMachineController;
+//
+//        protected override void OnFrameAssigned(){
+//            base.OnFrameAssigned();
+//            if (Application.CanBuildSecurityObjects()) {
+//                _stateMachineController = Frame.GetController<StateMachineController>();
+//                _stateMachineController.ActionStateUpdated+=StateMachineControllerOnActionStateUpdated;
+//                _stateMachineController.ChangeStateAction.Executing+=ChangeStateActionOnExecuting;
+//                Frame.Disposing += FrameOnDisposing;
+//            }
+//        }
+//
+//        private void FrameOnDisposing(object sender, EventArgs eventArgs){
+//            Frame.Disposing-=FrameOnDisposing;
+//            _stateMachineController.ActionStateUpdated-=StateMachineControllerOnActionStateUpdated;
+//            _stateMachineController.ChangeStateAction.Executing -= ChangeStateActionOnExecuting;
+//        }
+//
+//        private void ChangeStateActionOnExecuting(object sender, CancelEventArgs cancelEventArgs){
+//            var state = _stateMachineController.ChangeStateAction.SelectedItem.Data as IState;
+//            if (state != null){
+//                cancelEventArgs.Cancel =state.StateMachine.CanExecuteAllTransitions();
+//                if (cancelEventArgs.Cancel){
+//                    
+//                    var transition = state.StateMachine.States.SelectMany(state1 => state.Transitions).FirstOrDefault(tr => tr.TargetState==state);
+//                    if (transition != null)
+//                        _stateMachineController.CallMethod("ExecuteTransition", View.CurrentObject, transition);
+//                }
+//            }
+//        }
+//
+//        private void StateMachineControllerOnActionStateUpdated(object sender, EventArgs eventArgs){
+//            Object viewCurrentObject = (View != null && View.SelectedObjects.Count == 1) ? View.SelectedObjects[0] : null;
+//            if (viewCurrentObject != null) {
+//                var changeStateAction = _stateMachineController.ChangeStateAction;
+//                var stateMachines = GetStateMachines().Where(machine => machine.CanExecuteAllTransitions()).ToArray();
+//                foreach (IStateMachine stateMachine in stateMachines) {
+//                    var choiceActionItem = changeStateAction.Items.FirstOrDefault(item => item.Caption==stateMachine.Name);
+//                    if (choiceActionItem != null){
+//                        choiceActionItem.Items.Clear();
+//                        var choiceActionItems = stateMachine.States.Select(state => new ChoiceActionItem(state.Caption,state)).ToArray();
+//                        choiceActionItem.Items.AddRange(choiceActionItems);
+//                    }
+//                }
+//            }
+//
+//        }
+//    }
+//}
