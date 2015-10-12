22adf38e5cbca86a2bcf8923ac13a70ef9dd958b
Jim Horvath <jhorv@users.noreply.github.com>
2015-10-12 10:42:42 -0400

    fixes which assemblies in Xpand.DLL are installed into the GAC

--- clear.bat
+++ clear.bat
@@ -4,6 +4,6 @@
 call defines.bat
 %msbuild% /nologo /t:Rebuild /verbosity:quiet /p:Configuration=%configuration% ".\Support\ToolBoxCreator\Xpand.ToolboxCreator.csproj"
 call ".\Xpand.DLL\Xpand.ToolBoxCreator.exe" u
-call GACUnistall.bat
+call GACUninstall.bat
 del /q Xpand.dll
 pause
