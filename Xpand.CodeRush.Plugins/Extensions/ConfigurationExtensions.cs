d79a40fc0345b41bd226b2b71df1d2b9227cafcb
Jim Horvath <jhorv@users.noreply.github.com>
2015-10-27 09:09:52 -0400

    fix file/type name spelling, move ToString() call out of loop

--- Xpand.CodeRush.Plugins/Extensions/ConfigurationExtensions.cs
+++ Xpand.CodeRush.Plugins/Extensions/ConfigurationExtensions.cs
@@ -3,9 +3,10 @@ using EnvDTE;
 using Xpand.CodeRush.Plugins.Enums;
 
 namespace Xpand.CodeRush.Plugins.Extensions {
-    public static class ConfiguartionExtensions {
+    public static class ConfigurationExtensions {
         public static Property FindProperty(this Configuration configuration, ConfigurationProperty configurationProperty) {
-            return configuration.Properties.Cast<Property>().Single(property => property.Name == configurationProperty.ToString());
+            string configPropertyStr = configurationProperty.ToString();
+            return configuration.Properties.Cast<Property>().Single(property => property.Name == configPropertyStr);
         }
     }
 }
