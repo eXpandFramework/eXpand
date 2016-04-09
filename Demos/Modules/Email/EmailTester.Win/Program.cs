71f320be11e67226a6b68bc064341bda375ae5b0
apobekiaris <apostolis.bekiaris@gmail.com>
2016-04-09 17:49:27 +0300

    Minor Updates

--- Demos/Modules/Email/EmailTester.Win/Program.cs
+++ Demos/Modules/Email/EmailTester.Win/Program.cs
@@ -1,41 +1,9 @@
 using System;
 using System.Configuration;
-using System.IO;
 using System.Windows.Forms;
-using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
-using RazorEngine.Configuration;
-using RazorEngine.Templating;
-using Xpand.ExpressApp.Security.Core;
-using Xpand.ExpressApp.Security.Registration;
 
 namespace EmailTester.Win {
-    public class MyClass{
-        // Fields...
-        private string _UserName;
-
-        public string UserName {
-            get { return "aaaaaaaaaaaaaaaaaa"; }
-            set {
-                _UserName = value;
-            }
-        }
-         
-    }
-
-//    public class RegisterUserParameters {
-//        // Fields...
-//        private object _User;
-//
-//        public object User {
-//            get { return _User; }
-//            set {
-//                _User = value;
-//            }
-//        }
-//         
-//    }
     static class Program {
         /// <summary>
         /// The main entry point for the application.
@@ -63,11 +31,6 @@ namespace EmailTester.Win {
             try {
                 winApplication.UseOldTemplates=false;
                 winApplication.Setup();
-//                var razorEngineService = RazorEngineService.Create(new TemplateServiceConfiguration() { Debug = true });
-//                var objectSpace = winApplication.CreateObjectSpace();
-//                var xpandUser = objectSpace.FindObject<XpandUser>(CriteriaOperator.Parse(""));
-//                var registerUserParameters = new RegisterUserParameters() { User = xpandUser };
-//                            var runCompile = razorEngineService.RunCompile("eee @Model.User.UserName ", Guid.NewGuid().ToString(), null, registerUserParameters);
                 winApplication.Start();
             } catch (Exception e) {
                 winApplication.HandleException(e);
