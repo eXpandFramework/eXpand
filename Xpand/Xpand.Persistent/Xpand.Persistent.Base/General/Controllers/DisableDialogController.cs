45f9a29f12ca477b125adddd59d197711ffa7f36
apobekiaris <apostolis.bekiaris@gmail.com>
2016-04-18 02:00:01 +0300

    useful controller when creating views

---
+++ Xpand/Xpand.Persistent/Xpand.Persistent.Base/General/Controllers/DisableDialogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.ExpressApp;
+
+namespace Xpand.Persistent.Base.General.Controllers{
+    public class DisableDialogController : Controller {
+        private readonly Type[] _types;
+
+        public DisableDialogController(params Type[] types) {
+            _types = types;
+        }
+
+        protected override void OnFrameAssigned() {
+            base.OnFrameAssigned();
+            foreach (var type in _types) {
+                Frame.GetController(type).Active[GetType().Name] = false;
+            }
+        }
+    }
+}
