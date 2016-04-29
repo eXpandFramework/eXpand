b701271799bd8b07f688c489c3c232df75b73228
apobekiaris <apostolis.bekiaris@gmail.com>
2016-04-29 19:41:52 +0300

    [Map Win DashboardDesignerSettings to module]
    http://www.expandframework.com/forum/8-discussion/4767-dashboard-module-and-stored-procedures/9786.html

---
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/Controllers/DashboardDesignerModelAdapterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using DevExpress.DashboardWin;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using Xpand.ExpressApp.Dashboard.Filter;
+using Xpand.Persistent.Base.ModelAdapter;
+
+namespace Xpand.ExpressApp.XtraDashboard.Win.Controllers {
+    public interface IModelDashboardModuleDesignerSettings:IModelNode{
+        IModelDashboardDesignerSettings DesignerSettings { get; }
+    }
+    public interface IModelDashboardDesignerSettings : IModelNode {
+    }
+
+    public class DashboardDesignerModelAdapterController:ModelAdapterController,IModelExtender {
+        protected override void OnActivated(){
+            base.OnActivated();
+            Frame.GetController<DashboardDesignerController>().DashboardDesignerOpening+=OnDashboardDesignerOpening;
+        }
+
+        protected override void OnDeactivated(){
+            base.OnDeactivated();
+            Frame.GetController<DashboardDesignerController>().DashboardDesignerOpening -= OnDashboardDesignerOpening;
+        }
+
+        private void OnDashboardDesignerOpening(object sender, Dashboard.Controllers.DashboardDesignerOpeningEventArgs e){
+            var args = ((DashboardDesignerOpeningEventArgs) e);
+            var settings = ((IModelDashboardModuleDesignerSettings) ((IModelApplicationDashboardModule) Application.Model).DashboardModule).DesignerSettings;
+            new ObjectModelSynchronizer(args.Designer.DataSourceWizardSettings,settings).ApplyModel();
+        }
+
+        public void ExtendModelInterfaces(ModelInterfaceExtenders extenders){
+            extenders.Add<IModelDashboardModule, IModelDashboardModuleDesignerSettings>();
+            var builder = new InterfaceBuilder(extenders);
+            var assembly = builder.Build(BuilderDatas(), GetPath(typeof(DashboardDataSourceWizardSettings).Name));
+            builder.ExtendInteface<IModelDashboardDesignerSettings, DashboardDataSourceWizardSettings>(assembly);
+        }
+
+        private static IEnumerable<InterfaceBuilderData> BuilderDatas() {
+            yield return new InterfaceBuilderData(typeof(DashboardDataSourceWizardSettings)) {
+                Act = info => (info.DXFilter())
+            };
+        }
+    }
+}
