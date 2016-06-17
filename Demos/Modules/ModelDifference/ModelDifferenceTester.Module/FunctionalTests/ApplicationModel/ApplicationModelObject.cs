be15d9727508efc083e38a1e86e3363efa8cc4d8
apobekiaris <apostolis.bekiaris@gmail.com>
2016-06-17 17:24:51 +0300

    [EasyTest for the  ModelDifference ApplicationModel]

---
+++ Demos/Modules/ModelDifference/ModelDifferenceTester.Module/FunctionalTests/ApplicationModel/ApplicationModelObject.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ModelDifferenceTester.Module.FunctionalTests.ApplicationModel {
+    [DefaultClassOptions]
+    public class ApplicationModelObject:BaseObject {
+        public ApplicationModelObject(Session session) : base(session){
+        }
+
+        public string Name { get; set; }
+    }
+}
