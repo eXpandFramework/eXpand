83f9c446ffdb088b4c06cfed4dbd6b867bd9b3fb
apobekiaris <apostolis.bekiaris@gmail.com>
2016-06-17 17:25:29 +0300

    [EasyTest for the ModelDifference RuntimeMember integration]

---
+++ Demos/Modules/ModelDifference/ModelDifferenceTester.Module/FunctionalTests/RuntimeMembers/RuntimeMembersObject.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ModelDifferenceTester.Module.FunctionalTests.RuntimeMembers {
+    [DefaultClassOptions]
+    public class RuntimeMembersObject:BaseObject {
+        public RuntimeMembersObject(Session session) : base(session){
+        }
+
+        public string Name { get; set; }
+    }
+}
