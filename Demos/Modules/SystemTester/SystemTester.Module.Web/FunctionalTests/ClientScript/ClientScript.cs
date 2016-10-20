e340163ad712253f46547c3a24ed0441fe5a1c4d
apobekiaris <apostolis.bekiaris@gmail.com>
2016-10-20 16:25:20 +0300

    [EasyTests for Actions ClientScript]

---
+++ Demos/Modules/SystemTester/SystemTester.Module.Web/FunctionalTests/ClientScript/ClientScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+
+namespace SystemTester.Module.Web.FunctionalTests.ClientScript {
+    public class ClientScriptController:ObjectViewController<ObjectView,ClientScriptObject>{
+        public ClientScriptController(){
+            var simpleAction = new SimpleAction(this,"ClientScript",PredefinedCategory.View);
+            simpleAction.Execute+=SimpleActionOnExecute;
+        }
+
+        private void SimpleActionOnExecute(object sender, SimpleActionExecuteEventArgs simpleActionExecuteEventArgs){
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demos/Modules/SystemTester/SystemTester.Module.Web/FunctionalTests/ClientScript/ClientScriptObject.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace SystemTester.Module.Web.FunctionalTests.ClientScript {
+    [DefaultClassOptions]
+    public class ClientScriptObject:BaseObject {
+        public ClientScriptObject(Session session) : base(session){
+        }
+    }
+}
