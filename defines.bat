1017f2344a70ec589007117342c7e4ab02c646e6
apobekiaris <apostolis.bekiaris@gmail.com>
2017-04-26 08:31:08 +0300

    VS 2017 support

--- defines.bat
+++ defines.bat
@@ -9,15 +9,18 @@ set Gac4path="%WinDir%\Microsoft.NET\assembly\GAC_MSIL\"
 
 
 if '%vsver%'=='vs2015' goto vs2015
+if '%vsver%'=='vs2017' goto vs2017
 
 :vs2015
 set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
 
+IF NOT EXIST %msbuild% goto VS2015Tools
+goto other
+
 :vs2017
 set msbuild="%ProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
-IF NOT EXIST %msbuild% goto VS2015Tools
-
+:other
 set sn="%ProgramFiles%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe"
 set gacutil="%ProgramFiles%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\gacutil.exe"
 goto end
