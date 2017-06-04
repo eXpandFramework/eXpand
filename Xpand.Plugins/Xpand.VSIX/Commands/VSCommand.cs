ccd11cd0b161ef200b05402df4ee8490dd41175a
apobekiaris <apostolis.bekiaris@gmail.com>
2017-06-04 12:55:41 +0300

    Allow VSComands init even if keyb binding fails
    http://www.expandframework.com/forum/5-installation-configuration-a-updates/5027-toolboxcreator-and-vsix-paket-error-in-17-1-3-4.html#10872

--- Xpand.Plugins/Xpand.VSIX/Commands/VSCommand.cs
+++ Xpand.Plugins/Xpand.VSIX/Commands/VSCommand.cs
@@ -5,9 +5,12 @@ using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using Xpand.VSIX.Extensions;
+using MessageBox = System.Windows.MessageBox;
+
 
 namespace Xpand.VSIX.Commands{
     public abstract class VSCommand : OleMenuCommand, IDTE2Provider{
+        private static bool _errorMessage;
         protected VSCommand( EventHandler invokeHandler,
             CommandID commandID) : base(invokeHandler, commandID){
             var commandService = (OleMenuCommandService) VSPackage.VSPackage.Instance.GetService(typeof(IMenuCommandService));
@@ -15,8 +18,18 @@ namespace Xpand.VSIX.Commands{
         }
 
         protected void BindCommand(params object[] bindings) {
-            if (!((object[])Command.Bindings).Any())
-                Command.Bindings = bindings;
+            try{
+                if (!((object[])Command.Bindings).Any())
+                    Command.Bindings = bindings;
+            }
+            catch (Exception e){
+                DTE2.LogError($"bindings:{bindings+Environment.NewLine+e}");
+                if (!_errorMessage){
+                    _errorMessage = true;
+                    MessageBox.Show($@"Errors during command binding, please check your %APPDATA%\Microsoft\VisualStudio\{DTE2.Version}\ActivityLog.xml");
+                }
+                throw;
+            }
         }
 
         protected Command Command{
