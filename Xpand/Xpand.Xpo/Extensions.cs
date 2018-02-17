170bcdb26ddc61e1207da714ed3ebe10c7713d98
apobekiaris <apostolis.bekiaris@gmail.com>
2018-02-17 14:34:32 +0200

    Helper method to detect Locked persistent objects

---
+++ Xpand/Xpand.Xpo/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpo;
+
+namespace Xpand.Xpo{
+    public static class Extensions{
+        public static bool IsOutdated(this XPBaseObject obj) {
+            using (UnitOfWork uow = new UnitOfWork(obj.Session.DataLayer)) {
+                XPBaseObject obj1 = (XPBaseObject)uow.GetObjectByKey(obj.GetType(), obj.Session.GetKeyValue(obj));
+                return !Equals(obj1.ClassInfo.OptimisticLockField.GetValue(obj1), obj.ClassInfo.OptimisticLockField.GetValue(obj));
+            }
+        }
+    }
+}
