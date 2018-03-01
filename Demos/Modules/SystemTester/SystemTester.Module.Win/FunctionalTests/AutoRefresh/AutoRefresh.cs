727be1a51977901779090e8d53d49db7ba6194e3
apobekiaris <apostolis.bekiaris@gmail.com>
2018-03-01 11:19:42 +0200

    EasyTest for AUtoRefresh

---
+++ Demos/Modules/SystemTester/SystemTester.Module.Win/FunctionalTests/AutoRefresh/AutoRefresh.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace SystemTester.Module.Win.FunctionalTests.AutoRefresh{
+    public class AutoRefreshController:ObjectViewController<ListView,AutoRefresh>{
+        protected override void OnActivated(){
+            base.OnActivated();
+            View.CollectionSource.CollectionReloading+=CollectionSourceOnCollectionReloading;
+            
+        }
+
+        private void CollectionSourceOnCollectionReloading(object sender, EventArgs eventArgs){
+            var autoRefresh = ObjectSpace.CreateObject<AutoRefresh>();
+            ObjectSpace.CommitChanges();
+            View.CollectionSource.Add(autoRefresh);
+        }
+
+        protected override void OnDeactivated(){
+            base.OnDeactivated();
+            View.CollectionSource.CollectionReloading-=CollectionSourceOnCollectionReloading;
+        }
+    }
+    [DefaultClassOptions]
+    public class AutoRefresh:BaseObject{
+        public AutoRefresh(Session session) : base(session){
+        }
+
+        string _name;
+
+        public string Name{
+            get{ return _name; }
+            set{ SetPropertyValue(nameof(Name), ref _name, value); }
+        }
+    }
+
+}
