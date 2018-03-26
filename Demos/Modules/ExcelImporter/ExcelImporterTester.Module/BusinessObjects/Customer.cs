eb192faa2c93cd17c5c98220fd2e1fc816c85aba
apobekiaris <apostolis.bekiaris@gmail.com>
2018-03-26 18:12:54 +0300

    ExcelImporter suppport for Browsable, VisibileInListView, VisibileInLookupListView, VisibleInExcelMap to configure selected type propertyname lookup

--- Demos/Modules/ExcelImporter/ExcelImporterTester.Module/BusinessObjects/Customer.cs
+++ Demos/Modules/ExcelImporter/ExcelImporterTester.Module/BusinessObjects/Customer.cs
@@ -2,10 +2,11 @@
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.Xpo;
+using Xpand.ExpressApp.ExcelImporter.Services;
 
 namespace ExcelImporterTester.Module.BusinessObjects{
     [DefaultClassOptions]
-    [DefaultProperty(nameof(ImportName))]
+    [DefaultProperty(nameof(ImportName1))]
     public class Customer : BaseObject{
         // Fields...
         private GenderObject _gender;
@@ -18,10 +19,39 @@ namespace ExcelImporterTester.Module.BusinessObjects{
             get => _gender;
             set => SetPropertyValue("Gender", ref _gender, value);
         }
-
-        public string ImportName{
+        [DevExpress.Xpo.DisplayName("ImportName")]
+        public string ImportName1{
             get => _importName;
             set => SetPropertyValue("ImportName", ref _importName, value);
+        }
+
+        string _hidden1;
+
+        [VisibleInListView(false)]
+        public string Hidden1{
+            get => _hidden1;
+            set => SetPropertyValue(nameof(Hidden1), ref _hidden1, value);
+        }
+
+        string _hidden4;
+        [Browsable(false)]
+        public string Hidden4{
+            get => _hidden4;
+            set => SetPropertyValue(nameof(Hidden4), ref _hidden4, value);
+        }
+        string _hidden2;
+
+        [VisibleInLookupListView(false)]
+        public string Hidden2{
+            get => _hidden2;
+            set => SetPropertyValue(nameof(Hidden2), ref _hidden2, value);
+        }
+
+        string _hidden3;
+        [VisibleInExcelMap(false)]
+        public string Hidden3{
+            get => _hidden3;
+            set => SetPropertyValue(nameof(Hidden3), ref _hidden3, value);
         }
     }
 }
--- Demos/Modules/ExcelImporter/ExcelImporterTester.Module/Extensions.cs
+++ Demos/Modules/ExcelImporter/ExcelImporterTester.Module/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
 using DevExpress.ExpressApp.Xpo;
 using Xpand.ExpressApp.Security.Core;
 using Xpand.Persistent.BaseImpl.Security;
@@ -8,7 +9,7 @@ namespace ExcelImporterTester.Module{
         public static void ProjectSetup(this XafApplication application) {
             application.OptimizedControllersCreation = true;
             application.ConnectionString = InMemoryDataStoreProvider.ConnectionString;
-            application.NewSecurityStrategyComplexV2<XpandPermissionPolicyUser, XpandPermissionPolicyRole>();
+            application.NewSecurityStrategyComplexV2<XpandPermissionPolicyUser, XpandPermissionPolicyRole>(logonParametersType:typeof(AuthenticationStandardLogonParameters));
         }
     }
 }
