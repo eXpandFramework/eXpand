99d62abbb5b5d3b9ea450b5016b0b251ec49aae4
apobekiaris <apostolis.bekiaris@gmail.com>
2018-04-27 10:54:07 +0300

    New FileBrowsePropertyEditor added in System.Win
    http://www.expandframework.com/forum/13-suggestions/5214-filebrowsepropertyeditor.html

---
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Win/PropertyEditors/FileBrowserEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Repository;
+using EditorAliases = Xpand.Persistent.Base.General.EditorAliases;
+
+namespace Xpand.ExpressApp.Win.PropertyEditors{
+    [PropertyEditor(typeof(string), EditorAliases.FileBrowserEditor, false)]
+    public class FileBrowserEditor : DXPropertyEditor{
+        public FileBrowserEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model){
+        }
+
+        private void buttonEdit_ButtonClick(object sender, ButtonPressedEventArgs e){
+            using (var dialog = new OpenFileDialog()){
+                if (dialog.ShowDialog() != DialogResult.Cancel) ((ButtonEdit) sender).Text = dialog.FileName;
+            }
+        }
+
+        protected override object CreateControlCore(){
+            return new ButtonEdit();
+        }
+
+        protected override RepositoryItem CreateRepositoryItem(){
+            return new RepositoryItemButtonEdit();
+        }
+
+        protected override void SetupRepositoryItem(RepositoryItem item){
+            base.SetupRepositoryItem(item);
+            ((RepositoryItemButtonEdit) item).ButtonClick += buttonEdit_ButtonClick;
+        }
+
+        protected override void SetRepositoryItemReadOnly(RepositoryItem item, bool readOnly){
+            base.SetRepositoryItemReadOnly(item, readOnly);
+            ((RepositoryItemButtonEdit) item).Buttons[0].Enabled = !readOnly;
+        }
+    }
+}
