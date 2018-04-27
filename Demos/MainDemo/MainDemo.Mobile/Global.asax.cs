42d581d80916c264ff22232dc4848ef7dd9750e0
apobekiaris <apostolis.bekiaris@gmail.com>
2018-04-27 11:24:58 +0300

    project converter 18.1.2

--- Demos/MainDemo/MainDemo.Mobile/Global.asax.cs
+++ Demos/MainDemo/MainDemo.Mobile/Global.asax.cs
@@ -12,5 +12,8 @@ namespace MainDemo.Mobile {
         protected void Application_BeginRequest(object sender, EventArgs e) {
             CorsSupport.HandlePreflightRequest(HttpContext.Current);
         }
+        protected void Application_Start(Object sender, EventArgs e) {
+            DevExpress.XtraReports.Web.WebDocumentViewer.DefaultWebDocumentViewerContainer.Register<DevExpress.XtraReports.Web.WebDocumentViewer.IWebDocumentViewerReportResolver, DevExpress.ExpressApp.ReportsV2.Mobile.XafReportsResolver<MainDemoMobileApplication>>();
+        }
     }
 }
