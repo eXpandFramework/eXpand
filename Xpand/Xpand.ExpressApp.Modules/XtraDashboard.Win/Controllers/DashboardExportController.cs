2cbca999c8038b1c1cc305f3dc1f9de0f7e529eb
apobekiaris <apostolis.bekiaris@gmail.com>
2018-08-24 20:11:38 +0300

    Dashboard Export action is not functional #145

--- Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/Controllers/DashboardExportController.cs
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Win/Controllers/DashboardExportController.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using System.Windows.Forms;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardWin;
-using DevExpress.DashboardWin.Bars;
-using DevExpress.DashboardWin.Native;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Actions;
 using DevExpress.Persistent.Base;
+using Fasterflect;
 using Xpand.ExpressApp.Dashboard.BusinessObjects;
 using Xpand.ExpressApp.XtraDashboard.Win.PropertyEditors;
 using ListView = DevExpress.ExpressApp.ListView;
@@ -15,104 +14,48 @@ using ListView = DevExpress.ExpressApp.ListView;
 namespace Xpand.ExpressApp.XtraDashboard.Win.Controllers {
     public class DashboardExportController : ObjectViewController<ListView,IDashboardDefinition> {
         private readonly SingleChoiceAction _exportDashboardAction;
-        private DashboardViewEditor _dashboardViewEditor;
         private const string ExportToPdf = "PDF";
         private const string ExportToImage = "Image";
+        private const string ExportToExcel = "Excel";
         private const string PrintPreview = "PrintPreview";
         public DashboardExportController() {
             _exportDashboardAction = new SingleChoiceAction(this, "ExportDashboardTo", PredefinedCategory.Export) { Caption = "Export Dashboard To" };
             _exportDashboardAction.Items.Add(new ChoiceActionItem(PrintPreview, PrintPreview) { ImageName = "Action_Printing_Preview" });
             _exportDashboardAction.Items.Add(new ChoiceActionItem(ExportToPdf, ExportToPdf) { ImageName = "Action_Export_ToPDF" });
             _exportDashboardAction.Items.Add(new ChoiceActionItem(ExportToImage, ExportToImage) { ImageName = "Action_Export_ToImage" });
+            _exportDashboardAction.Items.Add(new ChoiceActionItem(ExportToExcel, ExportToExcel) { ImageName = "Action_Export_ToXlsx" });
+            
             _exportDashboardAction.SelectionDependencyType = SelectionDependencyType.RequireSingleObject;
             _exportDashboardAction.ItemType = SingleChoiceActionItemType.ItemIsOperation;
             _exportDashboardAction.EmptyItemsBehavior = EmptyItemsBehavior.Deactivate;
             _exportDashboardAction.Execute += ExportDashboardActionOnExecute;
-            SaveFileDialog = new SaveFileDialog { OverwritePrompt = true };
         }
 
         public SingleChoiceAction ExportDashboardAction => _exportDashboardAction;
 
-        public SaveFileDialog SaveFileDialog { get; }
-
-        protected override void OnViewControlsCreated() {
-            base.OnViewControlsCreated();
-            if (View.Id== "DashboardDefinitionViewer_DetailView") {
-                var xmlViewItem = View.FindItem("Xml");
-                if (xmlViewItem is DashboardViewEditor item) {
-                    _dashboardViewEditor = item;
-                    _dashboardViewEditor.DashboardViewer.PopupMenuShowing += DashboardViewer_PopupMenuShowing;
-                }
-            }
-        }
-
-        protected override void OnDeactivated() {
-            if (_dashboardViewEditor?.DashboardViewer != null) {
-                _dashboardViewEditor.DashboardViewer.PopupMenuShowing -= DashboardViewer_PopupMenuShowing;
-            }
-            base.OnDeactivated();
-        }
-
-        private void DashboardViewer_PopupMenuShowing(object sender, DashboardPopupMenuShowingEventArgs e){
-            if (!string.IsNullOrEmpty(e.DashboardItemName)){
-                var viewer = (DashboardViewer) sender;
-                DashboardItemViewer itemViewer = null;
-
-                var fi =
-                    typeof(DashboardItemMouseHitTestEventArgs).GetField("dashboardItemViewer",
-                        BindingFlags.NonPublic | BindingFlags.Instance);
-                var obj = fi?.GetValue(e);
-                if (obj is DashboardItemViewer dashboardItemViewer){
-                    itemViewer = dashboardItemViewer;
-                }
-
-                if (itemViewer == null){
+        private void ExportDashboardActionOnExecute(object sender, SingleChoiceActionExecuteEventArgs e) {
+            var dashboardViewer = CreateDashboardViewer(null);
+            dashboardViewer.DashboardChanged += (o, args) => {
+                if ((string)e.SelectedChoiceActionItem.Data == PrintPreview) {
+                    ShowRibbonPrintPreview(dashboardViewer);
                     return;
                 }
 
-
-                var printPreviewItemBarItem = new PrintPreviewItemBarItem();
-                printPreviewItemBarItem.ItemClick += (o, args) =>((IDashboardViewerCommandBarItem) printPreviewItemBarItem).ExecuteCommand(viewer, itemViewer);
-                e.Menu.AddItem(printPreviewItemBarItem);
-
-                var exportItemToPdfBarItem = new ExportItemToPdfBarItem();
-                exportItemToPdfBarItem.ItemClick += (o, args) =>((IDashboardViewerCommandBarItem) exportItemToPdfBarItem).ExecuteCommand(viewer, itemViewer);
-                e.Menu.AddItem(exportItemToPdfBarItem);
-
-                var exportItemToImageBarItem = new ExportItemToImageBarItem();
-                exportItemToImageBarItem.ItemClick += (o, args) => ((IDashboardViewerCommandBarItem) exportItemToImageBarItem).ExecuteCommand(viewer, itemViewer);
-                e.Menu.AddItem(exportItemToImageBarItem);
-
-                var exportItemToExcelBarItem = new ExportItemToExcelBarItem();
-                exportItemToExcelBarItem.ItemClick += (o, args) => ((IDashboardViewerCommandBarItem) exportItemToExcelBarItem).ExecuteCommand(viewer, itemViewer);
-                e.Menu.AddItem(exportItemToExcelBarItem);
-            }
+                ShowExportForm(e, dashboardViewer);
+            };
         }
 
-        private void ExportDashboardActionOnExecute(object sender, SingleChoiceActionExecuteEventArgs e) {
-            var dashboardViewer = CreateDashboardViewer();
-            if ((string)e.SelectedChoiceActionItem.Data == PrintPreview) {
-                dashboardViewer.PrintPreviewType = DashboardPrintPreviewType.RibbonPreview;
-                dashboardViewer.ShowRibbonPrintPreview();
-                return;
-            }
-            if ((string)e.SelectedChoiceActionItem.Data == ExportToPdf)
-                ConfigureSaveFileDialog("pdf", "Pdf Files (*.pdf)|*.pdf");
-            else if ((string)e.SelectedChoiceActionItem.Data == ExportToImage) {
-                ConfigureSaveFileDialog("png", "Png Files (*.png)|*.png");
-            }
-            var dialogResult = SaveFileDialog.ShowDialog();
-            if (dialogResult == DialogResult.OK && Directory.Exists(Path.GetDirectoryName(SaveFileDialog.FileName) + "")) {
-                if ((string)e.SelectedChoiceActionItem.Data == ExportToPdf) {
-                    dashboardViewer.ExportToPdf(SaveFileDialog.FileName);
-                }
-                else if ((string)e.SelectedChoiceActionItem.Data == ExportToImage) {
-                    dashboardViewer.ExportToImage(SaveFileDialog.FileName);
-                }
-            }
+        protected virtual void ShowExportForm(SingleChoiceActionExecuteEventArgs e, DashboardViewer dashboardViewer){
+            dashboardViewer.CallMethod("ShowExportForm",
+                (DashboardExportFormat) Enum.Parse(typeof(DashboardExportFormat), (string) e.SelectedChoiceActionItem.Data));
         }
 
-        protected virtual DashboardViewer CreateDashboardViewer() {
+        protected virtual void ShowRibbonPrintPreview(DashboardViewer dashboardViewer){
+            dashboardViewer.PrintPreviewType = DashboardPrintPreviewType.RibbonPreview;
+            dashboardViewer.ShowRibbonPrintPreview();
+        }
+
+        protected virtual DashboardViewer CreateDashboardViewer(Action<MemoryStream> exported) {
             var dashboardViewer = new DashboardViewer {
                 Width = ((Form)Application.MainWindow.Template).Width,
                 Height = ((Form)Application.MainWindow.Template).Height
@@ -121,11 +64,5 @@ namespace Xpand.ExpressApp.XtraDashboard.Win.Controllers {
             return dashboardViewer;
         }
 
-        protected virtual void ConfigureSaveFileDialog(string defaultExt, string filter) {
-            SaveFileDialog.DefaultExt = defaultExt;
-            SaveFileDialog.AddExtension = true;
-            SaveFileDialog.Filter = filter;
-            SaveFileDialog.FileName = ((IDashboardDefinition)View.CurrentObject).Name;
-        }
     }
 }
