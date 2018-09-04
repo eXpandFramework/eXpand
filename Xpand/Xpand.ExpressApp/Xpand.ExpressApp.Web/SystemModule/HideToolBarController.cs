cdcc1f0f617da1d6e7f1e5ef16abe6c993003248
apobekiaris <apostolis.bekiaris@gmail.com>
2018-09-04 21:11:11 +0300

    Hidetoolbar option hides every nested Listview #150

--- Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/HideToolBarController.cs
+++ Xpand/Xpand.ExpressApp/Xpand.ExpressApp.Web/SystemModule/HideToolBarController.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.CodeParser;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Templates;
 using DevExpress.ExpressApp.Web;
 using DevExpress.ExpressApp.Web.Templates.ActionContainers;
+using Fasterflect;
 using Xpand.ExpressApp.SystemModule;
 using Xpand.Utils.Web;
 
@@ -12,6 +15,8 @@ namespace Xpand.ExpressApp.Web.SystemModule {
     public class HideToolBarController : ExpressApp.SystemModule.HideToolBarController {
         private IEnumerable<ActionContainerHolder> _containerHolders;
         private string _visibility;
+        private ISupportActionsToolbarVisibility _template;
+
         protected override void OnActivated(){
             base.OnActivated();
             if (WebApplicationStyleManager.IsNewStyle){
@@ -20,29 +25,50 @@ namespace Xpand.ExpressApp.Web.SystemModule {
         }
 
 
+
         private void ApplicationOnCustomizeTemplate(object sender, CustomizeTemplateEventArgs e){
             if (e.Context == TemplateContext.NestedFrame) {
-                var modelViewHideViewToolBar = ((IModelClassHideViewToolBar)View.Model);
-                if (modelViewHideViewToolBar.HideToolBar.HasValue && modelViewHideViewToolBar.HideToolBar.Value){
-                    var template = e.Template as ISupportActionsToolbarVisibility;
-                    template?.SetVisible(false);
+                _template = e.Template as ISupportActionsToolbarVisibility;
+                if (_template != null) {
+                    var modelViewHideViewToolBar = ((IModelClassHideViewToolBar)View.Model);
+                    var hideToolBar = modelViewHideViewToolBar.HideToolBar.HasValue &&modelViewHideViewToolBar.HideToolBar.Value;
+                    ((Control) _template).Init += (o, args) => {
+                        var actionContainerHolders =new[]{_template.GetFieldValue("ToolBar"),_template.GetFieldValue("ObjectsCreation")}
+                            .Cast<ActionContainerHolder>()
+                            .Where(containerHolder => containerHolder!=null);
+                        foreach (var container in actionContainerHolders) {
+                            container.Style.Remove(HtmlTextWriterStyle.Visibility);
+                            container.Style.Remove(HtmlTextWriterStyle.Display);
+                            if (hideToolBar)
+                                ApplyStyle(container);
+                        }
+                    };
                 }
             }
         }
 
         protected override void OnViewControlsCreated() {
             base.OnViewControlsCreated();
-            if (Frame.Template != null){
-                _containerHolders = ((Control)Frame.Template).FindNestedControls<ActionContainerHolder>().Where(holder => holder.ContainerStyle==ActionContainerStyle.ToolBar);
-                foreach (var containerHolder in _containerHolders){
-                    var modelViewHideViewToolBar = ((IModelClassHideViewToolBar)View.Model);
-                    if (_containerHolders != null && modelViewHideViewToolBar.HideToolBar.HasValue && modelViewHideViewToolBar.HideToolBar.Value) {
-                        _visibility = containerHolder.Style[HtmlTextWriterStyle.Display];
-                        containerHolder.Style[HtmlTextWriterStyle.Visibility] = "Hidden";
-                        containerHolder.Style[HtmlTextWriterStyle.Display] = "None";
-                    }    
+            if (Frame.Template != null) {
+                StyleToolBar();
+            }
+        }
+
+        private void StyleToolBar(){
+            _containerHolders = ((Control) Frame.Template).FindNestedControls<ActionContainerHolder>()
+                .Where(holder => holder.ContainerStyle == ActionContainerStyle.ToolBar);
+            foreach (var containerHolder in _containerHolders){
+                var modelViewHideViewToolBar = ((IModelClassHideViewToolBar) View.Model);
+                if (modelViewHideViewToolBar.HideToolBar.HasValue && modelViewHideViewToolBar.HideToolBar.Value) {
+                    _visibility = containerHolder.Style[HtmlTextWriterStyle.Display];
+                    ApplyStyle(containerHolder);
                 }
             }
+        }
+
+        private static void ApplyStyle(ActionContainerHolder containerHolder){
+            containerHolder.Style[HtmlTextWriterStyle.Visibility] = "Hidden";
+            containerHolder.Style[HtmlTextWriterStyle.Display] = "None";
         }
 
         protected override void OnDeactivated(){
