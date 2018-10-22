4f0a9447b65f15c954dff4b0c09bac1d86215d6c
apobekiaris <apostolis.bekiaris@gmail.com>
2018-10-22 22:46:00 +0300

    Parameterize build configuration

--- go.ps1
+++ go.ps1
@@ -1,4 +1,3 @@
-
 $configuration="Release"
 $location=Get-Location
 set-location "C:\Program Files (x86)\Microsoft Visual Studio\Installer\"
@@ -8,5 +7,5 @@ Copy-Item $msbuild "$location\MSBuild.exe"
 $assemblyInfo="Xpand\Xpand.Utils\Properties\XpandAssemblyInfo.cs"
 set-location $location
 $version=(Get-Content $assemblyInfo -ErrorAction Stop | Select-String 'public const string Version = \"([^\"]*)')[0].Matches.Groups[1].Value
-& msbuild Xpand.Build /fl /t:Release /p:Version=$version 
+Invoke-Expression "msbuild Xpand.Build  /fl /t:Release /p:Version=$version;Configuration=$configuration"
 Remove-Item msbuild.exe
