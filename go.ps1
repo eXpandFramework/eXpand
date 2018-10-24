eb644b5fbce9c6c78679b894a2c0809e8754a384
apobekiaris <apostolis.bekiaris@gmail.com>
2018-10-24 18:56:06 +0300

    detect msbuild with vssetup

--- go.ps1
+++ go.ps1
@@ -1,11 +1,12 @@
 $configuration="Release"
-$location=Get-Location
-set-location "C:\Program Files (x86)\Microsoft Visual Studio\Installer\"
-$msbuild = .\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
-$msbuild = join-path $msbuild 'MSBuild\15.0\Bin\MSBuild.exe'
-Copy-Item $msbuild "$location\MSBuild.exe"
+if (!(Get-Module -ListAvailable -Name VSSetup)) {
+    Write-Host "Module exists"
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Module VSSetup
+}
+$msbuild=Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Latest |Select-Object -ExpandProperty InstallationPath
+$msbuild=join-Path $msbuild MSBuild\15.0\Bin\MSBuild.exe
 $assemblyInfo="Xpand\Xpand.Utils\Properties\XpandAssemblyInfo.cs"
-set-location $location
 $version=(Get-Content $assemblyInfo -ErrorAction Stop | Select-String 'public const string Version = \"([^\"]*)')[0].Matches.Groups[1].Value
-Invoke-Expression "msbuild Xpand.Build  /fl /t:Release /p:Version=$version;Configuration=$configuration"
-Remove-Item msbuild.exe
+$params ="Xpand.Build", "/fl", "/t:Release", "/p:Version=$version;Configuration=$configuration"
+& $msbuild $params
