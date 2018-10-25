438ad5838631109fc877f69b4657f5b2fc002eda
apobekiaris <apostolis.bekiaris@gmail.com>
2018-10-25 20:28:36 +0300

    detect msbuild with vssetup

--- go.ps1
+++ go.ps1
@@ -1,10 +1,12 @@
+$configuration="Release"
 if (!(Get-Module -ListAvailable -Name VSSetup)) {
     Write-Host "Module exists"
     Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
     Install-Module VSSetup
 }
-$path=Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Latest |Select-Object -ExpandProperty InstallationPath
-$path=join-path $path MSBuild\15.0\Bin\MSBuild.exe
+$msbuild=Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Latest |Select-Object -ExpandProperty InstallationPath
+$msbuild=join-Path $msbuild MSBuild\15.0\Bin\MSBuild.exe
 $assemblyInfo="Xpand\Xpand.Utils\Properties\XpandAssemblyInfo.cs"
 $version=(Get-Content $assemblyInfo -ErrorAction Stop | Select-String 'public const string Version = \"([^\"]*)')[0].Matches.Groups[1].Value
-Invoke-Expression "'$path' Xpand.Build  /fl /t:Release /p:Version=$version;Configuration=$configuration"
+$params ="Xpand.Build", "/fl", "/t:Release", "/p:Version=$version;Configuration=$configuration"
+& $msbuild $params
