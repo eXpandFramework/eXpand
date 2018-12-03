629e8d2b709f2c95e42fbeca1381fa96578fdf6a
apobekiaris <apostolis.bekiaris@gmail.com>
2018-12-03 15:45:25 +0200

    Build script fail if required VS workloads not installed #265

--- go.ps1
+++ go.ps1
@@ -1,12 +1,21 @@
 $configuration="Release"
 if (!(Get-Module -ListAvailable -Name VSSetup)) {
-    Write-Host "Module exists"
     Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
     Install-Module VSSetup
 }
-$msbuild=Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Latest |Select-Object -ExpandProperty InstallationPath
-$msbuild=join-Path $msbuild MSBuild\15.0\Bin\MSBuild.exe
-$assemblyInfo="Xpand\Xpand.Utils\Properties\XpandAssemblyInfo.cs"
+$msbuildPath=Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Latest |Select-Object -ExpandProperty InstallationPath
+if (!$msbuildPath){
+    throw "VS 2017 build tools not found. Please download from https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2017"
+}
+
+if (!(Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Require Microsoft.VisualStudio.Component.NuGet.BuildTools -Latest|Select-Object -ExcludeProperty InstallationPath)){
+    throw "MsBuild Nuget targets missing. https://stackoverflow.com/questions/47797510/the-getreferencenearesttargetframeworktask-task-was-not-found"
+}
+if (!(Get-VSSetupInstance  | Select-VSSetupInstance -Product Microsoft.VisualStudio.Product.BuildTools -Require Microsoft.VisualStudio.Workload.WebBuildTools -Latest|Select-Object -ExcludeProperty InstallationPath)){
+    throw "MsBuild WebBuildTools missing. https://stackoverflow.com/questions/44061932/ms-build-2017-microsoft-webapplication-targets-is-missing"
+}
+$msbuildPath=join-Path $msbuildPath MSBuild\15.0\Bin\MSBuild.exe
+$assemblyInfo="$PSScriptRoot\Xpand\Xpand.Utils\Properties\XpandAssemblyInfo.cs"
 $version=(Get-Content $assemblyInfo -ErrorAction Stop | Select-String 'public const string Version = \"([^\"]*)')[0].Matches.Groups[1].Value
-$params ="Xpand.Build", "/fl", "/t:Release", "/p:Version=$version;Configuration=$configuration"
+$params ="$PSScriptRoot\Xpand.Build", "/fl", "/t:Release", "/p:Version=$version;Configuration=$configuration"
 & $msbuild $params
