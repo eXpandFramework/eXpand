2f388e9593652286869322a98f8ff38bbd4b2591
apobekiaris <apostolis.bekiaris@gmail.com>
2018-12-21 17:48:21 +0200

    PermissionProviderStorage throws if SecuredObjectSpaceProvider is used with ExcelImporter module #275

--- Xpand/Xpand.ExpressApp.Modules/Security/Permissions/PermissionProviderStorage.cs
+++ Xpand/Xpand.ExpressApp.Modules/Security/Permissions/PermissionProviderStorage.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using DevExpress.ExpressApp.Security;
-using DevExpress.Persistent.Base;
 using Fasterflect;
 
 namespace Xpand.ExpressApp.Security.Permissions {
@@ -9,41 +9,20 @@ namespace Xpand.ExpressApp.Security.Permissions {
         IEnumerable<IOperationPermission> GetPermissions(ISecurityRole securityRole);
     }
     public static class PermissionRequestProcessorStorage {
-        private static readonly string ValueManagerKey = typeof(PermissionRequestProcessorStorage).Name;
-        private static volatile IValueManager<Dictionary<Type,IPermissionRequestProcessor>> _instanceValueManager;
-        private static readonly object SyncRoot = new object();
+        static PermissionRequestProcessorStorage() {
+            Instance = new ConcurrentDictionary<Type, IPermissionRequestProcessor>();
+        } 
 
-        public static Dictionary<Type, IPermissionRequestProcessor> Instance {
-            get {
-                if (_instanceValueManager == null) {
-                    lock (SyncRoot) {
-                        if (_instanceValueManager == null) {
-                            _instanceValueManager = ValueManager.GetValueManager<Dictionary<Type, IPermissionRequestProcessor>>(ValueManagerKey);
-                        }
-                    }
-                }
-	            if (_instanceValueManager.CanManageValue){
-
-		            if (_instanceValueManager.Value == null) {
-			            lock (SyncRoot) {
-				            if (_instanceValueManager.Value == null) {
-					            _instanceValueManager.Value = new Dictionary<Type, IPermissionRequestProcessor>();
-				            }
-			            }
-		            }
-		            return _instanceValueManager.Value;
-	            }
-	            return new Dictionary<Type, IPermissionRequestProcessor>();
-            }
-        }
 
         public static T GetProcessor<T>(this IPermissionDictionary customPermissions) where T : ICustomPermissionRequestProccesor {
             if (!Instance.ContainsKey(typeof(T)))
-                Instance.Add(typeof(T), (IPermissionRequestProcessor)typeof(T).CreateInstance());
+                Instance.TryAdd(typeof(T), (IPermissionRequestProcessor)typeof(T).CreateInstance());
             var customPermissionRequestProccesor = (ICustomPermissionRequestProccesor)Instance[typeof(T)];
             customPermissionRequestProccesor.Permissions = customPermissions;
             return (T)customPermissionRequestProccesor;
         }
+
+        public static ConcurrentDictionary<Type, IPermissionRequestProcessor> Instance { get; }
     }
 
     public class PermissionProviderStorage : HashSet<IPermissionInfo> {
