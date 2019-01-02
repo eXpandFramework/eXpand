fd1758d25738f113ca2172dcad3f72867d795e30
apobekiaris <apostolis.bekiaris@gmail.com>
2019-01-02 10:13:40 +0200

    Improve scripts

--- build.ps1
+++ build.ps1
@@ -1,4 +1,7 @@
 $sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget","C:\Program Files (x86)\DevExpress 18.2\Components\System\Components\packages")   
-& $PSScriptRoot\support\build\go.ps1 -packageSources $sources -configuration "Release"
+& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m")
+
+Write-Host "LastExitCode=$LastExitCode"
+exit $LastExitCode
 
 
