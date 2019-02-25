f9239d0e99f9d9302e91b97114f0b225eade9ba7
apobekiaris <apostolis.bekiaris@gmail.com>
2019-02-25 15:18:11 +0200

    Dashboard MobileLayoutEnabled model option and default to Always #324

--- Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/XtraDashboardWebModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/XtraDashboardWebModule.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using DevExpress.ExpressApp.Model;
 using DevExpress.Utils;
 using Xpand.ExpressApp.Dashboard;
 using Xpand.ExpressApp.XtraDashboard.Web.Controllers;
+using Xpand.ExpressApp.XtraDashboard.Web.PropertyEditors;
 using Xpand.Persistent.Base.General;
 using SupressConfirmationController = Xpand.ExpressApp.Web.SystemModule.SupressConfirmationController;
 
@@ -18,6 +20,11 @@ namespace Xpand.ExpressApp.XtraDashboard.Web {
             RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule));
             RequiredModuleTypes.Add(typeof(Security.Web.XpandSecurityWebModule));
             RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule));
+        }
+
+        public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders) {
+            base.ExtendModelInterfaces(extenders);
+            extenders.Add<IModelDashboardModule,IModelDashboardModuleASPxDashboard>();
         }
 
         protected override IEnumerable<Type> GetDeclaredControllerTypesCore(IEnumerable<Type> declaredControllerTypes){
