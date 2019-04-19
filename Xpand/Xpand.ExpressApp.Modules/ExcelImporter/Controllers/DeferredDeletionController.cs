79bf159c22fe557fc2e0b246daaba82e6c0b4c1a
apobekiaris <apostolis.bekiaris@gmail.com>
2019-04-19 12:20:42 +0300

    Excelimporter: Support Deffered deletions #373

---
+++ Xpand/Xpand.ExpressApp.Modules/ExcelImporter/Controllers/DeferredDeletionController.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Linq;
+//using System.Reactive.Linq;
+//using DevExpress.ExpressApp;
+//using DevExpress.Xpo;
+//using DevExpress.Xpo.Metadata.Helpers;
+//using Xpand.ExpressApp.ExcelImporter.BusinessObjects;
+//using Xpand.ExpressApp.ExcelImporter.Services;
+//using Xpand.Persistent.Base.General;
+//
+//namespace Xpand.ExpressApp.ExcelImporter.Controllers {
+//    public class DeferredDeletionController:ObjectViewController<DetailView,ExcelImport> {
+//        protected override void OnActivated() {
+//            base.OnActivated();
+//            Frame.GetController<ExcelImportDetailViewController>().BeginImport
+//                .SelectMany(_ => _.progress.OfType<RequestImportTargetObject>().Where(_.excelImport))
+//                .Select(_ => {
+//                    if (_.Args.objectType.GetTypeInfo().FindAttributes<DeferredDeletionAttribute>().Any()) {
+//                        var session = _.ObjectSpace.Session();
+//                        _.TargetObject = session.FindObject(_.Args.objectType, _.Args.criteria,true);
+//                        if (_.TargetObject != null) {
+//                            var memberInfo = session.GetClassInfo(_.TargetObject).FindMember(GCRecordField.StaticName);
+//                            memberInfo.SetValue(_.TargetObject,null);
+//                        }
+//                    }
+//                    
+//                    return _;
+//
+//                })
+//                .TakeUntil(this.WhenDeActivating())
+//                .Subscribe();
+//        }
+//    }
+//}
