98d2528b0876f27a18dc5dfb9c2e2fd40fb1a63d
apobekiaris <apostolis.bekiaris@gmail.com>
2019-04-20 19:39:54 +0300

    18.2.704.0

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget",(Get-Feed -DX))   
-& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "18.2.703.0" 
+& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "18.2.704.0" 
 exit $LastExitCode
 
 
