0f1841fb1bbc4de50b06553f362e6bd58f0af31e
apobekiaris <apostolis.bekiaris@gmail.com>
2019-05-09 19:41:17 +0300

    Dashboard: DisableMultiDashboardTypeController #363

---
+++ Xpand/Xpand.ExpressApp.Modules/Dashboard/Controllers/DisableMultiDashboardTypeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using DevExpress.ExpressApp;
+using Xpand.ExpressApp.Dashboard.BusinessObjects;
+using Xpand.Persistent.Base.General;
+
+namespace Xpand.ExpressApp.Dashboard.Controllers {
+    public abstract class DisableMultiDashboardTypeController : ObjectViewController<DetailView, DashboardDefinition> {
+        protected override void OnActivated() {
+            base.OnActivated();
+            View.GetItems<IChooseFromListCollectionEditor>().First().ControlCreated += OnControlCreated;
+        }
+
+        protected virtual void OnControlCreated(object sender, EventArgs e) {
+            
+        }
+    }
+}
---
+++ Xpand/Xpand.ExpressApp.Modules/XtraDashboard.Web/Controllers/DisableMultiDashboardTypeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Web;
+using Xpand.ExpressApp.XtraDashboard.Web.PropertyEditors;
+
+namespace Xpand.ExpressApp.XtraDashboard.Web.Controllers {
+    public class DisableMultiDashboardTypeController : Dashboard.Controllers.DisableMultiDashboardTypeController {
+        protected override void OnControlCreated(object sender, EventArgs e) {
+            ((DashboardTypesEditor) sender).ListBoxTemplate.SelectionMode = ListEditSelectionMode.Single;
+        }
+    }
+}
