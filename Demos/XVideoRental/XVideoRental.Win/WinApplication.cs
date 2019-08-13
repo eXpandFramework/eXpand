f57a4845c4e0868bfff4b5e27a6e0864e06e11bb
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-08-13 08:45:40 +0300

    19.1.503.0

--- Demos/XVideoRental/XVideoRental.Win/WinApplication.cs
+++ Demos/XVideoRental/XVideoRental.Win/WinApplication.cs
@@ -17,7 +17,7 @@ namespace XVideoRental.Win {
             InitializeComponent();
             DelayedViewItemsInitialization = true;
             LastLogonParametersRead += OnLastLogonParametersRead;
-//            EnableModelCache = true;
+            EnableModelCache = true;
         }
 
         protected override ApplicationModulesManager CreateApplicationModulesManager(ControllersManager controllersManager) {
@@ -44,22 +44,22 @@ namespace XVideoRental.Win {
 #if EASYTEST
             
 #else
-            if (this.DropDatabaseOnVersionMissmatch() > 0)
-                Application.ExitThread();
-            if (Debugger.IsAttached) {
-                e.Updater.Update();
-                e.Handled = true;
-            }
-            else {
-                throw new InvalidOperationException(
-                    "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
-                    "This error occurred  because the automatic database update was disabled when the application was started without debugging.\r\n" +
-                    "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " +
-                    "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " +
-                    "or manually create a database using the 'DBUpdater' tool.\r\n" +
-                    "Anyway, refer to the 'Update Application and Database Versions' help topic at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm " +
-                    "for more detailed information. If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/");
-            }
+//            if (this.DropDatabaseOnVersionMissmatch() > 0)
+//                Application.ExitThread();
+//            if (Debugger.IsAttached) {
+//                e.Updater.Update();
+//                e.Handled = true;
+//            }
+//            else {
+//                throw new InvalidOperationException(
+//                    "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
+//                    "This error occurred  because the automatic database update was disabled when the application was started without debugging.\r\n" +
+//                    "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " +
+//                    "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " +
+//                    "or manually create a database using the 'DBUpdater' tool.\r\n" +
+//                    "Anyway, refer to the 'Update Application and Database Versions' help topic at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm " +
+//                    "for more detailed information. If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/");
+//            }
 #endif
         }
 
--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 
 $sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget","C:\Program Files (x86)\DevExpress 19.1\Components\System\Components\packages")   
-& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "19.1.502.0" 
+& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "19.1.503.0" 
 exit $LastExitCode
 
 
