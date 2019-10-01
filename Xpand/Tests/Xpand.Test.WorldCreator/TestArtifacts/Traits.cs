454abfd6cc1875ededc627777e8be9245668e98d
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-01 23:46:02 +0300

    remove unused tests

--- Xpand/Tests/Xpand.Test.WorldCreator/TestArtifacts/Traits.cs
+++
@@ -1,6 +0,0 @@
-namespace Xpand.Test.WorldCreator.TestArtifacts{
-    public class Traits{
-        public const string Validation = "Validation";
-        public const string Compilation = "Validation";
-    }
-}
