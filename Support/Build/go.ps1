68a2b2f81f824599f597320a00fa1eb53aeeaa0f
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-11-19 16:49:42 +0200

    VSIX lock pakets to VS15

--- Support/Build/go.ps1
+++ Support/Build/go.ps1
@@ -7,7 +7,7 @@ param(
     [string[]]$taskList = @("Release"),
     [string]$nugetApiKey = $null,
     [string]$Repository = "eXpand",
-    [string]$XpandPwshVersion = "0.30.7",
+    [string]$XpandPwshVersion = "0.31.0",
     [bool]$ResolveNugetDependecies
 )
 
