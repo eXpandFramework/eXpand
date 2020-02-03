7bd7ed3b304b95272f9007d6e92cf0df75da963c
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-03 10:34:39 +0200

    Bump Standalone packages - Release

--- Support/Build/go.ps1
+++ Support/Build/go.ps1
@@ -7,7 +7,7 @@ param(
     [string[]]$taskList = @("Release"),
     [string]$nugetApiKey = $null,
     [string]$Repository = "eXpand",
-    [string]$XpandPwshVersion = "1.201.7.3",
+    [string]$XpandPwshVersion = "1.201.7.4",
     [bool]$ResolveNugetDependecies
 )
 
