ec9ca896a642224ed331598d4e8d157b9bf73261
apobekiaris <apostolis.bekiaris@gmail.com>
2020-05-01 13:46:01 +0300

    20.1.301.0

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 
-$sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget","C:\Program Files (x86)\DevExpress 19.2\Components\System\Components\packages")   
-& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "20.1.300.0" 
+$sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget","C:\Program Files (x86)\DevExpress 20.1\Components\System\Components\packages")   
+& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "20.1.301.0" 
 exit $LastExitCode
 
 
