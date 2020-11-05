3b6054fb6f2e2dcebc3c5179b08a5581537cfc0e
apobekiaris <apostolis.bekiaris@gmail.com>
2020-11-05 16:55:43 +0200

    Bump Standalone package version #800

--- Support/Build/go.ps1
+++ Support/Build/go.ps1
@@ -7,7 +7,7 @@ param(
     [string[]]$taskList = @("Release"),
     [string]$nugetApiKey = $null,
     [string]$Repository = "eXpand",
-    [string]$XpandPwshVersion = "1.202.46.1",
+    [string]$XpandPwshVersion = "1.202.46.4",
     [bool]$ResolveNugetDependecies
 )
 
