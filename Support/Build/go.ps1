0bbfc4e990fb7fd431a877ec3a574044ef27eff1
apobekiaris <apostolis.bekiaris@gmail.com>
2020-12-20 22:55:47 +0200

    Update invalid Xpand.XAF.ModelEditor.exe resource #821

--- Support/Build/go.ps1
+++ Support/Build/go.ps1
@@ -7,7 +7,7 @@ param(
     [string[]]$taskList = @("Release"),
     [string]$nugetApiKey = $null,
     [string]$Repository = "eXpand",
-    [string]$XpandPwshVersion = "1.202.47.9",
+    [string]$XpandPwshVersion = "1.202.48.1",
     [bool]$ResolveNugetDependecies
 )
 
