a02461832414c7ff2b50bfee1f4b5400b05795c4
apobekiaris <apostolis.bekiaris@gmail.com>
2022-06-14 11:41:27 +0300

    21.2.800

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 
 $sources=@("https://api.nuget.org/v3/index.json","https://xpandnugetserver.azurewebsites.net/nuget","C:\Program Files (x86)\DevExpress 21.2\Components\System\Components\packages")   
-& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "21.2.701.0" 
+& $PSScriptRoot\support\build\go.ps1 -taskList @("Release") -packageSources $sources -msbuildArgs @("/p:Configuration=Release","/WarnAsError","/v:m") -version "21.2.800.0" 
 exit $LastExitCode
 
 
