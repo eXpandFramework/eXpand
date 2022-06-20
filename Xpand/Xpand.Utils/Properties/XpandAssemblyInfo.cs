2797c4b05c8ec0697b9bee705809c3a4d029fa85
apobekiaris <apostolis.bekiaris@gmail.com>
2022-06-20 16:59:35 +0300

    21.2.801.0

--- Xpand/Xpand.Utils/Properties/XpandAssemblyInfo.cs
+++ Xpand/Xpand.Utils/Properties/XpandAssemblyInfo.cs
@@ -1,5 +1,5 @@
 public class XpandAssemblyInfo {
-    public const string Version = "21.2.800.0";
+    public const string Version = "21.2.801.0";
     public const string FileVersion = Version;
     public const string Token = "c52ffed5d5ff0958";
     public const string TabAspNetModules = "eXpand: Web Modules";
