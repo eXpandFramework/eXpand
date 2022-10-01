fb30c9fcc71ef0cf826443e0a101a6c7ffc95add
apobekiaris <apostolis.bekiaris@gmail.com>
2022-10-01 08:00:01 +0300

    22.1.503

--- Support/Build/CopySymbols.ps1
+++ Support/Build/CopySymbols.ps1
@@ -2,9 +2,22 @@ param(
     $TargetPath="C:\Work\eXpandFramework\Issues\Solution10\Solution10.Module\bin\Debug\Solution10.Module.dll",
     $ProjectPath="DoNotRemove"
 )
+$msSpeechPath="$PSScriptRoot\..\..\..\microsoft.cognitiveservices.speech"
+if (Test-Path $msSpeechPath){
+    Get-ChildItem $msSpeechPath "Microsoft.CognitiveServices.Speech.csharp.dll" -Recurse|Where-Object{
+        $_.Directory.Name -eq "netstandard2.0" 
+    }|ForEach-Object{
+        $corelib="$($_.DirectoryName)\Microsoft.CognitiveServices.Speech.core.dll"
+        if (!(Test-Path $corelib)){
+            Copy-Item $_.FullName $corelib
+        }
+    }
+}
+
 $pdbname = "$((Get-ChildItem $PSScriptRoot *.targets).BaseName).pdb"
 $targetDir=[System.IO.Path]::GetDirectoryName($TargetPath)
 $pdbPath=Get-ChildItem $PSScriptRoot\..\lib *.pdb -Recurse
+
 if (!(Test-Path "$targetDir\$pdbname")){
     $pdbPath|Copy-Item -Destination $targetDir
 }
