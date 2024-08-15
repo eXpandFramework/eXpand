5e56773b1739b275f480d616529533b41b999f65
apobekiaris <apostolis.bekiaris@gmail.com>
2024-08-15 17:47:27 +0300

    24.1.500.0

--- Support/Build/go.ps1
+++ Support/Build/go.ps1
@@ -7,7 +7,7 @@ param(
     [string[]]$taskList = @("Release"),
     [string]$nugetApiKey = $null,
     [string]$Repository = "eXpand",
-    [string]$XpandPwshVersion = "1.221.0.19",
+    [string]$XpandPwshVersion = "1.221.0.20",
     [bool]$ResolveNugetDependecies
 )
 
